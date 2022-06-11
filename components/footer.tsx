386549fafd54ee3580f81540dedf9acfec95a2ea
Chad Stewart <chadrhonanstewart@gmail.com>
2022-06-10 19:19:20 -0500

    Separated everything into components to make it easier to edit and change when needed.

---
+++ components/footer.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="flex h-24 w-full items-center justify-center border-t">
+          <a
+            className="flex items-center justify-center gap-2"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </a>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ components/nav.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+import { test } from '../test'
+
+const Nav: React.FC = () => {
+
+    const defaultTools = [
+      {
+        name: "default"
+      },
+      {
+        name: "default"
+      },
+      {
+        name: "default"
+      }  
+    ]
+
+    const toolList = defaultTools.concat(test);
+
+    return (
+        <nav className='tool-list-nav flex flex-row min-h-[50px] p-2 border'>
+          {toolList.map((tool, index) => 
+            <Link
+              className='nav-tool-item'
+              href=''
+              key={index}
+            >
+              <h5 className='pr-2'>
+                {tool.name}
+              </h5>
+            </Link>
+          )}
+        </nav>
+    )
+}
+
+export default Nav;
