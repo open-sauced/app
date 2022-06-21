71ec902354f46004e532d96b1929ae1c6ea0f633
Chad Stewart <chadrhonanstewart@gmail.com>
2022-06-21 07:36:43 -0500

    Started working on tables and updated top nav to new design.

--- components/tools.tsx
+++ components/tools.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@supabase/ui";
 import { useRouter } from "next/router";
 import React from "react";
+import Table from "./misc/table";
 
 const Tool: React.FC = () => {
     const router = useRouter();
@@ -28,33 +29,33 @@ const Tool: React.FC = () => {
                     <>
                         <div className="flex w-[80vw] h-[500px] mb-10">
                             <div className="flex flex-col w-[60%] mr-10">
-                                <div className="w-full h-[50%] mb-10 border-4">
+                                <div className="w-full h-[50%] mb-10 p-5 border-4">
 
                                 </div>
-                                <div className="w-full h-[50%] border-4">
+                                <div className="w-full h-[50%] p-5 border-4">
 
                                 </div>
                             </div>
-                            <div className="w-[40%] border-4">
-                                
+                            <div className="w-[40%] p-5 border-4">
+                                <Table />
                             </div>
                         </div>
                         <div className="flex w-[80vw] h-[500px] mb-10">
                             <div className="flex flex-col w-[60%] mr-10">
-                                <div className="w-full h-[20%] mb-10 border-4">
+                                <div className="w-full h-[20%] mb-10 p-5 border-4">
 
                                 </div>
                                 <div className="flex w-full h-[80%]">
-                                    <div className="w-[50%] mr-10 h-full border-4">
-
+                                    <div className="w-[50%] mr-10 h-full p-5 border-4">
+                                        <Table />
                                     </div>
-                                    <div className="w-[50%] h-full border-4">
-
+                                    <div className="w-[50%] h-full p-5 border-4">
+                                        <Table />
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-[40%] border-4">
-                                
+                            <div className="w-[40%] p-5 border-4">
+                                <Table />
                             </div>
                         </div>
                     </>
--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Typography } from '@supabase/ui';
+import { Typography, Input, Button } from '@supabase/ui';
+import Image from 'next/image';
+import pizzaImage from '../public/noto_pizza.svg';
+import downArrow from '../public/chevron-down.svg';
+import avatar from '../public/ellipse-1.png';
 
 const TopNav: React.FC = () => {
     const { Text } = Typography;
 
     return (
-        <div className="top-nav-container flex justify-between px-8 py-1 border-b">
-            <Text>Logo</Text>
-            <Text>Username</Text>    
+        <div className="top-nav-container flex justify-between items-center h-[60px] px-8 py-1 border-b">
+            <div className='flex items-center'>
+                <Image src={pizzaImage} />
+                <Text className='font-semibold px-2 text-[16px]'>OpenSauced</Text>
+                <Input
+                    placeholder='Search Portals'
+                />
+            </div>
+            <div className='flex'>
+                <Button className='mr-5' size='small'>Button +</Button>
+                <div className='flex items-center'>
+                    <Image src={avatar}/>
+                    <Image className='mx-2' src={downArrow}/>
+                </div>    
+            </div>
         </div>
     )
 };
