81790d6cfa0e037471e74d295b1f56b1946ac6a0
Chad Stewart <chadrhonanstewart@gmail.com>
2022-06-24 19:25:16 -0500

    Split tables into 4 different components to facilitate hand off to Eryc.

---
+++ components/misc/table-test/table.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+const Table: React.FC = () => {
+    return (
+        <table className="w-full border">
+            <tr>
+                <th>
+                    
+                </th>
+                <th>
+                    hello
+                </th>
+                <th>
+                    hello
+                </th>
+                <th>
+                    hello
+                </th>
+                <th>
+                    hello
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+                <td>
+                    hi
+                </td>
+            </tr>
+            <tr className="h-[26px]">
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+            </tr>
+            <tr className="h-[26px]">
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+            </tr>
+            <tr className="h-[26px]">
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    
+                </td>
+            </tr>
+        </table>
+    )
+}
+
+export default Table;
--- components/tools.tsx
+++ components/tools.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@supabase/ui";
 import { useRouter } from "next/router";
 import React from "react";
-import Table from "./misc/table";
+import Table from "./misc/table-test/table";
+import Table2 from "./misc/table-test/table-2";
+import Table3 from "./misc/table-test/table-3";
+import Table4 from "./misc/table-test/table-4";
 
 const Tool: React.FC = () => {
     const router = useRouter();
@@ -9,63 +12,47 @@ const Tool: React.FC = () => {
     const { tool } = router.query;
     
     return (
-        <>
-            <nav className="pb-8">
-                <Button size="medium" type="outline">
-                    Daily
-                </Button>
-                <Button size="medium" type="outline">
-                    Weekly
-                </Button>
-                <Button size="medium" type="outline">
-                    Monthly
-                </Button>
-                <Button size="medium" type="outline">
-                    Year
-                </Button>
-            </nav>
-            <div className="flex flex-col w-full">
-                {tool === "Dashboard" ?
-                    <>
-                        <div className="flex max-w-full h-[500px] mb-10">
-                            <div className="flex flex-col w-[60%] mr-10">
-                                <div className="w-full h-[50%] mb-10 p-5 border-4">
+        <div className="flex flex-col w-full">
+            {tool === "Dashboard" ?
+                <>
+                    <div className="flex max-w-full h-[500px] mb-10">
+                        <div className="flex flex-col w-[60%] mr-10">
+                            <div className="w-full h-[50%] mb-10 p-5 border-4">
 
-                                </div>
-                                <div className="w-full h-[50%] p-5 border-4">
-
-                                </div>
                             </div>
-                            <div className="w-[40%] p-5 border-4">
-                                <Table />
+                            <div className="w-full h-[50%] p-5 border-4">
+
                             </div>
                         </div>
-                        <div className="flex w-full h-[500px] mb-10">
-                            <div className="flex flex-col w-[60%] mr-10">
-                                <div className="w-full h-[20%] mb-10 p-5 border-4">
+                        <div className="w-[40%] p-5 border-4">
+                            <Table />
+                        </div>
+                    </div>
+                    <div className="flex w-full h-[500px] mb-10">
+                        <div className="flex flex-col w-[60%] mr-10">
+                            <div className="w-full h-[20%] mb-10 p-5 border-4">
 
-                                </div>
-                                <div className="flex max-w-full h-[80%]">
-                                    <div className="w-[50%] mr-10 h-full p-5 border-4">
-                                        <Table />
-                                    </div>
-                                    <div className="w-[50%] h-full p-5 border-4">
-                                        <Table />
-                                    </div>
-                                </div>
                             </div>
-                            <div className="w-[40%] p-5 border-4">
-                                <Table />
+                            <div className="flex max-w-full h-[80%]">
+                                <div className="w-[50%] mr-10 h-full p-5 border-4">
+                                    <Table2 />
+                                </div>
+                                <div className="w-[50%] h-full p-5 border-4">
+                                    <Table3 />
+                                </div>
                             </div>
                         </div>
-                    </>
-                    :
-                    <>
-                        {tool ? `${tool}` : "Test"} Tool Page
-                    </>
-                }
-            </div>
-        </>
+                        <div className="w-[40%] p-5 border-4">
+                            <Table4 />
+                        </div>
+                    </div>
+                </>
+                :
+                <>
+                    {tool ? `${tool}` : "Test"} Tool Page
+                </>
+            }
+        </div>
     )
 };
 
