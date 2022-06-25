c139d9e8f8575abe4c4c48a573bed61dab0657b1
Chad Stewart <chadrhonanstewart@gmail.com>
2022-06-25 06:42:07 -0500

    Updated top nav to meet design specs.

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import { Typography, Input, Button } from '@supabase/ui';
+import { Typography } from '@supabase/ui';
 import Image from 'next/image';
-import pizzaImage from '../public/noto_pizza.svg';
+import openSaucedImg from '../public/oslogo.png';
+import notifications from '../public/notifications.svg';
 import downArrow from '../public/chevron-down.svg';
-import avatar from '../public/ellipse-1.png';
 
 const TopNav: React.FC = () => {
     const { Text } = Typography;
 
     return (
-        <div className="top-nav-container flex justify-between items-center h-[60px] px-8 py-1 border-b">
+        <div className="top-nav-container flex justify-between items-center h-[60px] px-8 py-1 bg-[#26292B] border-b">
             <div className='flex items-center'>
-                <Image src={pizzaImage} />
-                <Text className='font-semibold px-2 text-[16px]'>OpenSauced</Text>
-                <Input
-                    placeholder='Search Portals'
-                />
+                <Image width={32} height={32} src={openSaucedImg} />
+                <Text className='font-semibold px-2 text-[16px]' strong style={{ color: 'white' }}>OpenSauced</Text>
             </div>
             <div className='flex'>
                 <div className='flex items-center'>
-                    <Image src='/ellipse-1.png' height={32} width={32} />
-                    <Image className='mx-2' src={downArrow}/>
-                </div>    
-                <Button 
-                    className='ml-5'
-                    size='small'
-                    style={{ backgroundColor: '#0091FF' }}
-                >
-                    Button    +
-                </Button>
+                    <Image src={notifications} />
+                    <div className='ml-3 mr-1'>
+                        <Image src='/ellipse-1.png' height={32} width={32} />
+                    </div>
+                    <div>
+                        <Image src={downArrow}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
