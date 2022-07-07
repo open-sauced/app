efdb9dd805374d6682baf3a9c1a234077a08b21f
Chad Stewart <chadrhonanstewart@gmail.com>
2022-07-07 06:33:59 -0500

    Adding Accessibility addon to Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
     "storybook-addon-next",
     {
       // From stack-overflow answer: https://stackoverflow.com/a/68757745
