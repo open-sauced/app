749788d47352ea281971a957c762a4375ae28833
Chad Stewart <chadrhonanstewart@gmail.com>
2022-07-07 11:25:20 -0500

    Updated logo in header and favicon.

--- components/molecules/HeaderLogo/header-logo.tsx
+++ components/molecules/HeaderLogo/header-logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography } from "@supabase/ui";
 import Image from "next/image";
-import openSaucedImg from "../../../public/oslogo.png";
+import openSaucedImg from "../../../public/openSauced-icon.png";
 
 const HeaderLogo: React.FC = () => {
     
@@ -9,7 +9,7 @@ const HeaderLogo: React.FC = () => {
 
   return (
     <div className="flex items-center p-2">
-      <Image alt="Open Sauced Logo" width={32} height={32} src={openSaucedImg} />
+      <Image className="rounded" alt="Open Sauced Logo" width={32} height={32} src={openSaucedImg} />
       <Text className="font-semibold px-2 text-[16px]" strong style={{ color: "white" }}>OpenSauced</Text>
     </div>
   );
