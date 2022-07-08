bb189cc2802ffd7d99e159248937700fc35a2b68
WAGMI Times <wagmitimes@gmail.com>
2022-07-08 20:00:39 -0300

    refactor: Add text styles to the `Button` in Nav component
    style: Use Tailwind colors

--- components/organisms/toolist/nav.tsx
+++ components/organisms/toolist/nav.tsx
@@ -8,8 +8,8 @@ const Nav: React.FC = () => {
   const { toolList, navbarTool, portalName } = useNav();
 
   return (
-      <div className="px-[64px] flex flex-row">
     <nav className="tool-list-nav min-h-[50px] bg-slate-50 border-b pt-3">
+      <div className="px-[64px] flex flex-row gap-2">
         {toolList.map((tool, index) => 
           <div 
             key={index} className={`nav-tool-item ${navbarTool === tool.name ? "border-b-2 border-[#FFA01C]" : ""}`}>
@@ -19,10 +19,10 @@ const Nav: React.FC = () => {
                 `${portalName}?tool=${tool.name}`
               }
             >
-              <Button size="xlarge" type="text" disabled={navbarTool === tool.name}>
-                <h5 className={navbarTool === tool.name ? "text-black" : ""}>
+              <Button size="xlarge" type="text" className="!px-4 hover:!bg-slate-100">
+                <span className={"text-base " + (navbarTool === tool.name ? "text-slate-900" : "text-slate-500")}>
                   {tool.name}
-                </h5>
+                </span>
                 {
                   tool.numOf && 
                     <div className="ml-2 py-0.5 px-1.5 bg-slate-200 text-slate-500 border rounded-full text-xs font-semibold">
