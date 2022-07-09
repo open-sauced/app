09790f14909c8d24a2722d6fcb3b7d6a8fcbb1d0
WAGMI Times <wagmitimes@gmail.com>
2022-07-09 03:39:27 -0300

    refactor: Remove div that wraps the Link component

--- components/organisms/toolist/nav.tsx
+++ components/organisms/toolist/nav.tsx
@@ -16,27 +16,25 @@ const Nav: React.FC = () => {
       className="tool-list-nav flex flex-row gap-2 px-16 bg-slate-50 border-b pt-3">
       
       {toolList.map((tool, index) => 
-        <div 
-          key={index} className={`nav-tool-item ${navbarTool === tool.name ? "border-b-2 border-[#FFA01C]" : ""}`}>
-          <Link
-            href={tool.name === "nextjs" ?
-              `${portalName}` :
-              `${portalName}?tool=${tool.name}`
+        <Link
+          href={tool.name === "nextjs" ?
+            `${portalName}` :
+            `${portalName}?tool=${tool.name}`
+          }
+          key={index} className={`nav-tool-item ${navbarTool === tool.name ? "border-b-2 border-slate-400" : ""}`}
+        >
+          <Button disabled={navbarTool === tool.name} size="xlarge" type="text" className="!px-4 hover:!bg-slate-100">
+            <span className={"text-base " + (navbarTool === tool.name ? "text-slate-900" : "text-slate-500")}>
+              {tool.name}
+            </span>
+            {
+              tool.numOf && 
+                <div className="ml-2 py-0.5 px-1.5 bg-slate-200 text-slate-500 border rounded-full text-xs font-semibold">
+                  {tool.numOf}
+                </div>
             }
-          >
-            <Button disabled={navbarTool === tool.name} size="xlarge" type="text" className="!px-4 hover:!bg-slate-100">
-              <span className={"text-base " + (navbarTool === tool.name ? "text-slate-900" : "text-slate-500")}>
-                {tool.name}
-              </span>
-              {
-                tool.numOf && 
-                  <div className="ml-2 py-0.5 px-1.5 bg-slate-200 text-slate-500 border rounded-full text-xs font-semibold">
-                    {tool.numOf}
-                  </div>
-              }
-            </Button>
-          </Link>
-        </div>
+          </Button>
+        </Link>
       )}
     </nav>
   );
