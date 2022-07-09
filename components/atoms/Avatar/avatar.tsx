da6984d06099d567bc78e5c5065a3a57bbb488a8
WAGMI Times <wagmitimes@gmail.com>
2022-07-09 05:31:59 -0300

    feat: Create Avatar component

---
+++ components/atoms/Avatar/avatar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+
+interface AvatarProps {
+    className?: string;
+    customTailwindStyles?: string;
+    avatarURL?: string | StaticImageData;
+    alt?: string;
+    size?: string | number;
+}
+
+const avatarLoader = () => {
+  return `https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80`
+}
+
+const Avatar: React.FC<AvatarProps> = ({ className, customTailwindStyles, avatarURL, alt, size }) => {
+  return (
+    <Image 
+      className={`${className && className} ${customTailwindStyles && customTailwindStyles} rounded-full object-cover`}
+      alt={alt ? alt : "Avatar"} width={size} height={size} loader={avatarLoader} src={avatarURL ? avatarURL : avatarLoader} />
+  );
+};
+
+export default Avatar;
