2649650b2a861c4c4b4de077b46d79a67b406ae7
WAGMI Times <wagmitimes@gmail.com>
2022-07-09 05:47:41 -0300

    refactor: Remove div from Avatar story

--- stories/atoms.stories.tsx
+++ stories/atoms.stories.tsx
@@ -9,7 +9,7 @@ const storyConfig = {
 
 export default storyConfig;
 
-export const AvatarAtom = () => <Avatar size={40} avatarURL="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"><div>Test</div></Avatar>;
+export const AvatarAtom = () => <Avatar size={40} avatarURL="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"></Avatar>;
 export const CardAtom = () => <Card><div>Test</div></Card>;
 export const TextAtom = () => <Text><div>Test</div></Text>;
 export const StrongTextAtom = () => <StrongText><div>Test</div></StrongText>;
