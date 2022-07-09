c93b890099ab70f279b10de546444237906421f7
WAGMI Times <wagmitimes@gmail.com>
2022-07-09 05:32:11 -0300

    docs: Add Avatar component to Stories

--- stories/atoms.stories.tsx
+++ stories/atoms.stories.tsx
@@ -1,3 +1,4 @@
+import Avatar from "../components/atoms/Avatar/avatar";
 import Card from "../components/atoms/Card/card";
 import Text from "../components/atoms/Typography/text";
 import StrongText from "../components/atoms/Typography/strong-text";
@@ -8,6 +9,7 @@ const storyConfig = {
 
 export default storyConfig;
 
+export const AvatarAtom = () => <Avatar size={40} avatarURL="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"><div>Test</div></Avatar>;
 export const CardAtom = () => <Card><div>Test</div></Card>;
 export const TextAtom = () => <Text><div>Test</div></Text>;
 export const StrongTextAtom = () => <StrongText><div>Test</div></StrongText>;
