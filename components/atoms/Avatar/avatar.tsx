b1bdf1ef160f4260da61ed4707e7e7c8c0977bdf
Chad Stewart <chadrhonanstewart@gmail.com>
2022-07-09 10:32:02 -0500

    Updated secondary link to Avatar component to hacktoberfest for now. Will probably need a placeholder image in the future.

--- components/atoms/Avatar/avatar.tsx
+++ components/atoms/Avatar/avatar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
-import AvatarImage from "../public/hacktoberfest.png";
+import AvatarImage from "../../../public/hacktoberfest-icon.png";
 
 interface AvatarProps {
     className?: string;
