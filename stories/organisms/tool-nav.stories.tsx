d568ccbc32659eba2c190ed052296c55a7d61496
chadstewart <chadrhonanstewart@gmail.com>
2022-07-10 11:33:05 -0500

    Converted organism stories into individual stories.

---
+++ stories/organisms/tool-nav.stories.tsx
@@ -0,0 +1,9 @@
+import Nav from "../../components/organisms/toolist/nav";
+
+const storyConfig = {
+  title: "Design System/Organisms/Tools Nav"
+};
+
+export default storyConfig;
+
+export const ToolNavOrganism = () => <Nav />;
