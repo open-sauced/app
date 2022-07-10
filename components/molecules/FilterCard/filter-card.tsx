04b727c1eadd466308a63abad5c40c6514278891
WAGMI Times <wagmitimes@gmail.com>
2022-07-09 22:08:59 -0300

    feat: Add icon prop to FilterCard with pre-defined icons

--- components/molecules/FilterCard/filter-card.tsx
+++ components/molecules/FilterCard/filter-card.tsx
@@ -10,18 +10,40 @@ import cancelIcon from "../../../public/x-circle.svg";
 interface FilterCardProps {
     filterName: string;
     bgColor?: string;
-    hashtagIcon?: boolean;
     isRemovable?: boolean;
+    icon?: "topic" | "repo" | "org" | "contributor";
 }
 
-const FilterCard: React.FC<FilterCardProps> = ({ filterName, bgColor, hashtagIcon, isRemovable }) => {
+const icons = {
+  topic: {
+    src: hashIcon.src,
+    alt: "Topic"
+  },
+  org: {
+    src: orgIcon.src,
+    alt: "Organization"
+  },
+  contributor: {
+    src: personIcon.src,
+    alt: "Contributor"
+  },
+  repo: {
+    src: repoIcon.src,
+    alt: "Repository"
+  }
+};
+
+const FilterCard: React.FC<FilterCardProps> = ({ filterName, bgColor, icon, isRemovable }) => {
   return (
     <div 
       tabIndex={0}
       className={`inline-block mr-3 py-1 px-2 py-0.5 border border-slate-300 outline-none hover:bg-slate-50 focus:ring-2 ${bgColor && `bg-${bgColor}`} ${isRemovable ? "focus:ring-orange-500" : "bg-slate-100 focus:ring-slate-300" } rounded-lg`}>
-      { hashtagIcon ?
+      { icon ?
         <div className="flex items-center gap-1">
-          <Image alt="Hashtag Icon" src={hashtag} />
+          <Image 
+            width={14} height={14}
+            alt={icon === "topic" ? icons.topic.alt : icon === "org" ? icons.org.alt : icon === "contributor" ? icons.contributor.alt : icon === "repo" ? icons.repo.alt : "Icon"} 
+            src={icon === "topic" ? icons.topic.src : icon === "org" ? icons.org.src : icon === "contributor" ? icons.contributor.src : icon === "repo" ? icons.repo.src : icons.topic.src} />
           <Text className="!text-sm font-semibold tracking-tight !text-slate-900">
             {filterName}
           </Text>
