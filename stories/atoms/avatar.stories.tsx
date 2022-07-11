1fc1d547841ae79ea479bd7ac31b2aa2a5f685d3
WAGMI Times <wagmitimes@gmail.com>
2022-07-11 20:18:02 -0300

    docs: Add controls to Avatar storybook

--- stories/atoms/avatar.stories.tsx
+++ stories/atoms/avatar.stories.tsx
@@ -1,9 +1,24 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Avatar from "../../components/atoms/Avatar/avatar";
 
 const storyConfig = {
-  title: "Design System/Atoms/Avatar"
+  title: "Design System/Atoms/Avatar",
+
+  argTypes: {
+    size: {
+      control: { type: "number" }
+    },
+    hasBorder: {
+      options: [true, false],
+      control: {type: "radio"}
+    }
+  }
 };
 
 export default storyConfig;
 
-export const AvatarAtom = () => <Avatar size={40} avatarURL="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"></Avatar>;
+const AvatarTemplate: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+
+export const Default = AvatarTemplate.bind({});
+Default.args = { size: 32, hasBorder: true, avatarURL: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80", alt:"Hello" };
