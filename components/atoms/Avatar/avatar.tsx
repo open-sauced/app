ce53adff9f4129791a2f421eb83bc75518f72450
WAGMI Times <wagmitimes@gmail.com>
2022-07-11 20:17:28 -0300

    feat: Add border to Avatar component

--- components/atoms/Avatar/avatar.tsx
+++ components/atoms/Avatar/avatar.tsx
@@ -7,18 +7,20 @@ interface AvatarProps {
     customTailwindStyles?: string;
     avatarURL?: string | StaticImageData;
     alt?: string;
-    size?: string | number;
+    size?: number;
+    hasBorder?: boolean;
 }
 
 const avatarLoader = () => {
   return "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80";
 };
 
-const Avatar: React.FC<AvatarProps> = ({ className, customTailwindStyles, avatarURL, alt, size }) => {
+const Avatar: React.FC<AvatarProps> = ({ className, customTailwindStyles, avatarURL, alt, size, hasBorder }) => {
+
   return (
     <Image 
-      className={`${className && className} ${customTailwindStyles && customTailwindStyles} rounded-full object-cover`}
-      alt={alt ? alt : "Avatar"} width={size} height={size} loader={avatarLoader} src={avatarURL ? avatarURL : AvatarImage} />
+      className={`${className && className} ${customTailwindStyles && customTailwindStyles} rounded-full object-cover ${hasBorder ? "border border-white" : ""}`}
+      alt={alt ? alt : "Avatar"} width={!size ? 32 : size} height={!size ? 32 : size} loader={avatarLoader} src={avatarURL ? avatarURL : AvatarImage} />
   );
 };
 
