182c055504d84494124cdca4f6d4e439e024a579
WAGMI Times <wagmitimes@gmail.com>
2022-07-12 20:06:04 -0300

    docs: Document new previews for Avatar component

--- stories/atoms/avatar.stories.tsx
+++ stories/atoms/avatar.stories.tsx
@@ -7,7 +7,8 @@ const storyConfig = {
 
   argTypes: {
     size: {
-      control: { type: "number" }
+      options: ["sm", "base", "lg"],
+      control: { type: "select" }
     },
     hasBorder: {
       options: [true, false],
@@ -21,4 +22,10 @@ export default storyConfig;
 const AvatarTemplate: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
 
 export const Default = AvatarTemplate.bind({});
-Default.args = { size: 32, hasBorder: true, avatarURL: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80", alt:"Hello" };
+Default.args = { size: "base", hasBorder: false, avatarURL: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80", initials: "BD", alt:"Hello" };
+
+export const HasBorder = AvatarTemplate.bind({});
+HasBorder.args = { size: "base", hasBorder: true, avatarURL: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80", initials: "BD", alt:"Hello" };
+
+export const NoURL = AvatarTemplate.bind({});
+NoURL.args = { size: "base", hasBorder: true, initials: "BD", alt:"Hello" };
