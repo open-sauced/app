e7859c10bde1f2b5fe3939d6cf7550ec74247bf1
chadstewart <chadrhonanstewart@gmail.com>
2022-07-12 18:06:58 -0500

    Updated eslint formatter to fix problem of formatter not linting every file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     function ({ addVariant }) {
-      addVariant('child', '& > *');
-      addVariant('child-hover', '& > *:hover');
-      addVariant('child-active', '& > *:active');
-      addVariant('child-focus', '& > *:focus');
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+      addVariant("child-active", "& > *:active");
+      addVariant("child-focus", "& > *:focus");
     }
-  ],
-}
+  ]
+};
