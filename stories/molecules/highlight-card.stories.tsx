6ba8998afa0e283008297856d94cc1dbfd154cb8
WAGMI Times <wagmitimes@gmail.com>
2022-07-13 17:22:25 -0300

    docs: Update HighlightCard storybook documentation

--- stories/molecules/highlight-card.stories.tsx
+++ stories/molecules/highlight-card.stories.tsx
@@ -1,9 +1,44 @@
+import React from "react";
+import { ComponentStory } from "@storybook/react";
 import HighlightCard from "../../components/molecules/HighlightCard/highlight-card";
 
 const storyConfig = {
-  title: "Design System/Molecules/Highlight Card"
+  title: "Design System/Molecules/Highlight Card",
+  component: "HighlightCard",
+  argTypes: {
+    label: {
+      control: { type: "text" }
+    },
+    icon: {
+      options: ["repo", "topic", "org", "contributor"],
+      control: { type: "select" }
+    }
+  }
 };
 
 export default storyConfig;
 
-export const HighlightCardMolecule = () => <HighlightCard />;
+// HighlightCard Template
+const HighlightCardTemplate: ComponentStory<typeof HighlightCard> = (args) => <HighlightCard {...args} />;
+
+// HighlightCard: Metric Increases
+export const Increases = HighlightCardTemplate.bind({});
+Increases.args = { 
+  label: "Participation",
+  icon: "repo",
+  increased: true,
+  numChanged: 38,
+  percentage: 42,
+  percentageLabel: "of 49,999"
+};
+
+// HighlightCard: Metric Decreases
+export const Decreases = HighlightCardTemplate.bind({});
+Decreases.args = { 
+  label: "Participation",
+  icon: "repo",
+  increased: false,
+  numChanged: 38,
+  percentage: 42,
+  percentageLabel: "of 49,999"
+};
