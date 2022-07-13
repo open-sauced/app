e851d034b70e50f2af0f72398687c93619540a72
WAGMI Times <wagmitimes@gmail.com>
2022-07-12 21:29:02 -0300

    docs: Remove filterName attribute from storybook

--- stories/molecules/highlight-card.stories.tsx
+++ stories/molecules/highlight-card.stories.tsx
@@ -6,4 +6,4 @@ const storyConfig = {
 
 export default storyConfig;
 
-export const HighlightCardMolecule = () => <HighlightCard filterName={""} />;
+export const HighlightCardMolecule = () => <HighlightCard />;
