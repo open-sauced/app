ab11b8e32a8c010ad6e1d7adb3534d061aab9bfc
WAGMI Times <wagmitimes@gmail.com>
2022-07-12 21:24:56 -0300

    docs: Add HighlightCard to storybook

---
+++ stories/molecules/highlight-card.stories.tsx
@@ -0,0 +1,9 @@
+import HighlightCard from "../../components/molecules/HighlightCard/highlight-card";
+
+const storyConfig = {
+  title: "Design System/Molecules/Highlight Card"
+};
+
+export default storyConfig;
+
+export const HighlightCardMolecule = () => <HighlightCard filterName={""} />;
