c2b9d9f6436708b13ea546d07ca9d91c46e9b002
chadstewart <chadrhonanstewart@gmail.com>
2022-07-13 20:05:53 -0500

    Made some minor changes to selectable table component and added more dummy data to default prop in story.

--- components/molecules/SelectableTable/selectable-table.tsx
+++ components/molecules/SelectableTable/selectable-table.tsx
@@ -3,6 +3,7 @@ import Card from "components/atoms/Card/card";
 import Text from "components/atoms/Typography/text";
 import Icon from "components/atoms/Icon/icon";
 import IconButton from "components/atoms/IconButton/icon-button";
+import { Checkbox } from "@supabase/ui";
 import ForkIcon from "public/icons/fork-icon.svg";
 import StarIcon from "public/icons/star-icon.svg";
 import Person from "public/icons/person.svg";
@@ -49,19 +50,19 @@ const SelectableTable: React.FC<SelectableTableProps> = ({ title, tableType, row
           <table className="table-auto w-full">
             <thead className="border-b-[1px]">
               <tr>
-                <th className="w-[50%] p-2">
-                            
-                </th>
                 <th className="p-2">
+                  <Checkbox label=""/>
+                </th>
+                <th className="text-right p-2">
                   <Icon IconImage={iconSuite[tableType].star} />
                 </th>
-                <th className="p-2">
+                <th className="text-right p-2">
                   <Icon IconImage={iconSuite[tableType].fork} />
                 </th>
-                <th className="p-2">
+                <th className="text-right p-2">
                   <Icon IconImage={iconSuite[tableType].person} />
                 </th>
-                <th className="p-2">
+                <th className="text-right p-2">
                   <Icon IconImage={iconSuite[tableType].icon3} />
                 </th>
               </tr>
@@ -72,19 +73,19 @@ const SelectableTable: React.FC<SelectableTableProps> = ({ title, tableType, row
               {rows?.map((row, index) => {
                 return (
                   <tr key={index}>
-                    <td className="w-[50%] text-left p-2">
-                      {row.title}
+                    <td className="flex flex-row text-left p-2">
+                      <Checkbox label=""/> {row.title}
                     </td>
-                    <td className="p-2">
+                    <td className="text-right p-2">
                       {row.stars}%
                     </td>
-                    <td className="p-2">
+                    <td className="text-right p-2">
                       {row.forks}%
                     </td>
-                    <td className="p-2">
+                    <td className="text-right p-2">
                       {row.persons}%
                     </td>
-                    <td className="p-2">
+                    <td className="text-right p-2">
                       {row.unknown}%
                     </td>
                   </tr>);
--- stories/molecules/selectable-table.stories.tsx
+++ stories/molecules/selectable-table.stories.tsx
@@ -18,11 +18,32 @@ const testRows = [
     unknown: 5
   },
   {
-    title: "freecodecamp",
+    title: "free-programming-books",
+    stars: 60,
+    forks: 10,
+    persons: 20,
+    unknown: 5
+  },
+  {
+    title: "material-ui",
+    stars: 20,
+    forks: 30,
+    persons: 10,
+    unknown: 2
+  },
+  {
+    title: "react",
     stars: 100,
     forks: 20,
-    persons: 40,
-    unknown: 5
+    persons: 30,
+    unknown: 10
+  },
+  {
+    title: "java-design-patterns",
+    stars: 20,
+    forks: 14,
+    persons: 10,
+    unknown: 0
   }
 ];
 
