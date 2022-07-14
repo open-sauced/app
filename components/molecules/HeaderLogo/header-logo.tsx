c8da4b0ad1dd9bb8b0cf05e6bcf23c413e9c88b2
chadstewart <chadrhonanstewart@gmail.com>
2022-07-13 21:09:13 -0500

    Made minor change to header-logo component to remove strong prop.

--- components/molecules/HeaderLogo/header-logo.tsx
+++ components/molecules/HeaderLogo/header-logo.tsx
@@ -10,7 +10,7 @@ const HeaderLogo: React.FC = () => {
   return (
     <div className="flex items-center p-2">
       <Image className="rounded" alt="Open Sauced Logo" width={32} height={32} src={openSaucedImg} />
-      <Text className="font-semibold px-2 text-[16px] !text-white" strong>OpenSauced</Text>
+      <Text className="font-semibold px-2 text-[16px] !text-white">OpenSauced</Text>
     </div>
   );
 };
