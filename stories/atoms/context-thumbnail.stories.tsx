e3254e21df6a8b5f01d2ac2bbcce657367ff0351
chadstewart <chadrhonanstewart@gmail.com>
2022-07-15 18:15:55 -0500

    Updated relevant atoms to have controls in storybook.

--- stories/atoms/context-thumbnail.stories.tsx
+++ stories/atoms/context-thumbnail.stories.tsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import ContextThumbnail from "../../components/atoms/ContextThumbnail/context-thumbnail";
+import Thumbnail from "../../public/hacktoberfest-icon.png";
 
 const storyConfig = {
-  title: "Design System/Atoms/Context Thumbnail"
+  title: "Design System/Atoms/Context Thumbnail",
+  component: "ContextThumbnail"
 };
 
 export default storyConfig;
 
-export const ContextThumbnailAtom = () => <ContextThumbnail size={96}></ContextThumbnail>;
+//ContextThumbnail Template
+const ContextThumbnailTemplate: ComponentStory<typeof ContextThumbnail> = (args) => <ContextThumbnail {...args} />;
+
+export const Default = ContextThumbnailTemplate.bind({});
+
+Default.args = {
+  ContextThumbnailURL: Thumbnail.src,
+  alt: "Test",
+  size: 96
+};
