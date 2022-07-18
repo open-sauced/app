5517b5f0b24d3ee9c29012fef756b795e463df6b
WAGMI Times <wagmitimes@gmail.com>
2022-07-18 19:59:55 -0300

    docs: Add OnboardingButton to storybook

---
+++ stories/molecules/onboarding-button.stories.tsx
@@ -0,0 +1,9 @@
+import OnboardingButton from "../../components/molecules/OnboardingButton/onboarding-button";
+
+const storyConfig = {
+  title: "Design System/Molecules/Onboarding Button"
+};
+
+export default storyConfig;
+
+export const OnboardingButtonMolecule = () => <OnboardingButton />;
