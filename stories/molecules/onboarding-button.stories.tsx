9e4972e9ebb677d936d96f88d3d76747316873e6
WAGMI Times <wagmitimes@gmail.com>
2022-07-18 20:33:52 -0300

    docs: Add background to OnboardingButton story

--- stories/molecules/onboarding-button.stories.tsx
+++ stories/molecules/onboarding-button.stories.tsx
@@ -1,9 +1,18 @@
+import React from 'react';
+import { ComponentMeta } from "@storybook/react";
 import OnboardingButton from "../../components/molecules/OnboardingButton/onboarding-button";
 
 const storyConfig = {
-  title: "Design System/Molecules/Onboarding Button"
+  title: "Design System/Molecules/Onboarding Button",
+  decorators: [
+    (Story) => (
+      <div style={{ backgroundColor: "#222", padding: "1rem" }}>
+        <Story />
+      </div>
+    )
+  ]
 };
 
-export default storyConfig;
+export default storyConfig as ComponentMeta<typeof OnboardingButton>;
 
 export const OnboardingButtonMolecule = () => <OnboardingButton />;
