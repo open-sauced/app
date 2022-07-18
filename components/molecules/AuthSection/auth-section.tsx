58979ce552457d0a706e52caf8a53b4a9de85f96
WAGMI Times <wagmitimes@gmail.com>
2022-07-18 20:01:16 -0300

    feat: Add OnboardingButton to AuthSection

--- components/molecules/AuthSection/auth-section.tsx
+++ components/molecules/AuthSection/auth-section.tsx
@@ -4,11 +4,15 @@ import notifications from "../../../public/notifications.svg";
 import downArrow from "../../../public/chevron-down.svg";
 import userAvatar from "../../../public/ellipse-1.png";
 import Avatar from "components/atoms/Avatar/avatar";
+import OnboardingButton from "../OnboardingButton/onboarding-button";
+import { Divider } from "@supabase/ui";
 
 const AuthSection: React.FC = () => {
   return (
     <div className="flex">
       <div className="flex items-center gap-3">
+        <OnboardingButton />
+        <Divider type="vertical" className="!h-6 !bg-gray-600"></Divider>
         <Image alt="Notification Icon" src={notifications} />
         <div className="flex gap-2">
           <Avatar alt="User Avatar" avatarURL={userAvatar} size={"base"} hasBorder={true} />
