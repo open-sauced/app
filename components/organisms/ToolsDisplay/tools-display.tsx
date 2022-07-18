7e2fd78c353207623717fe275b2a805ca676d25a
chadstewart <chadrhonanstewart@gmail.com>
2022-07-18 17:00:40 -0500

    Updated Tools Display component with tools displaying dummy data.

--- components/organisms/ToolsDisplay/tools-display.tsx
+++ components/organisms/ToolsDisplay/tools-display.tsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 import React from "react";
 import Card from "../../atoms/Card/card";
 import SelectableTable from "components/molecules/SelectableTable/selectable-table";
-import { testRows } from "./fake-data";
+import ScatterChart from "components/molecules/ScatterChart/scatter-chart";
+import { testRows, testOptions } from "./fake-data";
 
 const Tool: React.FC = () => {
   const router = useRouter();
@@ -13,22 +14,15 @@ const Tool: React.FC = () => {
     <div className="flex flex-col w-full">
       {tool === "Dashboard" ?
         <>
-          <div className="flex max-w-full h-[500px] mb-10">
+          <div className="flex max-w-full mb-10">
             <div className="flex flex-col w-[60%] mr-10">
-              <Card className="w-full h-[50%] mb-10 p-5">
-                <></>
+              <Card className="w-full mb-10 p-5">
+                <ScatterChart title="Test Title" option={testOptions} />
               </Card>
-              <Card className="w-full h-[50%] p-5">
-                <></>
+              <Card className="w-full mb-10 p-5">
+                <ScatterChart title="Test Title" option={testOptions} />
               </Card>
-            </div>
-            <Card className="w-[40%] p-5">
-              <SelectableTable rows={testRows} title="Test Title" tableType="participants" />
-            </Card>
-          </div>
-          <div className="flex w-full h-[500px] mb-10">
-            <div className="flex flex-col w-[70%] mr-10">
-              <div className="flex max-w-full h-[80%]">
+              <div className="flex w-full h-[500px]">
                 <Card className="w-[50%] mr-10 h-full p-5">
                   <SelectableTable rows={testRows} title="Test Title" tableType="participants" />
                 </Card>
@@ -37,7 +31,7 @@ const Tool: React.FC = () => {
                 </Card>
               </div>
             </div>
-            <Card className="w-[30%] p-5">
+            <Card className="w-[40%] p-5">
               <SelectableTable rows={testRows} title="Test Title" tableType="participants" />
             </Card>
           </div>
