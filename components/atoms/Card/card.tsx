a9d12b3ddac631b6f72041a9802c4165afa71b0c
chadstewart <chadrhonanstewart@gmail.com>
2022-07-19 14:15:07 -0500

    Removed drop shadow from Card component.

--- components/atoms/Card/card.tsx
+++ components/atoms/Card/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ className, children }) => {
   return (
-    <div className={`${className && className} block p-6 bg-white rounded-lg border border-gray-200 shadow-md`}>
+    <div className={`${className && className} block p-6 bg-white rounded-lg border border-slate-300`}>
       {children}
     </div>
   );
