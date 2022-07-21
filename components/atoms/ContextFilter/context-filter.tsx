e7df3c247bb6754efad51c32c833bbc82e5d5d7c
WAGMI Times <wagmitimes@gmail.com>
2022-07-21 14:57:37 -0300

    feat: Create ContextFilter component

---
+++ components/atoms/ContextFilter/context-filter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface ContextFilterProps {
+    className?: string;
+}
+
+const ContextFilter: React.FC<ContextFilterProps> = ({ className }) => {
+  return (
+    <div>Context Filter</div>
+  );
+};
+
+export default ContextFilter;
