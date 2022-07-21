606e38cb36af60021495a83d986bd695bfabea03
WAGMI Times <wagmitimes@gmail.com>
2022-07-21 15:19:12 -0300

    feat: Structure the component

--- components/atoms/ContextFilter/context-filter.tsx
+++ components/atoms/ContextFilter/context-filter.tsx
@@ -19,7 +19,22 @@ interface ContextFilterProps {
 
 const ContextFilter: React.FC<ContextFilterProps> = ({ className }) => {
   return (
-    <div>Context Filter</div>
+    <div className="flex items-center">
+
+      {/* ContextFilterButton */}
+      <ContextFilterButton>
+        Add Filter
+      </ContextFilterButton>
+
+      {/* ContextFilterOptions */}
+      <div className="flex items-center py-1 px-2">
+
+        {/* ContextFilterOption */}
+        <div>
+          Filter
+        </div>
+      </div>
+    </div>
   );
 };
 
