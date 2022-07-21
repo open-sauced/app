8ac670b32dc4da6d7b472b03068531e6a03ec890
WAGMI Times <wagmitimes@gmail.com>
2022-07-21 14:04:24 -0300

    docs: Add Button with Icons to storybook

--- stories/atoms/button.stories.tsx
+++ stories/atoms/button.stories.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Button from "../../components/atoms/Button/button";
+import Icon from "components/atoms/Icon/icon";
+import ForkIcon from "public/icons/fork-icon.svg";
 
 const storyConfig = {
   title: "Design System/Atoms/Button",
@@ -46,3 +48,13 @@ Outline.args = { type: "outline" };
 export const Link = ButtonTemplate.bind({});
 Link.args = { type: "link" };
 
+export const HasIcons: ComponentStory<typeof Button> = (args) => (
+  <div className="flex flex-wrap gap-2">
+    <Button type="default" icon={<Icon IconImage={ForkIcon.src}></Icon>}>
+      Icon Left
+    </Button>
+    <Button type="default" iconRight={<Icon IconImage={ForkIcon.src}></Icon>}>
+      Icon Right
+    </Button>
+  </div>
+);
