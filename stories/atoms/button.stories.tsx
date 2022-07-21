0e28aa6e076f620cc29efbbcbaf56c0031a3dc77
WAGMI Times <wagmitimes@gmail.com>
2022-07-21 14:04:12 -0300

    docs: Add Button Types to storybook

--- stories/atoms/button.stories.tsx
+++ stories/atoms/button.stories.tsx
@@ -14,7 +14,35 @@ const storyConfig = {
 
 export default storyConfig;
 
+
 const ButtonTemplate: ComponentStory<typeof Button> = (args) => <Button {...args}>Button</Button>;
 
+export const ButtonTypes: ComponentStory<typeof Button> = (args) => (
+  <div className="flex gap-2">
+    <Button type="default">
+      Default Button
+    </Button>
+    <Button type="primary">
+      Primary Button
+    </Button>
+    <Button type="outline">
+      Outline Button
+    </Button>
+    <Button type="link">
+      Link Button
+    </Button>
+  </div>
+);
+
 export const Default = ButtonTemplate.bind({});
-Default.args = { type: "primary" };
+Default.args = { type: "default" };
+
+export const Primary = ButtonTemplate.bind({});
+Primary.args = { type: "primary" };
+
+export const Outline = ButtonTemplate.bind({});
+Outline.args = { type: "outline" };
+
+export const Link = ButtonTemplate.bind({});
+Link.args = { type: "link" };
+
