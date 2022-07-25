bd063edf30cad92459c6674d9fe434c8f87d387c
chadstewart <chadrhonanstewart@gmail.com>
2022-07-25 17:47:46 -0500

    Deleted the default component that had the next homepage that was generated when the project was started.

--- stories/organisms/default.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Default from "../../components/organisms/Default/default";
-
-const storyConfig = {
-  title: "Design System/Organisms/Default"
-};
-
-export default storyConfig;
-
-export const DefaultOrganism = () => <Default />;
