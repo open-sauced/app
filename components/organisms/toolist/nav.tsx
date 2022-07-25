178fdcb21f38b2c9e6616886f30311e92e49992e
chadstewart <chadrhonanstewart@gmail.com>
2022-07-25 18:26:52 -0500

    Fixing bug in nav component that on hitting reload button, app would crash.

--- components/organisms/toolist/nav.tsx
+++ components/organisms/toolist/nav.tsx
@@ -32,8 +32,8 @@ const Nav: React.FC<NavProps> = ({ toolList, selectedTool, filterName }) => {
           key={index} className={`tool-list-item ${selectedTool === tool.name ? "" : ""}`}>
           <Link
             href={tool.name === "Dashboard" ?
-              `/${(filterName as string).toLowerCase()}` :
-              `/${(filterName as string).toLowerCase()}/${tool.name.toLowerCase()}`
+              `/${filterName}` :
+              `/${filterName}/${tool.name.toLowerCase()}`
             }
           >
             <Button size="xlarge" type="text" className={`!px-2 md:!px-4  hover:!bg-slate-100 after:block after:absolute after:inset-x-0 after:-bottom-0.5 after:h-0.5 after:rounded-lg ${selectedTool === tool.name ? "after:bg-orange-500" : "focus:after:bg-slate-400"} focus:bg-slate-100 focus:ring-slate-300 child:flex child:items-center`}>
