8afc203020f9f55455cac1a3dbc50c2a2ae20b21
chadstewart <chadrhonanstewart@gmail.com>
2022-07-26 11:24:11 -0500

    Added supabase creds to .env variable.

--- lib/utils/supabase.ts
+++ lib/utils/supabase.ts
@@ -1,11 +1,12 @@
-export interface ProcessEnv {
-  [key: string]: string | undefined
-}
-
 import {createClient, User} from "@supabase/supabase-js";
 
 // TODO: Move these to ENV on PR #96
+const supabaseCreds = {
+  url: process.env.NEXT_SUPABASE_URL ? process.env.NEXT_SUPABASE_URL : "",
+  key: process.env.NEXT_SUPABASE_KEY ? process.env.NEXT_SUPABASE_KEY : ""
+};
+
 export const supabase = createClient(
-  "https://lkkownkrbkmblczeoyqb.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyNDU2MTc0LCJleHAiOjE5NTgwMzIxNzR9.c6nlkT05GnNacQ6OYuGcjBsILmGsSDwEEtN2zZVXFgY"
+  supabaseCreds.url,
+  supabaseCreds.key
 );
