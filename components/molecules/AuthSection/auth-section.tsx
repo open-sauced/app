cfb91efc53fdee4cfd7789987dcb85257af97533
chadstewart <chadrhonanstewart@gmail.com>
2022-07-26 20:31:14 -0500

    Updated auth section to only output a login button when there is no user present.

--- components/molecules/AuthSection/auth-section.tsx
+++ components/molecules/AuthSection/auth-section.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import notifications from "../../../public/notifications.svg";
 import downArrow from "../../../public/chevron-down.svg";
 import Avatar from "components/atoms/Avatar/avatar";
+import Button from "components/atoms/Button/button";
 import userAvatar from "../../../public/ellipse-1.png";
 import OnboardingButton from "../OnboardingButton/onboarding-button";
 import DropdownList from "../DropdownList/dropdown-list";
@@ -19,30 +20,29 @@ const AuthSection: React.FC = ({  }) => {
       <span onClick={async () => await signOut()} key="authorized" className="block px-4 py-2 rounded-md cursor-pointer">
         <Text>Logout</Text>
       </span>
-    ],
-    unauthed: [
-      <span onClick={async () => await signIn({ provider: "github" })} key="unauthorized" className="block px-4 py-2 rounded-md cursor-pointer">
-        <Text>Login</Text>
-      </span>
     ]
   };
 
   return (
     <div className="flex">
       <div className="flex items-center gap-2 lg:gap-3">
-        {user && 
+        {user ? 
           <>
             <OnboardingButton />
             <Divider type="vertical" className="!h-6 !bg-gray-600"></Divider>
             <Image alt="Notification Icon" src={notifications} />
+            <div className="flex justify-end min-w-[60px] gap-2">
+              <DropdownList menuContent={authMenu.authed}>
+                <Avatar alt="User Avatar" avatarURL={user ? user.user_metadata.avatar_url : userAvatar} size={"base"} hasBorder={true} />
+              </DropdownList>
+              <Image alt="Down Arrow" src={downArrow}/>
+            </div>
           </>
+          
+          :
+
+          <Button type="primary" onClick={async () => await signIn({ provider: "github" })} >Login</Button>
         }
-        <div className="flex justify-end min-w-[60px] gap-2">
-          <DropdownList menuContent={user ? authMenu.authed : authMenu.unauthed}>
-            <Avatar alt="User Avatar" avatarURL={user ? user.user_metadata.avatar_url : userAvatar} size={"base"} hasBorder={true} />
-          </DropdownList>
-          <Image alt="Down Arrow" src={downArrow}/>
-        </div>
       </div>
     </div>
   );
