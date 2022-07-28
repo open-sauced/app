8cb8cb8a332030f25168928a6d1c1bf474ea267b
chadstewart <chadrhonanstewart@gmail.com>
2022-07-28 11:59:12 -0500

    Created Login layout to simplify development of login page.

---
+++ layouts/login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../components/organisms/footer/footer";
+import TopNav from "../components/organisms/TopNav/top-nav";
+
+const LoginLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <>
+      <TopNav />
+      <main className="flex w-full h-[calc(100vh-(50px+95px))] flex-1 items-center justify-center text-center">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default LoginLayout;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import { WithPageLayout } from "../interfaces/with-page-layout";
+import LoginLayout from "layouts/login";
 import { useRouter } from "next/router";
 import Card from "components/atoms/Card/card";
 
@@ -6,19 +7,19 @@ const Login: WithPageLayout = () => {
   //const router = useRouter();
 
   return (
-    <main className="flex w-full h-[100vh] flex-1 items-center justify-center text-center">
-      <Card className="flex w-[870px] h-[436px] !p-0 bg-light-slate-3 shadow-login border-none">
-        <>
-          <div className="w-full h-full p-9">
-            Test
-          </div>
-          <div className="w-full h-full p-9 rounded-r-lg bg-white">
-            Test
-          </div>
-        </>
-      </Card>
-    </main>
+    <Card className="flex w-[870px] h-[436px] !p-0 !bg-light-slate-2 shadow-login border-none">
+      <>
+        <div className="w-full h-full p-9">
+          Test
+        </div>
+        <div className="w-full h-full p-9 rounded-r-lg bg-white">
+          Test
+        </div>
+      </>
+    </Card>
   );
 };
+
+Login.PageLayout = LoginLayout;
 
 export default Login;
