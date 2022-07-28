39d2e6a06854c5ac9f947d0998882992e9e5c92f
chadstewart <chadrhonanstewart@gmail.com>
2022-07-28 13:09:48 -0500

    Completed adding icons with text to left side of Login Screen.

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,6 +5,34 @@ import Card from "components/atoms/Card/card";
 import ProgressPie from "components/atoms/ProgressPie/progress-pie";
 import Title from "components/atoms/Typography/title";
 import Text from "components/atoms/Typography/text";
+import Icon from "components/atoms/Icon/icon";
+import Completed from "public/icons/completed-icon.svg";
+import GitHubAuthActive from "public/icons/github-auth-active-icon.svg";
+import ChooseRepo from "public/icons/choose-repo-icon.svg";
+import ChooseRepoActive from "public/icons/choose-repo-active-icon.svg";
+import PAT from "public/icons/pat-icon.svg";
+import PATActive from "public/icons/pat-active-icon.svg";
+
+const LoginStep1: React.FC = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+const LoginStep2: React.FC = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+const LoginStep3: React.FC = () => {
+  return (
+    <>
+    </>
+  );
+};
 
 const Login: WithPageLayout = () => {
   //const router = useRouter();
@@ -16,8 +44,17 @@ const Login: WithPageLayout = () => {
           <div className="flex gap-2 mb-6">
             <ProgressPie percentage={66} /> <Title className="!text-2xl">Let&apos;s get started</Title>
           </div>
-          <div className="text-left text-sm mb-8">
-            <Text>Open Sauced is a platform to provide insights on open source contributions. Let&apos;s start by logging in with GItHub.</Text>
+          <div className="mb-8 text-left ">
+            <Text className="!text-sm">Open Sauced is a platform to provide insights on open source contributions. Let&apos;s start by logging in with GItHub.</Text>
+          </div>
+          <div className="flex gap-2 items-center mb-8">
+            <Icon IconImage={GitHubAuthActive /* Completed */} size={48} /> <Text className="!text-[16px]">Authenicate with GitHub</Text>
+          </div>
+          <div className="flex gap-2 items-center mb-8">
+            <Icon IconImage={PAT /* Completed */} size={48} /> <Text className="!text-[16px]">Provide a Personal Access Token</Text>
+          </div>
+          <div className="flex gap-2 items-center mb-8">
+            <Icon IconImage={ChooseRepo /* Completed */} size={48} /> <Text className="!text-[16px]">Choose some repositories</Text>
           </div>
         </div>
         <div className="w-full h-full p-9 rounded-r-lg bg-white">
