2b44bb4e6df9f3178a1215d9230d03ae601aa310
chadstewart <chadrhonanstewart@gmail.com>
2022-07-28 07:35:35 -0500

    Began making the main containers that will hold in the login ui.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,22 @@
 import { WithPageLayout } from "../interfaces/with-page-layout";
 import { useRouter } from "next/router";
+import Card from "components/atoms/Card/card";
 
 const Login: WithPageLayout = () => {
   //const router = useRouter();
 
   return (
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      Test Page
+    <main className="flex w-full h-[100vh] flex-1 items-center justify-center text-center">
+      <Card className="flex w-[870px] h-[436px] p-0 bg-light-slate-2">
+        <>
+          <div className="w-full h-full p-9">
+            Test
+          </div>
+          <div className="w-full h-full p-9 rounded-r-lg bg-white">
+            Test
+          </div>
+        </>
+      </Card>
     </main>
   );
 };
