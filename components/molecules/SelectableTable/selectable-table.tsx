b0719c33c167947deebe082a839de33a829bc919
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2022-08-01 16:15:48 -0500

    feat: Change all checkboxes in Selectable Table Component (#123)
    * Updated selectable table to select all checkboxes whencheckbox on top of table is selected.
    
    * Fixing issue with supabase API key.
    
    * Destructured row object from map in selectable table.

--- components/molecules/SelectableTable/selectable-table.tsx
+++ components/molecules/SelectableTable/selectable-table.tsx
@@ -33,6 +33,20 @@ const iconSuite = {
 
 const SelectableTable: React.FC<SelectableTableProps> = ({ title, tableType, rows }) => {
   const ref = useRef<HTMLDivElement>(null);
+  const allCheckboxRefs = useRef<HTMLElement[]>([]);
+
+  const addCheckboxToRef = (element: any) => {
+    if(element && !allCheckboxRefs.current.includes(element)) allCheckboxRefs.current.push(element);
+  };
+
+  const changeAllCheckboxes = (event: any) => {
+    const checked = event.target.checked;
+
+    allCheckboxRefs.current.forEach(element => {
+      const checkbox: HTMLInputElement | null = element.querySelector("input[type='checkbox']");
+      if(checkbox) checkbox.checked = checked;
+    });
+  };
 
   const [divSize, setDivSize] = useState(0);
   
@@ -48,7 +62,7 @@ const SelectableTable: React.FC<SelectableTableProps> = ({ title, tableType, row
           <thead className="border-b-[1px]">
             <tr>
               <th className="p-2">
-                <Checkbox label=""/>
+                <Checkbox onChange={event => changeAllCheckboxes(event)} label=""/>
               </th>
               <th className="text-right p-2">
                 <Icon IconImage={iconSuite[tableType].star} />
@@ -67,23 +81,23 @@ const SelectableTable: React.FC<SelectableTableProps> = ({ title, tableType, row
           </thead>
           <tbody>
             <tr className="h-3"></tr>
-            {rows?.map((row, index) => {
+            {rows?.map(({title, stars, forks, persons, unknown}, index) => {
               return (
-                <tr className={`hover:content-['${row.title}']`} key={index}>
-                  <td className="flex flex-row text-left p-2">
-                    <Checkbox label=""/> {divSize > 0 && divSize < 350 ? truncateString(row.title, 3) : row.title}
+                <tr className={`hover:content-['${title}']`} key={index}>
+                  <td className="flex flex-row text-left p-2" ref={element => addCheckboxToRef(element)}>
+                    <Checkbox label="" /> {divSize > 0 && divSize < 350 ? truncateString(title, 3) : title}
                   </td>
                   <td className="text-right p-2">
-                    {row.stars}%
+                    {stars}%
                   </td>
                   <td className="text-right p-2">
-                    {row.forks}%
+                    {forks}%
                   </td>
                   <td className="text-right p-2">
-                    {row.persons}%
+                    {persons}%
                   </td>
                   <td className="text-right p-2">
-                    {row.unknown}%
+                    {unknown}%
                   </td>
                 </tr>);
             })}
