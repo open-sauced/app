2f87a6ca8531b8fd64ee73fcd71455a8a21ccadd
chadstewart <chadrhonanstewart@gmail.com>
2022-08-01 09:35:51 -0500

    Removed comment to add ENV since it was completed and updated repoIcon interface definition from any to string.

--- interfaces/login-repo-object-interface.ts
+++ interfaces/login-repo-object-interface.ts
@@ -1,5 +1,5 @@
 export interface LoginRepoObjectInterface {
   repoOwner: string;
   repoName: string;
-  repoIcon: any;
+  repoIcon: string;
 }
--- lib/utils/supabase.ts
+++ lib/utils/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient, User } from "@supabase/supabase-js";
 
-// TODO: Move these to ENV on PR #96
 const supabaseCreds = {
   url: process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : "",
   apiKey: process.env.NEXT_PUBLIC_SUPABASE_API_KEY ? process.env.NEXT_PUBLIC_SUPABASE_API_KEY : ""
