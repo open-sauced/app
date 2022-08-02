a54fa937b37f697cabfe6838a853fe5bfc259f6d
chadstewart <chadrhonanstewart@gmail.com>
2022-08-02 17:44:11 -0500

    Update header component to put the filter variables into the global app state.

--- components/organisms/header/header.tsx
+++ components/organisms/header/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import Text from "../../atoms/Typography/text";
 import Title from "../../atoms/Typography/title";
@@ -6,13 +6,26 @@ import ContextThumbnail from "../../atoms/ContextThumbnail/context-thumbnail";
 import FilterCard from "../../atoms/FilterCard/filter-card";
 import Thumbnail from "../../../public/hacktoberfest-icon.png";
 import splitQuery from "lib/utils/split-query";
+import { useGlobalStateContext } from "context/global-state";
 
 const Header: React.FC = () => {
   const router = useRouter();
+  const { appState, setAppState } = useGlobalStateContext();
 
   const { filter } = router.query;
-  let filterArray = null;
-  if( filter ) filterArray = splitQuery(filter as string);
+  
+  useEffect(() => {
+    let filterArray: string[] | null = null;
+    if( filter ) filterArray = splitQuery(filter as string);
+    
+    setAppState(prevState => {
+      return {
+        ...prevState,
+        filter: filterArray
+      };
+    });
+  }, [filter]);
+
 
   return (
     <header className="header flex flex-col md:flex-row pt-6 px-4 md:px-16 bg-slate-50">
@@ -21,11 +34,11 @@ const Header: React.FC = () => {
           size={120} ContextThumbnailURL={Thumbnail.src}></ContextThumbnail>
       </div>
       <div className="header-info flex flex-col grow justify-center p-2">
-        <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900">Hacktoberfest 2022{filterArray && ` - Filters:${filterArray.map((filter: any) => ` ${filter}`)}`}</Title>
+        <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900">Hacktoberfest 2022{appState.filter && ` - Filters:${appState.filter.map((filter: any) => ` ${filter}`)}`}</Title>
         <Text className="mt-1 !text-base font-medium text-slate-500">Open source projects and samples for Microsoft.</Text>
         <div className="flex mt-4">
           <FilterCard filterName="hacktoberfest" isRemovable={false} icon="topic" />
-          {filterArray && filterArray.map((filter: string, index: number) => <FilterCard key={index} filterName={filter} bgColor="white"/>)}
+          {appState.filter && appState.filter.map((filter: string, index: number) => <FilterCard key={index} filterName={filter} bgColor="white"/>)}
         </div>
       </div>
     </header>
