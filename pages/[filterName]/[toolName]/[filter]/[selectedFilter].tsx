d34e20eb74ffc6e473a2792a0208ab42b8ec1b1f
chadstewart <chadrhonanstewart@gmail.com>
2022-08-05 09:26:56 -0500

    Added filter routing to app. Not exactly the routing we that we wanted but it's close enough.

--- pages/[filterName]/[toolName]/[filter]/[selectedFilter].tsx
+++ pages/[filterName]/[toolName]/[filter]/[selectedFilter].tsx
@@ -1,5 +1,5 @@
-import FilterLayout from "../../layouts/filter";
-import { WithPageLayout } from "../../interfaces/with-page-layout";
+import FilterLayout from "layouts/filter";
+import { WithPageLayout } from "interfaces/with-page-layout";
 import { useRouter } from "next/router";
 import Tool from "components/organisms/ToolsDisplay/tools-display";
 import changeCapitalization from "lib/utils/change-capitalization";
