ef564876850f1eff933bf0e546e10e46491b720d
chadstewart <chadrhonanstewart@gmail.com>
2022-08-11 14:19:00 -0500

    Added checkbox story to storybook.

---
+++ stories/atoms/checkbox.stories.tsx
@@ -0,0 +1,15 @@
+import Checkbox from "components/atoms/Checkbox/checkbox";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+const storyConfig = {
+  title: "Design System/Atoms/Checkbox"
+};
+
+export default storyConfig;
+
+// FilterCard Template
+const CheckboxTemplate: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />;
+
+// FilterCard Default
+export const Default = CheckboxTemplate.bind({});
+Default.args = { label: "test" };
