aee1cb58c965192f51463c58636c67aaf7405e2f
Brian 'bdougie' Douglas <bdougie@github.com>
2022-08-15 22:41:50 -0700

    declares dbrepo type

--- lib/hooks/useRepos.ts
+++ lib/hooks/useRepos.ts
@@ -1,7 +1,35 @@
 import useSWR from "swr";
 
+interface GsDbRepo {
+  readonly id: number;
+  readonly default_branch_head_time: string;
+  readonly size?: string;
+  readonly stars: number;
+  readonly host_id: number;
+  readonly indexing_frequency?: string;
+  readonly update_started_at?: string;
+  readonly update_finished_at?: boolean;
+  readonly update_synced_issues?: boolean;
+  readonly update_synced_poms: boolean;
+  readonly cloned_at: string;
+  readonly created_at?: string;
+  readonly updated_at?: string;
+  readonly host_pushed_at?: string;
+  readonly host_created_at?: string;
+  readonly host_updated_at?: string;
+  readonly host?: string;
+  readonly owner?: string;
+  readonly name?: string;
+  readonly description?: string;
+  readonly source_uri?: string;
+  readonly owner_avatar?: string;
+  readonly default_branch?: string;
+  readonly default_branch_head?: string;
+  readonly host_parent_repo?: string;
+}
+
 const useRepo = (name: string) => {
-  const { data, error, mutate } = useSWR<DbRepo, Error>(`repos/${name}`);
+  const { data, error, mutate } = useSWR<GsDbRepo, Error>(`repos/${name}`);
 
   return {
     repo: data,
@@ -12,7 +40,7 @@ const useRepo = (name: string) => {
 };
 
 const useRepoList = () => {
-  const { data, error, mutate } = useSWR<DbRepo, Error>("repos/list");
+  const { data, error, mutate } = useSWR<GsDbRepo, Error>("repos/list");
 
   return {
     repoList: data,
