6bc752a03dd688093bc7bbf50b79f05552db13a1
chadstewart <chadrhonanstewart@gmail.com>
2022-08-16 11:49:56 -0500

    Making progress on reports history page. Making the segments for each part of the component.

--- components/molecules/ReportsHistory/reports-history.tsx
+++ components/molecules/ReportsHistory/reports-history.tsx
@@ -2,12 +2,37 @@ import Card from "components/atoms/Card/card";
 import Title from "components/atoms/Typography/title";
 
 const ReportsHistory = (): JSX.Element => {
-  const heading = <Title level={5} className="!text-light-slate-9">Hello</Title>;
+  const heading = <Title level={5} className="!text-light-slate-9 uppercase">
+    <div className="flex justify-between w-full">
+      <div className="w-[40%] border-1 p-2">
+        Report Date
+      </div>
+      <div className="w-[20%] border-1 p-2">
+        issue date
+      </div>
+      <div className="w-[20%] border-1 p-2">
+        Format
+      </div>
+      <div className="w-[20%] border-1 p-2">
+      </div>
+    </div>
+  </Title>;
   
   return (
     <Card heading={heading}>
-      <div>
-        Hello
+      <div className="flex justify-between w-full">
+        <div className="w-[40%] border-1 p-2">
+          Test
+        </div>
+        <div className="w-[20%] border-1 p-2">
+          Test
+        </div>
+        <div className="w-[20%] border-1 p-2">
+          Test
+        </div>
+        <div className="w-[20%] border-1 p-2">
+          Test
+        </div>
       </div>
     </Card>
   );
