165846b3221e4750e919f50da4d1a8669dfa81ec
chadstewart <chadrhonanstewart@gmail.com>
2022-08-16 11:14:50 -0500

    Updated Card component to accept a heading and display it properly.

--- components/atoms/Card/card.tsx
+++ components/atoms/Card/card.tsx
@@ -2,13 +2,28 @@ import React from "react";
 
 interface CardProps {
     className?: string;
+    heading?: JSX.Element;
     children: JSX.Element;
 }
 
-const Card: React.FC<CardProps> = ({ className, children }) => {
+const Card: React.FC<CardProps> = ({ className, children, heading }) => {
   return (
-    <article className={`${className ? className : ""} block p-6 bg-white rounded-lg border border-slate-300`}>
-      {children}
+    <article className={`${className ? className : ""} block ${heading ? "" : "p-6"} bg-white rounded-lg border border-slate-300`}>
+      {
+        heading ?
+          <>
+            <div className="px-6 py-3 rounded-t-lg bg-light-slate-3">
+              {heading}
+            </div>
+            <div className="p-6">
+              {children}
+            </div>
+          </>
+          
+          :
+          
+          children
+      }
     </article>
   );
 };
--- components/molecules/ReportsHistory/reports-history.tsx
+++ components/molecules/ReportsHistory/reports-history.tsx
@@ -1,6 +1,15 @@
+import Card from "components/atoms/Card/card";
+import Title from "components/atoms/Typography/title";
+
 const ReportsHistory = (): JSX.Element => {
+  const heading = <Title level={5} className="!text-light-slate-9">Hello</Title>;
+  
   return (
-    <></>
+    <Card heading={heading}>
+      <div>
+        Hello
+      </div>
+    </Card>
   );
 };
 
