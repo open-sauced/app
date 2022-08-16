11b15a1a11064e0cb4f45eafaa678e053fd762cd
chadstewart <chadrhonanstewart@gmail.com>
2022-08-16 07:58:37 -0500

    Added Reports Filter story to Storybook.

---
+++ stories/molecules/reports-filter.stories.tsx
@@ -0,0 +1,9 @@
+import ReportsFilter from "../../components/molecules/ReportsFilter/reports-filter";
+
+const storyConfig = {
+  title: "Design System/Molecules/Reports Filter"
+};
+
+export default storyConfig;
+
+export const ReportsFilterMolecule = () => <ReportsFilter />;
