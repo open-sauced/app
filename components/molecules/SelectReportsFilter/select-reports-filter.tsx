efced2319433532e9ff124291dc64467da99abd8
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2022-08-16 23:33:49 -0500

    feat: Create Reports Filter Component (#207)
    * Initial commit for Reports Filter component.
    
    * Added Reports Filter story to Storybook.
    
    * Built out Selector Filter Component without dropdown.
    
    * Added Select component and updated reports filter to accept an array of filters that will be mapped in the component.
    
    * Updated reports filter to accept a callback which takes a state value of a string.
    
    * Updated Reports filter story to select reports filter.
    
    * Created Select and Select Option Atoms and replaced direct Select import with created Atoms.
    
    * Added Select atom story to storybook.
    
    * Updated button in Select Reports Filter to use the primary type and updated story to actually say Select Reports Filter.
    
    * Updated Select Reports Filter story to have both populated and unpopulated filter list.
    
    * Fixed misspelling of Supabase component name in select and select option atom.
    
    Fixes #166

---
+++ components/molecules/SelectReportsFilter/select-reports-filter.tsx
@@ -0,0 +1,40 @@
+import Button from "components/atoms/Button/button";
+import Text from "components/atoms/Typography/text";
+import Title from "components/atoms/Typography/title";
+import Select from "components/atoms/Select/select";
+import SelectOption from "components/atoms/Select/select-option";
+import { FilterOptions } from "interfaces/filter-object-types";
+import { useState } from "react";
+
+interface SelectReportsFilterProps {
+  filterList?: FilterOptions[];
+  callback: (selectedValue: string) => any;
+}
+
+const SelectReportsFilter = ({ filterList, callback }: SelectReportsFilterProps): JSX.Element => {
+  const [ selectedValue, setSelectedValue ] = useState("");
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Title level={4} >
+        Select a Filter
+      </Title>
+      <Text>
+        Download the filtered contributions from the last 30 days as a CSV. Selecting a filter will remove all the added repositories.
+      </Text>
+      <div className="flex gap-2">
+        <Select onChange={event => setSelectedValue(event.target.value)} className="w-full">
+          <SelectOption value="">Select a Filter</SelectOption>
+          {filterList?.map(({filterName, filterValue}, index) => 
+            <SelectOption key={index} value={filterValue}>{filterName}</SelectOption>
+          )}
+        </Select>
+        <Button type="primary" onClick={() => callback(selectedValue)} className="w-52">
+          Download CSV
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectReportsFilter;
