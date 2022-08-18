de2d2e9c85e86c98309881bec6c441d7b39e1ca4
smith <oliviamegan11@gmail.com>
2022-08-18 15:52:39 +0100

    fix repositories component alignments

--- components/organisms/Repositories/repositories.tsx
+++ components/organisms/Repositories/repositories.tsx
@@ -5,11 +5,11 @@ import useRepositoriesList from "lib/hooks/useRepositoriesList";
 const Reports = (): JSX.Element => {
   const { repositoriesList } = useRepositoriesList();
   return (
-    <>
+    <div className="flex flex-col w-full gap-4">
       <Card className="w-full lg:w-[calc(50%-(1rem/2))] xl:!w-[calc(40%-(1rem/2))] px-1 xs:px-5 py-5">
         <SelectableTable rows={repositoriesList} title="Repositories" tableType="participants" />
       </Card>
-    </>
+    </div>
   );
 };
 
