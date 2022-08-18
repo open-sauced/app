d43e8a70979e6a9560676c5fd1dea6ed7a7b96c9
smith <oliviamegan11@gmail.com>
2022-08-18 08:09:12 +0100

    changed type defination to interface

--- components/molecules/ComponentGradient/component-gradient.tsx
+++ components/molecules/ComponentGradient/component-gradient.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-type Props = {
+interface ComponentGradientProps  {
   children: React.ReactNode;
   css?: string;
 };
 
-const ComponentGradient = ({ children, css }: Props) => {
+const ComponentGradient = ({ children, css }: ComponentGradientProps) => {
   return (
     <div className="relative overflow-hidden flex justify-center  h-screen bg-gradient-to-b from-gradient-dark-one   to-gradient-dark-two ">
       <div className="absolute -bottom-56 blur-4xl w-96 h-96 self-end rounded-full   bg-gradient-radial  from-gradient-orange-one to-gradient-orange-two "></div>
