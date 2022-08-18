d992aeba7110ece0fbe925ddd23eccc71047e303
smith <oliviamegan11@gmail.com>
2022-08-18 06:30:41 +0100

    Fix name on the repositories component, was previously named reports :)

--- components/organisms/ToolsDisplay/tools-display.tsx
+++ components/organisms/ToolsDisplay/tools-display.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import Dashboard from "../Dashboard/dashboard";
-import Reports from "../Reports/reports";
+import Repositories from "../Repositories/repositories";
 
 interface ToolProps {
   tool?: string;
 }
 
 const Tool: React.FC<ToolProps> = ({ tool }) => {
-
   switch (tool) {
+    case "Dashboard":
+      return <Dashboard />;
 
-  case "Dashboard":
-    return <Dashboard />;
+    case "Repositories":
+      return <Repositories />;
 
-  case "Repositories":
-    return <Reports />;
-
-  default:
-    return <>{tool ? `${tool}` : "Test"} Tool Page</>;
+    default:
+      return <>{tool ? `${tool}` : "Test"} Tool Page</>;
   }
 };
 
--- stories/organisms/reports.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Reports from "components/organisms/Reports/reports";
-
-const StoryConfig = {
-  title: "Design System/organisms/Reports"
-};
-
-export default StoryConfig;
-
-export const ReportsOrganism = (): JSX.Element => <Reports />;
