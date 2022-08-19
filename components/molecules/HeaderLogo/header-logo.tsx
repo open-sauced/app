2ebad073b4bc077b12a4e1edac0ca3c917f05295
smith <oliviamegan11@gmail.com>
2022-08-19 15:59:15 +0100

    refactor code to look cleaner

--- components/molecules/HeaderLogo/header-logo.tsx
+++ components/molecules/HeaderLogo/header-logo.tsx
@@ -10,25 +10,24 @@ interface HeaderLogoProps {
 }
 
 const HeaderLogo: React.FC<HeaderLogoProps> = ({ loginScreen, withBg = false }) => {
-  if (withBg)
-    return (
-      <div className="flex items-center p-2">
-        <Image className="rounded" alt="Open Sauced Logo" width={32} height={32} src={openSaucedImgWithBg} />
-        <Text
-          className={`font-semibold px-2 text-[16px] hidden xs:block ${loginScreen ? "!text-black" : "!text-white"}`}
-        >
-          OpenSauced
-        </Text>
-      </div>
-    );
+  
   return (
     <div className="flex items-center p-2">
-      <Image className="rounded" alt="Open Sauced Logo" width={32} height={32} src={openSaucedImg} />
-      <Text className={`font-semibold px-2 text-[16px] hidden xs:block ${loginScreen ? "!text-black" : "!text-white"}`}>
-        OpenSauced
+      <Image
+        className="rounded"
+        alt="Open Sauced Logo"
+        width={32}
+        height={32}
+        src={withBg ? openSaucedImgWithBg : openSaucedImg}
+      />
+      <Text
+        className={`font-semibold px-2 text-base hidden xs:block ${loginScreen ? "!text-black" : "!text-white"}`}
+      >
+          OpenSauced
       </Text>
     </div>
   );
+
 
 
 };
