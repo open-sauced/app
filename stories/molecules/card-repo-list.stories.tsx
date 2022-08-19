4a58eea43cbab731b7813c3f674df40880fb85b8
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2022-08-19 16:08:25 -0500

    feat: Implement Card Repo List component (#234)
    * Initial commit for Card Repo List component.
    
    * First pass at creating card repo list component.
    
    * Completed card repo list component.
    
    * Updated card repo list component to output a message when the list of repos is empty. Updated card repo list story to show the 3 possible states of card repo list.
    
    * Changed the card repo list component based on code review.
    
    * Updating card repo list based on code review.
    
    * Refactored card repo list to use filter and map functions based on code review suggestion.

---
+++ stories/molecules/card-repo-list.stories.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CardRepoList from "components/molecules/CardRepoList/card-repo-list";
+import TestRepoAvatar from "public/icons/test-repo-avatar.svg";
+
+
+const storyConfig = {
+  title: "Design System/Molecules/Card Repo List",
+  component: "Card Repo List"
+};
+
+export default storyConfig;
+
+const testRepoList = [
+  {
+    repoName: "test",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test2",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test3",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test4",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test5",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test6",
+    repoIcon: TestRepoAvatar
+  }
+];
+
+//CardRepoList Template
+const CardRepoListTemplate: ComponentStory<typeof CardRepoList> = (args) => <CardRepoList {...args} />;
+
+export const MoreThanFiveRepos = CardRepoListTemplate.bind({});
+
+MoreThanFiveRepos.args = {
+  repoList: testRepoList
+};
+
+export const LessThanFiveRepos = CardRepoListTemplate.bind({});
+
+LessThanFiveRepos.args = {
+  repoList: [
+    {
+      repoName: "test",
+      repoIcon: TestRepoAvatar
+    }
+  ]
+};
+
+export const NoRepos = CardRepoListTemplate.bind({});
+
+NoRepos.args = {
+  repoList: []
+};
