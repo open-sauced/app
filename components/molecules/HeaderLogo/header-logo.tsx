6bf820ecb08bd4c1a77c2e398e086d24d56e6d18
smith <oliviamegan11@gmail.com>
2022-08-19 07:37:38 +0100

    add extra props to headerLogo

--- components/molecules/HeaderLogo/header-logo.tsx
+++ components/molecules/HeaderLogo/header-logo.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import Text from "components/atoms/Typography/text";
 import Image from "next/image";
 import openSaucedImg from "../../../public/openSauced-icon.png";
+import openSaucedImgWithBg from "../../../public/open-sourced-with-bg-icon.png";
 
 interface HeaderLogoProps {
   loginScreen?: boolean;
+  withBg: boolean;
 }
 
-const HeaderLogo: React.FC<HeaderLogoProps> = ({ loginScreen }) => {
+const HeaderLogo: React.FC<HeaderLogoProps> = ({ loginScreen, withBg = false }) => {
+  if (withBg)
+    return (
+      <div className="flex items-center p-2">
+        <Image className="rounded" alt="Open Sauced Logo" width={32} height={32} src={openSaucedImgWithBg} />
+        <Text
+          className={`font-semibold px-2 text-[16px] hidden xs:block ${loginScreen ? "!text-black" : "!text-white"}`}
+        >
+          OpenSauced
+        </Text>
+      </div>
+    );
   return (
     <div className="flex items-center p-2">
       <Image className="rounded" alt="Open Sauced Logo" width={32} height={32} src={openSaucedImg} />
-      <Text className={`font-semibold px-2 text-[16px] hidden xs:block ${loginScreen ? "!text-black" : "!text-white"}`}>OpenSauced</Text>
+      <Text className={`font-semibold px-2 text-[16px] hidden xs:block ${loginScreen ? "!text-black" : "!text-white"}`}>
+        OpenSauced
+      </Text>
     </div>
   );
+
+
 };
 
 export default HeaderLogo;
