01bf8c6258257268f2857a257a514318e91a6327
chadstewart <chadrhonanstewart@gmail.com>
2022-08-19 15:15:18 -0500

    Refactored card repo list to use filter and map functions based on code review suggestion.

--- components/molecules/CardRepoList/card-repo-list.tsx
+++ components/molecules/CardRepoList/card-repo-list.tsx
@@ -13,28 +13,21 @@ interface CardRepoListProps {
 }
 
 const CardRepoList = ({ repoList }: CardRepoListProps): JSX.Element => {
-  const jsxToRender: JSX.Element[] = [];
-
-  const renderListOfRepos = (repoList: RepoList[]) => {
-    for(let i = 0; i < REPOLISTLIMIT; i++) {
-      if (!repoList[i]) break;
-
-      const jsxElement = <div className="flex gap-1 p-1 pr-2 border-[1px] border-light-slate-6 rounded-lg text-light-slate-12">
-        <Icon IconImage={repoList[i].repoIcon} />
-        {repoList[i].repoName}
-      </div>;
-      jsxToRender.push(jsxElement);
-    }
-
-    return jsxToRender;
-  };
-
   return (
     <div className="flex gap-2 items-center font-medium text-xs text-light-slate-11">
       {
         repoList.length > 0 ?
           <>
-            {renderListOfRepos(repoList)}
+            {
+              repoList
+                .filter((repo, arrCount) => arrCount < REPOLISTLIMIT)
+                .map(({repoName, repoIcon}, index) => 
+                  <div key={index} className="flex gap-1 p-1 pr-2 border-[1px] border-light-slate-6 rounded-lg text-light-slate-12">
+                    <Icon IconImage={repoIcon} />
+                    {repoName}
+                  </div>
+                )
+            }
             <div>
               {repoList.length > REPOLISTLIMIT - 1 ? `+${repoList.length - REPOLISTLIMIT}` : null}
             </div>
