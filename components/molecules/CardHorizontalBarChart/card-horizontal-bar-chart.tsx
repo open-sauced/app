b38a06c3c756767c6a9607eb75d6e1017fdd0f6f
chadstewart <chadrhonanstewart@gmail.com>
2022-08-19 11:08:34 -0500

    Some code clean up. Currently having an issue where bar is not showing up when it should.

--- components/molecules/CardHorizontalBarChart/card-horizontal-bar-chart.tsx
+++ components/molecules/CardHorizontalBarChart/card-horizontal-bar-chart.tsx
@@ -24,14 +24,16 @@ const CardHorizontalBarChart = ({ languagesUsed }: CardHorizontalBarChartProps):
   return (
     <div className="flex flex-col gap-2">
       {/* Progress Bar */}
-      <div className={`flex items-center w-full rounded-full mt-7 ${(languagesUsed[0].percentageUsed && (languagesUsed[0].percentageUsed > 0 || languagesUsed[0].percentageUsed < 99)) ? "gap-2" : ""}`}>
-        <div
-          onMouseOver={() => handleChangeDescriptText(languagesUsed[0].languageName)}
-          className={`${languageToColor[languagesUsed[0].languageName] ? languageToColor[languagesUsed[0].languageName] : ""} h-3 rounded-full transition-all duration-500 ease-in-out`}
-          style={{width: (languagesUsed[0].percentageUsed ? languagesUsed[0].percentageUsed : 0) + "%"}} />
+      <div className="flex items-center w-full rounded-full mt-7">
+        {languagesUsed.map(({ languageName, percentageUsed }, index) => 
+          <div
+            key={index}
+            onMouseOver={() => handleChangeDescriptText(languageName)}
+            className={`w-[${percentageUsed}%] ${languageToColor[languageName] ? languageToColor[languageName] : ""} h-3 rounded-full transition-all duration-500 ease-in-out`} />
+        )}
       </div>
       <div className="flex gap-2">
-        <div className={`w-6 rounded-full ${languageToColor[languagesUsed[0].languageName] ? languageToColor[languagesUsed[0].languageName] : ""}`}/>{descriptText}
+        <div className={`w-6 rounded-full ${languageToColor[descriptText] ? languageToColor[descriptText] : ""}`}/>{descriptText}
       </div>
     </div>
   );
