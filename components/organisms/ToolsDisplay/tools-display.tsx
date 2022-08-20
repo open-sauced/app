4c93dffebdff121c5f93ba195e7c0c5722d7a0cd
smith <oliviamegan11@gmail.com>
2022-08-20 06:38:02 +0100

    cleaning up the code and testing

--- components/organisms/ToolsDisplay/tools-display.tsx
+++ components/organisms/ToolsDisplay/tools-display.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Dashboard from "../Dashboard/dashboard";
+import Reports from "../Reports/reports";
 import Repositories from "../Repositories/repositories";
 
 interface ToolProps {
@@ -15,6 +16,8 @@ const Tool = ({ tool }: ToolProps): JSX.Element => {
     case "Repositories":
       return <Repositories />;
 
+    case "Reports":
+      return <Reports/>
     default:
       return <> {tool ? `${tool}` : "Test"} Tool Page</>;
   }
