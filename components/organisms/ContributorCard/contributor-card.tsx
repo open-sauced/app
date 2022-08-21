ad53e351586780bd4d0286f13d1bce5181cc941a
chadstewart <chadrhonanstewart@gmail.com>
2022-08-21 11:50:50 -0500

    Completed as much as possible with current components. Left markers for where other components would need to be added.

--- components/organisms/ContributorCard/contributor-card.tsx
+++ components/organisms/ContributorCard/contributor-card.tsx
@@ -1,19 +1,40 @@
+import Button from "components/atoms/Button/button";
 import Card from "components/atoms/Card/card";
+import Text from "components/atoms/Typography/text";
 import CardLineChart from "components/molecules/CardLineChart/card-line-chart";
 import CardProfile from "components/molecules/CardProfile/card-profile";
 import CardRepoList from "components/molecules/CardRepoList/card-repo-list";
 import useContributorCard from "lib/hooks/useContributorCard";
+import { useState } from "react";
 
 const ContributorCard = () => {
   const { profile, repoList, lineChart } = useContributorCard();
+  const [showPRs, setShowPRs] = useState(false);
 
   return (
     <Card>
-      <>
-        <CardProfile {...profile} />
+      <div className="flex flex-col gap-2">
+        <div className="flex w-full justify-between gap-2">
+          <CardProfile {...profile} />
+          <div>Replace with Card Line Chart</div>
+        </div>
         <CardLineChart option={lineChart}/>
         <CardRepoList repoList={repoList}/>
-      </>
+        {showPRs ? 
+          <div>Replace with Card PRs</div>
+        
+          :
+        
+          null
+        }
+        <div className="flex w-full py-3 justify-center">
+          <Button onClick={() => setShowPRs(prevState => !prevState)} type="link">
+            <Text className="!text-xs !text-light-slate-11 font-medium">
+              {showPRs ? "Hide" : "Show"} latest pull requests
+            </Text>
+          </Button>
+        </div>
+      </div>
     </Card>
   );
 };
