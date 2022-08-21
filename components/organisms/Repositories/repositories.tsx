4abe985bd1fe9a11d59dd772e9bbdc233b6e41c3
Brian 'bdougie' Douglas <bdougie@github.com>
2022-08-21 06:53:59 -0700

    feat(data): add repo data again
    data was removed due to merge conflicts, added it back

--- components/organisms/Repositories/repositories.tsx
+++ components/organisms/Repositories/repositories.tsx
@@ -1,13 +1,15 @@
 import Card from "../../atoms/Card/card";
 import SelectableTable from "components/molecules/SelectableTable/selectable-table";
-import useRepositoriesList from "lib/hooks/useRepositoriesList";
+import {useRepoList} from "lib/hooks/useRepoList";
 
 const Reports = (): JSX.Element => {
-  const { repositoriesList } = useRepositoriesList();
+  const { repoList, isLoading } = useRepoList();
+  const data = repoList.data || {data: []};
+
   return (
     <div className="flex flex-col w-full gap-4">
       <Card className="w-full lg:w-[calc(50%-(1rem/2))] xl:!w-[calc(40%-(1rem/2))] px-1 xs:px-5 py-5">
-        <SelectableTable rows={repositoriesList} title="Repositories" tableType="participants" />
+        {isLoading ? <>...Loading</> : <SelectableTable rows={data} title="Repositories" tableType="participants" />}
       </Card>
     </div>
   );
