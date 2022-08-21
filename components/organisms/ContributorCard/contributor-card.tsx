321b44b7acf4be815fa4bb947a9f15c4739c8dc6
chadstewart <chadrhonanstewart@gmail.com>
2022-08-21 11:34:12 -0500

    Starting to work on contributor card component. Made quick hook that'll give component all the data it needs.

--- components/organisms/ContributorCard/contributor-card.tsx
+++ components/organisms/ContributorCard/contributor-card.tsx
@@ -1,6 +1,20 @@
+import Card from "components/atoms/Card/card";
+import CardLineChart from "components/molecules/CardLineChart/card-line-chart";
+import CardProfile from "components/molecules/CardProfile/card-profile";
+import CardRepoList from "components/molecules/CardRepoList/card-repo-list";
+import useContributorCard from "lib/hooks/useContributorCard";
+
 const ContributorCard = () => {
+  const { profile, repoList, lineChart } = useContributorCard();
+
   return (
-    <></>
+    <Card>
+      <>
+        <CardProfile {...profile} />
+        <CardLineChart option={lineChart}/>
+        <CardRepoList repoList={repoList}/>
+      </>
+    </Card>
   );
 };
 
