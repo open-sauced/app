b2caf3ff432809b38219d1f1bfd8ff60df41ea1e
chadstewart <chadrhonanstewart@gmail.com>
2022-08-21 10:18:46 -0500

    Working on table body

--- components/molecules/CardTable/card-table.tsx
+++ components/molecules/CardTable/card-table.tsx
@@ -3,7 +3,20 @@ import { IconContext } from "react-icons";
 import { FaRegDotCircle, FaRegCheckCircle } from "react-icons/fa";
 import { BsFileEarmarkDiff, BsFileDiff } from "react-icons/bs";
 
-const CardTable = () => {
+interface PRs {
+  prStatus: "open" | "closed" | "draft" | "merged";
+  prName: "string";
+  item1: string;
+  item2: string;
+  item3: number;
+  item4: number;
+}
+
+interface CardTableProps {
+  prList: PRs[];
+}
+
+const CardTable = ({ prList }: CardTableProps) => {
   return (
     <div>
       <div className="flex gap-2 items-center bg-light-slate-3 rounded-md px-8 py-4">
@@ -33,6 +46,33 @@ const CardTable = () => {
           </div>
         </IconContext.Provider>
       </div>
+      {prList.map(({prName, prStatus, item1, item2, item3, item4}, index) => 
+        <div key={index} className="flex gap-2 items-center px-8 py-4">
+          <div className="flex gap-2 w-3/5">
+            <IconContext.Provider  value={{ color: "gray", style: { width: 14, height: 14 } }}>
+              <BsFileDiff />
+            </IconContext.Provider>
+            <Text>
+              {item1}
+            </Text>
+            <Text className="!text-light-slate-12 !font-medium">
+              {prName}
+            </Text>
+          </div>
+          <div className="flex justify-end w-[calc(10%-4px)]">
+            {item1}
+          </div>
+          <div className="flex justify-end w-[calc(10%-4px)]">
+            {item2}
+          </div>
+          <div className="flex justify-end w-[calc(10%-4px)]">
+            {item3}
+          </div>
+          <div className="flex justify-end w-[calc(10%-4px)]">
+            {item4}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- stories/molecules/card-table.stories.tsx
+++ stories/molecules/card-table.stories.tsx
@@ -9,11 +9,46 @@ const storyConfig = {
 
 export default storyConfig;
 
+const testPRs = [
+  {
+    prName: "Merging some work",
+    prStatus: "merged",
+    item1: "2mo",
+    item2: "2mo",
+    item3: 13,
+    item4: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "closed",
+    item1: "2mo",
+    item2: "2mo",
+    item3: 13,
+    item4: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "open",
+    item1: "2mo",
+    item2: "2mo",
+    item3: 13,
+    item4: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "draft",
+    item1: "2mo",
+    item2: "2mo",
+    item3: 13,
+    item4: 837
+  }
+];
+
 //CardTable Template
-const CardTableTemplate: ComponentStory<typeof CardTable> = (args) => <CardTable />;
+const CardTableTemplate: ComponentStory<typeof CardTable> = (args) => <CardTable {...args} />;
 
 export const Default = CardTableTemplate.bind({});
 
 Default.args = {
-  children: <>Test</>
+  prList: testPRs
 };
