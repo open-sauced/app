87924d8eae0ff4a7d4c80af9867e0db084974e05
chadstewart <chadrhonanstewart@gmail.com>
2022-08-21 09:12:53 -0500

    Working on general structure of table and header.

--- components/molecules/CardTable/card-table.tsx
+++ components/molecules/CardTable/card-table.tsx
@@ -1,6 +1,20 @@
+import Text from "components/atoms/Typography/text";
+
 const CardTable = () => {
   return (
-    <></>
+    <div>
+      <div className="flex gap-2 bg-light-slate-3 rounded-md px-8 py-4">
+        <div className="w-3/5">
+          <Text>
+            Latest PRs
+          </Text>
+        </div>
+        <div className="flex justify-end w-[calc(10%-4px)]">test</div>
+        <div className="flex justify-end w-[calc(10%-4px)]">test</div>
+        <div className="flex justify-end w-[calc(10%-4px)]">test</div>
+        <div className="flex justify-end w-[calc(10%-4px)]">test</div>
+      </div>
+    </div>
   );
 };
 
