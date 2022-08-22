f006bbc7538e92c8b5130cdceb35fe91fe2c0844
smith <oliviamegan11@gmail.com>
2022-08-22 17:43:13 +0100

    initial commit

--- components/atoms/Avatar/avatar.tsx
+++ components/atoms/Avatar/avatar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
 import AvatarImage from "../../../public/hacktoberfest-icon.png";
+import { number } from "echarts";
 
 interface AvatarProps {
-    className?: string;
-    avatarURL?: string | StaticImageData;
-    initials?: string;
-    alt?: string;
-    size?: "sm" | "base" | "lg";
-    hasBorder?: boolean;
+  className?: string;
+  avatarURL?: string | StaticImageData;
+  initials?: string;
+  alt?: string;
+  size?: "sm" | "base" | "lg" | number;
+  hasBorder?: boolean;
 }
 
 const avatarLoader = () => {
@@ -16,19 +17,40 @@ const avatarLoader = () => {
 };
 
 const Avatar: React.FC<AvatarProps> = ({ className, avatarURL, initials, alt, size, hasBorder }) => {
-
   return (
     <div
-      className={`inline-flex bg-orange-500 justify-center items-center rounded-full overflow-hidden ${hasBorder ? "ring-2 ring-slate-200" : ""} ${size === "sm" ? "w-6 h-6" : size === "base" ? "w-8 h-8" : size === "lg" ? "w-12 h-12" : "w-8 h-8"}`}>
-      { avatarURL ?
-        <Image 
+      className={`inline-flex bg-orange-500 justify-center relative items-center rounded-full overflow-hidden ${
+        hasBorder ? "ring-2 ring-slate-200" : ""
+      } ${
+        size === "sm"
+          ? `w-6 h-6`
+          : size === "base"
+          ? `w-8 h-8`
+          : size === "lg"
+          ? `w-12 h-12`
+          : typeof size === "number"
+          ? `w-${size} h-${size}`
+          : "w-8 h-8"
+      }`}
+    >
+      {avatarURL ? (
+        <Image
           className={`${className ? className : ""} object-cover`}
-          alt={alt ? alt : "Avatar"} width={size === "sm" ? 16 : size === "base" ? 32 : size === "lg" ? 48 : 32} height={size === "sm" ? 16 : size === "base" ? 32 : size === "lg" ? 48 : 32} /* loader={avatarLoader} */ src={avatarURL ? avatarURL : AvatarImage} />
-        :
-        <div className={`font-bold leading-none text-slate-50 mb-0.25 ${size === "sm" ? "text-xs" : size === "base" ? "text-sm" : size === "lg" ? "text-lg" : "text-sm"}`}>
-          { initials }
+          alt={alt ? alt : "Avatar"}
+          layout="fill"
+          // width={size === "sm" ? 16 : size === "base" ? 32 : size === "lg" ? 48 : 32}
+          // height={size === "sm" ? 16 : size === "base" ? 32 : size === "lg" ? 48 : 32}
+          /* loader={avatarLoader} */ src={avatarURL ? avatarURL : AvatarImage}
+        />
+      ) : (
+        <div
+          className={`font-bold leading-none text-slate-50 mb-0.25 ${
+            size === "sm" ? "text-xs" : size === "base" ? "text-sm" : size === "lg" ? "text-lg" : "text-sm"
+          }`}
+        >
+          {initials}
         </div>
-      }
+      )}
     </div>
   );
 };
