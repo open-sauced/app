0cbc6ea13cbe2e66d8e57d62ef7e030fef17a54b
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2022-08-23 16:25:21 -0500

    feat: Implement Card Table Component (#243)
    * Initial commit for card table component.
    
    * Working on general structure of table and header.
    
    * Completed header for card table.
    
    * Working on table body
    
    * Finished card table component.
    
    * Renamed card table to contributor table and changed padding for table component based on code review feedback.
    
    * Forgot to update padding on copmonent all components.
    
    * Updated interface in contributor table based on code review.
    
    * Forgot to update the story with the changes of the interface.
    
    * Updated alt text for icons. Please note that you need to use title in React-icons to get alt text.
    
    * Updating CSS based on code review.
    
    * Updated react icons to get them to match more closely with one another.

---
+++ stories/molecules/contributor-table.stories.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import ContributorTable from "components/molecules/ContributorTable/contributor-table";
+
+const storyConfig = {
+  title: "Design System/Molecules/Contributor Table",
+  component: "Card Table"
+};
+
+export default storyConfig;
+
+const testPRs = [
+  {
+    prName: "Merging some work",
+    prStatus: "merged",
+    prIssuedTime: "2mo",
+    prClosedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "closed",
+    prIssuedTime: "2mo",
+    prClosedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "open",
+    prIssuedTime: "2mo",
+    prClosedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "draft",
+    prIssuedTime: "2mo",
+    prClosedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  }
+];
+
+//CardTable Template
+const ContributorTableTemplate: ComponentStory<typeof ContributorTable> = (args) => <ContributorTable {...args} />;
+
+export const AddedPullRequests = ContributorTableTemplate.bind({});
+export const NoPullRequests = ContributorTableTemplate.bind({});
+
+AddedPullRequests.args = {
+  prList: testPRs
+};
+
+NoPullRequests.args = {
+  prList: []
+};
