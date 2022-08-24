dacf6e91d4dba2cc95828fcc2d4810aab80862d5
smith <oliviamegan11@gmail.com>
2022-08-24 08:09:58 +0100

    fix avatar toll sizes and spacing 🍕

--- components/molecules/AvatarRoll/avatar-roll.tsx
+++ components/molecules/AvatarRoll/avatar-roll.tsx
@@ -4,31 +4,27 @@ interface AvatarRollProps {
   avatarCount?: number;
 }
 
-const AvatarRoll = ({ avatarCount = 9 }: AvatarRollProps):JSX.Element => {
+const AvatarRoll = ({ avatarCount = 9 }: AvatarRollProps): JSX.Element => {
   const rollCount = Array.apply(null, Array(avatarCount));
 
   return (
     <div className="flex gap-3 items-center">
       {rollCount.map((roll, index) => (
-        <div
+        <Avatar
           key={index}
-          className={ 
-            `${index === 0 ? "scale-50" : ""} ` +
-            `${index === 1 ? "scale-75" : ""}` +
-            `${index === 2 ? "scale-100" : ""}` +
-            `${index === 3 ? "scale-110" : ""}` +
-            `${index === 4 ? "scale-110" : ""}` +
-            `${index === 5 ? "scale-110" : ""}` +
-            `${index === 6 ? "scale-100" : ""} ` +
-            `${index === 7 ? "scale-75" : ""}` +
-            `${index === 8 ? "scale-50" : ""}`
+          size={
+            index === 0 || index === 8
+              ? 24
+              : index === 1 || index === 7
+                ? 32
+                : index === 2 || index === 6
+                  ? 40
+                  : index === 3 || index === 5
+                    ? 44
+                    : 48
           }
-        >
-          <Avatar
-            size="lg"
-            avatarURL="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
-          />
-        </div>
+          avatarURL="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+        />
       ))}
     </div>
   );
