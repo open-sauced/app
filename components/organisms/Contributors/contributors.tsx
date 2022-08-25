ebffe8988eca517cc75973214fb289d535d0cff4
smith <oliviamegan11@gmail.com>
2022-08-25 15:14:37 +0100

    update naming convention on contributors component

--- components/organisms/Contributors/contributors.tsx
+++ components/organisms/Contributors/contributors.tsx
@@ -1,12 +1,12 @@
 import useContributorData from "lib/hooks/useContributorData";
 import ContributorCard from "../ContributorCard/contributor-card";
 const Contributors = (): JSX.Element =>{
-  const data = useContributorData();
-  const array = Array.apply(null, Array(9));
+  const contributorData = useContributorData();
+  const randomArray = Array.apply(null, Array(9));
   return (
     <div className="w-full grid grid-cols-automobile  md:grid-cols-autodesktop gap-3">
-      {array.map((contributor, index) => (
-        <ContributorCard key={index} className="" contributor={{ ...data }} />
+      {randomArray.map((contributor, index) => (
+        <ContributorCard key={index} className="" contributor={{ ...contributorData }} />
       ))}
     </div>
   );
