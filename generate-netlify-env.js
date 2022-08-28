6b25aaf97a51cac8b2dbd4b9d3d2ee5c71811133
TED Vortex <ted.vortex@gmail.com>
2022-08-28 21:46:31 +0200

    fix: correct redirect env variables into prebuild script

---
+++ generate-netlify-env.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+// check if script is running in netlify CI
+if (process.env.CI === "true") {
+  console.log("Running in Netlify CI environment");
+  let base = process.env.URL;
+
+  if (process.env.CONTEXT === "deploy-preview") {
+    console.log("Running in Netlify deploy-preview environment");
+
+    base = `${process.env.DEPLOY_PRIME_URL}/`;
+  } else {
+    if (process.env.CHANNEL !== undefined && ["alpha", "beta"].includes(process.env.CHANNEL)) {
+      console.log(`Running in Netlify ${process.env.CHANNEL} environment`);
+
+      const {protocol, hostname} = new URL(process.env.URL);
+      base = `${protocol}//${process.env.CHANNEL}.${hostname}/`;
+    }
+  }
+
+  // write BASE_URL env variable to .env file
+  fs.writeFileSync(".env.local", `NEXT_PUBLIC_BASE_URL=${base}`);
+}
