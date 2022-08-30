72c74a8e7b69e806efcc47c54f3f77eeb324549e
chadstewart <chadrhonanstewart@gmail.com>
2022-08-30 08:50:49 -0500

    Moving repo type definitions to a new file called additional.d.ts so that the entire app has access to these types.

---
+++ additional.d.ts
@@ -0,0 +1,43 @@
+// User defined type definitions. Please add type definitions for global types here
+
+interface RepoData {
+  readonly id: number,
+  readonly default_branch_head_time: number,
+  readonly size: number,
+  readonly stars: number,
+  readonly host_id: number,
+  readonly indexing_frequency: number,
+  readonly update_started_at: number,
+  readonly update_finished_at: number,
+  readonly update_synced_issues: boolean,
+  readonly update_synced_poms: boolean,
+  readonly cloned_at: number,
+  readonly created_at: number,
+  readonly updated_at: number,
+  readonly host_pushed_at: Date,
+  readonly host_created_at: Date,
+  readonly host_updated_at: Date,
+  readonly host: string,
+  readonly owner: string,
+  readonly name: string,
+  readonly description: string,
+  readonly source_uri: string,
+  readonly owner_avatar: string,
+  readonly default_branch: string,
+  readonly default_branch_head: string,
+  readonly host_parent_repo: string
+}
+
+interface MetaData {
+  readonly page: number,
+  readonly limit: number,
+  readonly itemCount: number,
+  readonly pageCount: number,
+  readonly hasPreviousPage: boolean,
+  readonly hasNextPage: boolean
+}
+
+interface RepoListResponse {
+  readonly data: RepoData[];
+  readonly meta: MetaData;
+}
--- lib/hooks/useRepositoriesList.ts
+++ lib/hooks/useRepositoriesList.ts
@@ -1,49 +1,7 @@
 import useSWR from "swr";
 
-interface RepoData {
-  id: number,
-  default_branch_head_time: number,
-  size: number,
-  stars: number,
-  host_id: number,
-  indexing_frequency: number,
-  update_started_at: number,
-  update_finished_at: number,
-  update_synced_issues: boolean,
-  update_synced_poms: boolean,
-  cloned_at: number,
-  created_at: number,
-  updated_at: number,
-  host_pushed_at: Date,
-  host_created_at: Date,
-  host_updated_at: Date,
-  host: string,
-  owner: string,
-  name: string,
-  description: string,
-  source_uri: string,
-  owner_avatar: string,
-  default_branch: string,
-  default_branch_head: string,
-  host_parent_repo: string
-}
-
-interface MetaData {
-  page: number,
-  limit: number,
-  itemCount: number,
-  pageCount: number,
-  hasPreviousPage: boolean,
-  hasNextPage: boolean
-}
-
-interface RepoListResponse {
-  data: RepoData[];
-  meta: MetaData;
-}
-
 const useRepositoriesList = () => {
-  const { data, error, mutate } = useSWR<RepoListResponse>("repo/list");
+  const { data, error, mutate } = useSWR<RepoListResponse, Error>("repo/list");
 
   return {
     repoList: data || {data: []},
