9489e9cd4839c0e04c64bea76914f9f77e495e95
smith <oliviamegan11@gmail.com>
2022-09-02 16:28:11 +0100

    Created toggle option component

---
+++ components/atoms/ToggleOption/toggle-option.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ToggleSwitch from "../ToggleSwitch/toggle-switch";
+import { HiInformationCircle } from "react-icons/hi";
+interface ToogleOptionProps {
+  optionText: string;
+  withIcon?: boolean;
+  checked: boolean
+  handleToggle: ()=> void;
+}
+const ToggleOption = ({ optionText, withIcon , checked, handleToggle}: ToogleOptionProps): JSX.Element => {
+  return (
+    <div onClick={()=> handleToggle()} className="inline-flex cursor-pointer items-center gap-2 rounded-md px-2 py-0.5 border border-light-slate-6 bg-light-slate-1">
+      <ToggleSwitch handleToggle={handleToggle} name={optionText}  checked={checked} />
+      <span className="text-light-slate-11">{optionText}</span>
+      {withIcon && <HiInformationCircle className="text-light-slate-9" />}
+    </div>
+  );
+};
+export default ToggleOption;
