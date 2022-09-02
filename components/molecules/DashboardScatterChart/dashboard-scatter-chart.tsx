f01ab0088745300e7af156e3ce89a66c3fdcbf02
Ogbonna Sunday <oliviamegan11@gmail.com>
2022-09-02 21:18:06 +0100

    removed the scatter chart component and moved all modifications to the dashboard scatter chart component

--- components/molecules/DashboardScatterChart/dashboard-scatter-chart.tsx
+++ components/molecules/DashboardScatterChart/dashboard-scatter-chart.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import EChartWrapper from "components/atoms/EChartWrapper/echart-wrapper";
-import ComponentHeader from "../ComponentHeader/component-header";
-import ScatterChart from "../ScatterChart/scatter-cart";
+import ToggleOption from "components/atoms/ToggleOption/toggle-option";
+import Text from "components/atoms/Typography/text";
+import Title from "components/atoms/Typography/title";
+
 
 interface DashboardScatterChartProps {
     title: string;
@@ -9,10 +11,37 @@ interface DashboardScatterChartProps {
 }
 
 const DashboardScatterChart: React.FC<DashboardScatterChartProps> = ({ title, option }) => {
+  const [showMembers, setShowMembers] = useState(false);
+  const [showBots, setShowBots] = useState(false);
+  const handleShowMembers = () => {
+    setShowMembers(!showMembers);
+    // Additional logic for showing members
+  };
+  const handleShowBots = () => {
+    setShowBots(!showBots);
+    // Additional logic for showing bots
+  };
   return (
-    <>
-      <ScatterChart title={title} option={option}/>
-    </>
+    <div>
+      <div className="flex justify-between">
+        <Title level={4}>{title}</Title>
+        <div className="flex gap-2 pr-10">
+          <ToggleOption handleToggle={handleShowBots} checked={showBots} optionText="Show Bots"></ToggleOption>
+          <ToggleOption
+            handleToggle={handleShowMembers}
+            withIcon={true}
+            optionText="Show Member Contributions"
+            checked={showMembers}
+          ></ToggleOption>
+        </div>
+      </div>
+      <div className="mt-3">
+        <Text className="-rotate-90 -left-12 text-light-slate-10 top-[50%] absolute ">Lines of Code Changed</Text>
+        <div className="ml-5">
+          <EChartWrapper option={option} />
+        </div>
+      </div>
+    </div>
   );
 };
 
