a89b63de39494bffa8f8cee8f112349a7a60a535
Ogbonna Sunday <oliviamegan11@gmail.com>
2022-09-05 09:48:36 +0100

    Removed onClick handler from toggle options body

--- components/atoms/ToggleOption/toggle-option.tsx
+++ components/atoms/ToggleOption/toggle-option.tsx
@@ -9,8 +9,10 @@ interface ToogleOptionProps {
 }
 const ToggleOption = ({ optionText, withIcon , checked, handleToggle}: ToogleOptionProps): JSX.Element => {
   return (
-    <div onClick={()=> handleToggle()} className="inline-flex cursor-pointer items-center gap-2 rounded-md px-2 py-0.5 border border-light-slate-6 bg-light-slate-1">
-      <ToggleSwitch handleToggle={handleToggle} name={optionText}  checked={checked} />
+    <div
+      className="inline-flex cursor-pointer items-center gap-2 rounded-md px-2 py-0.5 border border-light-slate-6 bg-light-slate-1"
+    >
+      <ToggleSwitch handleToggle={handleToggle} name={optionText} checked={checked} />
       <span className="text-light-slate-11">{optionText}</span>
       {withIcon && <HiInformationCircle className="text-light-slate-9" />}
     </div>
--- components/atoms/ToggleSwitch/toggle-switch.tsx
+++ components/atoms/ToggleSwitch/toggle-switch.tsx
@@ -10,18 +10,15 @@ interface ToggleSwitchProps {
 const ToggleSwitch = ({ name, checked = false , className , handleToggle}: ToggleSwitchProps): JSX.Element => {
   return (
     <div className={`${className ? className : ""} relative w-7 h-4 inline-block`}>
-      <input type="checkbox" className="hidden" checked={checked} name={name} id={name} />
-      <label
-        onChange={() => handleToggle()}
-        className="h-full block cursor-pointer  overflow-hidden rounded-2xl"
-        htmlFor={name}
-      >
+      <input type="checkbox" className="hidden" defaultChecked={checked} name={name} id={name} />
+      <label className="h-full block cursor-pointer  overflow-hidden rounded-2xl" htmlFor={name}>
         <span
           className={`${
             checked ? "bg-light-orange-10 " : "bg-light-slate-8 "
           }  transition-all duration-300 block ease-in-out w-[200%] h-full `}
         />
         <span
+          onClick={() => handleToggle()}
           className={`${
             checked ? "right-0" : "left-0"
           } w-3 h-3 absolute top-0 m-[2px] bg-white block rounded-full transition-all duration-300 ease-in-out`}
