d9908515837796d0bff09721e88e2584bc4fec3f
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-07 11:50:11 +0100

    feat: add comma option to humanized number

--- lib/utils/humanizeNumber.ts
+++ lib/utils/humanizeNumber.ts
@@ -3,9 +3,7 @@ const humanizeNumber = (num: number | string) => {
   const abs = Math.abs(number);
   const sign = Math.sign(number);
 
-  return abs > 999
-    ? `${(sign * abs / 1000).toFixed(1)}k`
-    : `${sign * abs}`;
+  return abs > 999 ? `${((sign * abs) / 1000).toFixed(3).replace(".", ",")}` : `${sign * abs}`;
 };
 
 export default humanizeNumber;
