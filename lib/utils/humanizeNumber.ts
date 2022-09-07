7ef46ee7960681cab751c057c5cdcf7f76c60c81
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-07 20:25:47 +0100

    update humanized number function and test

--- lib/utils/humanizeNumber.ts
+++ lib/utils/humanizeNumber.ts
@@ -1,9 +1,11 @@
-const humanizeNumber = (num: number | string) => {
+const humanizeNumber = (num: number | string, type: "comma" | "abbreviation" | null) => {
   const number = typeof num !== "number" ? parseFloat(num) : num;
   const abs = Math.abs(number);
   const sign = Math.sign(number);
+  const commaConverted = abs > 999 ? `${((sign * abs) / 1000).toFixed(3).replace(".", ",")}` : `${sign * abs}`;
+  const abbreviated = abs > 999 ? `${((sign * abs) / 1000).toFixed(1)}k` : `${sign * abs}`;
 
-  return abs > 999 ? `${((sign * abs) / 1000).toFixed(3).replace(".", ",")}` : `${sign * abs}`;
+  return type === "comma" ? commaConverted : abbreviated;
 };
 
 export default humanizeNumber;
--- tests/lib/utils/humanizeNumber.test.ts
+++ tests/lib/utils/humanizeNumber.test.ts
@@ -1,23 +1,36 @@
 import humanizeNumber from "../../../lib/utils/humanizeNumber";
 
 describe("Test: humanizeNumber()", () => {
-  test("1000 should humanize", () => {
+  test("1000 should humanize with abbreviation", () => {
     const num = 1000;
-    const result = humanizeNumber(num);
+    const result = humanizeNumber(num, "abbreviation");
+
+    expect(result).toBe("1.0k");
+  });
+
+  test("1000 should humanize with comma", () => {
+    const num = 1000;
+    const result = humanizeNumber(num, "comma");
 
     expect(result).toBe("1,000");
   });
-
   test("'100' should not humanize", () => {
     const num = "100";
-    const result = humanizeNumber(num);
+    const result = humanizeNumber(num, null);
 
     expect(result).toBe("100");
   });
 
-  test("1234 should humanize", () => {
+  test("1234 should humanize with abbreviation", () => {
     const num = 1234;
-    const result = humanizeNumber(num);
+    const result = humanizeNumber(num, "abbreviation");
+
+    expect(result).toBe("1.2k");
+  });
+
+  test("1234 should humanize with comma", () => {
+    const num = 1234;
+    const result = humanizeNumber(num, "comma");
 
     expect(result).toBe("1,234");
   });
