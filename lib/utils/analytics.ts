c1dac47a9a08725934ef7f149b10a7827369f3a3
chadstewart <chadrhonanstewart@gmail.com>
2022-09-08 08:16:52 -0500

    Initial commit for adding posthog to project.

---
+++ lib/utils/analytics.ts
@@ -0,0 +1,24 @@
+import posthog from "posthog-js";
+
+//Mission Start!
+
+function initiatePostHog () {
+  posthog.init(import.meta.env.VITE_POSTHOG_ID, { api_host: "https://app.posthog.com" });
+}
+
+function capturePostHogAnayltics (
+  analyticsTitle: string,
+  analyticsProperty: string,
+  analyticsValue: string
+) {
+  const analyticsObject: Record<string, string> = {};
+
+  analyticsObject[analyticsProperty] = analyticsValue;
+
+  posthog.capture(analyticsTitle, analyticsObject);
+}
+
+export {
+  initiatePostHog,
+  capturePostHogAnayltics
+};
