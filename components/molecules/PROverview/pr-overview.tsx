a80b4be3fbeb5435e781c251c319da370564f8fa
WAGMI Times <wagmitimes@gmail.com>
2022-09-10 05:34:28 -0300

    feat: Create `<PROverview>` component

---
+++ components/molecules/PROverview/pr-overview.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface PROverviewProps {
+  className?: string;
+  PRData?: Object;
+}
+
+const PROverview: React.FC<PROverviewProps> = ({ className, PRData }) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default PROverview;
