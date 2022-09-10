5a5ea25857896b452a87b16345280d431ea46a30
WAGMI Times <wagmitimes@gmail.com>
2022-09-10 06:29:02 -0300

    feat: Add prop values to component

--- components/molecules/PullRequestOverview/pull-request-overview.tsx
+++ components/molecules/PullRequestOverview/pull-request-overview.tsx
@@ -15,7 +15,21 @@ const PullRequestOverview: React.FC<PullRequestOverviewProps> = ({ className, op
   const totalPullRequests = (!!open ? open : 0) + (!!merged ? merged : 0) + (!!closed ? closed : 0) + (!!draft ? draft : 0);
 
   return (
-    <div>
+    <div className="flex flex-col gap-1">
+      <div className="w-full flex justify-between gap-1">
+        
+        {/* Total Number of Pull Requests */}
+        <div className="font-medium text-base text-light-slate-11 tracking-tight">
+          {totalPullRequests} {`PR${totalPullRequests > 1 ? "s" : ""}`}
+        </div>
+
+        {/* Churn Number compared with previous date (default: last 30 days vs. previous 30 days range) */}
+        <div className={`
+          ${chrunDirection === "up" ? "text-light-grass-10" : "text-light-red-10"}
+          font-medium text-base tracking-tight`}>
+          {chrunDirection === "up" ? "+" : "-"}{churn}%
+        </div>
+      </div>
 
       <PullRequestOverviewChart open={open} merged={merged} closed={closed} draft={draft} totalPullRequests={totalPullRequests} />
     </div>
