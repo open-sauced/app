05e133a9259ca88584bdeff2f0870985561383f2
WAGMI Times <wagmitimes@gmail.com>
2022-09-10 06:25:41 -0300

    feat: Import `PullRequestOverviewChart`

--- components/molecules/PullRequestOverview/pull-request-overview.tsx
+++ components/molecules/PullRequestOverview/pull-request-overview.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PullRequestOverviewChart from "../../atoms/PullRequestOverviewChart/pull-request-overview-chart";
 
 interface PROverviewProps {
   className?: string;
