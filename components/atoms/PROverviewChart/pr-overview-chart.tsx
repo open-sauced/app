d07792d0dd806675c801c4568d1d6f5184cac8d1
WAGMI Times <wagmitimes@gmail.com>
2022-09-10 05:13:46 -0300

    feat: Create `<PROverviewChartBar>` to be used within `<PROverviewChart>`

--- components/atoms/PROverviewChart/pr-overview-chart.tsx
+++ components/atoms/PROverviewChart/pr-overview-chart.tsx
@@ -8,9 +8,28 @@ interface PROverviewChartProps {
     draft?: number;
 }
 
-const PROverviewChart: React.FC<PROverviewChartProps> = ({ className, open, merged, closed, draft }) => {
-  const hello ="";
-  
+interface PROverviewChartBarProps {
+  percent?: number;
+  type?: "open" | "merged" | "closed" | "draft";
+}
+
+const PROverviewChartBar: React.FC<PROverviewChartBarProps> = ({ percent, type }) => {
+  return (
+    <div 
+      className={`
+        ${ type === "open" ? "bg-light-grass-9" : type === "merged" ? "bg-purple-600" : type === "closed" ? "bg-light-red-9" : "bg-light-slate-9"}
+        transition-all duration-500 ease-in-out rounded-full`}
+      style={{width: `${percent}`}}></div>
+  );
+};
+
+
+const PROverviewChart: React.FC<PROverviewChartProps> = ({ className, open = 0, merged = 20, closed = 1, draft = 3 }) => {  
+  const totalPRs = open + merged + closed + draft;
+  const getPercentage = (PRType: number) => {
+    return ((PRType * 100) / totalPRs);
+  };
+
   return (
     <div
       className="bg-light-slate-2">
