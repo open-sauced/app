72e5ca5f9bf336d6c4c269481f940a13128223a0
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-10 06:14:42 +0100

    updated description on page size props

--- components/molecules/Pagination/pagination.tsx
+++ components/molecules/Pagination/pagination.tsx
@@ -6,13 +6,13 @@ interface PaginationProps {
   pages: number[];
   totalPage: number; // represents the total number of pages available from the source
   currentPage: number; // represents the current active page
-  pageSize?: number; // represents the maximum number of pages that  are visible in a single page
+  pageSize?: number; // represents the maximum number of pages button that  are visible in a single page
   onPageChange: (page: number) => void; //  callback function invoked with the updated page value when the page is changed
   divisor?: boolean;
   gotoPage?: boolean;
 }
 
-const Pagination = ({ pages, totalPage, currentPage, divisor, gotoPage = false }: PaginationProps): JSX.Element => {
+const Pagination = ({ pages, totalPage, currentPage, divisor, gotoPage = false, pageSize = 5 }: PaginationProps): JSX.Element => {
   const [selected, setSelected] = useState<number | null>(1);
   const handleSelected = (pageNumber: number) => {
     setSelected(pageNumber);
@@ -24,7 +24,7 @@ const Pagination = ({ pages, totalPage, currentPage, divisor, gotoPage = false }
 
         {pages.map((page, index) => {
           return (
-            index < 5 && (
+            index < pageSize && (
               <div
                 key={index}
                 onClick={() => handleSelected(page)}
