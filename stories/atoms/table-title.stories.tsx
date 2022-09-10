44361a9fdb2b79ebb17e26f07e9827129f49a151
Eryc <101464991+pixelsbyeryc@users.noreply.github.com>
2022-09-10 17:19:36 -0300

    feat: Implement `<TableTitle>` component (#340)

---
+++ stories/atoms/table-title.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import TableTitle from "components/atoms/TableTitle/table-title";
+
+const storyConfig = {
+  title: "Design System/Atoms/TableTitle",
+  component: "TableTitle"
+};
+
+export default storyConfig;
+
+//Title Template
+const TableTitleTemplate: ComponentStory<typeof TableTitle> = (args) => <TableTitle {...args} />;
+
+export const Default = TableTitleTemplate.bind({});
+
+Default.args = {
+  text: "Some text"
+};
