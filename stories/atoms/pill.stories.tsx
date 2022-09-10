4789e636b7991981aceeb1105aeb5916db4b0996
WAGMI Times <wagmitimes@gmail.com>
2022-09-09 23:08:40 -0300

    docs: Improve the storybook with multiple components on the page

--- stories/atoms/pill.stories.tsx
+++ stories/atoms/pill.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Pill from "components/atoms/Pill/pill";
+import { HiTrendingUp } from "react-icons/hi";
 
 const storyConfig = {
   title: "Design System/Atoms/Pill",
@@ -19,13 +20,43 @@ const storyConfig = {
 
 export default storyConfig;
 
-//Pill Template
-const PillTemplate: ComponentStory<typeof Pill> = (args) => <Pill {...args} />;
+const BasePills: ComponentStory<typeof Pill> = (args) => (
+  <div>
+    <Pill text="+150%" color="slate" size="base" />
+    <Pill text="+150%" color="green" size="base" />
+    <Pill text="+150%" color="yellow" size="base" />
+    <Pill text="+150%" color="red" size="base" />
+  </div>
+);
 
-export const Default = PillTemplate.bind({});
+export const Base = BasePills.bind({});
 
-Default.args = {
-  text: "+150%",
-  color: "green",
-  size: "base"
-};
+const SmallPills: ComponentStory<typeof Pill> = (args) => (
+  <div className="flex gap-2">
+    <Pill text="+150%" color="slate" size="small" />
+    <Pill text="+150%" color="green" size="small" />
+    <Pill text="+150%" color="yellow" size="small" />
+    <Pill text="+150%" color="red" size="small" />
+  </div>
+);
+
+export const Small = SmallPills.bind({});
+
+const IconPills: ComponentStory<typeof Pill> = (args) => (
+  <div className="flex flex-col gap-4">
+    <div className="flex gap-2">
+      <Pill text="+150%" color="slate" size="base" icon={<HiTrendingUp />} />
+      <Pill text="+150%" color="green" size="base" icon={<HiTrendingUp />} />
+      <Pill text="+150%" color="yellow" size="base" icon={<HiTrendingUp />} />
+      <Pill text="+150%" color="red" size="base" icon={<HiTrendingUp />} />
+    </div>
+    <div className="flex gap-2">
+      <Pill text="+150%" color="slate" size="small" icon={<HiTrendingUp />} />
+      <Pill text="+150%" color="green" size="small" icon={<HiTrendingUp />} />
+      <Pill text="+150%" color="yellow" size="small" icon={<HiTrendingUp />} />
+      <Pill text="+150%" color="red" size="small" icon={<HiTrendingUp />} />
+    </div>
+  </div>
+);
+
+export const HasIcon = IconPills.bind({});
