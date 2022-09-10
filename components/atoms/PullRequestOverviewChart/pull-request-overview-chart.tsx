af01072760588510c06c8fd6c9205dbb9383fec9
WAGMI Times <wagmitimes@gmail.com>
2022-09-10 05:44:28 -0300

    refactor: Rename components to remove abbreviation "PR" -> "PullRequest"

---
+++ components/atoms/PullRequestOverviewChart/pull-request-overview-chart.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+interface PullRequestOverviewChartBarProps {
+  percent?: number;
+  type?: "open" | "merged" | "closed" | "draft";
+}
+
+const PullRequestOverviewChartBar: React.FC<PullRequestOverviewChartBarProps> = ({ percent, type }) => {
+  return (
+    <div 
+      className={`
+        ${ type === "open" ? "bg-light-grass-9" : type === "merged" ? "bg-purple-600" : type === "closed" ? "bg-light-red-9" : "bg-light-slate-9"}
+        transition-all duration-500 ease-in-out rounded-full`}
+      style={{width: `${percent}`}}></div>
+  );
+};
+
+interface PullRequestOverviewChartProps {
+  className?: string;
+  open?: number;
+  merged?: number;
+  closed?: number;
+  draft?: number;
+}
+
+const PullRequestOverviewChart: React.FC<PullRequestOverviewChartProps> = ({ className, open = 0, merged = 20, closed = 1, draft = 3 }) => {  
+  const totalPRs = open + merged + closed + draft;
+  const getPercentage = (PRType: number) => {
+    return ((PRType * 100) / totalPRs);
+  };
+
+  return (
+    <div
+      className="w-full h-3 flex gap-1 bg-light-slate-2 rounded-full overflow-hidden">
+      
+      {/* Open */}
+      <PullRequestOverviewChartBar percent={`${getPercentage(open)}%`} type="open" />
+
+      {/* Merged */}
+      <PullRequestOverviewChartBar percent={`${getPercentage(merged)}%`} type="merged" />
+
+      {/* Closed */}
+      <PullRequestOverviewChartBar percent={`${getPercentage(closed)}%`} type="closed" />
+      
+      {/* Draft */}
+      <PullRequestOverviewChartBar percent={`${getPercentage(draft)}%`} type="draft" />
+    </div>
+  );
+};
+
+export default PullRequestOverviewChart;
