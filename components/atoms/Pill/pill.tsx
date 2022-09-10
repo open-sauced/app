ed0d74f107ddefaf662f9a9e58e47a2cfd0abf85
WAGMI Times <wagmitimes@gmail.com>
2022-09-09 22:42:33 -0300

    feat: Add variable color to `<Pill>` component

--- components/atoms/Pill/pill.tsx
+++ components/atoms/Pill/pill.tsx
@@ -10,10 +10,18 @@ interface PillProps {
 
 const Pill: React.FC<PillProps> = ({ className, text, color = "slate", size = "base", icon}) => {
   return (
-    <div className="">
+    <div 
+      className={`
+        ${color === "green" ? "bg-light-grass-4" : color === "yellow" ? "bg-light-amber-4" : color === "red" ? "bg-light-red-4" : "bg-light-slate-4"}
+        inline-flex py-0.5 px-2 rounded-full`}>
       {icon}
 
-      
+      <div 
+        className={`
+        ${color === "green" ? "text-light-grass-11" : color === "yellow" ? "text-light-amber-11" : color === "red" ? "text-light-red-11" : "text-light-slate-11"}
+          font-medium text-sm`}>
+        {text}
+      </div>
     </div>
   );
 };
