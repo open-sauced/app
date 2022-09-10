ae49d70b257b84659e9bdf3e1d13a525eb608742
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-10 07:58:42 +0100

    removed the close icon from search input

--- components/atoms/Search/search.tsx
+++ components/atoms/Search/search.tsx
@@ -8,10 +8,10 @@ interface SearchProps {
 }
 const Search = ({ placeholder, name, value , autoFocus}: SearchProps): JSX.Element => {
   return (
-    <div className="flex  py-2 w-max px-3 shadow-search border transition focus-within:ring focus-within:border-orange-500 focus-within:ring-orange-100 rounded-lg   ring-light-slate-6 items-center">
+    <div className="flex  py-2 w-max px-3 shadow-input border transition focus-within:ring focus-within:border-orange-500 focus-within:ring-orange-100 rounded-lg   ring-light-slate-6 items-center">
       <FaSearch className="text-light-slate-9" fontSize={18} />
       <input
-        className="pl-2 w-52  placeholder:text-md  placeholder:font-medium text-md focus:outline-none placeholder:text-light-slate-9"
+        className="pl-2 w-52 font-medium placeholder:text-md  placeholder:font-medium text-md focus:outline-none placeholder:text-light-slate-9"
         autoFocus={autoFocus}
         placeholder={placeholder}
         name={name}
