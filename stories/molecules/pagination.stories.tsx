dbbbc67afdcfbe973def753745f51d3e8f5ecd1b
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-10 07:07:23 +0100

    Add Nodivisor story version

--- stories/molecules/pagination.stories.tsx
+++ stories/molecules/pagination.stories.tsx
@@ -10,9 +10,17 @@ export default StoryConfig;
 const PaginationTemplate: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
 
 export const Default = PaginationTemplate.bind({});
+export const NoDivisor = PaginationTemplate.bind({});
 
 Default.args = {
   pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   totalPage: 2003,
   currentPage: 5
 };
+NoDivisor.args = {
+  pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  totalPage: 2003,
+  currentPage: 5,
+  divisor: false
+};
+
