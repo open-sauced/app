5188fc8b7e8ff36c7b22963f802e76b1e19f545d
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-11 08:18:30 +0100

    feat: Implement footer component

--- components/organisms/Footer/footer.tsx
+++ components/organisms/Footer/footer.tsx
@@ -1,16 +1,79 @@
 import React from "react";
+import Image from "next/image";
+import OpenSaucedLogo from "public/openSauced-icon.png";
+import Text from "components/atoms/Typography/text";
 
-const Footer: React.FC = () => {
+// icons
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
+import { FaDev } from "react-icons/fa";
+
+const footerContext = [
+  {
+    privacy: { url: "", text: "privacy" },
+    terms: { url: "", text: "terms" },
+    status: { url: "", text: "status" }
+  },
+  {
+    hot: { url: "", text: "hot.opensauced.pizza" },
+    openSauced: { url: "", text: "opensauced.pizza" }
+  },
+  {
+    socials: [
+      { url: "", icon: <AiOutlineTwitter className="text-2xl text-light-slate-9" /> },
+      { url: "", icon: <AiOutlineGithub className="text-2xl text-light-slate-9"/> },
+      { url: "", icon: <AiFillInstagram className="text-2xl text-light-slate-9"/> },
+      { url: "", icon: <AiFillYoutube className="text-2xl text-light-slate-9" /> },
+      { url: "", icon: <FaDiscord className="text-2xl text-light-slate-9"/> },
+      { url: "", icon: <FaDev className="text-2xl text-light-slate-9"/> }
+    ]
+  }
+];
+
+const Footer = (): JSX.Element => {
   return (
-    <footer className="flex h-24 w-full bg-white items-center justify-center border-t">
-      <a
-        className="flex items-center justify-center gap-2"
-        href="https://www.youtube.com/opensauced"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by<div className="font-bold">🍕 & ☕ ️</div>
-      </a>
+    <footer className="px-6 md:px-16 h-24 w-full bg-white ">
+      <div className=" font-medium md:border-t md:py-8 md:items-center md:justify-between md:gap-x-4 flex flex-col gap-y-4 md:flex-row py-2 w-full">
+        <div className="text-center justify-center gap-1 flex items-center">
+          <Image width={24} height={24} alt="brand logo" src={OpenSaucedLogo} /> <span className="md:hidden font-bold text-light-slate-12 ">OpenSauced</span>
+          <Text className="hidden !text-light-slate-9 md:inline-block">
+          © 2022 <span className="hidden md:inline-block">Open sauced</span>
+          </Text>
+        </div>
+        <div className="flex md:mr-[300px] text-light-slate-11 justify-center gap-x-4">
+          <a target="_blank" href={footerContext[1].hot?.url} rel="noopener noreferrer">
+            {footerContext[1].hot?.text}
+          </a>
+          <a target="_blank" href={footerContext[1].openSauced?.url} rel="noopener noreferrer">
+            {footerContext[1].openSauced?.text}
+          </a>
+        </div>
+        <div className="flex justify-center gap-x-4 right-0">
+          <div className=" hidden md:flex items-center border-r pr-4   gap-x-4 text-light-slate-11 text-sm">
+            <a href="" target="_blank">{footerContext[0].terms?.text}</a>
+            <a href="" target="_blank">{footerContext[0].privacy?.text}</a>
+            <a href="" target="_blank">{footerContext[0].status?.text}</a>
+          </div>
+          {footerContext[2].socials?.map(({ url, icon }, index) => (
+            <a target="_blank" href={url} key={index} rel="noopener noreferrer">
+              {icon}
+            </a>
+          ))}
+        </div>
+        <div className="flex md:hidden md:border-none md:order-2 border-t py-3 pb-4 mt-2 text-sm justify-between">
+          <Text className="text-light-slate-9">
+          © 2022 <span className="hidden md:inline-block">Open sauced</span>
+          </Text>
+          <div className="flex items-center gap-x-4 text-light-slate-11 text-sm">
+            <a href="" target="_blank">{footerContext[0].terms?.text}</a>
+            <a href="" target="_blank">{footerContext[0].privacy?.text}</a>
+            <a href="" target="_blank">{footerContext[0].status?.text}</a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
