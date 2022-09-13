03caf9fa03d7427783d4879b5907e9e211afeb6b
WAGMI Times <wagmitimes@gmail.com>
2022-09-13 17:08:21 -0300

    feat: Create `<TableRow>` component

---
+++ components/molecules/TableRow/table-row.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface TableRowProps {
+  rowColumns?: Object[];
+}
+
+const TableRow = ({ rowColumns }: TableRowProps): JSX.Element => {
+  return (
+
+    <div>
+      
+    </div>
+
+  );
+};
