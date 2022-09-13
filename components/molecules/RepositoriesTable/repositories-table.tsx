6f60c2cfccbf0beff4e5d9b2a3ae028b6a7ff643
WAGMI Times <wagmitimes@gmail.com>
2022-09-13 18:19:33 -0300

    feat: Add table header and its contents

--- components/molecules/RepositoriesTable/repositories-table.tsx
+++ components/molecules/RepositoriesTable/repositories-table.tsx
@@ -1,14 +1,32 @@
+import TableTitle from "components/atoms/TableTitle/table-title";
 import React from "react";
 
 interface RepositoriesTableProps {
-  title: string;
-  tableType: "participants";
-  rows: DBRepo[];
 }
 
-const RepositoriesTable: React.FC<RepositoriesTableProps> = ({ title, tableType, rows }) => {
+const RepositoriesTable: React.FC<RepositoriesTableProps> = ({  }) => {
   return (
-    <div>
+    <div className="">
+
+      {/* Table Header */}
+      <header className="flex py-4 px-6 bg-light-slate-3 gap-2">
+        <div className=""><TableTitle text="Repository"></TableTitle></div>
+        <div className=""><TableTitle text="Activity"></TableTitle></div>
+        <div className=""><TableTitle text="PR Overview"></TableTitle></div>
+        <div className=""><TableTitle text="PR Velocity"></TableTitle></div>
+        <div className=""><TableTitle text="SPAM"></TableTitle></div>
+        <div className=""><TableTitle text="Contributors"></TableTitle></div>
+        <div className=""><TableTitle text="Last 30 Days"></TableTitle></div>
+      </header>
+
+      {/* Table Rows */}
+      <section className="flex flex-col">
+      </section>
+
+      {/* Table Footer */}
+      <footer>
+
+      </footer>
     </div>
   );
 };
