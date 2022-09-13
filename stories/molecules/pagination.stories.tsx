2cffc905b2f26bb97c14827bab53f71bb2cc45a8
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-13 17:09:48 +0100

    Bug: more error fixing in paginate stories

--- stories/molecules/pagination.stories.tsx
+++ stories/molecules/pagination.stories.tsx
@@ -15,12 +15,12 @@ export const NoDivisor = PaginationTemplate.bind({});
 Default.args = {
   pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   totalPage: 2003,
-  currentPage: 5
+  page: 5
 };
 NoDivisor.args = {
   pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   totalPage: 2003,
-  currentPage: 5,
+  page: 5,
   divisor: false
 };
 
