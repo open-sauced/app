6b1fbdff874a8ccb0fcff68dc6ac3613cdbfe9a6
Brandon Roberts <robertsbt@gmail.com>
2022-09-16 10:25:00 -0500

    chore: delete placeholder api route

--- pages/api/onboarding.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.json({ success: true });
-}
