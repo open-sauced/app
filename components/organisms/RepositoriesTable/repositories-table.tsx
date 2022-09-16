d7d6630d5e4e2158bf135e7d3c7e69f885611181
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-16 16:11:33 +0100

    next step is implementing pagination 🙏

--- components/organisms/RepositoriesTable/repositories-table.tsx
+++ components/organisms/RepositoriesTable/repositories-table.tsx
@@ -5,7 +5,6 @@ import { StaticImageData } from "next/image";
 import Pagination from "../../molecules/Pagination/pagination";
 import PaginationResults from "../../molecules/PaginationResults/pagination-result";
 
-
 export interface ContributorsRows {
   avatarURL?: string | StaticImageData;
   initials?: string;
@@ -16,32 +15,29 @@ export interface RepositoriesRows {
   name?: string;
   handle?: string;
   activity?: string;
-  owner_avatar?: string
-  prOverview?: {
-    open?: number;
-    merged?: number;
-    closed?: number;
-    draft?: number;
-    churn?: number;
-    churnDirection?: string;
-  };
-  prVelocity?: {
+  owner_avatar?: string;
+
+  openPrsCount?: number;
+  mergedPrsCount?: number;
+  closedPrsCount?: number;
+  draftPrsCount?: number;
+  churnTotalCount?: number;
+  churnDirection?: string;
+
+
     amount?: string;
     churn?: string;
-    churnDirection?: string;
-  };
-  spam?: {
-    amount?: string;
-    churn?: string;
-    churnDirection?: string;
-  };
+
+
+
+  spamPrsCount?: number;
   contributors?: ContributorsRows[];
   last30days?: Serie[];
 }
 
 interface RepositoriesTableProps {
   listOfRepositories: RepositoriesRows[];
-  meta: Meta
+  meta: Meta;
 }
 
 export const classNames = {
@@ -58,42 +54,62 @@ export const classNames = {
 };
 
 const RepositoriesTable: React.FC<RepositoriesTableProps> = ({ listOfRepositories, meta }) => {
-
-
   return (
     <div className="flex flex-col rounded-lg overflow-hidden border">
-
       {/* Table Header */}
       <div className="flex py-4 px-6 bg-light-slate-3 gap-2">
-        <div className={classNames.cols.repository}><TableTitle text="Repository"></TableTitle></div>
-        <div className={classNames.cols.activity}><TableTitle text="Activity"></TableTitle></div>
-        <div className={classNames.cols.prOverview}><TableTitle text="PR Overview"></TableTitle></div>
-        <div className={classNames.cols.prVelocity}><TableTitle text="PR Velocity"></TableTitle></div>
-        <div className={classNames.cols.spam}><TableTitle text="SPAM"></TableTitle></div>
-        <div className={classNames.cols.contributors}><TableTitle text="Contributors"></TableTitle></div>
-        <div className={classNames.cols.last30days}><TableTitle text="Last 30 Days"></TableTitle></div>
+        <div className={classNames.cols.repository}>
+          <TableTitle text="Repository"></TableTitle>
+        </div>
+        <div className={classNames.cols.activity}>
+          <TableTitle text="Activity"></TableTitle>
+        </div>
+        <div className={classNames.cols.prOverview}>
+          <TableTitle text="PR Overview"></TableTitle>
+        </div>
+        <div className={classNames.cols.prVelocity}>
+          <TableTitle text="PR Velocity"></TableTitle>
+        </div>
+        <div className={classNames.cols.spam}>
+          <TableTitle text="SPAM"></TableTitle>
+        </div>
+        <div className={classNames.cols.contributors}>
+          <TableTitle text="Contributors"></TableTitle>
+        </div>
+        <div className={classNames.cols.last30days}>
+          <TableTitle text="Last 30 Days"></TableTitle>
+        </div>
       </div>
 
       {/* Table Rows */}
       <section className="flex flex-col">
         {/* Just making sure its not trying to loop through null or undefined values*/}
-        {Array.isArray(listOfRepositories)  && listOfRepositories.length > 0 && listOfRepositories.map((item) =>
-
-          <RepoRow  key={`${item.handle}/${item.name}`} repo={item} />
-
-        )}
+        {Array.isArray(listOfRepositories) &&
+          listOfRepositories.length > 0 &&
+          listOfRepositories.map((item) => <RepoRow key={`${item.handle}/${item.name}`} repo={item} />)}
       </section>
 
       {/* Table Footer */}
       <div className="flex justify-between items-center py-3 px-6 border-t">
         <div className="">
-          <PaginationResults from={meta.page} to={meta.page + meta.limit - 1} total={meta.pageCount} entity={"repositories"} />
+          <PaginationResults
+            from={meta.page}
+            to={meta.page + meta.limit - 1}
+            total={meta.pageCount}
+            entity={"repositories"}
+          />
         </div>
 
         <div className="flex items-center gap-4">
-          <Pagination pages={[]} totalPage={meta.pageCount} page={meta.page} onPageChange={function (page: number): void {
-            throw new Error("Function not implemented.");
-          } } goToPage />
+          <Pagination
+            pages={[]}
+            totalPage={meta.pageCount}
+            page={meta.page}
+            onPageChange={function (page: number): void {
+              throw new Error("Function not implemented.");
+            }}
+            goToPage
+          />
         </div>
       </div>
     </div>
