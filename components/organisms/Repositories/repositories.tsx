463edc0e6259d7a91b3e3dd8fcb86605b08d4ef2
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-16 08:48:26 +0100

    removed unnecessary comments

--- components/organisms/Repositories/repositories.tsx
+++ components/organisms/Repositories/repositories.tsx
@@ -7,19 +7,11 @@ import RepositoriesTable from "../RepositoriesTable/repositories-table";
 const Reports = (): JSX.Element => {
   const { data: repoListData, meta: repoMeta, isError: repoListIsError, isLoading: repoListIsLoading } = useRepositoriesList();
   const preparedIds = !repoListIsLoading && !repoListIsError ? repoListData.map(row => `${row.id}`) : [];
- 
+
   // How do I make multiple calls?
 
   return (
     <div className="flex flex-col w-full gap-4">
-      {/* <Card className="w-full lg:w-99 px-1 xs:px-5 py-5">
-        <>
-          {repoListIsLoading && <>...Loading</>}
-          {repoListIsError && <>An error has occured...</>}
-          {!repoListIsLoading && !repoListIsError && <RepositoryTable rows={repoListData} title="Repositories" tableType="participants" />}
-        </>
-      </Card> */}
-
       {repoListIsLoading && <>...Loading</>}
       {repoListIsError && <>An error has occured...</>}
       {!repoListIsLoading && !repoListIsError && <RepositoriesTable meta={repoMeta} listOfRepositories={repoListData}/>}
