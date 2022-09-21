741288e19a267e4ddcf5c1afb0fcc0e8308d75eb
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-21 20:43:46 +0100

    style: change text font and icon size to xs(12px) (#401)
    * style: change text font and icon size to xs(12px)
    
    * style: some update to the text size

--- components/molecules/CardProfile/card-profile.tsx
+++ components/molecules/CardProfile/card-profile.tsx
@@ -23,12 +23,12 @@ const CardProfile = ({ githubAvatar, githubName, totalPRs, dateOfFirstPR }: Card
         <div>
           <Text className="!text-base !text-black font-medium">{githubName}</Text>
         </div>
-        <div className="flex gap-2 font-medium text-sm">
-          <div className="flex items-center gap-1 text-light-slate-11">
-            <Icon alt="Total Pull-Requests" IconImage={ForkIcon} /> {totalPRs} PR{totalPRs === 1 ? "" : "s"}
+        <div className="flex gap-2 font-medium text-xs">
+          <div className="flex text-xs items-center gap-1 text-light-slate-11">
+            <Icon size={12} alt="Total Pull-Requests" IconImage={ForkIcon} /> {totalPRs} PR{totalPRs === 1 ? "" : "s"}
           </div>
           <div className="flex items-center gap-1 text-light-slate-11">
-            <Icon alt="Time from First Pull Request" IconImage={FirstPRIcon} /> {dateOfFirstPR}
+            <Icon size={12} alt="Time from First Pull Request" IconImage={FirstPRIcon} /> {dateOfFirstPR}
           </div>
         </div>
       </div>
