290e57a01f2a5632b625857a7d7abad0d8f02e0b
WAGMI Times <wagmitimes@gmail.com>
2022-09-22 17:38:57 -0300

    refactor: Update background color to match designs

--- components/molecules/OnboardingButton/onboarding-button.tsx
+++ components/molecules/OnboardingButton/onboarding-button.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const OnboardingButton: React.FC = () => {
   return (
-    <button className="flex items-center gap-2 border border-light-orange-6 py-1 px-1 pr-3 rounded-lg">
+    <button className="flex items-center gap-2 border border-light-orange-8 py-1 px-1 pr-3 rounded-lg">
       <ProgressPie percentage={66} />
       <Text className="text-sm !text-light-slate-12 font-medium">
         <Link href="/start">Complete the onboarding</Link>
--- components/organisms/ToolList/nav.tsx
+++ components/organisms/ToolList/nav.tsx
@@ -34,7 +34,7 @@ const Nav: React.FC<NavProps> = ({ toolList, selectedTool = "dashboard", selecte
           className={`tool-list-item border-b-2 transition-all ease-in-out ${selectedTool === tool.name.toLowerCase() ? "border-orange-500" : "border-transparent hover:border-light-slate-8"}`}>
           <Link href={`/${filterName}/${tool.name.toLowerCase()}${selectedFilter ? `/filter/${selectedFilter}` : ""}`} >
             {/* Button component was here and needed to be removed to resolve issue #187. Button component had styling that will eventually need to be replaced. */}
-            <div className={`flex h-11 px-2 md:px-4 items-center ${selectedTool === tool.name.toLowerCase() ? "" : "cursor-pointer hover:!bg-slate-100"} after:block after:relative after:inset-x-0 after:-bottom-0.5 after:h-0.5 after:rounded-lg ${selectedTool === tool.name.toLowerCase() ? "after:bg-orange-500" : "focus:after:bg-slate-400"} focus:bg-slate-100 focus:ring-slate-300 child:flex child:items-center`}>
+            <div className={`flex h-11 px-2 md:px-4 items-center rounded-t-lg ${selectedTool === tool.name.toLowerCase() ? "" : "cursor-pointer hover:!bg-light-slate-4"} after:block after:relative after:inset-x-0 after:-bottom-0.5 after:h-0.5 after:rounded-lg ${selectedTool === tool.name.toLowerCase() ? "after:bg-orange-500" : "focus:after:bg-slate-400"} focus:bg-slate-100 focus:ring-slate-300 child:flex child:items-center`}>
               <span className={"text-base whitespace-nowrap " + (selectedTool === tool.name.toLowerCase() ? "text-slate-900" : "text-slate-500")}>
                 {tool.name}
               </span>
