8000602b0bdfae9c8e5b38cf81030ef8d4096200
WAGMI Times <wagmitimes@gmail.com>
2022-09-22 13:19:56 -0300

    refactor: Update the table styles, and DRY the code

--- components/molecules/ReportsHistory/reports-history.tsx
+++ components/molecules/ReportsHistory/reports-history.tsx
@@ -11,30 +11,46 @@ interface ReportsHistoryProps {
 }
 
 const ReportsHistory = ({ reportList }: ReportsHistoryProps): JSX.Element => {
+
+  const tableStyles = {
+    text: "text-sm font-medium text-slate-900 tracking-tight",
+    col1: "flex-[1.5]",
+    col2: "flex-[1]",
+    col3: "flex-[1]",
+    col4: "flex-[0.5] text-center"
+  };
   
   return (
     <div className="flex flex-col bg-white border rounded-lg overflow-hidden">
       <div className="flex justify-between gap-2 px-3 md:px-6 py-3 rounded-t-lg bg-light-slate-3">
+        <div className={tableStyles.col1}>
           <TableTitle text={"Report Name"} />
+        </div>
+        <div className={tableStyles.col2}>
           <TableTitle text={"Issue Date"} />
+        </div>
+        <div className={tableStyles.col3}>
           <TableTitle text={"Format"} />
+        </div>
+        <div className={tableStyles.col4}>
           <TableTitle text={"Actions"} />
+        </div>
       </div>
 
       <div className="flex flex-col justify-between w-full gap-2">
         {reportList &&
           reportList.map(({reportDate, reportName, reportFormat, isGenerated, data}, index) => 
-            <div className={`flex items-center py-3 px-3 md:px-6 ${index % 2 === 0 ? "bg-slate-50" : "bg-white"} gap-2`} key={index}>
-              <div className="w-2/5 md:w-2/5 text-sm font-medium text-slate-900 tracking-tight">
+            <div className="flex items-center py-3 px-3 md:px-6 odd:bg-white even:bg-slate-50 gap-2" key={index}>
+              <div className={`${tableStyles.col1} ${tableStyles.text}`}>
                 {reportName}
               </div>
-              <div className="w-1/5 text-sm font-medium text-slate-900 tracking-tight">
+              <div className={`${tableStyles.col2} ${tableStyles.text}`}>
                 {reportDate}
               </div>
-              <div className="w-1/5 text-sm font-medium text-slate-900 tracking-tight">
+              <div className={`${tableStyles.col3} ${tableStyles.text}`}>
                 {reportFormat}
               </div>
-              <div className="w-2/5 text-sm font-medium text-slate-900 tracking-tight text-right">
+              <div className={`${tableStyles.col4} ${tableStyles.text}`}>
                 {isGenerated ?
                   <CSVLink data={data}>
                     <Button type="link" >Download</Button>
