86554599a62ff836649d2c5b7ba766b76f0f2092
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-22 23:03:39 +0100

    chore: added useMediaQuery for mobile responsiveness

--- components/molecules/PaginationResults/pagination-result.tsx
+++ components/molecules/PaginationResults/pagination-result.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "lib/hooks/useMediaQuery";
 import humanizeNumber from "lib/utils/humanizeNumber";
 import React from "react";
 
@@ -8,9 +9,12 @@ interface PaginationResultsProps {
   entity: string; // The entity name
 }
 const PaginationResults = ({ from, to, entity, total }: PaginationResultsProps): JSX.Element => {
+
+  const isNotMobile: boolean = useMediaQuery("(min-width: 768px)");
+
   return (
     <div className="flex items-center gap-x-1 font-medium text-sm text-light-slate-9 tracking-tight">
-      <span>Showing</span> <span className="text-light-slate-12">{from} -</span>
+      <span className={`${!isNotMobile && "hidden"}`}>Showing</span> <span className="text-light-slate-12">{from} -</span>
       <span className="text-light-slate-12">{to}</span> of {total > 999 ? humanizeNumber(total, null) : total}
       <span>{entity}</span>
     </div>
