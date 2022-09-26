25b6997f8dbce1213ddbbaa367df9ebdd28b643e
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-26 17:07:52 +0100

    fix: remove link from highlight card component (#435)
    * fix: remove link from highlight card component
    
    * fix: eslint errors

--- components/organisms/Dashboard/dashboard.tsx
+++ components/organisms/Dashboard/dashboard.tsx
@@ -145,7 +145,7 @@ export const Dashboard = (): JSX.Element => {
     <div className="flex flex-col w-full gap-4">
       <section className="flex flex-wrap gap-4 items-center lg:flex-row lg:flex-nowrap max-w-full">
         <HighlightCard
-          url="/hacktoberfest/people"
+ 
           label="Participation"
           icon="participation"
           metricIncreases={true}
@@ -155,7 +155,7 @@ export const Dashboard = (): JSX.Element => {
           percentageLabel={itemCountText}
         />
         <HighlightCard
-          url="/hacktoberfest/pull%20requests"
+         
           label="Spam"
           icon="spam"
           metricIncreases={false}
@@ -165,7 +165,7 @@ export const Dashboard = (): JSX.Element => {
           percentageLabel={itemCountText}
         />
         <HighlightCard
-          url="/hacktoberfest/pull%20requests"
+         
           label="Accepted PRs"
           icon="accepted-pr"
           metricIncreases={true}
@@ -175,7 +175,7 @@ export const Dashboard = (): JSX.Element => {
           percentageLabel={itemCountText}
         />
         <HighlightCard
-          url="/hacktoberfest/pull%20requests"
+         
           label="Unlabeled PRs"
           icon="unlabeled-pr"
           metricIncreases={false}
