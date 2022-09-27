5ca5bbf66752cca0229764ef5c8b94446261b72b
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-27 07:33:30 +0100

    fix: fix percentage issues on div

--- components/molecules/CardHorizontalBarChart/card-horizontal-bar-chart.tsx
+++ components/molecules/CardHorizontalBarChart/card-horizontal-bar-chart.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Text from "components/atoms/Typography/text";
 import colors from "../../../lib/utils/color.json";
 
@@ -24,24 +24,35 @@ const languageToColor: AllSimpleColors = colors as AllSimpleColors;
 
 const CardHorizontalBarChart = ({ languageList }: CardHorizontalBarChartProps): JSX.Element => {
   const sortedLangArray = languageList.sort((a, b) => b.percentageUsed - a.percentageUsed);
-
+  // used this state to calculate thte percentage of each language
+  const [percentage, setPercentage] = useState<any>();
+  
   const [descriptText, setDescriptText] = useState(sortedLangArray[0].languageName);
 
   const handleChangeDescriptText = (descriptText: string) => {
     setDescriptText(descriptText);
   };
 
+  useEffect(() =>{
+    const totalSumOfFirstFivePercentage = sortedLangArray.map((lang, index) => index < 5 && lang.percentageUsed).reduce((prev: any, next: any) => prev + next); // need some help fixing this type error, used any to bypass 🙏
+    setPercentage(totalSumOfFirstFivePercentage);
+  },[percentage, sortedLangArray]);
+
   return (
     <div className="flex flex-col gap-1 min-w-[120px]">
       {/* Progress Bar */}
       <div className="flex items-center w-full justify-end rounded-full gap-0.5 overflow-hidden">
         {sortedLangArray.map(({ languageName, percentageUsed }, index) =>
-          index < 5 &&  <div
+        {
+          
+          return  index < 5 && ( <div
             key={index}
             onMouseOver={() => handleChangeDescriptText(languageName)}
             className="h-2 transition-all duration-500 ease-in-out"
-            style={{ width: `${percentageUsed < 20 ? 20 - percentageUsed + percentageUsed : percentageUsed}%`, backgroundColor: languageToColor[languageName] ? languageToColor[languageName].color as string : NOTSUPPORTED }}
-          />
+            style={{ width: `${percentageUsed < 20 ? percentageUsed / percentage * 100 : percentageUsed}%`, backgroundColor: languageToColor[languageName] ? languageToColor[languageName].color as string : NOTSUPPORTED }}
+          />);
+        } 
+         
         )}
       </div>
       <div className="flex gap-2 items-center">
