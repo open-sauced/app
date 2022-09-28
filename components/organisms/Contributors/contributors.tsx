aace33b058c2e6a99864ce9b073dfa12d7545f11
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-28 21:11:36 +0100

    feat: add pagination to contributors page

--- components/organisms/Contributors/contributors.tsx
+++ components/organisms/Contributors/contributors.tsx
@@ -2,12 +2,13 @@ import { calcDistanceFromToday } from "lib/utils/date-utils";
 import ContributorCard from "../ContributorCard/contributor-card";
 import color from "lib/utils/color.json";
 import { useTopicContributions } from "lib/hooks/useTopicContributions";
+import Pagination from "components/molecules/Pagination/pagination";
+import PaginationResults from "components/molecules/PaginationResults/pagination-result";
 
 const colorKeys = Object.keys(color);
 
 const Contributors = (): JSX.Element => {
-  const { data,meta,setPage, isError, isLoading } = useTopicContributions();
-  console.log(meta);
+  const { data,meta,setPage ,page, isError, isLoading } = useTopicContributions();
   const contributorArray = isError
     ? []
     : data?.map((contributor) => {
@@ -43,12 +44,45 @@ const Contributors = (): JSX.Element => {
     });
 
   return (
-    <div className="w-full grid grid-cols-automobile  md:grid-cols-autodesktop gap-3">
-      {isLoading ? "Loading..." : ""}
-      {contributorArray.map((contributor, index) => (
-        <ContributorCard key={index} className="" contributor={{ ...contributor }} />
-      ))}
-    </div>
+    <>
+    
+
+      <div className="w-full grid grid-cols-automobile  md:grid-cols-autodesktop gap-3">
+        {isLoading ? "Loading..." : ""}
+        {contributorArray.map((contributor, index) => (
+          <ContributorCard key={index} className="" contributor={{ ...contributor }} />
+        ))}
+      </div>
+
+      {/* Table footer */}
+      <div className="py-4 flex w-full mt-5 justify-between items-center">
+        <div>
+          <div className="">
+            <PaginationResults
+              from={page === 1 ? page : page * meta.limit}
+              to={page === 1 ? meta.limit : page * meta.limit + meta.limit}
+              total={meta.itemCount}
+              entity={"repos"}
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-4">
+            <Pagination
+              pages={[]}
+              hasNextPage={meta.hasNextPage}
+              hasPreviousPage={meta.hasPreviousPage}
+              totalPage={meta.pageCount}
+              page={meta.page}
+              onPageChange={function (page: number): void {
+                setPage(page);
+              }}
+              divisor={false}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Contributors;
