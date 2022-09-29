49e59910799adf8641ce796341622a7febf1467d
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-09-29 22:52:53 +0100

    fix: tiny error

--- components/molecules/TableHeader/table-header.tsx
+++ components/molecules/TableHeader/table-header.tsx
@@ -33,7 +33,7 @@ const TableHeader = ({ title, showing,updateLimit }: TableHeaderProps): JSX.Elem
           label="Showing"
           onChange={function(limit: number):void{updateLimit(limit);}}
         ></Select>
-        <Search placeholder=`Showing ${showing.entity}` name={showing.entity} />
+        <Search placeholder={`Showing ${showing.entity}`} name={showing.entity} />
       </div>
     </div>
   );
