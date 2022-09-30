842993b29dc06d9200773d9ca02c62bc7f3b9ba4
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2022-09-30 16:24:35 -0700

    comment grammar

--- lib/utils/roundedImages.ts
+++ lib/utils/roundedImages.ts
@@ -1,5 +1,5 @@
 // Inspired by https://cloudinary.com/documentation/fetch_remote_images#remote_image_fetch_url
-// This is temporary but requires a Cloudinary account's cloud namea.
+// This is temporary and requires a Cloudinary account's cloud name.
 // In the future we can developer our own local image transformations.
 const roundedImage = (imageUrl: string, cloudName: string) => {
   return `https://res.cloudinary.com/${cloudName}/image/fetch/c_fill,g_face,h_300,w_300,bo_20px_solid_white,r_max/f_auto,e_shadow/${imageUrl}`;
