c3235b66d4f903e925d48819fa0a4bb6bcc2068e
Brian Douglas <bdougie@users.noreply.github.com>
2022-10-04 07:19:42 -0700

    Update github.ts

--- lib/utils/github.ts
+++ lib/utils/github.ts
@@ -1,11 +1,11 @@
 const getAvatarLink = (username: string | null) =>
-  `https://github.com/${username ?? "github"}.png?size=460`;
+  `https://res.cloudinary.com/dgxgziswe/image/fetch/https://github.com/${username ?? "github"}.png?size=460`;
 
 const getProfileLink = (username: string | null) =>
-  `https://github.com/${username ?? ""}`;
+  `https://res.cloudinary.com/dgxgziswe/image/fetch/https://github.com/${username ?? ""}`;
 
 const getRepoIssuesLink = (repoName: string | null) =>
-  `https://github.com/${repoName && `${repoName}/issues` || ""}`;
+  `https://res.cloudinary.com/dgxgziswe/image/fetch/https://github.com/${repoName && `${repoName}/issues` || ""}`;
 
 export {
   getAvatarLink,
