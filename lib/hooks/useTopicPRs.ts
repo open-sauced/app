3613c8dc5c5ce04663d29f887d6316e42e6e3d7c
Brandon Roberts <robertsbt@gmail.com>
2022-10-04 11:28:00 -0500

    feat: connect dashboard insights to the API

--- lib/hooks/useTopicPRs.ts
+++ lib/hooks/useTopicPRs.ts
@@ -6,14 +6,14 @@ interface PaginatedPRsResponse {
   readonly meta: Meta;
 }
 
-const useTopicPRs = () => {
+const useTopicPRs = (limit = 500) => {
   const router = useRouter();
   const { filterName, selectedFilter } = router.query;
   const topic = filterName as string;
   const filter = selectedFilter as string;
   const baseEndpoint = `${topic}/recent-prs`;
   const filterQuery = filter ? `filter=${filter}` : "";
-  const limitQuery = `${filterQuery ? "&": ""}limit=35`;
+  const limitQuery = `${filterQuery ? "&": ""}limit=${limit}`;
   const endpointString = `${baseEndpoint}?${filterQuery}${limitQuery}`;
 
   const { data, error, mutate } = useSWR<PaginatedPRsResponse, Error>(endpointString);
