a498ee34162f6f0965961dbddbf2104dd1ecb879
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-10-06 17:12:53 +0100

    feat: replace browser tooltip with custom

--- components/atoms/Tooltip/tooltip.tsx
+++ components/atoms/Tooltip/tooltip.tsx
@@ -6,8 +6,9 @@ interface TooltipProps {
   direction?: "top" | "right" | "left" | "bottom";
   delay?: number;
   className?: string
+  tipClassName?: string
 }
-const Tooltip = ({ children, content, direction, delay, className }: TooltipProps): JSX.Element => {
+const Tooltip = ({ children, content, direction, delay, className, tipClassName }: TooltipProps): JSX.Element => {
   const [active, setActive] = useState(false);
   let timeout: string | number | NodeJS.Timeout | undefined;
 
@@ -22,9 +23,9 @@ const Tooltip = ({ children, content, direction, delay, className }: TooltipProp
   };
 
   return (
-    <div onMouseEnter={()=> showTip()} onMouseLeave={()=> hideTip()} className="relative inline-flex">
+    <div onMouseEnter={()=> showTip()} onMouseLeave={()=> hideTip()} className={`${className && className} relative inline-flex`}>
       {children}
-      {active && <div className={`${className && className} ${direction === "top" ? "top-[-25px]" : direction === "left" ? "left-[-60px] translate-y[-50%] translate-x-0  top-[50%]": ""}  absolute z-[999] p-1 left-[50%] top-[25px] translate-x-[-50%] bg-dark-slate-2 rounded-[4px] text-dark-slate-12 font-medium text-[10px]`}>{content}</div>}
+      {active && <div className={`${tipClassName && tipClassName} ${direction === "top" ? "top-[-25px]" : direction === "left" ? "-left-14 -translate-y-1/2 translate-x-0  top-1/2": ""}  absolute z-50 p-1 left-1/2 top-[25px] -translate-x-1/2 bg-dark-slate-2 rounded-[4px] whitespace-nowrap text-dark-slate-12 font-medium text-[10px]`}>{content}</div>}
     </div>
   );
 };
