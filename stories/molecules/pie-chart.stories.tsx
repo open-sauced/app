a3c3aae9c0bf1a7bc75fa6eaeb88bc22c00e752f
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-10-10 08:15:47 +0100

    initial commit for the pie chart component

---
+++ stories/molecules/pie-chart.stories.tsx
@@ -0,0 +1,9 @@
+import PieChart from "components/molecules/PieChart/pie-chart";
+
+
+const StoryConfig = {
+  title: "Design System/Molecules/PieChart"
+};
+export default StoryConfig;
+
+export const PieChartStory = ()=> <PieChart/>;
