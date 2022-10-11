618d3dcb1abb80776bb4c3f1e94e1414bce9bd1e
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-10-11 10:00:23 +0100

    feat: update stacked avatar to accept visibleQuantity

--- components/molecules/InsightPageCard/insight-page-card.tsx
+++ components/molecules/InsightPageCard/insight-page-card.tsx
@@ -3,6 +3,8 @@ import CardRepoList, { RepoList } from "../CardRepoList/card-repo-list";
 import Button from "components/atoms/Button/button";
 import PieChart, { PieData } from "../PieChart/pie-chart";
 import StackedAvatar from "../StackedAvatar/stacked-avatar";
+import { LinkIcon } from "@primer/octicons-react";
+import Link from "next/link";
 interface InsightPageCardProps {
   title: string;
   members: DbContribution[];
@@ -49,14 +51,22 @@ const InsightPageCard = ({
     }
   ];
 
-  const totalPrs = (openPrsCount || 0 )+ (closedPrsCount || 0 )+ (draftPrsCount || 0 )+ (mergedPrsCount || 0);
+  // Function to handle copy to clipboard
+  const handleCopyToClipboard = async (content: any) => {
+    try {
+      await navigator.clipboard.writeText(content);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const totalPrs = (openPrsCount || 0) + (closedPrsCount || 0) + (draftPrsCount || 0) + (mergedPrsCount || 0);
   return (
     <div className=" w-[428px] py-[15px] px-[14px] rounded-lg flex flex-col gap-y-3 bg-white border">
       {/* Card header */}
       <div className="flex items-center justify-between">
         <div>{title}</div>
         <div className="items-center flex">
-          {members.length > 0 ? <StackedAvatar contributors={members}/> : "-"}
+          {members.length > 0 ? <StackedAvatar visibleQuantity={2} contributors={members} /> : "-"}
           {members.length >= 2 ? <div>&nbsp;{`+${members.length - 2} members`}</div> : ""}
         </div>
       </div>
@@ -71,8 +81,15 @@ const InsightPageCard = ({
 
       {/* Card footer */}
       <div className="flex mt-4 justify-between">
-        <Button className="!w-48" type="outline">Copy Link</Button>
-        <Button className="!w-48" type="primary">Go to Insight Page</Button>
+        <Button onClick={() => handleCopyToClipboard("text to be copied")} className="!w-48" type="outline">
+          {" "}
+          <LinkIcon size={16} className="mr-2" /> Copy Link
+        </Button>
+        <Link href={"#"}>
+          <Button className="!w-48" type="primary">
+            Go to Insight Page
+          </Button>
+        </Link>
       </div>
     </div>
   );
