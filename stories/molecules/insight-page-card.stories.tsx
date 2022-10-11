fe6027c8f001a197f80996cb926e7e4f96ecc4a8
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-10-11 09:32:24 +0100

    docs: add insight page card story to design system

---
+++ stories/molecules/insight-page-card.stories.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable camelcase */
+import { ComponentStory } from "@storybook/react";
+import { RepoList } from "components/molecules/CardRepoList/card-repo-list";
+import InsightPageCard from "components/molecules/InsightPageCard/insight-page-card";
+import { PieData } from "components/molecules/PieChart/pie-chart";
+import TestRepoAvatar from "public/icons/test-repo-avatar.svg";
+
+
+const storyConfig = {
+  title: "Design System/Molecules/InsightPageCard"
+};
+export default storyConfig;
+
+const contributors: DbContribution[] = [
+  {
+    commits: "2",
+    commit_days: "ES",
+    last_commit_time: "1661496920000",
+    files_modified: "",
+    first_commit_time: "1661436635000",
+    host_login: "10kartik",
+    email: "kartik@plgworks.com",
+    name: "10kartik",
+    langs: "markdown,python",
+    recent_pr_total: 30,
+    recent_repo_list: ""
+  },
+  {
+    commits: "2",
+    commit_days: "ES",
+    last_commit_time: "1661496920000",
+    files_modified: "",
+    first_commit_time: "1661436635000",
+    host_login: "10kartik",
+    email: "kartik@plgworks.com",
+    name: "10kartik",
+    langs: "markdown,python",
+    recent_pr_total: 30,
+    recent_repo_list: ""
+  },
+  {
+    commits: "2",
+    commit_days: "ES",
+    last_commit_time: "1661496920000",
+    files_modified: "",
+    first_commit_time: "1661436635000",
+    host_login: "10kartik",
+    email: "kartik@plgworks.com",
+    name: "10kartik",
+    langs: "markdown,python",
+    recent_pr_total: 30,
+    recent_repo_list: ""
+  },
+  {
+    commits: "2",
+    commit_days: "ES",
+    last_commit_time: "1661496920000",
+    files_modified: "",
+    first_commit_time: "1661436635000",
+    host_login: "10kartik",
+    email: "kartik@plgworks.com",
+    name: "10kartik",
+    langs: "markdown,python",
+    recent_pr_total: 30,
+    recent_repo_list: ""
+  }
+
+];
+
+const repoList: RepoList[] = [
+
+  {
+    repoName: "test",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test2",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test3",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test4",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test5",
+    repoIcon: TestRepoAvatar
+  },
+  {
+    repoName: "test6",
+    repoIcon: TestRepoAvatar
+  }
+
+];
+
+const InsightPageCardTemplate: ComponentStory<typeof InsightPageCard> = (args) => <InsightPageCard{...args}/>;
+
+export const InsightpageCardStory = InsightPageCardTemplate.bind({});
+InsightpageCardStory.args = {
+  title: "Strapi",
+  members: contributors,
+  openPrsCount: 23,
+  closedPrsCount: 12,
+  mergedPrsCount: 13,
+  draftPrsCount: 8,
+  repoList: repoList
+};
