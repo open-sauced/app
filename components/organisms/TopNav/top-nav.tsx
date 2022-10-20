6db45e2a95d9464f4c882f594f7d8f663fdc5133
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-10-20 17:29:42 +0100

    feat: add link to topNav component

--- components/organisms/TopNav/top-nav.tsx
+++ components/organisms/TopNav/top-nav.tsx
@@ -1,12 +1,19 @@
+import useSession from "lib/hooks/useSession";
+import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
+import Link from "next/link";
 import React from "react";
 import AuthSection from "../../molecules/AuthSection/auth-section";
 import HeaderLogo from "../../molecules/HeaderLogo/header-logo";
 
 const TopNav: React.FC = () => {
-
+  const { user } = useSupabaseAuth();
+  const { onboarded } = useSession();
   return (
     <header className="top-nav-container flex justify-between items-center px-2 md:px-16 py-0.5 bg-light-slate-3 border-b">
-      <HeaderLogo withBg={false} textIsBlack />
+      <div className="flex gap-8 items-center">
+        <HeaderLogo withBg={false} textIsBlack />
+        {!!user && onboarded ? <Link className="text-sm text-light-slate-10" href="#">Insights Hub</Link> : ""}
+      </div>
       <AuthSection />
     </header>
   );
