a79796f7a65b62ba0159b82ec6271492fa22436d
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-10-21 14:02:07 +0100

    style: add hover effect

--- components/atoms/FavoriteSelector/favorite-selector.tsx
+++ components/atoms/FavoriteSelector/favorite-selector.tsx
@@ -9,7 +9,7 @@ interface FavoriteSelectorProps {
 const FavoriteSelector = ({ isFavorite = true }: FavoriteSelectorProps): JSX.Element => {
   return (
     <div
-      className="inline-flex rounded-md hover:bg-light-slate-2 cursor-pointer bg-transparent items-center gap-2 transition py-1 px-3
+      className="inline-flex rounded-md hover:bg-light-slate-2 cursor-pointer bg-transparent items-center gap-1 transition py-1 px-3
     "
     >
       {isFavorite ? (
