afb481e5c71614de37bc82d0ad1e2f4fc0aa0f1e
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-10-24 14:53:05 +0100

    Revert "feat: updated text input component with new states"
    This reverts commit 6b34a4e6839856de39c2deb88ed465cdc6fe6c1d.

--- components/atoms/TextInput/text-input.tsx
+++ components/atoms/TextInput/text-input.tsx
@@ -1,92 +1,15 @@
-import { CheckCircleFillIcon, XCircleFillIcon } from "@primer/octicons-react";
-import React, { useState } from "react";
-import clsx from "clsx";
-interface TextInputProps {
-  label?: string;
-  placeholder?: string;
-  name: string;
-  state?: "default" | "valid" | "invalid";
-  id?: string;
-  disabled?: boolean;
-  autoFocus?: boolean;
-  borderless?: boolean;
-  descriptionText: string;
-  classNames?: string;
-  errorMsg?: string;
-  value: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+import React from "react";
+import { Input as SupbaseInputComponent } from "@supabase/ui";
+
+interface TextInputProps extends React.ComponentProps<typeof SupbaseInputComponent> {
+  //Add additional prop definitions here
 }
 
-const TextInput = ({
-  name,
-  label,
-  placeholder,
-  state = "default",
-  id,
-  descriptionText,
-  classNames,
-  disabled = false,
-  autoFocus,
-  borderless = false,
-  value,
-  onChange,
-  errorMsg
-}: TextInputProps) => {
-  const [inputValue, setInputValue] = useState(value);
-  const handleResetInput = () => {
-    setInputValue("");
-  };
+const TextInput: React.FC<TextInputProps> = ( props ) => {
   return (
-    <>
-      <label className="flex w-full flex-col">
-        {label && <p className="mb-2 text-light-slate-9 text-sm">{label}</p>}
-        <div
-          className={clsx(
-            classNames,
-            "flex-1 px-3 text-light-slate-12 shadow-input border transition rounded-lg py-1 flex items-center",
-            borderless && "!border-none",
-            state === "invalid"
-              ? " focus-within:border-light-red-10 "
-              : "focus-within:border-light-orange-9 ",
-            disabled && "bg-light-slate-3"
-          )}
-        >
-          <input
-            type="text"
-            name={name}
-            id={id || name || ""}
-            placeholder={placeholder || ""}
-            onChange={onChange}
-            value={inputValue}
-            className="flex-1 focus:outline-none "
-            autoFocus={autoFocus}
-            disabled={disabled}
-          />
-          {state === "valid" ? (
-            <CheckCircleFillIcon className="text-light-orange-9" size={14} />
-          ) : state === "invalid" ? (
-            <span
-              className="flex items-center"
-              onClick={() => handleResetInput()}
-            >
-              <XCircleFillIcon className="text-light-red-11" size={14} />
-            </span>
-          ) : (
-            ""
-          )}
-        </div>
-      </label>
-      {descriptionText ? (
-        <p className="mt-3 text-light-slate-9 text-sm">{descriptionText}</p>
-      ) : (
-        ""
-      )}
-      {state === "invalid" && errorMsg ? (
-        <p className="mt-3 text-sm text-light-red-11 font-medium">{errorMsg}</p>
-      ) : (
-        ""
-      )}
-    </>
+    <SupbaseInputComponent {...props}>
+      {props.children}
+    </SupbaseInputComponent>
   );
 };
 
