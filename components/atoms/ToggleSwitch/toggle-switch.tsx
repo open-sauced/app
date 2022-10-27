bcea6f4906218655d5e8494eb7f6738557ed8c08
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-10-27 23:06:56 +0100

    refactor: replaced toggle switch component to radix

--- components/atoms/ToggleSwitch/toggle-switch.tsx
+++ components/atoms/ToggleSwitch/toggle-switch.tsx
@@ -1,30 +1,28 @@
-import React from "react";
+import * as Switch from "@radix-ui/react-switch";
 
 interface ToggleSwitchProps {
   name: string;
   checked: boolean;
-  className?: string;
-  handleToggle: ()=> void;
+  handleToggle: () => void;
 }
 
-const ToggleSwitch = ({ name, checked = false , className , handleToggle}: ToggleSwitchProps): JSX.Element => {
+const ToggleSwitch = ({ name, checked = false, handleToggle }: ToggleSwitchProps): JSX.Element => {
   return (
-    <div className={`${className ? className : ""} relative w-7 h-4 inline-block`}>
-      <input type="checkbox" className="hidden" defaultChecked={checked} name={name} id={name} />
-      <label className="h-full block cursor-pointer  overflow-hidden rounded-2xl" htmlFor={name}>
-        <span
-          className={`${
-            checked ? "bg-light-orange-10 " : "bg-light-slate-8 "
-          }  transition-all duration-300 block ease-in-out w-[200%] h-full `}
-        />
-        <span
-          onClick={() => handleToggle()}
-          className={`${
-            checked ? "right-0" : "left-0"
-          } w-3 h-3 absolute top-0 m-[2px] bg-white block rounded-full transition-all duration-300 ease-in-out`}
-        />
-      </label>
-    </div>
+    <Switch.Root
+      defaultChecked
+      checked={checked}
+      onCheckedChange={handleToggle}
+      id={name}
+      className={`${
+        checked && "!bg-light-orange-10"
+      } w-7 h-4 relative rounded-2xl overflow-hidden bg-light-slate-8 data-[state=checked]:!bg-light-orange-10`}
+    >
+      <Switch.Thumb
+        className={`${
+          checked && "!translate-x-[14px]"
+        } bg-white w-3 h-3 block rounded-2xl translate-x-[2px] data-[state=checked]:!translate-x-6 `}
+      />
+    </Switch.Root>
   );
 };
 
