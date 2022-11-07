5573ef84825b015a223f09a1833c8287ed979942
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-11-07 21:55:02 +0100

    refactor: update styles for repo card

--- components/molecules/RepoCardProfile/repo-card-profile.tsx
+++ components/molecules/RepoCardProfile/repo-card-profile.tsx
@@ -12,19 +12,19 @@ export interface RepoCardProfileProps {
   issueCount?: number;
   className?: string;
 }
-const RepoCardProfile = ({ avatar, orgName, repoName, prCount, issueCount }: RepoCardProfileProps) => {
+const RepoCardProfile = ({ avatar, orgName, repoName, prCount, issueCount, className }: RepoCardProfileProps) => {
   return (
-    <div className="flex gap-3 ">
-      <div>
-        <Avatar size={40} avatarURL={avatar} />
+    <div className={`${className ?? className} flex gap-3 `}>
+      <div className="h-max group-hover:border rounded-lg">
+        <Avatar initials="BD" size={40} avatarURL={avatar} />
       </div>
-      <div className="flex flex-col gap-1 h-10">
+      <div className="flex flex-col gap-1 h-[46px]">
         <div className="text-base">
-          <p className="text-light-slate-11 -mt-1">
-            {orgName} / <span className="text-light-slate-12">{repoName}</span>
+          <p className="text-light-slate-11 group-hover:text-dark-orange-12 -mt-1">
+            {orgName} / <span className="text-light-slate-12 group-hover:text-white">{repoName}</span>
           </p>
         </div>
-        <div className="flex text-light-slate-10 gap-3">
+        <div className="flex group-hover:text-dark-orange-12 text-light-slate-10 gap-3">
           <span className="flex text-xs gap-0.5 items-center">
             <VscIssues className="text-[15px]" /> {issueCount}
           </span>
--- components/molecules/RepositoryResult/repository-result.tsx
+++ components/molecules/RepositoryResult/repository-result.tsx
@@ -2,13 +2,15 @@ import Button from "components/atoms/Button/button";
 import React from "react";
 import RepoCardProfile from "../RepoCardProfile/repo-card-profile";
 
-const Repositorieresult = () => {
+const RepositoryResult = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex py-3 justify-between group hover:bg-light-orange-9 px-4">
       <RepoCardProfile orgName="open sauced" prCount={23} issueCount={102} avatar="" repoName="insights" />
-      <Button type="text">Add Repository</Button>
+      <Button className="!text-light-orange-10 group-hover:!text-white" type="text">
+        Add Repository
+      </Button>
     </div>
   );
 };
 
-export default Repositorieresult;
+export default RepositoryResult;
