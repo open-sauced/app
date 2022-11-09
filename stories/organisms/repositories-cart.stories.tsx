1dd177a1be0f45b09a5d6728e55a1aa4efd1baf3
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-11-09 14:12:50 +0100

    docs: add stories for repository cart and items

---
+++ stories/organisms/repositories-cart.stories.tsx
@@ -0,0 +1,30 @@
+import RepositoriesCart from "components/organisms/RepositoriesCart/repositories-cart";
+import { ComponentStory } from "@storybook/react";
+import RepositoryCartItem from "components/molecules/ReposoitoryCartItem/repository-cart-item";
+
+const storyConfig = {
+  title: "Design System/Organisms/RepositoriesCart"
+};
+
+export default storyConfig;
+
+const RepositoriesCartTemplate: ComponentStory<typeof RepositoriesCart> = (args) => (
+  <RepositoriesCart {...args}>
+    <RepositoryCartItem avatar="" totalPrs={120} orgName="open sauced" repoName="hot" />
+    <RepositoryCartItem avatar="" totalPrs={120} orgName="open sauced" repoName="insights" />
+    <RepositoryCartItem avatar="" totalPrs={120} orgName="open sauced" repoName="workflow" />
+  </RepositoriesCart>
+);
+
+export const EmptyState = RepositoriesCartTemplate.bind({});
+
+export const FilledState = RepositoriesCartTemplate.bind({});
+export const WithHistory = RepositoriesCartTemplate.bind({});
+
+FilledState.args = {
+  hasItems: true
+};
+WithHistory.args = {
+  hasHistory: true,
+  hasItems: true
+};
