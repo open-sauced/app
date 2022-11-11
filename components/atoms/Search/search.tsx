cbc9e4357245d65ed90659629b1d4e6cff03d154
Brandon <robertsbt@gmail.com>
2022-11-11 15:50:02 -0600

    feat: add individual repo search on repositories page (#580)
    Closes #322

--- components/atoms/Search/search.tsx
+++ components/atoms/Search/search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 interface SearchProps {
   name: string;
@@ -6,15 +6,22 @@ interface SearchProps {
   placeholder?: string;
   autoFocus?: boolean;
   className?: string;
+  onSearch?: (search?: string) => void;
 }
-const Search = ({ placeholder, name, value, autoFocus, className }: SearchProps): JSX.Element => {
+const Search = ({ placeholder, name, value, autoFocus, className, onSearch }: SearchProps): JSX.Element => {
+  const [search, setSearch] = useState(value);
+
+  const handleOnSearch = () => {
+    onSearch?.(search);
+  };
+
   return (
     <div
       className={`${
         className && className
       } flex bg-white  py-1 w-max px-3 shadow-input border transition focus-within:ring focus-within:border-orange-500 focus-within:ring-orange-100 rounded-lg   ring-light-slate-6 items-center`}
     >
-      <FaSearch className="text-light-slate-9" fontSize={16} />
+      <FaSearch className="text-light-slate-9" fontSize={16} onClick={handleOnSearch}/>
       <input
         className="pl-2 w-52   placeholder:text-md  placeholder:  text-md focus:outline-none placeholder:text-light-slate-9"
         autoFocus={autoFocus}
@@ -23,6 +30,12 @@ const Search = ({ placeholder, name, value, autoFocus, className }: SearchProps)
         value={value}
         type="search"
         id={name}
+        onChange={(e: any) => setSearch(e.target.value)}
+        onKeyUp={e => {
+          if (e.code === "Enter") {
+            handleOnSearch();
+          }
+        }}
       />
     </div>
   );
