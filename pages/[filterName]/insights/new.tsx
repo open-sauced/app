65e9c147c9664bd87c905a8f696b383865813bd8
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2022-11-11 14:52:11 -0800

    adds link to discussion

--- pages/[filterName]/insights/new.tsx
+++ pages/[filterName]/insights/new.tsx
@@ -3,7 +3,7 @@ import HubLayout from "layouts/hub";
 
 const NewInsightPage: WithPageLayout = () => {
   return (
-    <div>Coming soon. Reach out to <a href="mailto:hello@opensauced.pizza">bdougie</a> for a demo.</div>
+    <div>Coming soon. Reach out to <a href="https://github.com/open-sauced/feedback/discussions/5">bdougie</a> for a demo.</div>
   );
 };
 
