f2dab35820bfc91add679c5931bc72240ce27290
OGBONNA SUNDAY <62995161+SunGoldTech@users.noreply.github.com>
2022-11-11 16:47:02 +0100

    feat: updated design for insights hub page (#579)
    * refactor: add custom tooltip to repolist card
    
    * feat: create insights row component
    
    * chore: replaced old insight page with new design
    
    * chore: hide search box temporarily
    
    * style: add responsiveness to insight page
    
    * Update pages/[filterName]/insights/index.tsx
    
    Co-authored-by: Brandon <robertsbt@gmail.com>
    
    * Update components/molecules/InsightRow/insight-row.tsx
    
    Co-authored-by: Brandon <robertsbt@gmail.com>
    
    * Update components/molecules/InsightRow/insight-row.tsx
    
    Co-authored-by: Brandon <robertsbt@gmail.com>
    
    * fix: build error
    
    * fix: type error on user type
    
    * fix: revert url for insight page
    
    * refactor: moved Auth hook to insight row component
    
    * fix: eslint error
    
    * refactor: slight change in approach to user fetching
    
    Co-authored-by: Brandon <robertsbt@gmail.com>

--- components/molecules/CardRepoList/card-repo-list.tsx
+++ components/molecules/CardRepoList/card-repo-list.tsx
@@ -1,4 +1,5 @@
 import Icon from "components/atoms/Icon/icon";
+import Tooltip from "components/atoms/Tooltip/tooltip";
 import { StaticImageData } from "next/image";
 
 export interface RepoList {
@@ -14,31 +15,24 @@ interface CardRepoListProps {
 const CardRepoList = ({ repoList, limit = 5 }: CardRepoListProps): JSX.Element => {
   const sanitizedRepoList = [...new Map(repoList.map((item) => [item["repoName"], item])).values()];
   return (
-    <div className="flex gap-2 items-center flex-wrap text-xs text-light-slate-11">
-      {
-        repoList.length > 0 ?
-          <>
-            {
-              sanitizedRepoList
-                .filter((repo, arrCount) => arrCount < limit)
-                .map(({repoName, repoIcon}, index) =>
-                  <div key={index} title={repoName} className="flex gap-1  p-1 pr-2 border-[1px] border-light-slate-6 rounded-lg text-light-slate-12">
-                    <Icon IconImage={repoIcon} className="rounded-[4px] overflow-hidden" />
-                    <span className="max-w-[45px] md:max-w-[100px] truncate">{repoName}</span>
-                  </div>
-                )
-            }
-            <div>
-              {repoList.length > limit ? `+${repoList.length - limit}` : null}
-            </div>
-          </>
-
-          :
-
-          <>
-            No repositories currently...
-          </>
-      }
+    <div className="flex gap-2 items-center max-w[175px] truncate flex-wrap text-xs text-light-slate-11">
+      {repoList.length > 0 ? (
+        <>
+          {sanitizedRepoList
+            .filter((repo, arrCount) => arrCount < limit)
+            .map(({ repoName, repoIcon }, index) => (
+              <Tooltip key={index} content={repoName}>
+                <div className="flex gap-1  p-1 pr-2 border-[1px] border-light-slate-6 rounded-lg text-light-slate-12">
+                  <Icon IconImage={repoIcon} className="rounded-[4px] overflow-hidden" />
+                  <span className="max-w-[45px] md:max-w-[100px] truncate">{repoName}</span>
+                </div>
+              </Tooltip>
+            ))}
+          <div>{repoList.length > limit ? `+${repoList.length - limit}` : null}</div>
+        </>
+      ) : (
+        <>No repositories currently...</>
+      )}
     </div>
   );
 };
