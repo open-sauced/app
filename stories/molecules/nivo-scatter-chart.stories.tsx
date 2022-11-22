0ad557271ba1ff6e1e41d8015023a7ddc4236619
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-11-22 18:26:49 +0100

    fix: fix story data for chart

--- stories/molecules/nivo-scatter-chart.stories.tsx
+++ stories/molecules/nivo-scatter-chart.stories.tsx
@@ -10,13 +10,13 @@ const data = [
   {
     id: "test1",
     data: [
-      { x: 10.0, y: 8.04, image: "" },
-      { x: 8.07, y: 3.08, image: "" },
-      { x: 8.07, y: 3.08, image: "" },
-      { x: 6.05, y: 8.03, image: "" },
-      { x: 10.02, y: 5.01, image: "" },
-      { x: 12.07, y: 9.08, image: "" },
-      { x: 18.01, y: 12.04, image: "" }
+      { x: 10.0, y: 8.04, image: "", contributor: "" },
+      { x: 8.07, y: 3.08, image: "", contributor: "" },
+      { x: 8.07, y: 3.08, image: "", contributor: "" },
+      { x: 6.05, y: 8.03, image: "", contributor: "" },
+      { x: 10.02, y: 5.01, image: "", contributor: "" },
+      { x: 12.07, y: 9.08, image: "", contributor: "" },
+      { x: 18.01, y: 12.04, image: "", contributor: "" }
     ]
   }
 ];
