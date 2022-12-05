162754a39f6f55655bb295d30c0e48ad8912bf88
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-05 14:09:32 +0100

    feat: add skeleton loader wrapper component

---
+++ components/atoms/SkeletonLoader/skeleton-wrapper.tsx
@@ -0,0 +1,24 @@
+import Skeleton from "react-loading-skeleton";
+
+interface SkeletonWrapperProps {
+  classNames?: string;
+  count?: number;
+  height: number;
+  width?: number;
+  radius?: number;
+}
+
+const SkeletonWrapper = ({ classNames, count, width, height, radius }: SkeletonWrapperProps): JSX.Element => {
+  const skeletonArray = Array.apply(null, Array(count || 1));
+  return (
+    <>
+      {skeletonArray.map((skeleton, index) => (
+        <div className={classNames || ""} key={index}>
+          <Skeleton height={height} width={width} borderRadius={radius} count={1} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SkeletonWrapper;
