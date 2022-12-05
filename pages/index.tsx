a32d672e77a3219b874a7204af899689e8fe7bb3
Brandon Roberts <robertsbt@gmail.com>
2022-12-05 12:16:08 -0600

    feat: navigate to insights hub if logged in and onboarded

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,44 @@
 import { WithPageLayout } from "../interfaces/with-page-layout";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+
 import Loader from "components/templates/Loader/loader";
+import { supabase } from "lib/utils/supabase";
+import useSession from "lib/hooks/useSession";
 
 const Home: WithPageLayout = () => {
   const router = useRouter();
+  const { onboarded } = useSession();
 
   useEffect(() => {
-    const redirect = ()=>{
-      router.push("javascript");
-    };
-    const timer = setTimeout(()=> redirect(),2000);
-    return () => clearTimeout(timer);
+    function redirect(destination: string) {
+      let redir = setTimeout(() => {
+        router.push(destination).then(() => {
+          clearTimeout(redir);
+        });
+      }, 2000);
+    }
 
-  }, [router]);
+    async function getAccess() {
+      const currentUser = await supabase.auth.getSession();
+
+      if (!currentUser?.data?.session) {
+        redirect("/javascript");
+      }
+
+      if (onboarded === undefined) {
+        return;
+      }
+
+      if (currentUser?.data?.session && onboarded === true) {
+        redirect("/hub/insights");
+      } else {
+        redirect("/javascript");
+      }
+    }
+
+    getAccess();
+  }, [router, onboarded]);
 
   return (
     <Loader theme={"dark"}/>
