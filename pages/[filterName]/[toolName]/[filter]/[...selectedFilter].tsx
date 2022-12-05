4ac44792787b765121918d64208876af5d25740f
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-05 23:27:34 +0100

    refactor: cleanup

--- pages/[filterName]/[toolName]/[filter]/[...selectedFilter].tsx
+++ pages/[filterName]/[toolName]/[filter]/[...selectedFilter].tsx
@@ -1,17 +1,16 @@
+import { useRouter } from "next/router";
+
 import FilterLayout from "layouts/filter";
 import { WithPageLayout } from "interfaces/with-page-layout";
-import { useRouter } from "next/router";
 import Tool from "components/organisms/ToolsDisplay/tools-display";
 import changeCapitalization from "lib/utils/change-capitalization";
 
 const SelectedFilter: WithPageLayout = () => {
   const router = useRouter();
-  
+
   const { toolName } = router.query;
 
-  return (
-    <Tool tool={toolName ? changeCapitalization(toolName.toString(), true) : undefined} />
-  );
+  return <Tool tool={toolName ? changeCapitalization(toolName.toString(), true) : undefined} />;
 };
 
 SelectedFilter.PageLayout = FilterLayout;
