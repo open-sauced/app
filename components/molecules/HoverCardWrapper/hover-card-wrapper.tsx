11599ff663a1bd73b56b647624a4af3e7be1a69e
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-07 17:07:38 +0100

    feat: add hover card wrapper

---
+++ components/molecules/HoverCardWrapper/hover-card-wrapper.tsx
@@ -0,0 +1,55 @@
+import { useSingleContributor } from "lib/hooks/useSingleContributor";
+import { useTopicContributions } from "lib/hooks/useTopicContributions";
+import { useRouter } from "next/router";
+import ContributorHoverCard, { ContributorsProfileType } from "../ContributorHoverCard/contributor-hover-card";
+
+interface HoverCardWrapperProps {
+  username: string;
+}
+const HoverCardWrapper = ({ username }: HoverCardWrapperProps) => {
+  const router = useRouter();
+  const { filterName } = router.query;
+  const topic = filterName as string;
+  const {
+    data: contributor,
+    isLoading: contributorLoading,
+    isError: contributorError
+  } = useSingleContributor("brandonaut");
+  const { data, isLoading, isError } = useTopicContributions(10, []);
+
+  console.log(username);
+
+  const repoList = (contributor[0]?.recent_repo_list || "").split(",").map((repo) => {
+    const [repoOwner, repoName] = repo.split("/");
+
+    return {
+      repoName,
+      repoIcon: `https://www.github.com/${repoOwner ?? "github"}.png?size=460`
+    };
+  });
+
+  const profile: ContributorsProfileType = {
+    githubAvatar: `https://www.github.com/${contributor[0]?.host_login}.png?size=60`,
+    githubName: contributor[0]?.host_login,
+    totalPR: contributor[0]?.recent_pr_total
+  };
+  console.log(data);
+  return (
+    <>
+      {contributorLoading ? (
+        <>Loading....</>
+      ) : (
+        <ContributorHoverCard
+          dateOfFirstPr={contributor[0].first_commit_time}
+          totalPR={profile.totalPR}
+          githubAvatar={profile.githubAvatar}
+          githubName={profile.githubName}
+          repoList={repoList}
+          topic={topic}
+        />
+      )}
+    </>
+  );
+};
+
+export default HoverCardWrapper;
