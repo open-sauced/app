347801de41c969e7b92dcb7bd522d231e6258de6
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-08 18:52:11 +0100

    refactor: code cleanups

--- components/molecules/ContributorTable/contributor-table.tsx
+++ components/molecules/ContributorTable/contributor-table.tsx
@@ -6,6 +6,8 @@ import { GoDiff } from "react-icons/go";
 import { VscGitPullRequest, VscGitPullRequestClosed, VscGitMerge, VscGitPullRequestDraft } from "react-icons/vsc";
 import { useTopicContributorPRs } from "lib/hooks/useTopicContributorPRs";
 import { calcDistanceFromToday } from "lib/utils/date-utils";
+import LatestPrTableHeader from "../LatestPrTableHeader/latest-pr-table-header";
+import LatestPrTableRow from "../LatestPrTableRow/latest-pr-table-row";
 
 export interface PRs {
   prStatus: string;
@@ -27,31 +29,7 @@ const ContributorTable = ({ contributor, topic, repositories }: CardTableProps):
   return data.length > 0 ? (
     <>
       <div className="flex flex-col">
-        <div className="flex gap-2 items-center bg-light-slate-3 rounded-md px-2 py-1">
-          <div className="w-3/5">
-            <Text className=" ">Latest PRs</Text>
-          </div>
-          <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14 } }}>
-            <div className="flex justify-end w-[calc(10%-4px)]">
-              <FaRegDotCircle title="When Pull Request was created" />
-            </div>
-          </IconContext.Provider>
-          <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14 } }}>
-            <div className="flex justify-end w-[calc(10%-4px)]">
-              <FaRegCheckCircle title="When Pull Request was merged" />
-            </div>
-          </IconContext.Provider>
-          <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14, strokeWidth: 0.3 } }}>
-            <div className="flex justify-end w-[calc(10%-4px)]">
-              <GoDiff title="Files changed in Pull Request" />
-            </div>
-          </IconContext.Provider>
-          <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14, strokeWidth: 0.5 } }}>
-            <div className="flex justify-end w-[calc(10%-4px)]">
-              <BsFileDiff title="Lines changed in Pull Request" />
-            </div>
-          </IconContext.Provider>
-        </div>
+        <LatestPrTableHeader />
         <div className="flex flex-col gap-0.5">
           {data.map(
             (
@@ -65,43 +43,19 @@ const ContributorTable = ({ contributor, topic, repositories }: CardTableProps):
                 linesCount: noOfLinesChanged
               },
               index
-            ) => (
-              <div key={index} className="flex gap-2 items-center px-2 py-1">
-                <div className="flex cursor-default item-center gap-2 w-3/5">
-                  {prStatus === "open" ? (
-                    <IconContext.Provider value={{ color: "green", style: { width: 14, height: 14, marginTop: 2 } }}>
-                      <VscGitPullRequest title="Open Pull Request" />
-                    </IconContext.Provider>
-                  ) : prStatus === "closed" ? (
-                    <IconContext.Provider
-                      value={{ color: merged ? "purple" : "red", style: { width: 14, height: 14, marginTop: 2 } }}
-                    >
-                      {merged ? (
-                        <VscGitMerge title="Merged Pull Request" />
-                      ) : (
-                        <VscGitPullRequestClosed title="Closed Pull Request" />
-                      )}
-                    </IconContext.Provider>
-                  ) : (
-                    <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14, marginTop: 2 } }}>
-                      <VscGitPullRequestDraft title="Draft Pull Request" />
-                    </IconContext.Provider>
-                  )}
-                  <Text>{calcDistanceFromToday(new Date(parseInt(prIssuedTime, 10)))}</Text>
-                  <Text title={prName} className="!text-light-slate-12 !w-32 md:!w-72  !truncate ">
-                    {prName}
-                  </Text>
-                </div>
-                <div className="flex justify-end w-[calc(10%-4px)] text-sm text-light-slate-11">
-                  {calcDistanceFromToday(new Date(parseInt(prIssuedTime, 10)))}
-                </div>
-                <div className="flex justify-end w-[calc(10%-4px)] text-sm text-light-slate-11">
-                  {merged ? calcDistanceFromToday(new Date(parseInt(prMergedTime, 10))) : "-"}
-                </div>
-                <div className="flex justify-end w-[calc(10%-4px)] text-sm text-light-slate-11">{noOfFilesChanged}</div>
-                <div className="flex justify-end w-[calc(10%-4px)] text-sm text-light-slate-11">{noOfLinesChanged}</div>
-              </div>
-            )
+            ) => {
+              const latestPrs = {
+                prName,
+                prIssuedTime,
+                prMergedTime,
+                prStatus,
+                merged,
+                noOfFilesChanged,
+                noOfLinesChanged
+              };
+
+              return <LatestPrTableRow key={index} {...latestPrs} />;
+            }
           )}
         </div>
       </div>
