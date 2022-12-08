dc489ef5d2be074a3c5b644e349e79aa46d99630
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-08 21:16:05 +0100

    style: mobile responsive padding

--- components/molecules/ContributorProfileHeader/contributor-profile-header.tsx
+++ components/molecules/ContributorProfileHeader/contributor-profile-header.tsx
@@ -7,7 +7,7 @@ interface ContributorProfileHeaderProps {
 }
 const ContributorProfileHeader = ({ avatarUrl }: ContributorProfileHeaderProps) => {
   return (
-    <div className="w-full flex items-end px-14 bg-[#DFE3E6] h-[216px]">
+    <div className="w-full flex items-end px-6 md:px-10 lg:px-16 bg-[#DFE3E6] h-[216px]">
       <div className="translate-y-[70px]">
         <Avatar hasBorder avatarURL={avatarUrl} size={184} isCircle />
       </div>
