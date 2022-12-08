0daaa0725d05a8de15ec1633e4b3d63e18edefa6
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-08 18:46:28 +0100

    docs: add contributors page story

---
+++ stories/organisms/contributor-profile-page.stories.tsx
@@ -0,0 +1,54 @@
+import ContributorProfilePage from "components/organisms/ContributorProfilePage/contributor-profile-page";
+import { ComponentStory } from "@storybook/react";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+
+const storyConfig = {
+  title: "Design System/Organisms/Contributor Profile Page"
+};
+export default storyConfig;
+const listOfPRs = [
+  {
+    prName: "Merging some work",
+    prStatus: "merged",
+    prIssuedTime: "2mo",
+    prMergedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "closed",
+    prIssuedTime: "2mo",
+    prMergedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "open",
+    prIssuedTime: "2mo",
+    prMergedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  },
+  {
+    prName: "Merging some work",
+    prStatus: "draft",
+    prIssuedTime: "2mo",
+    prMergedTime: "2mo",
+    noOfFilesChanged: 13,
+    noOfLinesChanged: 837
+  }
+];
+
+const ContributorProfilePageTemplate: ComponentStory<typeof ContributorProfilePage> = (args) => (
+  <TooltipProvider>
+    <ContributorProfilePage {...args} />
+  </TooltipProvider>
+);
+
+export const Default = ContributorProfilePageTemplate.bind({});
+
+Default.args = {
+  listOfPRs: listOfPRs
+};
