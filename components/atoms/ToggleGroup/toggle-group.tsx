59e3fa3fb797c0bccba66646f9d15d3414284aec
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-12-20 21:36:35 +0200

    docs: removed draft comment

--- components/atoms/ToggleGroup/toggle-group.tsx
+++ components/atoms/ToggleGroup/toggle-group.tsx
@@ -2,27 +2,6 @@ import * as GroupToggle from "@radix-ui/react-toggle-group";
 import { useState } from "react";
 import ToggleGroupItem from "../ToggleGroupItem/toggle-group-item";
 
-/* Draft. Will be removed on completion of the PR
-    This component is supposed to allow as many number of options as demanded
-
-    * There was 2 ways to implement this:
-        - Either by forcing other developers to use the ToggleGroupItem components inside the ToggleGroup component
-            - i.e. by returning an error if the children are not ToggleGroupItem components
-            - this allows for less flexibility
-            - developers may forget that they need to use the ToggleGroupItem component, which is less intuitive
-            - didnt think about how I would implement the logic control for it. Probably much harder
-
-        - Or by allowing any type of component to be passed as children, but wrapping those components in a ToggleGroupItem component manually
-            - this allows for more flexibility, and more control over the children
-            - developers now wont need to remember that they need to use the ToggleGroupItem component and can use anything, which is more intuitive
-            - this is the implementation I went with
-
-    * This implementation depends on the input children being static, or dynamic but without re-ordering the children
-        - as it uses the index of the children as keys, due to the possible absense of other unique identifiers
-        - possibly, a key prop could be added to children, but I am not sure if you can enforce that prop to be present with TypeScript
-        - if enforcing a prop for input children is possible, then we could also enforce a value prop, instead of using their indices as values.
-*/
-
 interface ToggleGroupProps {
     children: React.ReactNode,
     /** Allows the user to deselect an option, resulting in none being selected. */
@@ -34,6 +13,7 @@ interface ToggleGroupProps {
 }
 
 /** A ToggleGroup component that allows the user to select one option from a list of options.
+ * Warning: Do NOT re-order the children after the component has been rendered.
  *
  * @example
  * <ToggleGroup>
