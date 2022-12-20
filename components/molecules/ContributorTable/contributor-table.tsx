a0e4fd6e4ac2497b634d23f1cd6d556105bc9ae2
Sunday Ogbonna <oliviamegan11@gmail.com>
2022-12-20 17:00:56 +0100

    refactor: replace prIssuedTime with prUpdatedTime

--- components/molecules/ContributorTable/contributor-table.tsx
+++ components/molecules/ContributorTable/contributor-table.tsx
@@ -43,7 +43,8 @@ const ContributorTable = ({ contributor, topic, repositories, limit, isHoverCard
                 linesCount: noOfLinesChanged,
                 number: prNumber,
                 repo_name: repoName,
-                repo_owner: repoOwner
+                repo_owner: repoOwner,
+                updated_at: prUpdatedTime
               },
               index
             ) => {
@@ -57,7 +58,8 @@ const ContributorTable = ({ contributor, topic, repositories, limit, isHoverCard
                 noOfLinesChanged,
                 repoName,
                 repoOwner,
-                prNumber
+                prNumber,
+                prUpdatedTime
               };
 
               return <LatestPrTableRow isHoverCard={isHoverCard} key={index} {...latestPrs} />;
