4ff228845bac2f73856c2206f9881e50eda77e3a
takanome_dev <takanomedev221@gmail.com>
2022-12-21 21:47:10 +0000

    feat: handle close modal when click outside and fix accessibility issues

--- components/atoms/Select/custom-select.tsx
+++ components/atoms/Select/custom-select.tsx
@@ -1,5 +1,9 @@
+import * as Select from "@radix-ui/react-select";
 import React, { useState } from "react";
 import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import { MdCheck } from "react-icons/md";
+
+// import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
 
 interface SelectProps {
   placeholder: string;
@@ -14,68 +18,108 @@ type SelectedState = {
   name: string;
   value: any;
 };
-const Select = ({
-  placeholder = "Select an option",
+
+
+const CustomSelect = ({
+  placeholder = "10 per page",
   options,
-  error, // don't know if this is necessary
+  error, // TODO: see if this is necessary, probably not
   label,
-  className,
+  className, // TODO: use className with clsx
   onChange
 }: SelectProps): JSX.Element => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selected, setSelected] = useState<SelectedState>({ name: "", value: "" });
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-  const handleSelected = (option: { name: string; value: any }) => {
-    const { name, value } = option;
-    setSelected((prev) => ({ ...prev, name, value }));
-    onChange?.(value);
-    setIsOpen(false);
-  };
+  // TODO: remove this console.log
+  console.log({options, label, className});
 
   return (
-    <div
-      className={`${
-        className
-          ? className
-          : "relative bg-white cursor-pointer  focus:border-light-orange-9 focus:ring focus:ring-light-orange-5 items-center overflow-x-hidden  rounded-lg   text-base text-light-slate-10"
-      }`}
-    >
-      <div
-        onClick={() => handleToggle()}
-        className="flex px-4 py-1.5 bg-white border border-light-slate-6 rounded-lg focus-within:border-light-orange-9 focus-within:ring focus-within:ring-light-orange-5 items-center"
+    <Select.Root>
+      {/* TODO: add an outline orange color on focus */}
+      <Select.Trigger
+        aria-label="Select a limit"
+        // TODO: what does these focus-within classes do? Hummm
+        className="inline-flex px-4 bg-white border border-light-slate-6 rounded-lg focus-within:border-light-orange-9 focus-within:ring focus-within:ring-light-orange-5 items-center"
       >
-        {label && <span className="text-sm inline-flex text-light-slate-9 mr-2">{label}:</span>}
-        <input
-          value={selected.name === "" ? placeholder : selected.name}
-          readOnly
-          type="text"
-          className="overflow-scroll text-sm cursor-pointer text-light-slate-12 focus:outline-none bg-transparent"
-        />
-        <div className="w-6 h-4 relative overflow-hidden">
-          {" "}
-          <RiArrowUpSLine className="absolute bottom-1" /> <RiArrowDownSLine className="absolute top-1  " />
-        </div>
-      </div>
-      {isOpen && (
-        <div className="relative w-full">
-          <div className="left-0 right-0 transition bg-white overflow-hidden z-50 rounded-lg border shadow-superlative absolute font-normal ">
-            {options
-              ? options.map((option, index) => (
-                <div
-                  className="  cursor-pointer text-sm text-light-slate-12 hover:text-light-orange-11 hover:bg-light-orange-3 py-2 md:py-1 px-4 transition"
-                  onClick={() => handleSelected(option)}
+        <Select.Value placeholder={placeholder} />
+        <Select.Icon>
+          {/* TODO: remove these borders */}
+          <RiArrowUpSLine className="border border-red-400" />
+          <RiArrowDownSLine className="border border-red-400" />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        {/* TODO: fix dropdown position */}
+        <Select.Content className="bg-white overflow-hidden rounded-lg border shadow-superlative font-normal top-12 absolute w-48">
+          <Select.Viewport className="p-2 text-center">
+            {options ?
+              options.map((option, index) => (
+                <Select.Item
+                // TODO: not safe to use index as key, change it
                   key={index}
+                  className="group flex items-center text-sm text-light-slate-12 relative pl-8 pr-5 py-1.5 rounded-md outline-none hover:bg-light-orange-9 hover:text-white"
+                  value={option.name}
+                  onClick={() => onChange?.(option.value)}
                 >
-                  {option.name}
-                </div>
+                  <Select.ItemText>{option.name}</Select.ItemText>
+                  <Select.ItemIndicator
+                    className="absolute left-0 inline-flex items-center justify-center w-4"
+                  >
+                    <MdCheck
+                      className="ml-2 text-light-orange-9 group-hover:text-white" size="sm"
+                    />
+                  </Select.ItemIndicator>
+                </Select.Item>
               ))
-              : "No options"}
-          </div>
-        </div>
-      )}
-    </div>
+              : "No options"
+            }
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
   );
 };
-export default Select;
+
+export default CustomSelect;
+
+// return (
+//   <div
+//     className={`${
+//       className
+//         ? className
+//         : "relative bg-white cursor-pointer  focus:border-light-orange-9 focus:ring focus:ring-light-orange-5 items-center overflow-x-hidden  rounded-lg   text-base text-light-slate-10"
+//     }`}
+//   >
+//     <div
+//       onClick={() => handleToggle()}
+//       className="flex px-4 py-1.5 bg-white border border-light-slate-6 rounded-lg focus-within:border-light-orange-9 focus-within:ring focus-within:ring-light-orange-5 items-center"
+//     >
+//       {label && <span className="text-sm inline-flex text-light-slate-9 mr-2">{label}:</span>}
+//       <input
+//         value={selected.name === "" ? placeholder : selected.name}
+//         readOnly
+//         type="text"
+//         className="overflow-scroll text-sm cursor-pointer text-light-slate-12 focus:outline-none bg-transparent"
+//       />
+//       <div className="w-6 h-4 relative overflow-hidden">
+//         {" "}
+//         <RiArrowUpSLine className="absolute bottom-1" /> <RiArrowDownSLine className="absolute top-1  " />
+//       </div>
+//     </div>
+// {isOpen && (
+//   <div className="relative w-full">
+//     <div className="left-0 right-0 transition bg-white overflow-hidden z-50 rounded-lg border shadow-superlative absolute font-normal ">
+//       {options
+//         ? options.map((option, index) => (
+//           <div
+//             className="  cursor-pointer text-sm text-light-slate-12 hover:text-light-orange-11 hover:bg-light-orange-3 py-2 md:py-1 px-4 transition"
+//             onClick={() => handleSelected(option)}
+//             key={index}
+//           >
+//             {option.name}
+//           </div>
+//         ))
+//         : "No options"}
+//     </div>
+//   </div>
+// );}
+//   </div>
+// );
--- stories/atoms/custom-select.stories.tsx
+++ stories/atoms/custom-select.stories.tsx
@@ -1,26 +1,27 @@
 import { ComponentStory } from "@storybook/react";
 import Select from "components/atoms/Select/custom-select";
-import SelectOption from "components/atoms/Select/select-option";
+
 const StoryConfig = {
-  title: "Design System/Atoms/Custom select"
+  title: "Design System/Atoms/Custom Select"
 };
+
 export default StoryConfig;
 
 const SelectTemplate: ComponentStory<typeof Select> = (args) => <Select {...args} />;
 
 export const NoLabel = SelectTemplate.bind({});
-NoLabel.args = {
-  options: [
-    { name: "select", value: "select" },
-    { name: "food", value: "food" }
-  ],
-  placeholder: "Select an option"
-};
+// NoLabel.args = {
+//   options: [
+//     { name: "select", value: "select" },
+//     { name: "food", value: "food" }
+//   ],
+//   placeholder: "Select an option"
+// };
 export const WithLabel = SelectTemplate.bind({});
-WithLabel.args = {
-  options: [
-    { name: "select", value: "select" },
-    { name: "food", value: "food" }
-  ],
-  label: "Showing"
-};
+// WithLabel.args = {
+//   options: [
+//     { name: "select", value: "select" },
+//     { name: "food", value: "food" }
+//   ],
+//   label: "Showing"
+// };
