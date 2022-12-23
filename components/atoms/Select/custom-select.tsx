c1594cbaa0f7ded270c4244311a0b0f7194eba05
takanome_dev <takanomedev221@gmail.com>
2022-12-23 18:04:58 +0000

    refactor: update select styles

--- components/atoms/Select/custom-select.tsx
+++ components/atoms/Select/custom-select.tsx
@@ -3,7 +3,6 @@ import * as Select from "@radix-ui/react-select";
 import clsx from "clsx";
 
 import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
-import { MdCheck } from "react-icons/md";
 
 interface SelectProps {
   placeholder: string;
@@ -23,40 +22,33 @@ const CustomSelect = ({
 }: SelectProps): JSX.Element => {
 
   return (
-    <Select.Root>
-      <div className={clsx("relative bg-white cursor-pointer focus:border-light-orange-9 focus:ring focus:ring-light-orange-9 items-center overflow-x-hidden rounded-lg text-base text-light-slate-10 min-w-min", className || "")}>
+    <div className={clsx("custom-select__container relative bg-white cursor-pointer focus:border-light-orange-9 focus:ring focus:ring-light-orange-5 items-center overflow-x-hidden rounded-lg text-base text-light-slate-10", className || "")}>
+      <Select.Root>
         <Select.Trigger
           aria-label="Select a limit for the number of repositories to display"
-          className="inline-flex items-center text-sm text-light-slate-12 px-2 py-1.5 gap-2 w-full bg-white rounded-lg border border-light-slate-6 focus:outline-none focus-within:border-light-orange-8 focus-within:ring focus-within:ring-light-orange-8"
+          className="custom-select__trigger inline-flex items-center text-sm text-light-slate-12 px-2 py-1.5 gap-2 w-full bg-white rounded-lg font-semibold outline-none border border-light-slate-6 focus-within:border-light-orange-9 focus-within:ring focus-within:ring-light-orange-5"
         >
           {label && <span className="inline-flex text-light-slate-9">{label}:</span>}
           <Select.Value placeholder={placeholder} />
-          <Select.Icon>
-            <RiArrowUpSLine className="w-3 h-3" />
-            <RiArrowDownSLine className="w-3 h-3" />
+          <Select.Icon className="w-6 h-4 relative overflow-hidden">
+            <RiArrowUpSLine className="absolute bottom-1" />
+            <RiArrowDownSLine className="absolute top-1" />
           </Select.Icon>
         </Select.Trigger>
         <Select.Portal>
           <Select.Content
-            className={clsx("absolute right-0 top-12 bg-white overflow-hidden rounded-lg border shadow-superlative font-normal z-50", label ? "-left-10" : "left-0")}
+            className={clsx("absolute right-0 top-9 bg-white overflow-hidden rounded-lg border shadow-superlative", label ? "-left-18" : "left-0")}
           >
-            <Select.Viewport className="p-2">
+            <Select.Viewport>
               {options ?
                 options.map((option) => (
                   <Select.Item
                     key={option.name}
-                    className="custom-select__select-item flex items-center text-light-slate-12 relative pl-8 pr-5 py-1.5 rounded-md outline-none"
+                    className="custom-select__select-item flex items-center text-sm text-light-slate-12 cursor-pointer relative px-2 py-2 md:py-1 rounded-md outline-none"
                     value={option.name}
                     onClick={() => onChange?.(option.value)}
                   >
                     <Select.ItemText>{option.name}</Select.ItemText>
-                    <Select.ItemIndicator
-                      className="absolute left-0 inline-flex items-center justify-center w-6"
-                    >
-                      <MdCheck
-                        className="ml-2 text-light-orange-9"
-                      />
-                    </Select.ItemIndicator>
                   </Select.Item>
                 ))
                 : "No options"
@@ -64,8 +56,8 @@ const CustomSelect = ({
             </Select.Viewport>
           </Select.Content>
         </Select.Portal>
-      </div>
-    </Select.Root>
+      </Select.Root>
+    </div>
   );
 };
 
