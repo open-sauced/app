1da5a6724dc2216dd6fe59f1480b0d79d2514ac0
payn33 <amugenp2@gmail.com>
2022-12-24 19:01:24 +0100

    feat: add public/private insights badge

--- components/molecules/InsightHeader/insight-header.tsx
+++ components/molecules/InsightHeader/insight-header.tsx
@@ -1,5 +1,6 @@
 
 import Title from "components/atoms/Typography/title";
+import Text from "components/atoms/Typography/text";
 import ContextThumbnail from "components/atoms/ContextThumbnail/context-thumbnail";
 
 import getRepoInsights from "lib/utils/get-repo-insights";
@@ -22,9 +23,15 @@ const InsightHeader = ({ insight, repositories }: InsightHeaderProps): JSX.Eleme
         <ContextThumbnail size={120} ContextThumbnailURL={""}></ContextThumbnail>
       </div>
       <div className="header-info flex flex-col grow justify-center p-2">
-        <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900">
-          {insight && insight.name || "Insights"}
-        </Title>
+        <div className="flex items-baseline">
+          <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900 mr-[1rem]">
+            {insight && insight.name || "Insights"}
+          </Title>
+          {insight &&   
+          <Text className="flex text-xl self-end !text-light-slate-11 border border-light-slate-6 rounded-[26px] bg-light-slate-3 px-2.5 py-1 h-fit leading-3.5">
+            { !!insight.is_public ? "Public" : "Private" }
+          </Text>}
+        </div>
         <div className="flex mt-4 items-center relative  gap-2">
           {insight && insight.repos && insight.repos.length > 0 && <CardRepoList limit={2} repoList={repoList} />}
         </div>
