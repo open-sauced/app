1ce72a215c86b5dcbffe9a0602b231b0eba5f3b0
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-03 22:39:45 +0100

    update: update stacked avatar and table repo comp

--- components/molecules/StackedAvatar/stacked-avatar.tsx
+++ components/molecules/StackedAvatar/stacked-avatar.tsx
@@ -5,16 +5,27 @@ export declare interface StackedAvatarProps {
   contributors: DbContribution[];
   visibleQuantity?: number;
 }
-const StackedAvatar = ({ contributors , visibleQuantity = 5}: StackedAvatarProps) => (
+const StackedAvatar = ({ contributors, visibleQuantity = 5 }: StackedAvatarProps) => (
   <div className="-space-x-3 flex">
-    {contributors && contributors.slice(0, visibleQuantity).map(({ host_login: hostLogin, name }, index) => (
-      <div
-        key={`contributor-avatar-${hostLogin}`}
-        className={`w-8 h-8 overflow-hidden rounded-full border-2 border-solid border-white z-${50-(index+1)*10}`}
-      >
-        <Avatar key={`${hostLogin + index}`} avatarURL={getAvatarLink(hostLogin)} alt={name} size={32} hasBorder isCircle />
-      </div>
-    ))}
+    {contributors &&
+      contributors.slice(0, visibleQuantity).map(({ host_login: hostLogin, name }, index) => (
+        <div
+          key={`contributor-avatar-${hostLogin}`}
+          className={`w-8 h-8 overflow-hidden rounded-full border-2 border-solid border-white z-${
+            50 - (index + 1) * 10
+          }`}
+        >
+          <Avatar
+            isCached={true}
+            key={`${hostLogin + index}`}
+            avatarURL={getAvatarLink(hostLogin)}
+            alt={name}
+            size={32}
+            hasBorder
+            isCircle
+          />
+        </div>
+      ))}
   </div>
 );
 
