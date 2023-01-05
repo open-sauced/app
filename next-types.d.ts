dac42bab1065f184107482ed8e69c937bc4bf088
OGBONNA SUNDAY <62995161+OgDev-01@users.noreply.github.com>
2023-01-05 21:47:51 +0100

    feat: update icon PR  details row (#706)
    Fixes #696

--- next-types.d.ts
+++ next-types.d.ts
@@ -1,9 +1,9 @@
 // User defined type definitions. Please add type definitions for global types here
 
 interface DbRepo {
-  readonly id: string,
-  readonly size: number,
-  readonly stars: number,
+  readonly id: string;
+  readonly size: number;
+  readonly stars: number;
   readonly name: string;
   readonly owner: string;
   readonly prActiveCount: number;
@@ -41,27 +41,31 @@ interface DbRepoCommit {
 }
 
 interface Meta {
-  readonly itemCount: number,
-  readonly limit: number,
-  readonly page: number,
-  readonly pageCount: number,
-  readonly hasNextPage: boolean,
-  readonly hasPreviousPage: boolean
+  readonly itemCount: number;
+  readonly limit: number;
+  readonly page: number;
+  readonly pageCount: number;
+  readonly hasNextPage: boolean;
+  readonly hasPreviousPage: boolean;
 }
 
 interface DbContribution {
   readonly id: number;
-  readonly commits: string,
-  readonly commit_days: string,
-  readonly files_modified: string,
-  readonly first_commit_time: string,
-  readonly last_commit_time: string,
-  readonly email: string,
-  readonly name: string,
-  readonly host_login: string,
-  readonly langs: string,
+  readonly commits: string;
+  readonly commit_days: string;
+  readonly files_modified: string;
+  readonly first_commit_time: string;
+  readonly last_commit_time: string;
+  readonly email: string;
+  readonly name: string;
+  readonly host_login: string;
+  readonly langs: string;
   readonly recent_repo_list: string;
   readonly recent_pr_total: number;
+  readonly recent_contribution_count: number;
+  readonly recent_opened_prs: number;
+  readonly recent_pr_reviews: number;
+  readonly recent_pr_velocity: number;
 }
 
 interface DbInsight {
