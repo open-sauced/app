aace1c759edf49c66465dd949b64128d5c714c24
Brandon <robertsbt@gmail.com>
2023-01-09 14:09:13 -0600

    fix: fix placement of badge on insight page (#770)

--- components/molecules/InsightHeader/insight-header.tsx
+++ components/molecules/InsightHeader/insight-header.tsx
@@ -26,10 +26,12 @@ const InsightHeader = ({ insight, repositories, insightId, isOwner }: InsightHea
           <ContextThumbnail size={120} ContextThumbnailURL={""}></ContextThumbnail>
         </div>
         <div className="header-info flex flex-col grow justify-center p-2">
-          <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900">
-            {insight && insight.name || "Insights"}
-          </Title>
-          {insight && <Badge isPublic={insight?.is_public} />}
+          <div className="flex gap-2">
+            <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900">
+              {insight && insight.name || "Insights"}
+            </Title>
+            {insight && <Badge isPublic={insight?.is_public} />}
+          </div>
           <div className="flex mt-4 items-center relative  gap-2">
             {insight && insight.repos && insight.repos.length > 0 && <CardRepoList limit={2} repoList={repoList} />}
           </div>
