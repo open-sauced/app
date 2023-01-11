2b5560a1eb86cc3272a51698de9800b9c2924850
Brandon Roberts <robertsbt@gmail.com>
2023-01-11 10:20:05 -0600

    feat: add support for repo full name for onboarding/insight repos

--- components/organisms/InsightPage/InsightPage.tsx
+++ components/organisms/InsightPage/InsightPage.tsx
@@ -90,7 +90,7 @@ const InsightPage = ({ edit, insight, pageRepos }: InsightPageProps) => {
       },
       body: JSON.stringify({
         name,
-        ids: repos.map((repo) => repo.id),
+        repos: repos.map((repo) => ({id: repo.id, fullName: `${repo.owner}/${repo.name}`})),
         // eslint-disable-next-line
         is_public: isPublic
       })
@@ -120,7 +120,7 @@ const InsightPage = ({ edit, insight, pageRepos }: InsightPageProps) => {
       },
       body: JSON.stringify({
         name,
-        ids: repos.map((repo) => repo.id),
+        repos: repos.map((repo) => ({id: repo.id, fullName: `${repo.owner}/${repo.name}`})),
         // eslint-disable-next-line
         is_public: isPublic
       })
