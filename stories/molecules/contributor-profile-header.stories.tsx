c869ca4538556dc5fa7f93222778e915f6cfcb90
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-12 20:26:59 +0100

    docs: include additional props to storybook copmponent

--- stories/molecules/contributor-profile-header.stories.tsx
+++ stories/molecules/contributor-profile-header.stories.tsx
@@ -14,5 +14,6 @@ const ContributorProfileHeaderTemplate: ComponentStory<typeof ContributorProfile
 export const ContributorProfileHeaderStory = ContributorProfileHeaderTemplate.bind({});
 ContributorProfileHeaderStory.args = {
   avatarUrl:
-    "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+    "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+  isOpenSaucedUser: true
 };
