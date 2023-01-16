adc69af84865266fdc241bc2b0ff3be380390684
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-16 21:24:29 +0100

    refactor: replace supabase to radix

--- components/atoms/Checkbox/checkbox.tsx
+++ components/atoms/Checkbox/checkbox.tsx
@@ -1,13 +1,35 @@
 import React, { HTMLAttributes } from "react";
-import { Checkbox as SupbaseCheckboxComponent } from "@supabase/ui";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@primer/octicons-react";
 
-interface CheckboxProps extends React.ComponentProps<typeof SupbaseCheckboxComponent> {
+interface CheckboxProps {
   //Add additional prop definitions here
-  title: string
+  title: string;
+  name: string;
+  label: string;
+  value?: string | number | readonly string[];
+  defaultChecked?: boolean;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ( props ) => {
-  return <SupbaseCheckboxComponent {...props} />;
+const CustomCheckbox: React.FC<CheckboxProps> = ({ title, name, label, value, defaultChecked }) => {
+  return (
+    <div className="flex gap-2 items-center">
+      <Checkbox.Root
+        id={name}
+        title={title}
+        defaultChecked={defaultChecked}
+        value={value}
+        className="w-5 h-5 bg-light-slate-4 rounded-sm border border-light-slate-8"
+      >
+        <Checkbox.Indicator className="flex justify-center items-center">
+          <CheckIcon className="text-red-400" size={16} />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <label className="cursor-pointer text-sm text-light-slate-11" htmlFor={name}>
+        {label}
+      </label>
+    </div>
+  );
 };
 
-export default Checkbox;
+export default CustomCheckbox;
