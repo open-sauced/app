b0b1b81bb40ebb17d0c5e59112bcbdb6cd450ba2
OGBONNA SUNDAY <62995161+OgDev-01@users.noreply.github.com>
2023-01-19 17:20:35 +0100

    Update components/atoms/LanguagePill/LanguagePill.tsx
    Co-authored-by: Brandon <robertsbt@gmail.com>

--- components/atoms/LanguagePill/LanguagePill.tsx
+++ components/atoms/LanguagePill/LanguagePill.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 import JavascriptIcon from "img/icons/☕️.svg";
 import ReactIcon from "/img/icons/⚛️.svg";
