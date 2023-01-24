2d22de9ed7a5cc3bd2d25f58d642fcf99e132265
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-24 16:57:31 +0100

    chore: finishing touches and cleanup

--- components/molecules/PullRequestSocialCard/pull-request-social-card.tsx
+++ components/molecules/PullRequestSocialCard/pull-request-social-card.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import CardHorizontalBarChart, { LanguageObject } from "../CardHorizontalBarChart/card-horizontal-bar-chart";
-import Text from "components/atoms/Typography/text";
-import { getAvatarLink } from "lib/utils/github";
-import Avatar from "components/atoms/Avatar/avatar";
 import { IconContext } from "react-icons";
 import { VscGitPullRequest } from "react-icons/vsc";
-import { GoComment } from "react-icons/go";
 import { FaRegCommentAlt } from "react-icons/fa";
+
+import CardHorizontalBarChart, { LanguageObject } from "../CardHorizontalBarChart/card-horizontal-bar-chart";
+import Text from "components/atoms/Typography/text";
+import Avatar from "components/atoms/Avatar/avatar";
 
 interface PullRequestSocialCardProps {
   orgName: string;
@@ -33,7 +31,7 @@ const PullRequestSocialCard = ({
   return (
     <div className="w-[26.375rem] cursor-pointer border rounded-xl px-5 py-4">
       <div className="flex gap-2 items-center ">
-        <Avatar alt="" avatarURL={avatar} initials="" size="sm"  />
+        <Avatar alt="" avatarURL={avatar} initials="" size="sm" />
         <Text className="!text-sm !text-light-slate-11">
           {orgName}/<span className="text-light-slate-11">{repoName}</span>
         </Text>
@@ -54,16 +52,13 @@ const PullRequestSocialCard = ({
 
       <div className="flex items-center gap-5 text-light-slate-10 mt-3">
         <div className="flex justify-between gap-2">
-          {linesAdded && (
-            <span title="lines added" className="text-green-600">
-              +{linesAdded}
-            </span>
-          )}
-          {linesRemoved && (
-            <span title="lines removed" className="text-red-600">
-              -{linesRemoved}
-            </span>
-          )}
+          <span title="lines added" className="text-green-600">
+            +{linesAdded || 0}
+          </span>
+
+          <span title="lines removed" className="text-red-600">
+            -{linesRemoved || 0}
+          </span>
         </div>
         <div className="flex gap-0.5">
           <div className="w-3 h-3 bg-green-600 rounded"></div>
