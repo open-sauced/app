afcd6999d16c668acfaba683732de3750e3cd897
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-24 06:30:51 +0100

    fix: insights header overlap

--- components/atoms/InsightBadge/insight-badge.tsx
+++ components/atoms/InsightBadge/insight-badge.tsx
@@ -1,15 +1,13 @@
-import Text from "components/atoms/Typography/text";;
+import Text from "components/atoms/Typography/text";
 
 interface BadgeProps {
-    isPublic: boolean
+  isPublic: boolean;
 }
 
 const Badge = ({ isPublic }: BadgeProps): JSX.Element => {
   return (
-    <div className="inline-flex border border-light-slate-6 rounded-3xl bg-light-slate-3 px-2.5 py-1">
-      <span className="text-light-slate-11 leading-normal">
-        {!!isPublic ? "Public" : "Private"}
-      </span>
+    <div className="inline-flex border h-8 border-light-slate-6 rounded-3xl bg-light-slate-3 px-2.5 py-1">
+      <span className="text-light-slate-11 leading-normal">{!!isPublic ? "Public" : "Private"}</span>
     </div>
   );
 };
