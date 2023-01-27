7ed8cade332590fa10ef5860f37df421dd09951b
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-27 19:47:40 +0100

    feat: add profile update api function

---
+++ lib/hooks/update-user.ts
@@ -0,0 +1,26 @@
+interface useUpdateUserProps {
+  token: string;
+  data: { email?: string; interests?: string[] };
+  params?: string
+}
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const UpdateUser = async ({ token, data, params }: useUpdateUserProps) => {
+  try {
+    const res = await fetch(`${baseUrl}/auth/profile/${params || ""}`, {
+      headers: { Accept: "application/json", "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+      method: "PATCH",
+      body: JSON.stringify({ ...data })
+    });
+
+    if (res.status === 200) {
+      return res;
+    } else {
+      return res;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export { UpdateUser };
