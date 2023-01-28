446ffb5d1aa73240ef7e5657b790f07777941e67
takanome_dev <takanomedev221@gmail.com>
2023-01-28 18:44:06 +0000

    refactor: wrap pr title in a link

--- components/molecules/LatestPrTableRow/latest-pr-table-row.tsx
+++ components/molecules/LatestPrTableRow/latest-pr-table-row.tsx
@@ -37,47 +37,47 @@ const LatestPrTableRow = ({
   prUpdatedTime
 }: LatestPrTableRowProps) => {
   return (
-    <a href={`https://github.com/${repoOwner}/${repoName}/pull/${prNumber}`} className="!cursor-pointer" target="_blank" rel="noreferrer">
-      <div className="flex gap-2 items-center px-2 py-1">
-        <div className={`flex cursor-pointer item-center gap-2 w-3/5 ${isHoverCard && "w-5/6"}`}>
-          {prStatus === "open" ? (
-            <IconContext.Provider value={{ color: "green", style: { width: 14, height: 14, marginTop: 2 } }}>
-              <VscGitPullRequest title="Open Pull Request" />
-            </IconContext.Provider>
-          ) : prStatus === "closed" ? (
-            <IconContext.Provider
-              value={{ color: merged ? "purple" : "red", style: { width: 14, height: 14, marginTop: 2 } }}
-            >
-              {merged ? (
-                <VscGitMerge title="Merged Pull Request" />
-              ) : (
-                <VscGitPullRequestClosed title="Closed Pull Request" />
-              )}
-            </IconContext.Provider>
-          ) : (
-            <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14, marginTop: 2 } }}>
-              <VscGitPullRequestDraft title="Draft Pull Request" />
-            </IconContext.Provider>
-          )}
-          <Text title="updated date">{calcDistanceFromToday(new Date(parseInt(prUpdatedTime, 10)))}</Text>
-          <Text title={prName} className="!text-light-slate-12 !w-32 md:!w-72  !truncate ">
+    <div className="flex gap-2 items-center px-2 py-1">
+      <div className={`flex item-center gap-2 w-3/5 ${isHoverCard && "w-5/6"}`}>
+        {prStatus === "open" ? (
+          <IconContext.Provider value={{ color: "green", style: { width: 14, height: 14, marginTop: 2 } }}>
+            <VscGitPullRequest title="Open Pull Request" />
+          </IconContext.Provider>
+        ) : prStatus === "closed" ? (
+          <IconContext.Provider
+            value={{ color: merged ? "purple" : "red", style: { width: 14, height: 14, marginTop: 2 } }}
+          >
+            {merged ? (
+              <VscGitMerge title="Merged Pull Request" />
+            ) : (
+              <VscGitPullRequestClosed title="Closed Pull Request" />
+            )}
+          </IconContext.Provider>
+        ) : (
+          <IconContext.Provider value={{ color: "gray", style: { width: 14, height: 14, marginTop: 2 } }}>
+            <VscGitPullRequestDraft title="Draft Pull Request" />
+          </IconContext.Provider>
+        )}
+        <Text title="updated date">{calcDistanceFromToday(new Date(parseInt(prUpdatedTime, 10)))}</Text>
+        <Text title={prName} className="!text-light-slate-12 !w-32 md:!w-72 !truncate">
+          <a href={`https://github.com/${repoOwner}/${repoName}/pull/${prNumber}`} target="_blank" rel="noreferrer">
             {prName}
-          </Text>
-        </div>
-        <div className={`${isHoverCard && "ml-auto"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
-          {calcDistanceFromToday(new Date(parseInt(prIssuedTime, 10)))}
-        </div>
-        <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
-          {merged ? calcDistanceFromToday(new Date(parseInt(prMergedTime, 10))) : "-"}
-        </div>
-        <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
-          {noOfFilesChanged >= 1000 ? humanizeNumber(noOfFilesChanged, "abbreviation") : noOfFilesChanged}
-        </div>
-        <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
-          {noOfLinesChanged >= 1000 ? humanizeNumber(noOfLinesChanged, "abbreviation") : noOfLinesChanged}
-        </div>
+          </a>
+        </Text>
       </div>
-    </a>
+      <div className={`${isHoverCard && "ml-auto"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {calcDistanceFromToday(new Date(parseInt(prIssuedTime, 10)))}
+      </div>
+      <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {merged ? calcDistanceFromToday(new Date(parseInt(prMergedTime, 10))) : "-"}
+      </div>
+      <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {noOfFilesChanged >= 1000 ? humanizeNumber(noOfFilesChanged, "abbreviation") : noOfFilesChanged}
+      </div>
+      <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {noOfLinesChanged >= 1000 ? humanizeNumber(noOfLinesChanged, "abbreviation") : noOfLinesChanged}
+      </div>
+    </div>
   );
 };
 
