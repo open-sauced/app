e3a1fcfb5ab4e9d84a243a1034dcce35b917ab45
OGBONNA SUNDAY <62995161+OgDev-01@users.noreply.github.com>
2023-02-01 17:21:03 +0100

    feat: add profile badge to connected user profile (#824)
    Fixes #821
    Fixes #820

--- components/atoms/InsightBadge/insight-badge.tsx
+++ components/atoms/InsightBadge/insight-badge.tsx
@@ -1,10 +1,8 @@
-import Text from "components/atoms/Typography/text";
-
 interface BadgeProps {
   isPublic: boolean;
 }
 
-const Badge = ({ isPublic }: BadgeProps): JSX.Element => {
+const InsightsBadge = ({ isPublic }: BadgeProps): JSX.Element => {
   return (
     <div className="inline-flex border h-8 border-light-slate-6 rounded-3xl bg-light-slate-3 px-2.5 py-1">
       <span className="text-light-slate-11 leading-normal">{!!isPublic ? "Public" : "Private"}</span>
@@ -12,4 +10,4 @@ const Badge = ({ isPublic }: BadgeProps): JSX.Element => {
   );
 };
 
-export default Badge;
+export default InsightsBadge;
