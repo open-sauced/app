0c24cf1b193ea6dcee8b081d3c890e848e3cd037
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-03 17:37:16 +0200

    fix: remove old comments

--- tests/e2e-tests/UnsignedUsersRedirects.spec.ts
+++ tests/e2e-tests/UnsignedUsersRedirects.spec.ts
@@ -2,14 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('Homepage redirects to /javascript for unsigned users', async ({ page }) => {
   await page.goto('/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveURL(/.*\/javascript.*/)
 });
 
 test('Insights Hub redirects to /javascript for unsigned users', async ({ page }) => {
   await page.goto('/hub/insights');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveURL(/.*\/javascript.*/)
 });
