10e398d22288a5c9531233b881fbac40bc7708c9
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-03 18:16:28 +0200

    feat: extend timeout for redirects

--- e2e-tests/UnsignedUsersRedirects.spec.ts
+++ e2e-tests/UnsignedUsersRedirects.spec.ts
@@ -2,11 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('Homepage redirects to /javascript for unsigned users', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveURL(/.*\/javascript.*/)
+  await expect(page).toHaveURL(/.*\/javascript.*/, { timeout: 8000 })
 });
 
 test('Insights Hub redirects to /javascript for unsigned users', async ({ page }) => {
   await page.goto('/hub/insights');
   await expect(page).toHaveURL('/')
-  await expect(page).toHaveURL(/.*\/javascript.*/)
+  await expect(page).toHaveURL(/.*\/javascript.*/, { timeout: 8000 })
 });
