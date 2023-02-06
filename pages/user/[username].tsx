e0ca7dbd6472b60cdf5e77b9be402d1cfa79bc8b
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-06 16:58:16 +0200

    feat: meta tags setup for user pages

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -7,6 +7,7 @@ import { ContributorsProfileType } from "components/molecules/ContributorHoverCa
 
 import ProfileLayout from "layouts/profile";
 import { useFetchUser } from "lib/hooks/useFetchUser";
+import Head from "next/head";
 
 const Contributor = (): JSX.Element => {
   const router = useRouter();
@@ -26,23 +27,44 @@ const Contributor = (): JSX.Element => {
   };
 
   return (
-    <div className="w-full">
-      <ContributorProfilePage
-        prMerged={contributor[0]?.recent_merged_prs}
-        error={isError}
-        loading={userLoading}
-        user={user}
-        repoList={repoList}
-        langList={contributorLanguageList}
-        githubName={profile.githubName}
-        githubAvatar={profile.githubAvatar}
-        prTotal={contributor[0]?.recent_pr_total}
-        openPrs={contributor[0]?.recent_opened_prs}
-        recentContributionCount={contributor[0]?.recent_contribution_count}
-        prReviews={contributor[0]?.recent_pr_reviews}
-        prVelocity={contributor[0]?.recent_pr_velocity}
-      />
-    </div>
+    <>
+      <Head>
+        <title key="title">{contributorLogin} | Open Source Contributors</title>
+        <meta name="description" content={user?.bio || "I am an open source developer with a passion for music and video games. I strive to improve the open source community and am always looking for new ways to contribute."} key="description" />
+        <meta name="keywords" content="open source, github, contributor, profile, portfolio, developer, software engineer, software developer, software engineer portfolio, software developer portfolio, open source portfolio, open source developer, developer portfolio" key="keywords" />
+
+        <meta property="og:title" content={`${contributorLogin} | Open Source Contributors`} key="og:title" />
+        <meta property="og:description" content={"Profile Bio: " + user?.bio || "I am an open source developer with a passion for music and video games. I strive to improve the open source community and am always looking for new ways to contribute."} key="og:description" />
+        <meta property="og:image" content={profile.githubAvatar} key="og:image" />
+        <meta property="og:url" content={window.location.href} key="og:url" />
+        <meta property="og:site_name" content="OpenSauced Insights" key="og:site_name" />
+        <meta property="og:type" content="website" key="og:type" />
+
+        <meta name="twitter:title" content={`${contributorLogin} | Open Source Contributors`} key="twitter:title" />
+        <meta name="twitter:description" content={user?.bio || "I am an open source developer with a passion for music and video games. I strive to improve the open source community and am always looking for new ways to contribute."} key="twitter:description" />
+        <meta name="twitter:image" content={profile.githubAvatar} key="twitter:image" />
+        <meta name="twitter:card" content="summary_large_image" key="twitter:card" />
+        <meta name="twitter:url" content={window.location.href} key="twitter:url" />
+      </Head>
+    
+      <div className="w-full">
+        <ContributorProfilePage
+          prMerged={contributor[0]?.recent_merged_prs}
+          error={isError}
+          loading={userLoading}
+          user={user}
+          repoList={repoList}
+          langList={contributorLanguageList}
+          githubName={profile.githubName}
+          githubAvatar={profile.githubAvatar}
+          prTotal={contributor[0]?.recent_pr_total}
+          openPrs={contributor[0]?.recent_opened_prs}
+          recentContributionCount={contributor[0]?.recent_contribution_count}
+          prReviews={contributor[0]?.recent_pr_reviews}
+          prVelocity={contributor[0]?.recent_pr_velocity}
+        />
+      </div>
+    </>
   );
 };
 
