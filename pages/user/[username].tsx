8e4e42963ba84368e694bb33fc6bee489fd81740
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-06 17:24:55 +0200

    fix: remove unneeded tags

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -33,10 +33,8 @@ const Contributor = (): JSX.Element => {
         <meta name="description" content={`Profile Bio: ${user?.bio || "I am an open source developer with a passion for music and video games. I strive to improve the open source community and am always looking for new ways to contribute."}`} key="description" />
         <meta name="keywords" content="open source, github, contributor, profile, portfolio, developer, software engineer, software developer, software engineer portfolio, software developer portfolio, open source portfolio, open source developer, developer portfolio" key="keywords" />
 
-        <meta property="og:title" content={`${contributorLogin} | Open Source Contributors`} key="og:title" />
         <meta property="og:description" content={`Profile Bio: ${user?.bio || "I am an open source developer with a passion for music and video games. I strive to improve the open source community and am always looking for new ways to contribute."}`} key="og:description" />
         <meta property="og:image" content={profile.githubAvatar} key="og:image" />
-        <meta property="og:url" content={window.location.href} key="og:url" />
         <meta property="og:site_name" content="OpenSauced Insights" key="og:site_name" />
         <meta property="og:type" content="website" key="og:type" />
 
@@ -44,7 +42,6 @@ const Contributor = (): JSX.Element => {
         <meta name="twitter:description" content={`Profile Bio: ${user?.bio || "I am an open source developer with a passion for music and video games. I strive to improve the open source community and am always looking for new ways to contribute."}`} key="twitter:description" />
         <meta name="twitter:image" content={profile.githubAvatar} key="twitter:image" />
         <meta name="twitter:card" content="summary_large_image" key="twitter:card" />
-        <meta name="twitter:url" content={window.location.href} key="twitter:url" />
       </Head>
 
       <div className="w-full">
