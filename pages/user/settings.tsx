b46a90240181bcb0dd89569dab3ea4f940deec0f
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-06 17:35:26 +0200

    feat: Settings pages metadata

--- pages/user/settings.tsx
+++ pages/user/settings.tsx
@@ -2,12 +2,18 @@ import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
 
 import UserSettingsPage from "components/organisms/UserSettingsPage/user-settings-page";
 import ProfileLayout from "layouts/profile";
+import Head from "next/head";
 
 const ProfileSettings = (): JSX.Element => {
   const { user } = useSupabaseAuth();
 
   return (
     <>
+      <Head>
+        <title key="title">Account Settings | OpenSauced Insights</title>
+        <meta name="robots" content="noindex" key="robots" />
+      </Head>
+      
       {user ? (
         <div className="w-full px-4 md:px-16 lg:px-48 py-16">
           <UserSettingsPage  user={user} />
