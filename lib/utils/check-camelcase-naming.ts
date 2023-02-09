282bd70130248bfdca56c01f2dbc285ddc8cf791
NightKnight <Ahmedatwa866@yahoo.com>
2023-02-09 09:18:15 +0000

    lint fixes

--- lib/utils/check-camelcase-naming.ts
+++ lib/utils/check-camelcase-naming.ts
@@ -2,8 +2,8 @@ import uppercaseFirst from "./uppercase-first";
 
 /** Checks the input and if it should be camelcased, it will camelcase it. Uppercases the output. */
 const checkCamelCaseNaming = (name: string) => {
-    if(name.toLowerCase() == "javascript") return "JavaScript";
-    else return uppercaseFirst(name);
-}
+  if(name.toLowerCase() == "javascript") return "JavaScript";
+  else return uppercaseFirst(name);
+};
 
-export default checkCamelCaseNaming
+export default checkCamelCaseNaming;
