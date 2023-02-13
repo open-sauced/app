9c7a6f5bc167f55b102decb6faeceed1312a2fc7
Sebastian Gugulski <43274289+Themesses@users.noreply.github.com>
2023-02-13 13:43:00 -0500

    fix: truncate text on mobile and move dropdown for repositories (#863)
    Fixes #805
    Fixes #806

--- components/atoms/ContextFilterButton/context-filter-button.tsx
+++ components/atoms/ContextFilterButton/context-filter-button.tsx
@@ -12,9 +12,9 @@ const ContextFilterButton: React.FC<ContextFilterButtonProps> = ({ className, ch
   return (
     <button
       onClick={onClick}
-      className="flex items-center gap-1 py-1 px-2 bg-slate-100 border border-slate-300 text-sm text-slate-800   whitespace-nowrap rounded-md drop-shadow-sm hover:bg-slate-50 focus:outline-none focus:border-orange-500 focus:ring-orange-100 focus-visible:border-orange-500 focus:ring focus-visible:ring focus-visible:ring-orange-100 transition"
+      className="flex w-full items-center gap-1 py-1 px-2 bg-slate-100 border border-slate-300 text-sm text-slate-800   whitespace-nowrap rounded-md drop-shadow-sm hover:bg-slate-50 focus:outline-none focus:border-orange-500 focus:ring-orange-100 focus-visible:border-orange-500 focus:ring focus-visible:ring focus-visible:ring-orange-100 transition"
     >
-      <Text className="!text-sm font-semibold tracking-tight !text-slate-900">{children}</Text>
+      <Text className="!text-sm font-semibold truncate tracking-tight !text-slate-900">{children}</Text>
       {!isSelected && <FiPlus onClick={onClick} />}
     </button>
   );
