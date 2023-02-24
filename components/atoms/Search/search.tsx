97b81c07caba0c46e10ec0b6fa6d67c7a340ddf9
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-24 11:43:58 +0200

    feat: add suggestions to search component

--- components/atoms/Search/search.tsx
+++ components/atoms/Search/search.tsx
@@ -8,9 +8,11 @@ interface SearchProps {
   autoFocus?: boolean;
   className?: string;
   onSearch?: (search?: string) => void;
+  suggestions?: string[];
 }
-const Search = ({ placeholder, name, value, autoFocus, className, onSearch }: SearchProps): JSX.Element => {
+const Search = ({ placeholder, name, value, autoFocus, className, onSearch, suggestions }: SearchProps): JSX.Element => {
   const [search, setSearch] = useState(value);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleOnSearch = () => {
     onSearch?.(search);
@@ -21,11 +23,17 @@ const Search = ({ placeholder, name, value, autoFocus, className, onSearch }: Se
     onSearch?.("");
   };
 
+  const handleOnSelect = (suggestion: string) => {
+    setSearch(suggestion);
+    onSearch?.(suggestion);
+    setShowSuggestions(false);
+  };
+
   return (
     <div
       className={`${
         className && className
-      } flex bg-white  py-1 w-max px-3 shadow-input border transition focus-within:ring focus-within:border-orange-500 focus-within:ring-orange-100 rounded-lg   ring-light-slate-6 items-center`}
+      } flex bg-white  py-1 w-max px-3 shadow-input border transition focus-within:ring focus-within:border-orange-500 focus-within:ring-orange-100 rounded-lg   ring-light-slate-6 items-center relative`}
     >
       <FaSearch className="text-light-slate-9" fontSize={16} onClick={handleOnSearch}/>
       <input
@@ -42,7 +50,21 @@ const Search = ({ placeholder, name, value, autoFocus, className, onSearch }: Se
             handleOnSearch();
           }
         }}
+        onFocus={() => setShowSuggestions(true)}
+        onBlur={() => setShowSuggestions(false)}
       />
+  
+      { suggestions && suggestions.length > 0 && showSuggestions && (
+        <div className="absolute bg-white w-full shadow-input border border-light-slate-6 rounded-lg cursor-pointer top-full left-0">
+          {suggestions.map((suggestion, index) => (
+            <div className="px-4 py-2" key={index} onClick={() => handleOnSelect(suggestion)}>
+              <span className="text-light-slate-9 text-sm pl-5">{suggestion}</span>
+            </div>
+          ))}
+        </div>
+      )}
+
+
       { search && <GrClose className="text-light-slate-9" fontSize={16} onClick={handleEmpty}/> }
 
     </div>
