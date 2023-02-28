f2512dea5150713f2de8065776f4006cfde27ab5
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-02-28 15:04:19 +0200

    refractor: put req.json inside req.ok

--- components/molecules/TableHeader/table-header.tsx
+++ components/molecules/TableHeader/table-header.tsx
@@ -39,9 +39,9 @@ const TableHeader = ({
         }} : {}
     });
 
-    const res = await req.json();
 
     if(req.ok) {
+      const res = await req.json();
       const suggestions = res.items.map((item: any) => item.full_name);
       if(suggestions.length > 5) suggestions.length = 5;
       setSuggestions(suggestions);
