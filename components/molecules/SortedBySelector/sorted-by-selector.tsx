92707962b63a9d0f52ab2ed28fc603f5a59ccee6
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-03-07 20:43:51 +0200

    feat: add SortBySelector

---
+++ components/molecules/SortedBySelector/sorted-by-selector.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import ContextFilterButton from "components/atoms/ContextFilterButton/context-filter-button";
+import Icon from "components/atoms/Icon/icon";
+
+import cancelIcon from "img/x-circle.svg";
+
+interface SortedBySelectorProps {
+  handleCancelClick: () => void;
+  className?: string;
+  selected?: keyof typeof SortedOrders | '';
+}
+
+const SortedOrders = {
+  "name": "Repository",
+  "prsCount": "PR Count",
+  "prVelocityCount": "PR Velocity",
+  "spamPrsCount": "Spam"
+} as const;
+
+const SortedBySelector: React.FC<SortedBySelectorProps> = ({
+  handleCancelClick,
+  selected
+}) => {
+
+  if (!selected) return <></>;
+
+  return (
+    <div className="max-w-max relative">
+      <ContextFilterButton isSelected={!!selected}>
+        <div className="flex">
+          <span className="text-dark-slate-10">Sorted by:</span>
+          <div className="flex items-center ml-1 text-light-slate-12">
+            {SortedOrders[(selected as keyof typeof SortedOrders)]}
+
+            <Icon
+              className="ml-2 cursor-pointer"
+              IconImage={cancelIcon}
+              onClick={() => handleCancelClick()}
+            />
+          </div>
+        </div>
+      </ContextFilterButton>
+    </div>
+  );
+};
+
+export default SortedBySelector;
---
+++ stories/molecules/sorted-by-selector.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SortedBySelector from "components/molecules/SortedBySelector/sorted-by-selector";
+import { ComponentStory } from "@storybook/react";
+
+const storyConfig = {
+  title: "Design System/Molecules/SortedBySelector",
+  component: "SortedBySelector"
+};
+export default storyConfig;
+
+const sortingOptions = ["name", "spamPrsCount", "prVelocityCount", "prsCount"] as const;
+
+// SortedBySelector Template
+const SortedBySelectorTemplate: ComponentStory<typeof SortedBySelector> = (args) => (
+  <SortedBySelector {...args} />
+);
+
+export const Default = SortedBySelectorTemplate.bind({});
+export const Selected = SortedBySelectorTemplate.bind({});
+
+Default.args = {
+  selected: ""
+};
+Selected.args = {
+  selected: sortingOptions[0]
+};
