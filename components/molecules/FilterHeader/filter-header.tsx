34dc55cc01b32155bfe88ae7df8cdd64e4a18073
shamimbinnnur <sbnsbn00@gmail.com>
2023-03-07 19:23:31 +0600

    reset hacktober logic

--- components/molecules/FilterHeader/filter-header.tsx
+++ components/molecules/FilterHeader/filter-header.tsx
@@ -1,7 +1,5 @@
 import { useRouter } from "next/router";
 
-import Thumbnail from "../../../img/hacktoberfest-icon.png";
-
 import Text from "components/atoms/Typography/text";
 import Title from "components/atoms/Typography/title";
 import ContextThumbnail from "components/atoms/ContextThumbnail/context-thumbnail";
@@ -23,7 +21,6 @@ const HeaderFilter = () => {
 
   const { filterValues } = useFilterPrefetch();
   const { filterName, toolName, selectedFilter } = router.query;
-  const isHacktoberfest = filterName === "hacktoberfest";
   const filterBtnRouting = (filter: string) => {
     captureAnayltics("Filters", "toolsFilter", `${filter} applied`);
     router.push(`/${filterName}/${toolName}/filter/${filter.toLocaleLowerCase()}`);
@@ -40,15 +37,14 @@ const HeaderFilter = () => {
   return (
     <>
       <div className="header-image mr-2 p-2 min-w-[130px] ">
-        <ContextThumbnail size={120} ContextThumbnailURL={isHacktoberfest ? Thumbnail.src : ""}></ContextThumbnail>
+        <ContextThumbnail size={120} ContextThumbnailURL={""}></ContextThumbnail>
       </div>
       <div className="header-info md:truncate flex flex-col grow justify-center p-2">
         <Title level={1} className="!text-3xl font-semibold tracking-tight text-slate-900">
-          {isHacktoberfest ? "Hacktoberfest 2022" : topicNameFormatting(filterName as string)}
+          {topicNameFormatting(filterName as string)}
         </Title>
         <Text className="mt-1 !text-base   text-slate-500">
-          Insights on GitHub repositories{" "}
-          {isHacktoberfest ? "opted into the largest open source hackathon." : `using the ${filterName} topic.`}
+          {`Insights on GitHub repositories using the ${filterName} topic.`}
         </Text>
         <div className="flex mt-4 items-center gap-2">
           <FilterCardSelect
