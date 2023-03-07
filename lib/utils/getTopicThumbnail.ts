5e1fd98617f09da238266bfd59630d6830894c07
shamimbinnnur <sbnsbn00@gmail.com>
2023-03-08 00:31:18 +0600

    used dictionary/map

--- lib/utils/getTopicThumbnail.ts
+++ lib/utils/getTopicThumbnail.ts
@@ -12,62 +12,25 @@ import ml from "../../img/topicThumbnails/ml.svg";
 import react from "../../img/topicThumbnails/react.svg";
 import contextThumbnailImage from "../../img/open-sourced-with-bg-icon.png";
 
-const topicThumbnails = [
-  {
-    topic: "javascript",
-    thumbnail: javaScript
-  },
-  {
-    topic: "python",
-    thumbnail: python
-  },
-  {
-    topic: "java",
-    thumbnail: java
-  },
-  {
-    topic: "typescript",
-    thumbnail: typeScript
-  },
-  {
-    topic: "csharp",
-    thumbnail: cSharp
-  },
-  {
-    topic: "cpp",
-    thumbnail: cpp
-  },
-  {
-    topic: "php",
-    thumbnail: php
-  },
-  {
-    topic: "c",
-    thumbnail: c
-  },
-  {
-    topic: "ruby",
-    thumbnail: ruby
-  },
-  {
-    topic: "ai",
-    thumbnail: ai
-  },
-  {
-    topic: "ml",
-    thumbnail: ml
-  },
-  {
-    topic: "react",
-    thumbnail: react
-  }
-];
+const topicThumbnails = {
+  javascript: javaScript,
+  python: python,
+  typescript: typeScript,
+  java: java,
+  csharp: cSharp,
+  cpp: cpp,
+  php: php,
+  c: c,
+  ruby: ruby,
+  ai: ai,
+  ml: ml,
+  react: react
+};
+
+type ObjectKey = keyof typeof topicThumbnails;
 
 const getTopicThumbnail = (topic: string) => {
-  const topicThumbnail = topicThumbnails.find(
-    (topicThumbnail) => topicThumbnail.topic === topic
-  );
-  return topicThumbnail?.thumbnail || contextThumbnailImage;
+  return topicThumbnails[topic as ObjectKey] || contextThumbnailImage;
 };
 
 export default getTopicThumbnail;
