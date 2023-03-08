28a5270fd9b8fe281ed9360c1e3e2a8903042c01
Brandon Roberts <robertsbt@gmail.com>
2023-03-08 10:07:22 -0600

    fix: add Rust to interests options and combine utility function (#977)
    Closes #976

--- lib/utils/getTopicOptions.ts
+++
@@ -1,7 +0,0 @@
-const getTopicOptions = () => {
-  const topics = ["javascript", "python", "java", "typescript", "csharp", "cpp", "php", "c", "ruby" , "ai", "ml", "react"];
-
-  return topics;
-};
-
-export default getTopicOptions;
