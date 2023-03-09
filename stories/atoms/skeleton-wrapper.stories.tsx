8034a978966b62cbf7c95f983f451a7c7f9af9be
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-03-09 22:24:57 +0200

    feat: expand test dimensions

--- stories/atoms/skeleton-wrapper.stories.tsx
+++ stories/atoms/skeleton-wrapper.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ComponentStory } from "@storybook/react";
 import SkeletonWrapper from "components/atoms/SkeletonLoader/skeleton-wrapper";
-import "react-loading-skeleton/dist/skeleton.css";
 
 const storyConfig = {
   title: "Design System/Atoms/Skeleton Wrapper",
@@ -19,21 +18,21 @@ export const tripleWrappers = SkeletonTemplate.bind({});
 
 Default.args = {
   count: 1,
-  height: 60,
-  width: 100,
+  height: 180,
+  width: 300,
   radius: 4
 };
 
 doubleWrappers.args = {
   count: 2,
-  height: 30,
-  width: 100,
+  height: 90,
+  width: 300,
   radius: 4
 };
 
 tripleWrappers.args = {
   count: 3,
-  height: 20,
-  width: 100,
+  height: 60,
+  width: 300,
   radius: 4
 };
