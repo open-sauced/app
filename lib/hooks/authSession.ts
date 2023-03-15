1015cd04da070ca5ea71c8aa35e0744c2a2ffbc4
NightKnight <Ahmedatwa866@yahoo.com>
2023-03-15 06:57:07 +0200

    refractor: use Partial instead of extend

--- lib/hooks/authSession.ts
+++ lib/hooks/authSession.ts
@@ -1,7 +1,7 @@
 import { supabase } from "lib/utils/supabase";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-export interface UserResponse extends DbUser {}
+export type UserResponse = Partial<DbUser>
 const authSession = async () => {
   const sessionResponse = await supabase.auth.getSession();
   const sessionToken = sessionResponse?.data.session?.access_token;
