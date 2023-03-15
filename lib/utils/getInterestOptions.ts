72aa9a91490237ce831ad4a3385f2b0eb0d3a902
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-03-15 07:40:22 +0100

    fix: add golang language

--- lib/utils/getInterestOptions.ts
+++ lib/utils/getInterestOptions.ts
@@ -1,4 +1,18 @@
-const interests = ["javascript", "python", "java", "typescript", "csharp", "cpp", "php", "c", "ruby" , "ai", "ml", "react"] as const;
+const interests = [
+  "javascript",
+  "python",
+  "java",
+  "typescript",
+  "csharp",
+  "cpp",
+  "php",
+  "c",
+  "ruby",
+  "ai",
+  "ml",
+  "react",
+  "golang"
+] as const;
 export type interestsType = typeof interests[number];
 
 export const getInterestOptions = () => {
