3f64f6d3657abec66b227c04cd229df671c10689
OGBONNA SUNDAY <62995161+OgDev-01@users.noreply.github.com>
2023-03-16 00:09:51 +0100

    fix: thumbnail topic images (#1010)
    * fix: thumbnail topic images
    
    * fix: add golang language
    
    * merge: beta to good-icons-branch
    
    * fix: build error
    
    * fix: more build error
    
    * chore: update thumbnail images

--- lib/utils/getTopicThumbnail.ts
+++ lib/utils/getTopicThumbnail.ts
@@ -2,16 +2,17 @@ import javaScript from "img/topic-thumbnails/javascript.svg";
 import python from "img/topic-thumbnails/python.svg";
 import typeScript from "img/topic-thumbnails/typescript.svg";
 import java from "img/topic-thumbnails/java.svg";
-import cSharp from "img/topic-thumbnails/c-sharp.svg";
-import cpp from "img/topic-thumbnails/c-plus.svg";
+import cSharp from "img/topic-thumbnails/csharp.svg";
+import cpp from "img/topic-thumbnails/cpp.svg";
 import php from "img/topic-thumbnails/php.svg";
 import c from "img/topic-thumbnails/c.svg";
 import ruby from "img/topic-thumbnails/ruby.svg";
 import ai from "img/topic-thumbnails/ai.svg";
+import go from "img/topic-thumbnails/go.svg";
 import ml from "img/topic-thumbnails/machine-learning.svg";
 import react from "img/topic-thumbnails/react.svg";
-import golang from "img/topic-thumbnails/golang.svg";
 import rust from "img/topic-thumbnails/rust.svg";
+
 import contextThumbnailImage from "../../img/open-sourced-with-bg-icon.png";
 
 import { interestsType } from "./getInterestOptions";
@@ -29,7 +30,7 @@ const topicThumbnails: Record<interestsType, string> = {
   ai: ai,
   react: react,
   ml: ml,
-  golang: golang,
+  golang: go,
   rust: rust
 };
 
