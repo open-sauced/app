3718b6f0bf65b6a9486b42e475f75039a43d9a2f
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-03-16 01:57:16 +0530

    feat: added active styles to navbar links

--- components/organisms/TopNav/top-nav.tsx
+++ components/organisms/TopNav/top-nav.tsx
@@ -1,5 +1,6 @@
 import NavLinks from "components/molecules/NavLinks/nav-links";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 import AuthSection from "components/molecules/AuthSection/auth-section";
@@ -15,31 +16,46 @@ const TopNav: React.FC = () => {
 
   const { data: gitHubUser } = useFetchUser(user?.user_metadata.user_name);
   const userInterest = gitHubUser?.interests.split(",")[0] || "javascript";
+  const router = useRouter();
 
   return (
-
     <header className="top-nav-container flex justify-between items-center z-50 py-0.5 bg-light-slate-3 border-b">
       <div className="flex justify-between items-center mx-auto container px-2 md:px-16">
         <div className="flex gap-3 md:gap-8 items-center flex-wrap ">
           <HeaderLogo withBg={false} textIsBlack />
           {!!user && onboarded ? (
             <>
-              <Link className="text-sm text-light-slate-10" href={"/hub/insights"}>
+              <Link
+                className={
+                  "text-sm " + (router.asPath === "/hub/insights" ? "text-light-orange-10" : "text-light-slate-10")
+                }
+                href={"/hub/insights"}
+              >
                 Insights Hub
               </Link>
-              <Link className="text-sm text-light-slate-10" href={`/${userInterest}/dashboard/filter/recent`}>
+              <Link
+                className={
+                  "text-sm " +
+                  (router.asPath === `/${userInterest}/dashboard/filter/recent`
+                    ? "text-light-orange-10"
+                    : "text-light-slate-10")
+                }
+                href={`/${userInterest}/dashboard/filter/recent`}
+              >
                 Explore
               </Link>
             </>
           ) : (
             ""
           )}
-          <Link className="text-sm text-light-slate-10" href={"/feed"}>
+          <Link
+            className={"text-sm " + (router.asPath === "/feed" ? "text-light-orange-10" : "text-light-slate-10")}
+            href={"/feed"}
+          >
             Highlights
           </Link>
         </div>
         <AuthSection />
-
       </div>
     </header>
   );
