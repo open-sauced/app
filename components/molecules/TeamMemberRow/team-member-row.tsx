08816d9712f652cd13e82be860ac98b40616e28f
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-03-16 22:35:15 +0200

    feat: add menu to member row

--- components/molecules/TeamMemberRow/team-member-row.tsx
+++ components/molecules/TeamMemberRow/team-member-row.tsx
@@ -1,6 +1,8 @@
 import Avatar from "components/atoms/Avatar/avatar";
 import { AiOutlineCaretDown } from "react-icons/ai";
 import pendingImg from "img/icons/fallback-image-disabled-square.svg";
+import { useState } from "react";
+import Selector from "components/atoms/Selector/selector";
 
 interface TeamMemberRowProps {
   className?: string;
@@ -10,6 +12,8 @@ interface TeamMemberRowProps {
 }
 
 const TeamMemberRow  = ({ className, name, avatarUrl, role }: TeamMemberRowProps) => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const pending = role == "pending";
 
@@ -28,8 +32,14 @@ const TeamMemberRow  = ({ className, name, avatarUrl, role }: TeamMemberRowProps
       </div>
       <div>
         <div className="flex items-center gap-3">
-          {mapRoleToText[role]} {!pending && <AiOutlineCaretDown />}
+          {mapRoleToText[role]} {!pending && <AiOutlineCaretDown onClick={() => {setIsMenuOpen(!isMenuOpen)}} />}
         </div>
+        { !pending && isMenuOpen && (
+          // <div className="absolute bg-white rounded-md shadow-md w-40">
+          // </div>
+          <Selector filterOptions={["Admin", "can edit", "can view"]} selected={mapRoleToText[role]} variation="check" />
+        )}
+
       </div>
     </div>
   );
