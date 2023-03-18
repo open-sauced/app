054e23507eb7497e4767d9882cac4229c7a3996c
Nworie Kingsley <obizzkingsley@gmail.com>
2023-03-19 00:16:17 +0100

    feat: Make the top-nav more responsive on mobile

--- components/organisms/TopNav/top-nav.tsx
+++ components/organisms/TopNav/top-nav.tsx
@@ -20,23 +20,25 @@ const TopNav: React.FC = () => {
 
     <header className="top-nav-container flex justify-between items-center z-50 py-0.5 bg-light-slate-3 border-b">
       <div className="flex justify-between items-center mx-auto container px-2 md:px-16">
-        <div className="flex gap-3 md:gap-8 items-center flex-wrap ">
+        <div className="flex gap-3 md:gap-8 items-center flex-wrap">
           <HeaderLogo withBg={false} textIsBlack />
-          {!!user && onboarded ? (
-            <>
-              <Link className="text-sm text-light-slate-10" href={"/hub/insights"}>
-                Insights
-              </Link>
-              <Link className="text-sm text-light-slate-10" href={`/${userInterest}/dashboard/filter/recent`}>
-                Explore
-              </Link>
-            </>
-          ) : (
-            ""
-          )}
-          <Link className="text-sm text-light-slate-10" href={"/feed"}>
-            Highlights
-          </Link>
+          <div className="flex flex-wrap gap-3 md:gap-8 mb-3">
+            {!!user && onboarded ? (
+              <>
+                <Link className="text-sm text-light-slate-10" href={"/hub/insights"}>
+                  Insights
+                </Link>
+                <Link className="text-sm text-light-slate-10" href={`/${userInterest}/dashboard/filter/recent`}>
+                  Explore
+                </Link>
+              </>
+            ) : (
+              ""
+            )}
+            <Link className="text-sm text-light-slate-10" href={"/feed"}>
+              Highlights
+            </Link>
+          </div>
         </div>
         <AuthSection />
 
