8a88c2310357c491a990aaa35b3ba7b500f158f0
Anush <anushshetty90@gmail.com>
2023-03-19 00:28:18 +0530

    Removed unused imports int stories/atoms

--- stories/atoms/text-input.stories.tsx
+++ stories/atoms/text-input.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentStory } from "@storybook/react";
 import TextInput from "components/atoms/TextInput/text-input";
 
 const storyConfig = {
