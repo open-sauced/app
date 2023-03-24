acfa306514ffc019cf074908f0713fc8710e8d83
NightKnight <Ahmedatwa866@yahoo.com>
2023-03-24 18:12:46 +0200

    feat: add repository recommendation links (#1050)

---
+++ lib/utils/recommendations.ts
@@ -0,0 +1,21 @@
+import { interestsType } from "./getInterestOptions";
+
+
+const recommendations: Record<interestsType, string[]> = {
+  react: ["Skyscanner/backpack"],
+  javascript: ["EddieHubCommunity/LinkFree"],
+  python: ["randovania/randovania"],
+  ml: ["mindsdb/mindsdb"],
+  ai: ["LAION-AI/Open-Assistant"],
+  rust: ["swc-project/swc"],
+  ruby: ["rubocop/rubocop"],
+  c: ["systemd/systemd"],
+  cpp: ["redpanda-data/redpanda"],
+  csharp: ["ScottPlot/ScottPlot"],
+  php: ["laravel/framework"],
+  java: ["thingsboard/thingsboard"],
+  typescript: ["sequelize/sequelize"],
+  golang: ["cli/cli"]
+};
+
+export default recommendations;
