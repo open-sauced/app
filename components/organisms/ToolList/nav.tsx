ad7e99667671f55fc0ffdaebd4b228964c61e58e
Brandon Roberts <robertsbt@gmail.com>
2023-03-31 14:31:46 -0500

    fix: fix nav server render to match initial client render

--- components/organisms/ToolList/nav.tsx
+++ components/organisms/ToolList/nav.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Link from "next/link";
-import humanizeNumber from "../../../lib/utils/humanizeNumber";
+import NavItem from "./nav-item";
 
 type toolListArray = {
   name: string;
@@ -10,7 +9,7 @@ type toolListArray = {
 interface NavProps {
   toolList: toolListArray[];
   selectedTool: string | string[] | undefined;
-  filterName: string | string[] | undefined;
+  filterName?: string | string[] | undefined;
   selectedFilter?: string | string[] | undefined;
   username?: string | string[] | undefined;
 }
@@ -37,35 +36,13 @@ const Nav: React.FC<NavProps> = ({ toolList, selectedTool = "dashboard", selecte
               : "border-transparent hover:border-light-slate-8"
           }`}
         >
-          <Link
-            href={`/${username ? `pages/${username}/` : ""}${filterName}/${tool.name.toLowerCase()}${
-              selectedFilter
-                ? `/filter/${Array.isArray(selectedFilter) ? selectedFilter.join("/") : selectedFilter}`
-                : ""
-            }`}>
-            {/* Button component was here and needed to be removed to resolve issue #187. Button component had styling that will eventually need to be replaced. */}
-            <div
-              className={`flex h-11 px-2 md:px-4 items-center rounded-t-lg ${
-                selectedTool === tool.name.toLowerCase() ? "" : "cursor-pointer hover:!bg-light-slate-4"
-              } after:block after:relative after:inset-x-0 after:-bottom-0.5 after:h-0.5 after:rounded-lg ${
-                selectedTool === tool.name.toLowerCase() ? "after:bg-orange-500" : "focus:after:bg-slate-400"
-              } focus:bg-slate-100 focus:ring-slate-300 child:flex child:items-center`}
-            >
-              <span
-                className={
-                  "text-base whitespace-nowrap " +
-                  (selectedTool === tool.name.toLowerCase() ? "text-slate-900" : "text-slate-500")
-                }
-              >
-                {tool.name}
-              </span>
-              {(!!tool.numOf || tool.numOf === 0) && (
-                <div className="ml-2 py-0.5 px-1.5 h-fit bg-slate-200 text-slate-500 border rounded-full text-xs font-semibold">
-                  {humanizeNumber(tool.numOf, null)}
-                </div>
-              )}
-            </div>
-          </Link>
+          <NavItem
+            tool={tool}
+            selectedFilter={selectedFilter}
+            filterName={filterName}
+            username={username}
+            selectedTool={selectedTool}
+          />
         </div>
       ))}
     </nav>
