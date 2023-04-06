e381c0ff8b584e0401fbfbb742626cf51e590a96
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-04-06 21:31:07 +0100

    feat: create insight members hook

--- components/molecules/TeamMemberRow/team-member-row.tsx
+++ components/molecules/TeamMemberRow/team-member-row.tsx
@@ -10,21 +10,20 @@ interface TeamMemberRowProps extends TeamMemberData {
 }
 
 const mapRoleToText: Record<TeamMemberRowProps["role"], string> = {
-  admin: "Admin",
+  admin: "Owner",
   editor: "can edit",
   viewer: "can view",
   pending: "Pending"
 };
 
-const TeamMemberRow  = ({ className, name, avatarUrl, role }: TeamMemberRowProps) => {
-
+const TeamMemberRow = ({ className, name, avatarUrl, role }: TeamMemberRowProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const pending = role == "pending";
 
   const handleRoleChange = (role: string) => {};
 
-  return(
+  return (
     <div className={`flex justify-between items-center ${className && className} ${pending && "text-light-slate-10"}`}>
       <div className="flex items-center">
         <Avatar size={40} isCircle avatarURL={pending ? pendingImg : avatarUrl} />
@@ -32,14 +31,25 @@ const TeamMemberRow  = ({ className, name, avatarUrl, role }: TeamMemberRowProps
       </div>
       <div>
         <div className="flex items-center gap-3">
-          {mapRoleToText[role]} {!pending && <AiOutlineCaretDown onClick={() => {setIsMenuOpen(!isMenuOpen);}} />}
+          {mapRoleToText[role]}{" "}
+          {!pending && (
+            <AiOutlineCaretDown
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            />
+          )}
         </div>
-        { !pending && isMenuOpen && (
-          <Selector filterOptions={["Admin", "can edit", "can view"]} selected={mapRoleToText[role]} variation="check" handleFilterClick={handleRoleChange} />
+        {!pending && isMenuOpen && (
+          <Selector
+            filterOptions={["Owner", "can edit", "can view"]}
+            selected={mapRoleToText[role]}
+            variation="check"
+            handleFilterClick={handleRoleChange}
+          />
         )}
       </div>
     </div>
   );
-
 };
 export default TeamMemberRow;
--- lib/hooks/useInsightMembers.ts
+++ lib/hooks/useInsightMembers.ts
@@ -1,3 +1,23 @@
-interface PaginatedInsightMembers {}
+import useSWR, { Fetcher } from "swr";
+import useSupabaseAuth from "./useSupabaseAuth";
+import publicApiFetcher from "lib/utils/public-api-fetcher";
+interface PaginatedInsightMembers {
+  data: DbInsightMember[];
+  meta: Meta;
+}
 
-const useInsightMembers = () => {};
+const useInsightMembers = (page_id: number) => {
+  const { data, error, mutate } = useSWR<PaginatedInsightMembers, Error>(
+    `user/insights/${page_id}/members`,
+    publicApiFetcher as Fetcher<PaginatedInsightMembers, Error>
+  );
+
+  return {
+    data: data ?? [],
+    isLoading: !error && !data,
+    isError: !!error,
+    mutate
+  };
+};
+
+export default useInsightMembers;
