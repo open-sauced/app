d1a043b5e05471d770aa53cdf53f72fb46af7390
NightKnight <Ahmedatwa866@yahoo.com>
2023-04-17 15:02:19 +0200

    feat: server render user pages and add open graph image (#1095)

--- pages/user/[username]/index.tsx
+++ pages/user/[username]/index.tsx
@@ -1,74 +1,75 @@
-import { useRouter } from "next/router";
 import useRepoList from "lib/hooks/useRepoList";
 import { useSingleContributor } from "lib/hooks/useSingleContributor";
-
-import ContributorProfilePage from "components/organisms/ContributorProfilePage/contributor-profile-page";
-import { ContributorsProfileType } from "components/molecules/ContributorHoverCard/contributor-hover-card";
-
 import ProfileLayout from "layouts/profile";
-import { useFetchUser } from "lib/hooks/useFetchUser";
 import Head from "next/head";
 import { WithPageLayout } from "interfaces/with-page-layout";
-import { useEffect } from "react";
 import { Person } from "schema-dts";
 import { jsonLdScriptProps } from "react-schemaorg";
+import { GetServerSidePropsContext } from "next";
+import SEO from "layouts/SEO/SEO";
+import { supabase } from "lib/utils/supabase";
+import dynamic from "next/dynamic";
+import { getAvatarByUsername } from "lib/utils/github";
 
-const Contributor: WithPageLayout = () => {
-  const router = useRouter();
-  const { username } = router.query;
-  const contributorLogin = username as string;
+// A quick fix to the hydration issue. Should be replaced with a real solution.
+// Slows down the page's initial client rendering as the component won't be loaded on the server.
+const ClientOnlyContributorProfilePage = dynamic(
+  () => import("components/organisms/ContributorProfilePage/contributor-profile-page"),
+  { ssr: false }
+);
 
-  const { data: contributor, isError: contributorError } = useSingleContributor(contributorLogin);
-  const { data: user, isLoading: userLoading, isError: userError } = useFetchUser(contributorLogin);
+export type ContributorSSRProps = {
+  username: string;
+  user?: DbUser;
+  ogImage: string;
+}
+
+const Contributor: WithPageLayout<ContributorSSRProps> = ({ username, user, ogImage }) => {
+
+  const { data: contributor, isError: contributorError } = useSingleContributor(username);
 
   const isError = contributorError;
   const repoList = useRepoList(contributor[0]?.recent_repo_list || "");
   const contributorLanguageList = (contributor[0]?.langs || "").split(",");
-  const profile: ContributorsProfileType = {
-    githubAvatar: `https://www.github.com/${contributorLogin}.png?size=300`,
-    githubName: contributorLogin,
-    totalPR: contributor[0]?.recent_pr_total
-  };
-
-  useEffect(() => {
-    Contributor.updateSEO!({
-      title: `${contributorLogin} | OpenSauced`,
-      description: `${user?.bio || `${profile?.githubName} has connected their GitHub but has not added a bio.`}`,
-      image: profile.githubAvatar,
-      twitterCard: "summary_large_image"
-    });
-  }, [contributorLogin, user?.bio, profile.githubAvatar]);
+  const githubAvatar = getAvatarByUsername(username, 300);
 
   return (
     <>
+      <SEO
+        title={`${username} | OpenSauced`}
+        description={`${user?.bio || `${username} has connected their GitHub but has not added a bio.`}`}
+        image={ogImage}
+        twitterCard="summary_large_image"
+      />
+
       <Head>
         {user && (
           <script
             {...jsonLdScriptProps<Person>({
               "@context": "https://schema.org",
               "@type": "Person",
-              name: profile.githubName,
+              name: username,
               url: `https://www.github.com/${user.login}`,
-              image: profile.githubAvatar,
+              image: githubAvatar,
               sameAs: user.twitter_username ? `https://twitter.com/${user.twitter_username}` : undefined,
               description: user.bio ?? undefined,
               email: user.display_email ? user.email : undefined,
-              knowsAbout: contributorLanguageList.concat(user.interests.split(",")),
+              knowsAbout: contributorLanguageList.concat(user.interests?.split(",")),
               worksFor: user.company ?? undefined
             })}
           />
         )}
       </Head>
       <div className="w-full">
-        <ContributorProfilePage
+        <ClientOnlyContributorProfilePage
           prMerged={contributor[0]?.recent_merged_prs}
           error={isError}
-          loading={userLoading}
+          loading={false}
           user={user}
           repoList={repoList}
           langList={contributorLanguageList}
-          githubName={profile.githubName}
-          githubAvatar={profile.githubAvatar}
+          githubName={username}
+          githubAvatar={githubAvatar}
           prTotal={contributor[0]?.recent_pr_total}
           openPrs={contributor[0]?.recent_opened_prs}
           recentContributionCount={contributor[0]?.recent_contribution_count}
@@ -82,3 +83,47 @@ const Contributor: WithPageLayout = () => {
 
 Contributor.PageLayout = ProfileLayout;
 export default Contributor;
+
+export const getServerSideProps = async (context: UserSSRPropsContext) => {
+  return await handleUserSSR(context);
+};
+
+export type UserSSRPropsContext = GetServerSidePropsContext<{ username: string }>;
+
+export async function handleUserSSR({ params }: GetServerSidePropsContext<{ username: string }>) {
+  const { username } = params!;
+  const sessionResponse = await supabase.auth.getSession();
+  const sessionToken = sessionResponse?.data.session?.access_token;
+  let user;
+  let ogImage;
+
+  async function fetchUserData() {
+    const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${username}`, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${sessionToken}`
+      }
+    });
+
+    user = await req.json() as DbUser;
+  }
+
+  async function fetchSocialCardURL() {
+    const socialCardUrl = `${String(process.env.NEXT_PUBLIC_OPENGRAPH_URL ?? "")}/users/${username}`;
+    const ogReq = await fetch(`${socialCardUrl}/metadata`); //status returned: 204 or 304 or 404
+    if(ogReq.status !== 204) {
+      fetch(socialCardUrl, {
+        method: "HEAD"
+      }); // trigger the generation of the social card
+    }
+
+    ogImage = ogReq.headers.get("x-amz-meta-location");
+  }
+
+  // Runs the data fetching in parallel. Decreases the loading time by 50%.
+  await Promise.allSettled([fetchUserData(), fetchSocialCardURL()]);
+
+  return {
+    props: { username, user, ogImage }
+  };
+}
