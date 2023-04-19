0f2d95214b86bc768a583d0791155fc8577e3910
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-04-19 22:42:39 +0100

    refactor: replace reports filter usage

--- components/molecules/SelectReportsFilter/select-reports-filter.tsx
+++ components/molecules/SelectReportsFilter/select-reports-filter.tsx
@@ -1,7 +1,7 @@
 import Button from "components/atoms/Button/button";
 import Text from "components/atoms/Typography/text";
 import Title from "components/atoms/Typography/title";
-import Select from "components/atoms/Select/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "components/atoms/Select/select";
 import SelectOption from "components/atoms/Select/select-option";
 import { FilterOptions } from "interfaces/filter-object-types";
 import { useState } from "react";
@@ -28,14 +28,18 @@ const SelectReportsFilter = ({ filterList, callback }: SelectReportsFilterProps)
         Download the filtered contributions from the last 30 days as a CSV. Selecting a filter will remove all the added
         repositories.
       </Text>
-      <div className="flex flex-col md:flex-row gap-2">
-        <Select error={error} onChange={(event) => setSelectedValue(event.target.value)} className="w-full">
-          <SelectOption value="">Select a Filter</SelectOption>
-          {filterList?.map(({ filterName, filterValue }, index) => (
-            <SelectOption key={index} value={filterValue}>
-              {filterName}
-            </SelectOption>
-          ))}
+      <div className="flex flex-col gap-2 md:flex-row">
+        <Select>
+          <SelectTrigger>
+            <SelectValue>{selectedValue ? selectedValue : "Select a Filter"}</SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            {filterList?.map(({ filterName, filterValue }, index) => (
+              <SelectItem key={index} value={filterValue}>
+                {filterName}
+              </SelectItem>
+            ))}
+          </SelectContent>
         </Select>
         <Button variant="primary" onClick={handleButtonClick} className="w-52 h-[38px]">
           Generate CSV
