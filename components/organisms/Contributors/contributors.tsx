51e6fed755637af11f75a37902bc04ffe4d61081
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-04-21 16:38:37 +0100

    linting

--- components/organisms/Contributors/contributors.tsx
+++ components/organisms/Contributors/contributors.tsx
@@ -37,16 +37,16 @@ const Contributors = ({ repositories }: ContributorProps): JSX.Element => {
   const contributorArray = isError
     ? []
     : contributors.map((contributor) => {
-        const timeSinceFirstCommit = calcDistanceFromToday(new Date(contributor.first_commit_time));
+      const timeSinceFirstCommit = calcDistanceFromToday(new Date(contributor.first_commit_time));
 
-        return {
-          profile: {
-            githubAvatar: getAvatarByUsername(contributor.host_login),
-            githubName: contributor.host_login,
-            dateOfFirstPR: timeSinceFirstCommit
-          }
-        };
-      });
+      return {
+        profile: {
+          githubAvatar: getAvatarByUsername(contributor.host_login),
+          githubName: contributor.host_login,
+          dateOfFirstPR: timeSinceFirstCommit
+        }
+      };
+    });
 
   return (
     <>
