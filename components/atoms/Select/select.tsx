79b6ada98f9e4698a9f5dee19f57cf03a011baa3
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-04-21 18:29:41 +0100

    refactor: update insights page team role select

--- components/atoms/Select/select.tsx
+++ components/atoms/Select/select.tsx
@@ -80,7 +80,7 @@ const SelectItem = React.forwardRef<
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>{itemIndicatorIcon || ""}</SelectPrimitive.ItemIndicator>
+      <SelectPrimitive.ItemIndicator asChild>{itemIndicatorIcon || ""}</SelectPrimitive.ItemIndicator>
     </span>
 
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
