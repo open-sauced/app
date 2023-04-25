29d475e297b5ff445e3cd74f2dff174b0deb8584
Brandon Roberts <robertsbt@gmail.com>
2023-04-25 14:33:03 -0500

    chore: fix table alignment

--- components/molecules/ReportsHistory/reports-history.tsx
+++ components/molecules/ReportsHistory/reports-history.tsx
@@ -13,16 +13,16 @@ interface ReportsHistoryProps {
 const ReportsHistory = ({ reportList, repositories }: ReportsHistoryProps): JSX.Element => {
 
   const tableStyles = {
-    text: "text-sm   text-slate-900 tracking-tight",
-    col1: "flex-[1.5]",
-    col2: "flex-[1]",
-    col3: "flex-[1]",
-    col4: "flex-[0.5] text-center"
+    text: "text-sm text-slate-900 tracking-tight",
+    col1: "flex-[1] text-center",
+    col2: "flex-[1] text-center",
+    col3: "flex-[1] text-center",
+    col4: "flex-[1] text-center"
   };
 
   return (
     <div className="flex flex-col bg-white border rounded-lg overflow-hidden">
-      <div className="flex justify-between gap-2 px-3 md:px-6 py-3 rounded-t-lg bg-light-slate-3">
+      <div className="flex justify-between px-3 md:px-6 py-3 rounded-t-lg bg-light-slate-3">
         <div className={tableStyles.col1}>
           <TableTitle text={"Report Name"} />
         </div>
@@ -37,13 +37,13 @@ const ReportsHistory = ({ reportList, repositories }: ReportsHistoryProps): JSX.
         </div>
       </div>
 
-      <div className="flex flex-col justify-between w-full gap-2">
+      <div className="flex flex-col justify-between w-full">
         {reportList &&
           reportList.map((report, index) => {
             const { reportDate, reportName, reportFormat } = report;
 
             return (
-              <div className="flex items-center py-3 px-3 md:px-6 odd:bg-white even:bg-slate-50 gap-2" key={index}>
+              <div className="flex items-center py-3 px-3 md:px-6 odd:bg-white even:bg-slate-50" key={index}>
                 <div className={`${tableStyles.col1} ${tableStyles.text}`}>{reportName}</div>
                 <div className={`${tableStyles.col2} ${tableStyles.text}`}>{reportDate}</div>
                 <div className={`${tableStyles.col3} ${tableStyles.text}`}>{reportFormat}</div>
