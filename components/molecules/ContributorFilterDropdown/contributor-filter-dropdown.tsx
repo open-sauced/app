87e03740b0feaa4e87f4c65d5e51fd67409b2ddb
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-05-09 22:16:19 +0100

    chore: setup filter dropdown

---
+++ components/molecules/ContributorFilterDropdown/contributor-filter-dropdown.tsx
@@ -0,0 +1,13 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "components/atoms/Dropdown/dropdown";
+import React from "react";
+
+const ContributorFilterDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger></DropdownMenuTrigger>
+      <DropdownMenuContent></DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ContributorFilterDropdown;
---
+++ stories/molecules/contributor-filter-dropdown.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory } from "@storybook/react";
+import ContributorFilterDropdown from "components/molecules/ContributorFilterDropdown/contributor-filter-dropdown";
+
+const storyConfig = {
+  title: "Design System/Molecules/ContributorFilterDropdown",
+};
+
+export default storyConfig;
+
+const ContributorFilterDropdownTemplate: ComponentStory<typeof ContributorFilterDropdown> = (args) => (
+  <ContributorFilterDropdown />
+);
+
+export const DefaultStory = ContributorFilterDropdownTemplate.bind({});
