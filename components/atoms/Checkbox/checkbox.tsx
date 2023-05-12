4a09cf8e2a250686a210e9dcd0ca5c26a95ffdf3
OGBONNA SUNDAY <62995161+OgDev-01@users.noreply.github.com>
2023-05-12 14:05:08 +0100

    feat: add contributors filter/dropdown and insights list dropdown component to design system (#1165)

--- components/atoms/Checkbox/checkbox.tsx
+++ components/atoms/Checkbox/checkbox.tsx
@@ -8,37 +8,33 @@ interface CheckboxProps extends React.ComponentPropsWithoutRef<typeof CheckboxPr
   label?: string;
 }
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  CheckboxProps
->(({ className, label, id = "checkbox", ...props }, ref) => (
-  <div className="flex items-center">
-    <CheckboxPrimitive.Root
-      ref={ref}
-      className={clsx(
-        "peer h-4 w-4 shrink-0 rounded-[4px] cursor-pointer bg-white border border-light-slate-8 hover:border-orange-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-orange-500 data-[state=checked]:bg-orange-500",
-        className
+const Checkbox = React.forwardRef<React.ElementRef<typeof CheckboxPrimitive.Root>, CheckboxProps>(
+  ({ className, label, id = "checkbox", ...props }, ref) => (
+    <div className="flex items-center">
+      <CheckboxPrimitive.Root
+        ref={ref}
+        className={clsx(
+          "peer h-4 w-4 shrink-0 rounded-[4px] cursor-pointer bg-white border border-light-slate-8 hover:border-orange-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-orange-500 data-[state=checked]:bg-orange-500",
+          className
+        )}
+        {...props}
+        id={id}
+      >
+        <CheckboxPrimitive.Indicator className={clsx("flex items-center justify-center text-white")}>
+          <FiCheck className="w-full h-full" />
+        </CheckboxPrimitive.Indicator>
+      </CheckboxPrimitive.Root>
+      {label && (
+        <label
+          htmlFor={id}
+          className="ml-3 text-sm font-medium leading-none cursor-pointer text-light-slate-12 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          {label}
+        </label>
       )}
-      {...props}
-      id={id}
-    >
-      <CheckboxPrimitive.Indicator
-        className={clsx("flex items-center justify-center text-white")}
-      >
-        <FiCheck className="h-full w-full" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-    {label && (
-      <label
-        htmlFor={id}
-        className="text-sm ml-3 text-light-slate-12 font-medium leading-none cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-      >
-        {label}
-      </label>
-    )}
-  </div>
-));
+    </div>
+  )
+);
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export default Checkbox;
-
---
+++ components/molecules/ContributorFilterDropdown/contributor-filter-dropdown.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import clsx from "clsx";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+
+import Checkbox from "components/atoms/Checkbox/checkbox";
+
+// This list might have to come in as props in future implementations
+const filterOptions: { name: string; value: string }[] = [
+  { name: "Language", value: "language" },
+  { name: "Timezone", value: "timezone" },
+  { name: "Last Contributed", value: "last-contributed" },
+  { name: "Repositories", value: "repositories" },
+  { name: "Contribution", value: "contribution" },
+  { name: "Last 30 Days", value: "last-30-days" },
+  { name: "Activities", value: "activities" },
+  { name: "Seniority", value: "seniority" }
+];
+
+const ContributorFilterDropdown = () => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e: any) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (open && ref.current && !ref.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [open]);
+
+  return (
+    <div ref={ref} className="flex flex-col px-3 border rounded-lg shadow w-max">
+      <div
+        onClick={() => setOpen((prev) => !prev)}
+        className="flex items-center justify-between text-sm font-normal cursor-pointer text-light-slate-9 w-44 "
+      >
+        {open ? <span>Filter: Show/Hide</span> : <span>Filter list:</span>}
+
+        <div>
+          <BiChevronUp className="text-lg translate-y-1.5" />
+          <BiChevronDown className="text-lg -translate-y-1.5" />
+        </div>
+      </div>
+
+      <div
+        className={clsx(
+          " z-50 space-y-1.5 overflow-hidden transition-all text-light-slate-12",
+          open ? "h-auto pb-2" : "h-0"
+        )}
+      >
+        {filterOptions.map(({ name, value }, i) => (
+          <div key={i.toString()} className="flex items-center gap-2 text-sm ">
+            <Checkbox value={value} id={name} className="w-3 h-3 gap-2" />
+            <label className="font-normal" htmlFor={name}>
+              {name}
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ContributorFilterDropdown;
