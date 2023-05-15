7e8dfb23d7e6c39681ffff05a8a7b62f430a582e
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-05-15 16:56:17 +0100

    docs: update story component

--- stories/molecules/recommended-repo-card.stories.tsx
+++ stories/molecules/recommended-repo-card.stories.tsx
@@ -1,10 +1,9 @@
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { ComponentStory } from "@storybook/react";
 import RecommendedRepoCard from "components/molecules/RecommendedRepoCard/recommended-repo-card";
-import { mockDbContributions } from "../mockedData";
 
 const storyConfig = {
-  title: "Design System/Molecules/Recommended Repo Card"
+  title: "Design System/Molecules/Recommended Repo Card",
 };
 export default storyConfig;
 
@@ -16,10 +15,5 @@ const template: ComponentStory<typeof RecommendedRepoCard> = (args) => (
 
 export const Default = template.bind({});
 Default.args = {
-  contributions: mockDbContributions,
   fullname: "open-sauced/insights",
-  description: "A dashboard for your GitHub insights",
-  issues: 1200,
-  pullRequests: 50,
-  stars: 3001
 };
