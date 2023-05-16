46d5ae83a3afd159b59ba1f789a9a8cf53a960d7
takanome_dev <takanomedev221@gmail.com>
2023-05-16 23:19:09 +0000

    format highlight date in user profile and edit highlight

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
 
 import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
-import { getFormattedDate } from "lib/utils/date-utils";
 import { useFetchAllHighlights } from "lib/hooks/useFetchAllHighlights";
 import { useFetchHighlightRepos } from "lib/hooks/useFetchHiglightRepos";
 import { useFetchSingleHighlight } from "lib/hooks/useFetchSingleHighlight";
@@ -93,7 +92,9 @@ const Feeds: WithPageLayout = () => {
                   />
                   <strong>{singleHighlight.login}</strong>
                   <span className="text-xs font-normal text-light-slate-11">
-                    {getFormattedDate(singleHighlight.shipped_at)}
+                    {formatDistanceToNowStrict(new Date(singleHighlight.shipped_at), {
+                      addSuffix: true,
+                    })}
                   </span>
                   <DialogCloseButton onClick={() => router.push("/feed")} />
                 </div>
