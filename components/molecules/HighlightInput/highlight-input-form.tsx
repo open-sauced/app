2fb9ef76641cf3b8ee7c2ed8523595dacf55a664
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-05-18 16:54:36 +0300

    feat: limit length title in highlight (#1194)

--- components/molecules/HighlightInput/highlight-input-form.tsx
+++ components/molecules/HighlightInput/highlight-input-form.tsx
@@ -122,6 +122,7 @@ const HighlightInputForm = ({ refreshCallback }: HighlightInputFormProps): JSX.E
             <div className="flex pr-2">
               <input
                 value={title}
+                maxLength={50}
                 onChange={(e) => setTitle(e.target.value)}
                 className="flex-1 focus:outline-none"
                 type="text"
