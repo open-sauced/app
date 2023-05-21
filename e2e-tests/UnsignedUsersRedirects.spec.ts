ff8471b91b162405c326bfb3eba6009915521b01
takanome_dev <takanomedev221@gmail.com>
2023-05-21 19:33:03 +0000

    fix failed test

--- e2e-tests/UnsignedUsersRedirects.spec.ts
+++ e2e-tests/UnsignedUsersRedirects.spec.ts
@@ -7,5 +7,5 @@ test("Homepage redirects to /javascript for unsigned users", async ({ page }) =>
 
 test("Insights Hub redirects to /javascript for unsigned users", async ({ page }) => {
   await page.goto("/hub/insights");
-  await expect(page).toHaveURL(/.*\/javascript\/dashboard\/filter\/recent?redirectedFrom.*/, { timeout: 10000 });
+  await expect(page).toHaveURL("http://localhost:3000/javascript/dashboard/filter/recent?redirectedFrom=%2Fhub%2Finsights", { timeout: 10000 });
 });
