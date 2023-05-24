414fec2743c59639545e1b0ac5f521788b7ea68d
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-05-24 22:03:06 +0100

    chore: add collaboration global interface

--- next-types.d.ts
+++ next-types.d.ts
@@ -225,3 +225,15 @@ interface DbUserNotification {
   readonly user_id: number;
   readonly meta_id: string;
 }
+
+interface DbUserCollaboration {
+  readonly id: string;
+  readonly user_id: number;
+  readonly request_user_id: number;
+  readonly created_at: string;
+  readonly updated_at: string;
+  readonly deleted_at: string;
+  readonly request_emailed_at: string;
+  readonly collaboration_emailed_at: string;
+  readonly status: "pending" | "accepted" | "rejected";
+}
