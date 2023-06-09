9ef2780ea7fbfc170a8a2ead44f93fecd5777e56
Brandon Roberts <robertsbt@gmail.com>
2023-06-09 08:55:49 -0500

    fix: remove duplicate redirect check and use server props on insight page (#1255)

--- pages/pages/[userOrg]/[filterName]/[toolName].tsx
+++ pages/pages/[userOrg]/[filterName]/[toolName].tsx
@@ -1,69 +1,48 @@
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect } from "react";
 import { GetServerSidePropsContext } from "next";
-import { useRouter } from "next/router";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 import Tool from "components/organisms/ToolsDisplay/tools-display";
 
 import HubPageLayout from "layouts/hub-page";
 import { WithPageLayout } from "interfaces/with-page-layout";
 import changeCapitalization from "lib/utils/change-capitalization";
-import useInsight from "lib/hooks/useInsight";
-import { useEffect } from "react";
-import SpinLoader from "components/atoms/SpinLoader/spin-loader";
-import { useUser } from "@supabase/auth-helpers-react";
 
-const HubPage: WithPageLayout = () => {
-  const router = useRouter();
-  const { filterName, toolName } = router.query;
-  const insightId = filterName as string;
-  const { data: insight, isLoading, isError } = useInsight(insightId);
-  const repositories = insight ? insight.repos.map((repo) => repo.repo_id) : [];
+interface InsightPageProps {
+  insight: DbUserInsight;
+  pageName: string;
+}
 
-  const title = `${insight && insight.name} | Open Sauced Insights Hub`;
+const HubPage: WithPageLayout<InsightPageProps> = ({ insight, pageName }: InsightPageProps) => {
+  const repositories = insight.repos.map((repo) => repo.repo_id);
 
-  const user = useUser();
-
-  useEffect(() => {
-    if (!user && !insight?.is_public) router.replace("/javascript/dashboard/filter/recent");
-  },[user]);
+  const title = `${insight.name} | Open Sauced Insights Hub`;
 
   useEffect(() => {
     HubPage.updateSEO!({
-      title: title
+      title: title,
     });
   }, [title]);
 
-  return (
-    <>
-      {isLoading ? <SpinLoader /> : ""}
-      {isError ? <div>Error...</div> : ""}
-      {!isLoading && insight ? (
-        <Tool
-          tool={toolName ? changeCapitalization(toolName.toString(), true) : undefined}
-          repositories={repositories}
-        />
-      ) : (
-        <></>
-      )}
-    </>
-  );
+  return <Tool tool={changeCapitalization(pageName, true)} repositories={repositories} />;
 };
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const supabase = createServerSupabaseClient(ctx);
 
   const {
-    data: { session }
+    data: { session },
   } = await supabase.auth.getSession();
   const insightId = ctx.params!["filterName"] as string;
+  const pageName = ctx.params!["toolName"] as string;
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/insights/${insightId}`);
   const insight = response.ok ? ((await response.json()) as DbUserInsight) : null;
 
   if (!insight) {
     return {
       redirect: {
-        destination: "/"
-      }
+        destination: "/",
+      },
     };
   }
 
@@ -74,13 +53,16 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     return {
       redirect: {
         destination: "/",
-        permanent: false
-      }
+        permanent: false,
+      },
     };
   }
 
   return {
-    props: {}
+    props: {
+      insight,
+      pageName,
+    },
   };
 };
 
