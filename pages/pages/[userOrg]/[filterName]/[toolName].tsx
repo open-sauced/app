ec1f664729dc48a7e6eb1563d5aaf86436a7e9ed
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-06-12 15:31:55 +0100

    feat: add open-graph image to

--- pages/pages/[userOrg]/[filterName]/[toolName].tsx
+++ pages/pages/[userOrg]/[filterName]/[toolName].tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GetServerSidePropsContext } from "next";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
@@ -7,15 +7,21 @@ import Tool from "components/organisms/ToolsDisplay/tools-display";
 import HubPageLayout from "layouts/hub-page";
 import { WithPageLayout } from "interfaces/with-page-layout";
 import changeCapitalization from "lib/utils/change-capitalization";
+import SEO from "layouts/SEO/SEO";
 
 interface InsightPageProps {
   insight: DbUserInsight;
   pageName: string;
+  ogImage?: string;
 }
 
-const HubPage: WithPageLayout<InsightPageProps> = ({ insight, pageName }: InsightPageProps) => {
+const HubPage: WithPageLayout<InsightPageProps> = ({ insight, pageName, ogImage }: InsightPageProps) => {
   const repositories = insight.repos.map((repo) => repo.repo_id);
+  const [hydrated, setHydrated] = useState(false);
 
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
   const title = `${insight.name} | Open Sauced Insights Hub`;
 
   useEffect(() => {
@@ -24,7 +30,30 @@ const HubPage: WithPageLayout<InsightPageProps> = ({ insight, pageName }: Insigh
     });
   }, [title]);
 
-  return <Tool tool={changeCapitalization(pageName, true)} repositories={repositories} />;
+  if (!hydrated) {
+    return (
+      <>
+        <SEO
+          title={`${insight.name} | Open Sauced Insights Hub`}
+          description={`${insight.name} Insights page on OpenSauced`}
+          image={ogImage}
+          twitterCard="summary_large_image"
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <SEO
+        title={`${insight.name} | Open Sauced Insights Hub`}
+        description={`${insight.name} Insights page on OpenSauced`}
+        image={ogImage}
+        twitterCard="summary_large_image"
+      />
+      <Tool tool={changeCapitalization(pageName, true)} repositories={repositories} />
+    </>
+  );
 };
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
@@ -37,6 +66,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const pageName = ctx.params!["toolName"] as string;
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/insights/${insightId}`);
   const insight = response.ok ? ((await response.json()) as DbUserInsight) : null;
+  let ogImage;
 
   if (!insight) {
     return {
@@ -58,10 +88,33 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     };
   }
 
+  // Keeping this here so we are sure the page is not private before we fetch the social card.
+  async function fetchSocialCardURL() {
+    try {
+      const socialCardUrl = `${String(process.env.NEXT_PUBLIC_OPENGRAPH_URL ?? "")}/insights/${insightId}`;
+      const ogReq = await fetch(`${socialCardUrl}/metadata`); //status returned: 204 or 304 or 404
+      if (ogReq.status !== 204) {
+        fetch(socialCardUrl, {
+          method: "HEAD",
+        }); // trigger the generation of the social card
+      }
+      ogImage = ogReq.headers.get("x-amz-meta-location");
+      console.log(ogImage);
+    } catch (e) {
+      // This is to prevent the page from crashing if the social card is not generated for some reasons.
+      ogImage = "";
+      console.error(e);
+    }
+  }
+
+  // Runs the data fetching in parallel. Decreases the loading time by 50%.
+  await fetchSocialCardURL();
+
   return {
     props: {
       insight,
       pageName,
+      ogImage,
     },
   };
 };
