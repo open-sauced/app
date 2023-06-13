891e4c986cb7703b300c0a219aa4f710d2874df1
Brandon Roberts <robertsbt@gmail.com>
2023-06-12 21:00:49 -0500

    fix: add session check before redirecting in private wrapper component (#1261)
    Closes #1260

--- layouts/private-wrapper.tsx
+++ layouts/private-wrapper.tsx
@@ -1,19 +1,28 @@
 import { useUser } from "@supabase/auth-helpers-react";
+import { authSession } from "lib/hooks/authSession";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
 interface PrivateWrapperProps {
   isPrivateRoute?: boolean;
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const PrivateWrapper = ({ isPrivateRoute = false, children }: PrivateWrapperProps) => {
   const user = useUser();
   const router = useRouter();
-  
+
+  async function checkSession() {
+    const isValid = await authSession();
+
+    if (!isValid) {
+      router.replace("/javascript/dashboard/filter/recent");
+    }
+  }
+
   useEffect(() => {
-    if (isPrivateRoute) {
-      if (!user) router.replace("/javascript/dashboard/filter/recent");
+    if (isPrivateRoute && !user) {
+      checkSession();
     }
   }, [user]);
 
