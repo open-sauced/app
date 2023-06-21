8ffd4fa511784a4408d182ec9fe8f9ce358098a4
babblebey <babblebey@gmail.com>
2023-06-21 17:19:20 +0100

    feat: created ScrollArea components/atoms

---
+++ components/atoms/ScrollArea/scroll-area.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
+
+import clsx from "clsx";
+
+const ScrollArea = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
+  <ScrollAreaPrimitive.Root
+    ref={ref}
+    className={clsx(
+      "relative overflow-hidden", 
+      className
+    )}
+    {...props}
+  >
+    <ScrollViewport>
+      {children}
+    </ScrollViewport>
+  </ScrollAreaPrimitive.Root>
+));
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
+
+const ScrollViewport = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
+  <>
+    <ScrollAreaPrimitive.Viewport
+      ref={ref}
+      className={clsx(
+        "h-full w-full rounded-[inherit]",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </ScrollAreaPrimitive.Viewport>
+    <ScrollBar />
+    <ScrollAreaPrimitive.Corner />
+  </>
+));
+ScrollViewport.displayName = ScrollAreaPrimitive.Viewport.displayName;
+
+const ScrollBar = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Scrollbar>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Scrollbar>
+>(({ className, orientation = "vertical", ...props }, ref) => (
+  <ScrollAreaPrimitive.Scrollbar
+    ref={ref}
+    className={clsx(
+      "flex touch-none select-none p-[1px] transition-colors duration-[160ms] ease-out",
+      orientation === "vertical" &&
+        "h-full w-2 border-l border-l-transparent",
+      orientation === "horizontal" &&
+        "h-2 border-t border-t-transparent",
+      className
+    )}
+    orientation={orientation}
+    {...props}
+  >
+    <ScrollAreaPrimitive.Thumb className="relative bg-light-slate-5 flex-1 rounded-full bg-border" />
+  </ScrollAreaPrimitive.Scrollbar>
+));
+ScrollBar.displayName = ScrollAreaPrimitive.Scrollbar.displayName;
+
+export { ScrollArea, ScrollViewport, ScrollBar };
