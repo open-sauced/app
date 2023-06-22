8c3e1e812ae7b5939c6043810fec2964aab917a6
babblebey <babblebey@gmail.com>
2023-06-22 18:27:43 +0100

    fix: corrected file linting

--- stories/atoms/scroll-area.stories.tsx
+++ stories/atoms/scroll-area.stories.tsx
@@ -9,35 +9,34 @@ const storyConfig = {
 };
 
 export default storyConfig;
-
 const ScrollTemplate: ComponentStory<typeof ScrollArea> = (args) => (
-    <ScrollArea 
-        className="rounded-md border p-4"
-        {...args}
-    >
-        Jokester began sneaking into the castle in the middle of the night and leaving
-        jokes all over the place: under the king's pillow, in his soup, even in the
-        royal toilet. The king was furious, but he couldn't seem to stop Jokester. And
-        then, one day, the people of the kingdom discovered that the jokes left by
-        Jokester were so funny that they couldn't help but laugh. And once they
-        started laughing, they couldn't stop.
-    </ScrollArea>
+  <ScrollArea 
+    className="rounded-md border p-4"
+    {...args}
+  >
+    Jokester began sneaking into the castle in the middle of the night and leaving
+    jokes all over the place: under the king's pillow, in his soup, even in the
+    royal toilet. The king was furious, but he couldn't seem to stop Jokester. And
+    then, one day, the people of the kingdom discovered that the jokes left by
+    Jokester were so funny that they couldn't help but laugh. And once they
+    started laughing, they couldn't stop.
+  </ScrollArea>
 )
 
 export const Default = ScrollTemplate.bind({});
 
 Default.argTypes = {
-    type: {
-        options: ["auto", "always", "scroll", "hover"],
-        control: { type: "select" },
-    }
+  type: {
+    options: ["auto", "always", "scroll", "hover"],
+    control: { type: "select" },
+  }
 }
 Default.args = {
-    type: "auto",
-    style: {
-        height: "200px",
-        width: "350px"
-    }
+  type: "auto",
+  style: {
+    height: "200px",
+    width: "350px"
+  }
 }
 
 const SelectOptions = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
