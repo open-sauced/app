abcab5da07aafad4a169bfc4a7c2736d6e1df765
Brandon Roberts <robertsbt@gmail.com>
2023-06-26 16:27:11 -0500

    feat: upgrade Storybook to 7.0.x

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,15 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "storybook-addon-next",
-    {
-      // From stack-overflow answer: https://stackoverflow.com/a/68757745
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          // When you have splitted your css over multiple files
-          // and use @import('./other-styles.css')
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          // When using postCSS 8
-          implementation: require('postcss'),
-        },
-      },
-    },
+    "@storybook/addon-styling",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/nextjs",
+  options: {},
+  docs: {
+    autodocs: true,
+  },
+};
