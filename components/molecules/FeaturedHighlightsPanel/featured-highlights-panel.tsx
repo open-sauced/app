9d8501ea4832a5fbe27ad83da320d18bc67be87f
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-06-28 07:56:23 +0100

    misc: review changes

--- components/molecules/FeaturedHighlightsPanel/featured-highlights-panel.tsx
+++ components/molecules/FeaturedHighlightsPanel/featured-highlights-panel.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface FeaturedHighlightsPanelProps {
   highlightIds: { id: number; title: string }[];
 }
 const FeaturedHighlightsPanel = ({ highlightIds }: FeaturedHighlightsPanelProps) => {
+  const [host, setHost] = useState("");
+
+  useEffect(() => {
+    if (window !== undefined) {
+      setHost(window.location.origin as string);
+    }
+  }, []);
+
   return (
-    <div className="border max-w-[299px] rounded-lg p-5 space-y-4">
+    <div className="border max-w-[18.6rem] rounded-lg p-5 space-y-4">
       <h2 className="pb-2 text-lg border-b">Featured Highlights</h2>
       <div className="flex flex-col gap-4 text-sm">
         {highlightIds.map(({ id, title }) => (
-          <Link
-            className="transition hover:text-light-orange-10"
-            key={id}
-            href={`https://insights.opensauced.pizza/feed/${id}`}
-          >
+          <Link className="transition hover:text-light-orange-10" key={id} href={`${host}/feed/${id}`}>
             {title}
           </Link>
         ))}
