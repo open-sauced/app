47692340e7c75b3be5817e31af58c182efa19dfa
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-06-28 14:30:27 +0100

    feat: create newsletter form component story

--- components/molecules/NewsletterForm/newsletter-form.tsx
+++ components/molecules/NewsletterForm/newsletter-form.tsx
@@ -8,12 +8,18 @@ const NewsletterForm = () => {
     console.log("submitted");
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <TextInput type="email" placeholder="Email" />
-        <Button type="submit" variant="text">
-          Subscribe
-        </Button>
+    <div className="flex flex-col gap-3 p-4 border rounded-lg w-80">
+      <div className="space-y-1">
+        <h2 className="text-lg">Subscribe to our newsletter</h2>
+        <p className="text-sm text-light-slate-11">Stay up to date with the latest OpenSauced news and trends!</p>
+      </div>
+      <form className="w-full" onSubmit={handleSubmit}>
+        <div className="flex items-center gap-1 ">
+          <TextInput className="text-sm w-36 focus:outline-none" type="email" placeholder="Email" required />
+          <Button className="py-1 border-light-orange-7 text-light-orange-10" type="submit" variant="text">
+            Subscribe
+          </Button>
+        </div>
       </form>
     </div>
   );
