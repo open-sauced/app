90b5ac8af280f6d9565cef228b1a194c1e9d1e36
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-06-30 23:47:49 +0100

    refactor: add loading state to userCard

--- components/atoms/UserCard/user-card.tsx
+++ components/atoms/UserCard/user-card.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import { getAvatarByUsername } from "lib/utils/github";
+import { Spinner } from "../SpinLoader/spin-loader";
 
-type MetaObj = {
+export type MetaObj = {
   name: "Followers" | "Following" | "Highlights";
   count: number;
 };
@@ -10,47 +11,42 @@ interface UserCardProps {
   username: string;
   meta: MetaObj[];
   name: string;
+  loading?: boolean;
 }
-const UserCard = ({ username, name, meta }: UserCardProps) => {
+const UserCard = ({ username, name, meta, loading }: UserCardProps) => {
   const avatarUrl = getAvatarByUsername(username);
 
   return (
-    <div className="mt-10 bg-white border w-max rounded-2xl border-zinc-200">
-      <div className="flex flex-col items-center gap-6 px-6 -translate-y-12 ">
-        <div className="flex flex-col items-center gap-2">
-          <Image
-            className="border border-white rounded-full"
-            width={110}
-            height={110}
-            src={avatarUrl}
-            alt={`${username}'s avatar image`}
-          />
-          <div>
-            <h3 className="text-lg text-center">{name}</h3>
-            <p className="text-center text-light-slate-9">{`@${username}`}</p>
-          </div>
+    <div className="pb-6 bg-white border w-max rounded-2xl border-zinc-200">
+      {loading ? (
+        <div className="flex items-center justify-center h-32 w-72">
+          <Spinner className="mt-6 " />
         </div>
-        <div className="flex items-center gap-5 text-lg text-center">
-          {meta.map(({ name, count }, i) => (
-            <div key={i.toLocaleString()}>
-              <p className="text-center text-light-slate-9">{name}</p>
-              {count > 0 ? count : "-"}
+      ) : (
+        <div className="flex flex-col items-center gap-6 px-6 ">
+          <div className="flex flex-col items-center gap-2 -mt-10">
+            <Image
+              className="border border-white rounded-full "
+              width={100}
+              height={100}
+              src={avatarUrl}
+              alt={`${username}'s avatar image`}
+            />
+            <div>
+              <h3 className="text-base text-center">{name}</h3>
+              <p className="text-center text-light-slate-9">{`@${username}`}</p>
             </div>
-          ))}
-          {/* <div>
-            <p className="text-center text-light-slate-9">Followers</p>
-            {followersCount > 0 ? followersCount : "-"}
           </div>
-          <div>
-            <p className="text-center text-light-slate-9">Following</p>
-            {followingCount > 0 ? followingCount : "-"}
+          <div className="flex items-center gap-5 text-base text-center ">
+            {meta.map(({ name, count }, i) => (
+              <div key={i.toLocaleString()}>
+                <p className="text-center text-light-slate-9">{name}</p>
+                {count > 0 ? count : "-"}
+              </div>
+            ))}
           </div>
-          <div>
-            <p className="text-center text-light-slate-9">Highlights</p>
-            {highlightsCount > 0 ? highlightsCount : "-"}
-          </div> */}
         </div>
-      </div>
+      )}
     </div>
   );
 };
