c9859a020b4c919d2533e5c8e7ef2bc0505e72eb
babblebey <babblebey@gmail.com>
2023-07-04 15:08:14 +0100

    refactor: implemented Button(with Icon) component

--- components/molecules/AuthContentWrapper/auth-content-wrapper.tsx
+++ components/molecules/AuthContentWrapper/auth-content-wrapper.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import clsx from "clsx";
 
+import Button from "components/atoms/Button/button";
+import Icon from "components/atoms/Icon/icon";
+
+import GitHubIcon from "img/icons/github-icon.svg";
+
 interface AuthContentWrapperProps {
   isAuthenticated: boolean;
   children: React.ReactNode;
@@ -11,14 +16,14 @@ const AuthContentWrapper: React.FC<AuthContentWrapperProps> = ({ isAuthenticated
     <div className="relative">
       {!isAuthenticated && (
         <div className="absolute z-10 w-full h-full top-0 left-0 flex items-center justify-center">
-          <h1 className="text-2xl z-1">
-            Login Required
-          </h1>
+          <Button variant="primary" className="z-1 items-center px-2.5 py-1">
+            <Icon IconImage={GitHubIcon} className="mr-2" /> Connect with GitHub
+          </Button>
         </div>
       )}
       <div className={clsx(
         "relative",
-        !isAuthenticated && "blur",
+        !isAuthenticated && "blur-sm",
       )}>
         {children}
       </div>
