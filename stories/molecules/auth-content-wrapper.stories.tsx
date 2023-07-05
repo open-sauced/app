70a607dcc151f8aaca80c20fafff7152aa6f66b3
babblebey <babblebey@gmail.com>
2023-07-05 20:10:29 +0100

    doc: add story template for ReportsHistory component

--- stories/molecules/auth-content-wrapper.stories.tsx
+++ stories/molecules/auth-content-wrapper.stories.tsx
@@ -2,6 +2,9 @@ import { ComponentStory } from "@storybook/react";
 import AuthContentWrapper from "components/molecules/AuthContentWrapper/auth-content-wrapper";
 
 import Dashboard from "components/organisms/Dashboard/dashboard";
+import ReportsHistory from "components/molecules/ReportsHistory/reports-history";
+
+import type { Report } from "interfaces/report-type";
 
 const storyConfig = {
   title: "Design System/Molecules/Auth Content Wrapper"
@@ -16,3 +19,23 @@ const AuthContentWrapperTemplateWithDashboard: ComponentStory<typeof AuthContent
 );
 
 export const WithDashboard = AuthContentWrapperTemplateWithDashboard.bind({});
+
+const testReportList: Report[] = [
+  { reportName: "Top Ten", reportDate: "Jun 3, 2022", reportFormat: "CSV" },
+  { reportName: "Top Ten", reportDate: "Jun 3, 2022", reportFormat: "CSV" },
+  { reportName: "Top Ten", reportDate: "Jun 3, 2022", reportFormat: "CSV" },
+  { reportName: "Top Ten", reportDate: "Jun 3, 2022", reportFormat: "CSV" },
+  { reportName: "Top Ten", reportDate: "Jun 3, 2022", reportFormat: "CSV" }
+];
+
+const AuthContentWrapperTemplateWithReportsHistory: ComponentStory<typeof ReportsHistory> = (args) => (
+  <AuthContentWrapper>  
+    <ReportsHistory {...args}/>
+  </AuthContentWrapper>
+);
+
+export const WithReportsHistory = AuthContentWrapperTemplateWithReportsHistory.bind({});
+
+WithReportsHistory.args = {
+  reportList: testReportList
+};
