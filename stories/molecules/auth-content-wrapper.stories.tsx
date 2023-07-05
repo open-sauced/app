8542e2ffb0afb078a5e8ecc30ad849c827ceb6c5
babblebey <babblebey@gmail.com>
2023-07-05 20:20:51 +0100

    doc: add story template for Repositories component

--- stories/molecules/auth-content-wrapper.stories.tsx
+++ stories/molecules/auth-content-wrapper.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentStory } from "@storybook/react";
 import AuthContentWrapper from "components/molecules/AuthContentWrapper/auth-content-wrapper";
 
 import Dashboard from "components/organisms/Dashboard/dashboard";
+import Repositories from "components/organisms/Repositories/repositories";
 import ReportsHistory from "components/molecules/ReportsHistory/reports-history";
 
 import type { Report } from "interfaces/report-type";
@@ -19,6 +20,14 @@ const AuthContentWrapperTemplateWithDashboard: ComponentStory<typeof AuthContent
 );
 
 export const WithDashboard = AuthContentWrapperTemplateWithDashboard.bind({});
+
+const AuthContentWrapperTemplateWithRepositories: ComponentStory<typeof AuthContentWrapper> = (args) => (
+  <AuthContentWrapper {...args}>
+    <Repositories />
+  </AuthContentWrapper>
+);
+
+export const WithRepositories = AuthContentWrapperTemplateWithRepositories.bind({});
 
 const testReportList: Report[] = [
   { reportName: "Top Ten", reportDate: "Jun 3, 2022", reportFormat: "CSV" },
