9e193feae32739894051ec9d3236826cb00a1c88
Brandon Roberts <robertsbt@gmail.com>
2023-07-14 14:13:24 -0500

    fix: improve loading performance of individual highlight (#1349)

--- pages/feed/[id].tsx
+++ pages/feed/[id].tsx
@@ -1,7 +1,6 @@
 import { GetServerSidePropsContext } from "next";
 
-import fetchSocialCard from "lib/utils/fetch-social-card";
-import Feed from "../feed";
+import Feed from "./index";
 
 export default Feed;
 
@@ -13,10 +12,8 @@ export type HighlightSSRPropsContext = GetServerSidePropsContext<{ id: string }>
 
 export async function handleHighlightSSR({ params }: GetServerSidePropsContext<{ id: string }>) {
   const { id } = params!;
-  let highlight: DbHighlight | null = null;
-  let ogImage;
 
-  async function fetchUserData() {
+  async function fetchHighlight() {
     const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/highlights/${id}`, {
       headers: {
         accept: "application/json",
@@ -24,16 +21,15 @@ export async function handleHighlightSSR({ params }: GetServerSidePropsContext<{
     });
 
     if (req.ok) {
-      highlight = (await req.json()) as DbHighlight;
+      return (await req.json()) as DbHighlight;
     }
+
+    return null;
   }
 
-  // Runs the data fetching in parallel. Decreases the loading time by 50%.
-  const [, ogData] = await Promise.allSettled([fetchUserData(), fetchSocialCard(`highlights/${id}`)]);
-
-  ogImage = ogData.status === "fulfilled" ? ogData.value : "";
+  const highlight = await fetchHighlight();
 
   return {
-    props: { highlight, ogImage },
+    props: { highlight },
   };
 }
