d39e94fdb25c1b86ea57040737becdde6e38cb89
ir3ne <irene.tomaini@gmail.com>
2023-07-16 14:35:05 +0200

    feature: increase highlight characters limit

--- components/molecules/HighlightInput/highlight-input-form.tsx
+++ components/molecules/HighlightInput/highlight-input-form.tsx
@@ -111,7 +111,7 @@ const HighlightInputForm = ({ refreshCallback }: HighlightInputFormProps): JSX.E
             <div className="flex pr-2">
               <input
                 value={title}
-                maxLength={50}
+                maxLength={300}
                 onChange={(e) => setTitle(e.target.value)}
                 className="flex-1 focus:outline-none"
                 type="text"
