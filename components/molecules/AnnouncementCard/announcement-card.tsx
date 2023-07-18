3789ac7a3d16705a244b758e5805e305d05f05a8
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-07-18 19:44:28 +0100

    refactor: update component name and minor css rvmp

--- components/molecules/AnnouncementCard/announcement-card.tsx
+++ components/molecules/AnnouncementCard/announcement-card.tsx
@@ -1,19 +1,19 @@
 import * as AspectRatio from "@radix-ui/react-aspect-ratio";
 import React from "react";
 
-interface BillBoardProps {
+interface AnnouncementCardProps {
   title: string;
   description: string;
   bannerSrc: string;
-  repoLink: string;
+  url: string;
 }
-const BillBoard = ({ title, description, bannerSrc, repoLink }: BillBoardProps) => {
+const AnnouncementCard = ({ title, description, bannerSrc, url }: AnnouncementCardProps) => {
   return (
-    <div className="overflow-hidden rounded-lg max-w-[248px] border ">
+    <div className="overflow-hidden border bg-light-slate-1 rounded-xl">
       <div className="w-full p-0.5 ">
         <AspectRatio.Root ratio={1.85 / 1}>
           <picture>
-            <img src={bannerSrc} className="object-cover w-full h-full rounded-t-md" alt={`${title} banner image`} />
+            <img src={bannerSrc} className="object-cover w-full h-full rounded-t-xl" alt={`${title} banner image`} />
           </picture>
         </AspectRatio.Root>
       </div>
@@ -21,10 +21,10 @@ const BillBoard = ({ title, description, bannerSrc, repoLink }: BillBoardProps) 
         <h1>{title}</h1>
         <p className="font-light">{description}</p>
         <a
-          href={repoLink}
+          href={url}
           target="_blank"
           rel="noreferrer"
-          className="flex justify-center py-1.5 border rounded-lg border-light-orange-7 text-light-orange-10 "
+          className="flex justify-center py-1.5 border rounded-lg border-light-orange-7 text-light-orange-10 hover:border-light-orange-9 hover:border-2 hover:text-light-orange-10"
         >
           Learn more
         </a>
@@ -33,4 +33,4 @@ const BillBoard = ({ title, description, bannerSrc, repoLink }: BillBoardProps) 
   );
 };
 
-export default BillBoard;
+export default AnnouncementCard;
