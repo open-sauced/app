d0e7dae991f4320f0b767b39232b02499ce09b11
babblebey <babblebey@gmail.com>
2023-07-26 10:38:25 +0100

    refactor: modify `TypographyWrapper`comp. `size` prop implementation

--- components/atoms/Typography/wrapper.tsx
+++ components/atoms/Typography/wrapper.tsx
@@ -4,12 +4,12 @@ import clsx from "clsx";
 interface TypographyWrapperProps {
   children: React.ReactNode;
   className?: string;
-  size?: "sm" | "base" | "lg" | "xl" | "xxl";
+  size?: "sm" | "base" | "lg" | "xl" | "2xl";
 }
 
 const TypographyWrapper: React.FC<TypographyWrapperProps> = ({ children, className, size = "sm", ...props }) => {
   return (
-    <span className={clsx("prose", size !== "xxl" ? `prose-${size}` : "prose-2xl", className)} {...props}>
+    <span className={clsx("prose", `prose-${size}`, className)} {...props}>
       {children}
     </span>
   );
