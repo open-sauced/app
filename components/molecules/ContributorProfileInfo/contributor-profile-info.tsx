5331847e6ce78872ab8e37d4cf56a140462d1608
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-07-30 04:43:55 +0300

    Typos everywhere :3

--- components/molecules/ContributorProfileInfo/contributor-profile-info.tsx
+++ components/molecules/ContributorProfileInfo/contributor-profile-info.tsx
@@ -54,7 +54,7 @@ const ContributorProfileInfo = ({
           {isConnected && <Badge text="beta" />}
         </div>
         <div className="flex items-center text-sm gap-3">
-          <a className="text-light-slate-11 text-sm" href={`https://github.coom/${githubName}`}>{`@${githubName}`}</a>
+          <a className="text-light-slate-11 text-sm" href={`https://github.com/${githubName}`}>{`@${githubName}`}</a>
 
           {isConnected && (
             <>
