bc10b2d10e686f4b38e59ec5573ed6363ceaeee0
Jitto Joyes <jitto@qburst.com>
2023-08-01 16:02:33 +0530

    fix : onboarding button on mobile.

--- components/molecules/OnboardingButton/onboarding-button.tsx
+++ components/molecules/OnboardingButton/onboarding-button.tsx
@@ -5,7 +5,7 @@ import ProgressPie from "components/atoms/ProgressPie/progress-pie";
 
 const OnboardingButton: React.FC = () => {
   return (
-    <button className="flex items-center gap-2 border border-light-orange-8 py-1 px-1 pr-3 rounded-lg">
+    <button className="items-center gap-2 border border-light-orange-8 py-1 px-1 pr-3 rounded-lg hidden md:flex">
       <ProgressPie percentage={66} />
       <Text className="text-sm !text-light-slate-12  ">
         <Link href="/start">Complete the onboarding</Link>
