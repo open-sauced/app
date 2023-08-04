38cfb305f3abad32ffbac7acff3dbb9b08498b4e
Olabode Lawal-Shittabey <babblebey@gmail.com>
2023-08-04 18:18:49 +0100

    refactor: replace supabase/ui in design system typography components (#1438)

--- components/atoms/Typography/text.tsx
+++ components/atoms/Typography/text.tsx
@@ -1,18 +1,70 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
-import { Typography } from "@supabase/ui";
+import clsx from "clsx";
 
-const { Text: SupabaseTextComponent } = Typography;
+import TypographyWrapper from "./wrapper";
 
-interface TextProps extends React.ComponentProps<typeof SupabaseTextComponent> {
-  //Add additional prop definitions here
+interface TextProps {
+  children: React.ReactNode;
   title?: string;
+  className?: string;
+  type?: "default" | "secondary" | "success" | "warning" | "danger";
+  disabled?: boolean;
+  mark?: boolean;
+  code?: boolean;
+  keyboard?: boolean;
+  underline?: boolean;
+  strikethrough?: boolean;
+  strong?: boolean;
+  small?: boolean;
 }
 
-const Text: React.FC<TextProps> = (props) => {
+const Text: React.FC<TextProps> = ({
+  children,
+  title,
+  className,
+  type = "default",
+  disabled,
+  mark,
+  code,
+  keyboard,
+  underline,
+  strikethrough,
+  strong,
+  small,
+  ...props
+}) => {
+  const TextTag = (() => {
+    if (mark) return "mark";
+    if (code) return "code";
+    if (keyboard) return "kbd";
+    if (strong) return "strong";
+    if (small) return "small";
+    return "span";
+  })() as keyof JSX.IntrinsicElements;
+
+  const getTypeClass = (type: string) => {
+    if (type === "secondary") return "!text-light-slate-5";
+    if (type === "success") return "!text-dark-grass-10";
+    if (type === "warning") return "!text-yellow-500";
+    if (type === "danger") return "!text-dark-red-10";
+    return "";
+  };
+
   return (
-    <SupabaseTextComponent className={`${props.className && props.className}`} {...props}>
-      <span title={props.title}>{props.children}</span>
-    </SupabaseTextComponent>
+    <TypographyWrapper
+      className={clsx(
+        className,
+        disabled && "cursor-not-allowed select-none text-light-slate-11/50",
+        keyboard && "bg-light-slate-4/50 rounded px-2 border border-b-2 border-light-slate-8/50",
+        underline && "underline",
+        strikethrough && "line-through",
+        getTypeClass(type)
+      )}
+      {...props}
+    >
+      <TextTag title={title}>{children}</TextTag>
+    </TypographyWrapper>
   );
 };
 
