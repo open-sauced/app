5076a55fb34d8c9018d246d292aa1a47d1cca6aa
takanome_dev <takanomedev221@gmail.com>
2023-08-04 19:11:24 +0000

    add test for get notifications function

--- lib/utils/get-notification-url.ts
+++ lib/utils/get-notification-url.ts
@@ -1,15 +1,15 @@
 /**
  * Returns the URL for a notification based on its type and ID or username.
  * @param type - The type of the notification.
- * @param idOrUsername - The ID or username associated with the notification.
+ * @param id - The ID or username associated with the notification.
  * @returns The URL for the notification.
  */
-export const getNotificationURL = (type: string, idOrUsername: string) => {
+export const getNotificationURL = (type: string, id: string | number) => {
   switch (type) {
     case "highlight_reaction":
-      return `/feed/${idOrUsername}`;
+      return `/feed/${id}`;
     case "follow":
-      return `/user/${idOrUsername}`;
+      return `/user/${id}`;
     default:
       return "/";
   }
