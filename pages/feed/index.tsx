30f3e08e6a8ea2634226cde02b9a92f51c4e13af
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-08 22:33:56 +0100

    fix build error

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -183,183 +183,184 @@ const Feeds: WithPageLayout<HighlightSSRProps> = (props: HighlightSSRProps) => {
           )}
           <TopContributorsPanel loggedInUserLogin={loggedInUser?.login ?? ""} />
 
-        <div className="sticky top-8">
-          <div className="flex-col flex-1 xl:flex hidden gap-6 mt-12">
-            {user && (
-              <div className="md:w-1/2 lg:w-1/3">
-                <UserCard
-                  loading={loggedInUserLoading}
-                  username={loggedInUser?.login as string}
-                  meta={userMetaArray as MetaObj[]}
-                  name={loggedInUser?.name as string}
-                />
-              </div>
-            )}
-            <TopContributorsPanel loggedInUserLogin={loggedInUser?.login ?? ""} />
-            <AnnouncementCard
-              title="#100DaysOfOSS 🚀 "
-              description={
-                "Join us for 100 days of supporting, sharing knowledge, and exploring the open source ecosystem together."
-              }
-              bannerSrc={
-                "https://user-images.githubusercontent.com/5713670/254358937-8e9aa76d-4ed3-4616-a58a-2283796b10e1.png"
-              }
-              url={"https://dev.to/opensauced/100daysofoss-growing-skills-and-real-world-experience-3o5k"}
-            />
-          </div>
-        </div>
-        {singleHighlight && (
-          <Dialog
-            open={openSingleHighlight}
-            onOpenChange={(open) => {
-              if (openSingleHighlight && !open) {
-                if (props.referer !== null && !props.referer.includes("/feed")) {
-                  router.back();
-                } else {
-                  setOpenSingleHighlight(false);
-                  router.replace("/feed");
+          <div className="sticky top-8">
+            <div className="flex-col flex-1 xl:flex hidden gap-6 mt-12">
+              {user && (
+                <div className="md:w-1/2 lg:w-1/3">
+                  <UserCard
+                    loading={loggedInUserLoading}
+                    username={loggedInUser?.login as string}
+                    meta={userMetaArray as MetaObj[]}
+                    name={loggedInUser?.name as string}
+                  />
+                </div>
+              )}
+              <TopContributorsPanel loggedInUserLogin={loggedInUser?.login ?? ""} />
+              <AnnouncementCard
+                title="#100DaysOfOSS 🚀 "
+                description={
+                  "Join us for 100 days of supporting, sharing knowledge, and exploring the open source ecosystem together."
                 }
-              }
-            }}
-          >
-            <DialogContent className=" sm:max-w-[80%] w-full  sm:max-h-screen ">
-              <div className="flex flex-col gap-8 mx-auto mt-10">
-                <div className="flex flex-col gap-6 px-3 ">
-                  <div className="flex items-center gap-3 ">
-                    <Link href={`/user/${singleHighlight.login}`} className="flex items-center gap-3">
-                      <Avatar
-                        alt="user profile avatar"
-                        isCircle
-                        size="sm"
-                        avatarURL={`https://www.github.com/${singleHighlight.login}.png?size=300`}
-                      />
-                      <strong>{singleHighlight.login}</strong>
-                    </Link>
-                    <span className="text-xs font-normal text-light-slate-11">
-                      {formatDistanceToNowStrict(new Date(singleHighlight.created_at), {
-                        addSuffix: true,
-                      })}
-                    </span>
-                    {props.referer !== null && !props.referer.includes("/feed") ? (
-                      <Button
-                        variant="text"
-                        onClick={() => router.back()}
-                        className="!p-0 !border-0 absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100"
-                      >
-                        <AiOutlineClose size={20} />
-                        <span className="sr-only">Close</span>
-                      </Button>
-                    ) : (
-                      <DialogCloseButton onClick={() => router.replace("/feed")} />
-                    )}
-                  </div>
-
-                  <div className="w-full px-2 py-6 border bg-light-slate-1 md:px-6 lg:px-12 rounded-xl">
-                    <ContributorHighlightCard
-                      emojis={emojis}
-                      title={singleHighlight.title}
-                      desc={singleHighlight.highlight}
-                      prLink={singleHighlight.url}
-                      user={singleHighlight.login}
-                      id={singleHighlight.id}
-                      shipped_date={singleHighlight.shipped_at}
-                      refreshCallBack={mutate}
-                    />
-                  </div>
-                </div>
-              </div>
-            </DialogContent>
-          </Dialog>
-        )}
-        <Tabs
-          onValueChange={(value) => {
-            setActiveTab(value as activeTabType);
-          }}
-          defaultValue="home"
-          className="grow"
-        >
-          <TabsList className={clsx("justify-start  w-full border-b", !user && "hidden")}>
-            <TabsTrigger
-              className="data-[state=active]:border-sauced-orange data-[state=active]:border-b-2 text-2xl"
-              value="home"
-            >
-              Home
-            </TabsTrigger>
-            <TabsTrigger
-              className="data-[state=active]:border-sauced-orange  data-[state=active]:border-b-2 text-2xl"
-              value="following"
-            >
-              Following
-            </TabsTrigger>
-          </TabsList>
-
-          {user && (
-            <div className="lg:gap-x-3 px-3 pt-4 flex max-w-[48rem]">
-              <div className="hidden lg:inline-flex pt-[0.4rem]">
-                <Avatar
-                  alt="user profile avatar"
-                  isCircle
-                  size="sm"
-                  avatarURL={`https://www.github.com/${user?.user_metadata.user_name}.png?size=300`}
-                />
-              </div>
-
-              <HighlightInputForm refreshCallback={mutate} />
+                bannerSrc={
+                  "https://user-images.githubusercontent.com/5713670/254358937-8e9aa76d-4ed3-4616-a58a-2283796b10e1.png"
+                }
+                url={"https://dev.to/opensauced/100daysofoss-growing-skills-and-real-world-experience-3o5k"}
+              />
             </div>
-          )}
-          <TabsContent value="home">
-            <HomeHighlightsWrapper emojis={emojis} mutate={mutate} highlights={data} loading={isLoading} />
-            {meta.pageCount > 1 && (
-              <div className="mt-10 max-w-[48rem] flex px-2 items-center justify-between">
-                <div className="flex items-center w-max gap-x-4">
-                  <PaginationResults metaInfo={meta} total={meta.itemCount} entity={"highlights"} />
-                </div>
-                <Pagination
-                  pages={[]}
-                  totalPage={meta.pageCount}
-                  page={meta.page}
-                  pageSize={meta.itemCount}
-                  goToPage
-                  hasNextPage={meta.hasNextPage}
-                  hasPreviousPage={meta.hasPreviousPage}
-                  onPageChange={function (page: number): void {
-                    setPage(page);
-                    if (topRef.current) {
-                      topRef.current.scrollIntoView({
-                        behavior: "smooth",
-                      });
-                    }
-                  }}
-                />
-              </div>
-            )}
-          </TabsContent>
-          <TabsContent value="following">
-            <FollowingHighlightWrapper selectedFilter={selectedRepo} emojis={emojis} />
-          </TabsContent>
-        </Tabs>
-        <div className="hidden lg:w-[24%] w-[30%] gap-6 mt-10 flex-none lg:flex flex-col">
-          {repoList && repoList.length > 0 && (
-            <HighlightsFilterCard
-              selectedFilter={selectedRepo}
-              setSelected={(repo) => {
-                if (!openSingleHighlight) {
-                  const queryParams = new URLSearchParams(window.location.search);
-                  router.push(
-                    `/feed${repo ? `?repo=${repo}` : queryParams.toString() ? `?${queryParams.toString()}` : ""}`
-                  );
-                  setPage(1);
-                  setSelectedRepo(repo);
+          </div>
+          {singleHighlight && (
+            <Dialog
+              open={openSingleHighlight}
+              onOpenChange={(open) => {
+                if (openSingleHighlight && !open) {
+                  if (props.referer !== null && !props.referer.includes("/feed")) {
+                    router.back();
+                  } else {
+                    setOpenSingleHighlight(false);
+                    router.replace("/feed");
+                  }
                 }
               }}
-              repos={repoList}
-            />
-          )}
+            >
+              <DialogContent className=" sm:max-w-[80%] w-full  sm:max-h-screen ">
+                <div className="flex flex-col gap-8 mx-auto mt-10">
+                  <div className="flex flex-col gap-6 px-3 ">
+                    <div className="flex items-center gap-3 ">
+                      <Link href={`/user/${singleHighlight.login}`} className="flex items-center gap-3">
+                        <Avatar
+                          alt="user profile avatar"
+                          isCircle
+                          size="sm"
+                          avatarURL={`https://www.github.com/${singleHighlight.login}.png?size=300`}
+                        />
+                        <strong>{singleHighlight.login}</strong>
+                      </Link>
+                      <span className="text-xs font-normal text-light-slate-11">
+                        {formatDistanceToNowStrict(new Date(singleHighlight.created_at), {
+                          addSuffix: true,
+                        })}
+                      </span>
+                      {props.referer !== null && !props.referer.includes("/feed") ? (
+                        <Button
+                          variant="text"
+                          onClick={() => router.back()}
+                          className="!p-0 !border-0 absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100"
+                        >
+                          <AiOutlineClose size={20} />
+                          <span className="sr-only">Close</span>
+                        </Button>
+                      ) : (
+                        <DialogCloseButton onClick={() => router.replace("/feed")} />
+                      )}
+                    </div>
 
-          {featuredHighlights && featuredHighlights.length > 0 && (
-            <FeaturedHighlightsPanel highlights={featuredHighlights} />
+                    <div className="w-full px-2 py-6 border bg-light-slate-1 md:px-6 lg:px-12 rounded-xl">
+                      <ContributorHighlightCard
+                        emojis={emojis}
+                        title={singleHighlight.title}
+                        desc={singleHighlight.highlight}
+                        prLink={singleHighlight.url}
+                        user={singleHighlight.login}
+                        id={singleHighlight.id}
+                        shipped_date={singleHighlight.shipped_at}
+                        refreshCallBack={mutate}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
           )}
-          <NewsletterForm />
+          <Tabs
+            onValueChange={(value) => {
+              setActiveTab(value as activeTabType);
+            }}
+            defaultValue="home"
+            className="grow"
+          >
+            <TabsList className={clsx("justify-start  w-full border-b", !user && "hidden")}>
+              <TabsTrigger
+                className="data-[state=active]:border-sauced-orange data-[state=active]:border-b-2 text-2xl"
+                value="home"
+              >
+                Home
+              </TabsTrigger>
+              <TabsTrigger
+                className="data-[state=active]:border-sauced-orange  data-[state=active]:border-b-2 text-2xl"
+                value="following"
+              >
+                Following
+              </TabsTrigger>
+            </TabsList>
+
+            {user && (
+              <div className="lg:gap-x-3 px-3 pt-4 flex max-w-[48rem]">
+                <div className="hidden lg:inline-flex pt-[0.4rem]">
+                  <Avatar
+                    alt="user profile avatar"
+                    isCircle
+                    size="sm"
+                    avatarURL={`https://www.github.com/${user?.user_metadata.user_name}.png?size=300`}
+                  />
+                </div>
+
+                <HighlightInputForm refreshCallback={mutate} />
+              </div>
+            )}
+            <TabsContent value="home">
+              <HomeHighlightsWrapper emojis={emojis} mutate={mutate} highlights={data} loading={isLoading} />
+              {meta.pageCount > 1 && (
+                <div className="mt-10 max-w-[48rem] flex px-2 items-center justify-between">
+                  <div className="flex items-center w-max gap-x-4">
+                    <PaginationResults metaInfo={meta} total={meta.itemCount} entity={"highlights"} />
+                  </div>
+                  <Pagination
+                    pages={[]}
+                    totalPage={meta.pageCount}
+                    page={meta.page}
+                    pageSize={meta.itemCount}
+                    goToPage
+                    hasNextPage={meta.hasNextPage}
+                    hasPreviousPage={meta.hasPreviousPage}
+                    onPageChange={function (page: number): void {
+                      setPage(page);
+                      if (topRef.current) {
+                        topRef.current.scrollIntoView({
+                          behavior: "smooth",
+                        });
+                      }
+                    }}
+                  />
+                </div>
+              )}
+            </TabsContent>
+            <TabsContent value="following">
+              <FollowingHighlightWrapper selectedFilter={selectedRepo} emojis={emojis} />
+            </TabsContent>
+          </Tabs>
+          <div className="hidden lg:w-[24%] w-[30%] gap-6 mt-10 flex-none lg:flex flex-col">
+            {repoList && repoList.length > 0 && (
+              <HighlightsFilterCard
+                selectedFilter={selectedRepo}
+                setSelected={(repo) => {
+                  if (!openSingleHighlight) {
+                    const queryParams = new URLSearchParams(window.location.search);
+                    router.push(
+                      `/feed${repo ? `?repo=${repo}` : queryParams.toString() ? `?${queryParams.toString()}` : ""}`
+                    );
+                    setPage(1);
+                    setSelectedRepo(repo);
+                  }
+                }}
+                repos={repoList}
+              />
+            )}
+
+            {featuredHighlights && featuredHighlights.length > 0 && (
+              <FeaturedHighlightsPanel highlights={featuredHighlights} />
+            )}
+            <NewsletterForm />
+          </div>
         </div>
       </div>
     </>
