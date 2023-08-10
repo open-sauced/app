49e66dbd7f711cfede94bfae95cb7433d654f9f7
Deepak Paul <rudra.ad7@gmail.com>
2023-08-10 21:30:39 +0530

    fix: disabled the button while it's loading, so it doesn't add the same repo again

--- components/molecules/SuggestedRepo/suggested-repo.tsx
+++ components/molecules/SuggestedRepo/suggested-repo.tsx
@@ -4,12 +4,27 @@ import RepoCardProfile, { RepoCardProfileProps } from "../RepoCardProfile/repo-c
 interface SuggestedRopsitoryProps {
   data: RepoCardProfileProps;
   onAddRepo?: (repo: string) => void;
+  loadingData?: {
+    repoName: string;
+    isLoading: boolean;
+    isAddedFromCart: boolean;
+  };
 }
-const SuggestedRepository = ({ data, onAddRepo }: SuggestedRopsitoryProps) => {
+const SuggestedRepository = ({ data, onAddRepo, loadingData }: SuggestedRopsitoryProps) => {
   return (
     <div className="flex items-center justify-between">
       <RepoCardProfile {...data} />
       <Button
+        disabled={
+          `${data.orgName}/${data.repoName}` === loadingData?.repoName &&
+          loadingData?.isLoading &&
+          !loadingData?.isAddedFromCart
+        }
+        loading={
+          `${data.orgName}/${data.repoName}` === loadingData?.repoName &&
+          loadingData?.isLoading &&
+          !loadingData?.isAddedFromCart
+        }
         variant="text"
         className="border border-light-slate-6 shadow-input"
         onClick={() => onAddRepo && onAddRepo(`${data.orgName}/${data.repoName}`)}
