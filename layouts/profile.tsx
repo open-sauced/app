b07f9f661acb44fefc3dd19b2ad554ae25ce842e
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-10 18:47:54 +0100

    Update profile.tsx file

--- layouts/profile.tsx
+++ layouts/profile.tsx
@@ -8,7 +8,7 @@ const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex flex-col min-h-screen">
       <TopNav />
       <div className="page-container flex grow flex-col items-center">
-        <main className="pb-16 w-full flex-1 flex-col items-center bg-light-slate-2">{children}</main>
+        <main className="flex pb-16 w-full flex-1 flex-col items-center bg-light-slate-2">{children}</main>
       </div>
       <Footer />
     </div>
