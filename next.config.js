bf8c123e87e585c4b85de9e25ad2e70b8e053d1f
Chris Schlensker <chris@foxyblocks.com>
2023-08-11 18:27:35 -0700

    TEMP: turn on production sourcemaps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   images: {
     domains: [
       "avatars.githubusercontent.com",
