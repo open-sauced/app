00262c7fdbcc2377db2c6f3bab5e7cb18786b4a3
Chris Schlensker <chris@foxyblocks.com>
2023-08-14 14:11:17 -0700

    Remove changes to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,35 @@
-// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-// import { pathToRegexp } from "path-to-regexp";
-
-// HACK: this is to get around the fact that the normal next.js middleware is not always functioning
-// correctly.
-// see https://github.com/open-sauced/insights/pull/1549
-// prettier-ignore
-const pathsToMatch = [
-  "/hub/insights/:path*",
-  "/feed/",
-  "/user/notifications",
-  "/user/settings",
-];
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  return res;
+  // Create authenticated Supabase Client.
+  const supabase = createMiddlewareClient({ req, res });
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  // if (!pathsToMatch.some((matcher) => pathToRegexp(matcher).test(req.nextUrl.pathname))) {
-  //   return res;
-  // }
+  // Check auth condition
+  if (session?.user || req.nextUrl.searchParams.has("login")) {
+    // Authentication successful, forward request to protected route.
+    return res;
+  }
 
-  // // Create authenticated Supabase Client.
-  // const supabase = createMiddlewareClient({ req, res });
-  // // Check if we have a session
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
+  // Auth condition not met, redirect to home page.
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = "/feed";
+  if (req.nextUrl.pathname === "/feed" && req.nextUrl.searchParams.has("new")) {
+    redirectUrl.searchParams.set("signIn", "true");
+  }
+  redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
 
-  // // Check auth condition
-  // if (session?.user || req.nextUrl.searchParams.has("login")) {
-  //   // Authentication successful, forward request to protected route.
-  //   return res;
-  // }
-
-  // // Auth condition not met, redirect to home page.
-  // const redirectUrl = req.nextUrl.clone();
-  // redirectUrl.pathname = "/feed";
-  // if (req.nextUrl.pathname === "/feed" && req.nextUrl.searchParams.has("new")) {
-  //   redirectUrl.searchParams.set("signIn", "true");
-  // }
-  // redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
-
-  // if (!req.nextUrl.searchParams.has("redirectedFrom")) {
-  //   return NextResponse.redirect(redirectUrl);
-  // }
+  if (!req.nextUrl.searchParams.has("redirectedFrom")) {
+    return NextResponse.redirect(redirectUrl);
+  }
 }
+
+export const config = {
+  matcher: ["/hub/insights/:path*", "/user/:path", "/feed/"],
+};
