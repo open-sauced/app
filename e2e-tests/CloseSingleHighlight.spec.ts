a0ac2bcdf7d32f0c1ce37c5fcd1cab29b2d1ea68
babblebey <babblebey@gmail.com>
2023-08-15 02:47:18 +0100

    test: add test for case on click via `/feed` page; improve existing cases

--- e2e-tests/CloseSingleHighlight.spec.ts
+++ e2e-tests/CloseSingleHighlight.spec.ts
@@ -6,11 +6,31 @@ test("Close Single Highlight Opened from 'null' referer", async ({ page }) => {
 
   const dialog = page.getByRole("dialog");
   await expect(dialog).toBeVisible();
+  await expect(page).toHaveTitle(/Highlight | OpenSauced/);
 
   const closeBtn = page.getByRole("button", { name: "Close" });
   if (await closeBtn.isVisible()) await closeBtn.click();
 
   await expect(dialog).toBeHidden();
+  await expect(page).toHaveTitle(/Highlights | OpenSauced/);
+  await expect(page).toHaveURL(/\/feed/, { timeout: 10000 });
+});
+
+test("Close Single Highlight Opened from 'Feed Page' as referer", async ({ page }) => {
+  await page.goto("/feed", { waitUntil: "domcontentloaded" });
+
+  const singleHighlight = await page.waitForSelector(`a[href^="${/^\/feed\/\d+$/.source}"]`);
+  if (singleHighlight) await singleHighlight.click();
+
+  const dialog = page.getByRole("dialog");
+  await expect(dialog).toBeVisible();
+  await expect(page).toHaveTitle(/Highlight | OpenSauced/);
+
+  const closeBtn = page.getByRole("button", { name: "Close" });
+  if (await closeBtn.isVisible()) await closeBtn.click();
+
+  await expect(dialog).toBeHidden();
+  await expect(page).toHaveTitle(/Highlights | OpenSauced/);
   await expect(page).toHaveURL(/\/feed/, { timeout: 10000 });
 });
 
@@ -22,10 +42,9 @@ test("Close Single Highlight Opened from 'User Profile' as referer", async ({ pa
   const singleHighlight = await page.waitForSelector(`a[href="${highlightUrl}"]`);
   if (singleHighlight) await singleHighlight.click();
 
-  await expect(page).toHaveURL(/\/feed\/183/);
-
   const dialog = page.getByRole("dialog");
   await expect(dialog).toBeVisible();
+  await expect(page).toHaveURL(/\/feed\/183/);
 
   const closeBtn = page.getByRole("button", { name: "Close" });
   if (await closeBtn.isVisible()) await closeBtn.click();
