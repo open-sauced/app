e52a950e73e4005d84cb6f6de604b4e084af3257
Chris Schlensker <chris@foxyblocks.com>
2023-08-15 11:04:40 -0700

    Restore middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-// import { pathToRegexp } from "path-to-regexp";
+import { pathToRegexp } from "path-to-regexp";
 
 // HACK: this is to get around the fact that the normal next.js middleware is not always functioning
 // correctly.
@@ -16,34 +16,33 @@ const pathsToMatch = [
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  return res;
 
-  // if (!pathsToMatch.some((matcher) => pathToRegexp(matcher).test(req.nextUrl.pathname))) {
-  //   return res;
-  // }
+  if (!pathsToMatch.some((matcher) => pathToRegexp(matcher).test(req.nextUrl.pathname))) {
+    return res;
+  }
 
-  // // Create authenticated Supabase Client.
-  // const supabase = createMiddlewareClient({ req, res });
-  // // Check if we have a session
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
+  // Create authenticated Supabase Client.
+  const supabase = createMiddlewareClient({ req, res });
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  // // Check auth condition
-  // if (session?.user || req.nextUrl.searchParams.has("login")) {
-  //   // Authentication successful, forward request to protected route.
-  //   return res;
-  // }
+  // Check auth condition
+  if (session?.user || req.nextUrl.searchParams.has("login")) {
+    // Authentication successful, forward request to protected route.
+    return res;
+  }
 
-  // // Auth condition not met, redirect to home page.
-  // const redirectUrl = req.nextUrl.clone();
-  // redirectUrl.pathname = "/feed";
-  // if (req.nextUrl.pathname === "/feed" && req.nextUrl.searchParams.has("new")) {
-  //   redirectUrl.searchParams.set("signIn", "true");
-  // }
-  // redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
+  // Auth condition not met, redirect to home page.
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = "/feed";
+  if (req.nextUrl.pathname === "/feed" && req.nextUrl.searchParams.has("new")) {
+    redirectUrl.searchParams.set("signIn", "true");
+  }
+  redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
 
-  // if (!req.nextUrl.searchParams.has("redirectedFrom")) {
-  //   return NextResponse.redirect(redirectUrl);
-  // }
+  if (!req.nextUrl.searchParams.has("redirectedFrom")) {
+    return NextResponse.redirect(redirectUrl);
+  }
 }
