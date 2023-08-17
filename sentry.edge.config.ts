3a425a5a519ed1ea49b8a8b92fafa74ef69ad52b
Brandon Roberts <robertsbt@gmail.com>
2023-08-17 09:04:11 -0500

    chore: adjust sampling rates for Sentry (#1564)

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
