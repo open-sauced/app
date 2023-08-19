8b04a8e8c1e6128853efd15f625a0c82dc090491
babblebey <babblebey@gmail.com>
2023-08-19 21:26:16 +0100

    test: modify locator for `img` related assertions

--- e2e-tests/OpenUserProfile.spec.ts
+++ e2e-tests/OpenUserProfile.spec.ts
@@ -6,8 +6,8 @@ test("Open a User (with GitHub Account Connected) Profile", async ({ page }) => 
   await expect(page).toHaveURL(/\/user\/babblebey/, { timeout: 10000 });
   await expect(page).toHaveTitle(/babblebey | OpenSauced/);
   await expect(page.getByRole("heading", { name: "babblebey" })).toBeVisible();
-  await expect(page.getByAltText("Avatar")).toBeVisible();
-  await expect(page.getByAltText("user profile cover image")).toBeVisible();
+  await expect(page.getByRole("img", { name: "Avatar" })).toBeVisible();
+  await expect(page.getByRole("img", { name: "user profile cover image" })).toBeVisible();
   await expect(page.getByRole("link", { name: "Get Card" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Share" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Contributions" })).toBeVisible();
@@ -19,8 +19,8 @@ test("Open a User (without Github Account Connected) Profile", async ({ page }) 
   await expect(page).toHaveURL("/user/githubuser", { timeout: 10000 });
   await expect(page).toHaveTitle(/githubuser | OpenSauced/);
   await expect(page.getByRole("heading", { name: "githubuser" })).toBeVisible();
-  await expect(page.getByAltText("Avatar")).toBeHidden();
-  await expect(page.getByAltText("user profile cover image")).toBeHidden();
+  await expect(page.getByRole("img", { name: "Avatar" })).toBeHidden();
+  await expect(page.getByRole("img", { name: "user profile cover image" })).toBeHidden();
   await expect(page.getByRole("link", { name: "Get Card" })).toBeHidden();
   await expect(page.getByRole("button", { name: "Share" })).toBeHidden();
   await expect(page.getByRole("button", { name: "Contributions" })).toBeHidden();
