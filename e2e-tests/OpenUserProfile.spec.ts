46fe710f213167e163c2fa691260067748a0d678
babblebey <babblebey@gmail.com>
2023-08-20 20:00:04 +0100

    test: modify locator for profile tab

--- e2e-tests/OpenUserProfile.spec.ts
+++ e2e-tests/OpenUserProfile.spec.ts
@@ -10,7 +10,7 @@ test("Open a User (with GitHub Account Connected) Profile", async ({ page }) => 
   await expect(page.getByRole("img", { name: "user profile cover image" })).toBeVisible();
   await expect(page.getByRole("link", { name: "Get Card" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Share" })).toBeVisible();
-  await expect(page.getByRole("button", { name: "Contributions" })).toBeVisible();
+  await expect(page.getByRole("tab")).toBeVisible();
 });
 
 test("Open a User (without Github Account Connected) Profile", async ({ page }) => {
@@ -23,6 +23,6 @@ test("Open a User (without Github Account Connected) Profile", async ({ page }) 
   await expect(page.getByRole("img", { name: "user profile cover image" })).toBeHidden();
   await expect(page.getByRole("link", { name: "Get Card" })).toBeHidden();
   await expect(page.getByRole("button", { name: "Share" })).toBeHidden();
-  await expect(page.getByRole("button", { name: "Contributions" })).toBeHidden();
+  await expect(page.getByRole("tab")).toBeHidden();
   await expect(page.getByRole("heading", { name: "Contribution Insights" })).toBeVisible();
 });
