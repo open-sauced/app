16e0f73f594c5c771bf4cb586a2077ef6e91e61f
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-08-29 18:01:12 +0100

    fix: empty user profile display

--- components/organisms/ContributorProfileTab/contributor-profile-tab.tsx
+++ components/organisms/ContributorProfileTab/contributor-profile-tab.tsx
@@ -66,16 +66,20 @@ const ContributorProfileTab = ({
 }: ContributorProfileTabProps): JSX.Element => {
   const { login, interests: userInterests, receive_collaboration } = contributor || {};
   const { user } = useSupabaseAuth();
+  const { user_name } = user?.user_metadata || {};
 
   const { data: highlights, isError, isLoading, mutate, meta, setPage } = useFetchUserHighlights(login || "");
   const { data: emojis } = useFetchAllEmojis();
 
   const router = useRouter();
+  const { tab } = router.query;
 
   const hasHighlights = highlights ? highlights.length > 0 : false;
   const [inputVisible, setInputVisible] = useState(false);
 
-  const currentTab = (router.query.tab || "highlights") as TabKey;
+  const currentTab = (
+    (Object.keys(tabs).includes(tab as string) && tab) || hasHighlights ? "highlights" : "contributions"
+  ) as TabKey;
   function onTabChange(value: string) {
     const tabValue = value as TabKey;
     setQueryParams({ tab: tabValue } satisfies QueryParams);
@@ -83,30 +87,74 @@ const ContributorProfileTab = ({
 
   // Setting the query param "tab" if none exists
   useEffect(() => {
-    if (currentTab === "highlights" && !router.query.tab) {
-      setQueryParams({ tab: "highlights" } satisfies QueryParams);
+    if (!tab || !Object.keys(tabs).includes(tab as string)) {
+      setQueryParams({ tab: currentTab } satisfies QueryParams);
     }
-  }, [currentTab, router.query.tab]);
+
+    // If the user is not the owner of the profile, block them from accessing the recommendations tab
+    if (tab === "recommendations" && user_name !== login) {
+      setQueryParams({ tab: currentTab } satisfies QueryParams);
+    }
+  }, [tab, currentTab]);
+
+  const getTabTriggerClassName = (tab: TabKey): string => {
+    return clsx(
+      "data-[state=active]:border-sauced-orange shrink-0 data-[state=active]:border-b-2 text-lg",
+      tab === "recommendations" &&
+        "font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#EA4600] to-[#EB9B00]",
+      // user?.user_metadata.user_name !== login && !hasHighlights && tab === "highlights" && "hidden",
+      user && user_name !== login && tab === "recommendations" && "hidden",
+      user && user_name !== login && tab === "requests" && "hidden",
+      !user && tab === "recommendations" && "hidden",
+      !user && tab === "requests" && "hidden",
+      !receive_collaboration && tab === "requests" && "hidden"
+    );
+  };
+
+  const getEmptyHighlightPreset = (): JSX.Element => {
+    switch (user_name === login) {
+      case true:
+        return (
+          <DashContainer>
+            <div className="text-center">
+              <p>
+                You don&apos;t have any highlights yet! <br /> Highlights are a great way to show off your
+                contributions. Merge any pull requests recently?
+              </p>
+              {!inputVisible && (
+                <Button onClick={() => setInputVisible(true)} className="mt-5" variant="primary">
+                  Add a highlight
+                </Button>
+              )}
+            </div>
+          </DashContainer>
+        );
+      case false:
+        <DashContainer>
+          <div className="text-center">
+            <p>
+              <b>{login}</b> doesn&apos;t have any highlights yet!
+            </p>
+          </div>
+        </DashContainer>;
+      default:
+        return (
+          <DashContainer>
+            <div className="text-center">
+              <p>
+                <b>{login}</b> doesn&apos;t have any highlights yet!
+              </p>
+            </div>
+          </DashContainer>
+        );
+    }
+  };
 
   return (
-    <Tabs value={currentTab} onValueChange={onTabChange}>
+    <Tabs value={String(router.query.tab)} onValueChange={onTabChange}>
       <TabsList className="justify-start w-full overflow-x-auto border-b">
         {(Object.keys(tabs) as TabKey[]).map((tab) => (
-          <TabsTrigger
-            key={tab}
-            className={clsx(
-              "data-[state=active]:border-sauced-orange shrink-0 data-[state=active]:border-b-2 text-lg",
-              tab === "recommendations" &&
-                "font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#EA4600] to-[#EB9B00]",
-              user?.user_metadata.user_name !== login && !hasHighlights && tab === "highlights" && "hidden",
-              user && user.user_metadata.user_name !== login && tab === "recommendations" && "hidden",
-              user && user.user_metadata.user_name !== login && tab === "requests" && "hidden",
-              !user && tab === "recommendations" && "hidden",
-              !user && tab === "requests" && "hidden",
-              !receive_collaboration && tab === "requests" && "hidden"
-            )}
-            value={tab}
-          >
+          <TabsTrigger key={tab} className={getTabTriggerClassName(tab)} value={tab}>
             {tabs[tab]}
           </TabsTrigger>
         ))}
@@ -114,103 +162,89 @@ const ContributorProfileTab = ({
 
       {/* Highlights Tab details */}
 
-      {(user?.user_metadata.user_name === login || hasHighlights) && (
-        <TabsContent value={"highlights" satisfies TabKey}>
-          {(hasHighlights || inputVisible) && user?.user_metadata.user_name === login && (
-            <div className="lg:pl-20 lg:gap-x-4 pt-4 flex max-w-[48rem]">
-              <div className="hidden lg:inline-flex">
-                <Avatar
-                  alt="user profile avatar"
-                  size="sm"
-                  avatarURL={`https://www.github.com/${githubName}.png?size=300`}
-                />
-              </div>
-
-              <HighlightInputForm refreshCallback={mutate} />
+      <TabsContent value={"highlights" satisfies TabKey}>
+        {(hasHighlights || inputVisible) && user_name === login && (
+          <div className="lg:pl-20 lg:gap-x-4 pt-4 flex max-w-[48rem]">
+            <div className="hidden lg:inline-flex">
+              <Avatar
+                alt="user profile avatar"
+                size="sm"
+                avatarURL={`https://www.github.com/${githubName}.png?size=300`}
+              />
             </div>
-          )}
-          <div className="flex flex-col gap-8 mt-8">
-            {/* <HightlightEmptyState /> */}
 
-            {isError && <>An error occured</>}
-            {isLoading && (
-              <>
-                {Array.from({ length: 2 }).map((_, index) => (
-                  <div className="flex flex-col gap-2 lg:flex-row lg:gap-6" key={index}>
-                    <SkeletonWrapper width={100} height={20} />
-                    <div className="md:max-w-[40rem]">
-                      <SkeletonWrapper height={20} width={500} classNames="mb-2" />
-                      <SkeletonWrapper height={300} />
+            <HighlightInputForm refreshCallback={mutate} />
+          </div>
+        )}
+        <div className="flex flex-col gap-8 mt-8">
+          {/* <HightlightEmptyState /> */}
+
+          {isError && <>An error occured</>}
+          {isLoading && (
+            <>
+              {Array.from({ length: 2 }).map((_, index) => (
+                <div className="flex flex-col gap-2 lg:flex-row lg:gap-6" key={index}>
+                  <SkeletonWrapper width={100} height={20} />
+                  <div className="md:max-w-[40rem]">
+                    <SkeletonWrapper height={20} width={500} classNames="mb-2" />
+                    <SkeletonWrapper height={300} />
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
+          <>
+            {hasHighlights ? (
+              <div>
+                {highlights.map(({ id, title, highlight, url, shipped_at, created_at, type, tagged_repos }) => (
+                  <div className="flex flex-col gap-2 mb-6 lg:flex-row lg:gap-7" key={id}>
+                    <div>
+                      <Link href={`/feed/${id}`}>
+                        <p className="text-sm text-light-slate-10 w-28 max-w-28">
+                          {formatDistanceToNowStrict(new Date(created_at), { addSuffix: true })}
+                        </p>
+                      </Link>
                     </div>
+                    <ContributorHighlightCard
+                      emojis={emojis}
+                      id={id}
+                      user={login || ""}
+                      title={title}
+                      desc={highlight}
+                      highlightLink={url}
+                      shipped_date={shipped_at}
+                      type={type}
+                      refreshCallBack={mutate}
+                      taggedRepos={tagged_repos}
+                    />
                   </div>
                 ))}
-              </>
-            )}
-            <>
-              {!isError && hasHighlights ? (
-                <div>
-                  {highlights.map(({ id, title, highlight, url, shipped_at, created_at, type, tagged_repos }) => (
-                    <div className="flex flex-col gap-2 mb-6 lg:flex-row lg:gap-7" key={id}>
-                      <div>
-                        <Link href={`/feed/${id}`}>
-                          <p className="text-sm text-light-slate-10 w-28 max-w-28">
-                            {formatDistanceToNowStrict(new Date(created_at), { addSuffix: true })}
-                          </p>
-                        </Link>
-                      </div>
-                      <ContributorHighlightCard
-                        emojis={emojis}
-                        id={id}
-                        user={login || ""}
-                        title={title}
-                        desc={highlight}
-                        highlightLink={url}
-                        shipped_date={shipped_at}
-                        type={type}
-                        refreshCallBack={mutate}
-                        taggedRepos={tagged_repos}
-                      />
+                {meta.pageCount > 1 && (
+                  <div className="mt-10 max-w-[48rem] flex px-2 items-center justify-between">
+                    <div>
+                      <PaginationResults metaInfo={meta} total={meta.itemCount} entity={"highlights"} />
                     </div>
-                  ))}
-                  {meta.pageCount > 1 && (
-                    <div className="mt-10 max-w-[48rem] flex px-2 items-center justify-between">
-                      <div>
-                        <PaginationResults metaInfo={meta} total={meta.itemCount} entity={"highlights"} />
-                      </div>
-                      <Pagination
-                        pages={[]}
-                        totalPage={meta.pageCount}
-                        page={meta.page}
-                        pageSize={meta.itemCount}
-                        goToPage
-                        hasNextPage={meta.hasNextPage}
-                        hasPreviousPage={meta.hasPreviousPage}
-                        onPageChange={function (page: number): void {
-                          setPage(page);
-                        }}
-                      />
-                    </div>
-                  )}
-                </div>
-              ) : (
-                <DashContainer>
-                  <div className="text-center">
-                    <p>
-                      You don&apos;t have any highlights yet! <br /> Highlights are a great way to show off your
-                      contributions. Merge any pull requests recently?
-                    </p>
-                    {!inputVisible && (
-                      <Button onClick={() => setInputVisible(true)} className="mt-5" variant="primary">
-                        Add a highlight
-                      </Button>
-                    )}
+                    <Pagination
+                      pages={[]}
+                      totalPage={meta.pageCount}
+                      page={meta.page}
+                      pageSize={meta.itemCount}
+                      goToPage
+                      hasNextPage={meta.hasNextPage}
+                      hasPreviousPage={meta.hasPreviousPage}
+                      onPageChange={function (page: number): void {
+                        setPage(page);
+                      }}
+                    />
                   </div>
-                </DashContainer>
-              )}
-            </>
-          </div>
-        </TabsContent>
-      )}
+                )}
+              </div>
+            ) : (
+              getEmptyHighlightPreset()
+            )}
+          </>
+        </div>
+      </TabsContent>
 
       {/* Contributions Tab Details */}
 
