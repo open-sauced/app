6fa72879bd0a5770919da7c6c59ec183b02a3859
Chris Schlensker <chris@foxyblocks.com>
2023-09-12 07:00:52 -0700

    chore: Move TooltipProvider to storybook global decorator (#1697)

--- stories/molecules/recommended-repo-card.stories.tsx
+++ stories/molecules/recommended-repo-card.stories.tsx
@@ -1,19 +1,16 @@
-import { TooltipProvider } from "@radix-ui/react-tooltip";
-import { ComponentStory } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import RecommendedRepoCard from "components/molecules/RecommendedRepoCard/recommended-repo-card";
 
 const storyConfig = {
   title: "Design System/Molecules/Recommended Repo Card",
-};
+  component: RecommendedRepoCard,
+} satisfies Meta<typeof RecommendedRepoCard>;
 export default storyConfig;
 
-const template: ComponentStory<typeof RecommendedRepoCard> = (args) => (
-  <TooltipProvider>
-    <RecommendedRepoCard {...args} />
-  </TooltipProvider>
-);
+type Story = StoryObj<typeof RecommendedRepoCard>;
 
-export const Default = template.bind({});
-Default.args = {
-  fullName: "open-sauced/insights",
+export const Default: Story = {
+  args: {
+    fullName: "open-sauced/insights",
+  },
 };
