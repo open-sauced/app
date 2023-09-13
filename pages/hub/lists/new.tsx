2b53581bc7d580b16fa32b5794e43ec462d47272
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-09-13 16:04:46 +0100

    chore: initial commit for hub contributors table implementation

--- pages/hub/lists/new.tsx
+++ pages/hub/lists/new.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ContributorListTableHeaders from "components/molecules/ContributorListTableHeader/contributor-list-table-header";
+import { WithPageLayout } from "interfaces/with-page-layout";
+import HubContributorsPageLayout from "layouts/hub-contributors";
 
-const NewListCreationPage = () => {
-  return <div>NewListCreationPage</div>;
+const NewListCreationPage: WithPageLayout = () => {
+  return (
+    <div>
+      <div className="lg:min-w-[1150px]">
+        <ContributorListTableHeaders />
+        {/* <ContributorTable loading={isLoading} topic={topic} contributors={data}></ContributorTable> */}
+      </div>
+    </div>
+  );
 };
+
+NewListCreationPage.PageLayout = HubContributorsPageLayout;
 
 export default NewListCreationPage;
