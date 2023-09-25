2df066249975044fb4c10541412aa631ea7f4fcd
Nick Taylor <nick@nickyt.co>
2023-09-25 15:19:53 -0400

    chore: simplified fetchApiData helper

--- helpers/fetchApiData.ts
+++ helpers/fetchApiData.ts
@@ -1,38 +1,30 @@
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import { GetServerSidePropsContext } from "next";
-
 export async function fetchApiData<T>({
   baseUrl = process.env.NEXT_PUBLIC_API_URL,
   path,
+  method = "GET",
   headers,
-  context,
+  bearerToken,
 }: {
   baseUrl?: string;
   path: string;
+  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
   headers?: HeadersInit;
-  context: GetServerSidePropsContext;
+  bearerToken: string;
 }) {
-  const supabase = createPagesServerClient(context);
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session) {
-    return { data: null, error: { status: 401, statusText: "Unauthorized" } };
-  }
-
   const response = await fetch(`${baseUrl}/${path}`, {
+    method,
     headers: {
       ...headers,
       accept: "application/json",
-      Authorization: `Bearer ${session.access_token}`,
+      Authorization: `Bearer ${bearerToken}`,
     },
   });
 
-  const data = response.ok ? ((await response.json()) as T) : null;
-  const { status, statusText } = response;
-  const error = response.ok ? null : { status, statusText };
+  if (response.ok) {
+    return { data: (await response.json()) as T, error: null };
+  }
 
-  return { data, error };
+  const { status, statusText } = response;
+
+  return { data: null, error: { status, statusText } };
 }
