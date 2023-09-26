6f53fcd283840830af285bdb4f2db579ff7c3a8c
Olabode Lawal-Shittabey <babblebey@gmail.com>
2023-09-26 07:52:13 +0100

    Update lib/hooks/useDebounceTerm.ts
    Co-authored-by: Nick Taylor <nick@nickyt.co>

--- lib/hooks/useDebounceTerm.ts
+++ lib/hooks/useDebounceTerm.ts
@@ -2,10 +2,10 @@ import { useEffect, useRef, useState } from "react";
 
 const useDebounceTerm = (value: string, delay = 500) => {
   const [debouncedValue, setDebouncedValue] = useState("");
-  const timeoutRef = useRef();
+  const timeoutRef = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
-    timeoutRef.current = setTimeout(() => setDebouncedValue(value), delay) as unknown as typeof timeoutRef.current;
+    timeoutRef.current = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => {
       clearTimeout(timeoutRef.current);
