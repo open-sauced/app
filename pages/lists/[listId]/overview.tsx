4dc44dcd79775234d25ef91e7bf3897be701fc2d
Nick Taylor <nick@nickyt.co>
2023-09-29 11:31:13 -0400

    chore: refactored overview to work with contributors list changes

--- pages/lists/[listId]/overview.tsx
+++ pages/lists/[listId]/overview.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSidePropsContext } from "next";
 import ListPageLayout from "layouts/lists";
 
 import Card from "components/atoms/Card/card";
@@ -12,10 +14,50 @@ import { getAvatarByUsername } from "lib/utils/github";
 import useListContributions from "lib/hooks/useListContributions";
 import useListStats from "lib/hooks/useListStats";
 import HighlightCard from "components/molecules/HighlightCard/highlight-card";
+import { fetchApiData, validateListPath } from "helpers/fetchApiData";
+import ClientOnly from "components/atoms/ClientOnly/client-only";
 
 type ContributorPrMap = { [contributor: string]: DbRepoPR };
 
-const ListsOverview = (): JSX.Element => {
+interface ListsOverviewProps {
+  list: DBList;
+  numberOfContributors: number;
+}
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const supabase = createPagesServerClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const bearerToken = session ? session.access_token : "";
+
+  const { listId } = ctx.params as { listId: string };
+  const limit = 10; // Can pull this from the querystring in the future
+  const [{ data, error: contributorListError }, { data: list, error }] = await Promise.all([
+    fetchApiData<PagedData<DBListContributor>>({
+      path: `lists/${listId}/contributors?limit=${limit}`,
+      bearerToken,
+      pathValidator: validateListPath,
+    }),
+    fetchApiData<DBList>({ path: `lists/${listId}`, bearerToken, pathValidator: validateListPath }),
+  ]);
+
+  if (error?.status === 404) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      list,
+      numberOfContributors: data?.meta.itemCount || 0,
+    },
+  };
+};
+
+const ListsOverview = ({ list, numberOfContributors }: ListsOverviewProps): JSX.Element => {
   const router = useRouter();
   const { listId } = router.query;
   const { data: prData, isError: prError } = useListContributions(listId as string);
@@ -101,62 +143,64 @@ const ListsOverview = (): JSX.Element => {
   }, 0);
 
   return (
-    <div className="flex flex-col w-full gap-4">
-      <section className="flex flex-wrap items-center max-w-full gap-4 lg:flex-row lg:flex-nowrap">
-        <HighlightCard
-          label="Commits"
-          icon="commits"
-          metricIncreases={allContributorCommits > prevAllContributorCommits}
-          increased={allContributorCommits > prevAllContributorCommits}
-          value={allContributorCommits}
-          valueLabel="in the last 30d"
-        />
-        <HighlightCard
-          label="Active Contributors"
-          icon="active-contributors"
-          metricIncreases={allContributorMeta.itemCount > prevAllContributorMeta.itemCount}
-          increased={allContributorMeta.itemCount > prevAllContributorMeta.itemCount}
-          numChanged={allContributorMeta.itemCount - prevAllContributorMeta.itemCount}
-          value={allContributorMeta.itemCount}
-          isLoading={prevAllContributorStatsLoading || allContributorStatsLoading}
-        />
-        <HighlightCard
-          label="New Contributors"
-          icon="new-contributors"
-          metricIncreases={newContributorMeta.itemCount > prevNewContributorMeta.itemCount}
-          increased={newContributorMeta.itemCount > prevNewContributorMeta.itemCount}
-          numChanged={newContributorMeta.itemCount - prevNewContributorMeta.itemCount}
-          value={newContributorMeta.itemCount}
-          isLoading={prevNewContributorStatsLoading || newContributorStatsLoading}
-        />
-        <HighlightCard
-          label="Alumni Contributors"
-          icon="alumni-contributors"
-          metricIncreases={alumniContributorMeta.itemCount > prevAlumniContributorMeta.itemCount}
-          increased={alumniContributorMeta.itemCount > prevAlumniContributorMeta.itemCount}
-          numChanged={alumniContributorMeta.itemCount - prevAlumniContributorMeta.itemCount}
-          value={alumniContributorMeta.itemCount}
-          isLoading={prevAlumniContributorStatsLoading || alumniContributorStatsLoading}
-        />
-      </section>
-      <section className="flex flex-col max-w-full gap-4 mb-6 lg:flex-row">
-        <div className="flex flex-col w-full">
-          <Card className="w-full">
-            <NivoScatterPlot
-              title="Contributor Distribution"
-              showBots={showBots}
-              setShowBots={setShowBots}
-              data={[{ id: "Contributors", data: scatterChartData }]}
-              maxFilesModified={maxFilesModified}
-              isMobile={isMobile}
+    <ListPageLayout list={list} numberOfContributors={numberOfContributors} isOwner={false}>
+      <div className="flex flex-col w-full gap-4">
+        <ClientOnly>
+          <section className="flex flex-wrap items-center max-w-full gap-4 lg:flex-row lg:flex-nowrap">
+            <HighlightCard
+              label="Commits"
+              icon="commits"
+              metricIncreases={allContributorCommits > prevAllContributorCommits}
+              increased={allContributorCommits > prevAllContributorCommits}
+              value={allContributorCommits}
+              valueLabel="in the last 30d"
             />
-          </Card>
-        </div>
-      </section>
-    </div>
+            <HighlightCard
+              label="Active Contributors"
+              icon="active-contributors"
+              metricIncreases={allContributorMeta.itemCount > prevAllContributorMeta.itemCount}
+              increased={allContributorMeta.itemCount > prevAllContributorMeta.itemCount}
+              numChanged={allContributorMeta.itemCount - prevAllContributorMeta.itemCount}
+              value={allContributorMeta.itemCount}
+              isLoading={prevAllContributorStatsLoading || allContributorStatsLoading}
+            />
+            <HighlightCard
+              label="New Contributors"
+              icon="new-contributors"
+              metricIncreases={newContributorMeta.itemCount > prevNewContributorMeta.itemCount}
+              increased={newContributorMeta.itemCount > prevNewContributorMeta.itemCount}
+              numChanged={newContributorMeta.itemCount - prevNewContributorMeta.itemCount}
+              value={newContributorMeta.itemCount}
+              isLoading={prevNewContributorStatsLoading || newContributorStatsLoading}
+            />
+            <HighlightCard
+              label="Alumni Contributors"
+              icon="alumni-contributors"
+              metricIncreases={alumniContributorMeta.itemCount > prevAlumniContributorMeta.itemCount}
+              increased={alumniContributorMeta.itemCount > prevAlumniContributorMeta.itemCount}
+              numChanged={alumniContributorMeta.itemCount - prevAlumniContributorMeta.itemCount}
+              value={alumniContributorMeta.itemCount}
+              isLoading={prevAlumniContributorStatsLoading || alumniContributorStatsLoading}
+            />
+          </section>
+          <section className="flex flex-col max-w-full gap-4 mb-6 lg:flex-row">
+            <div className="flex flex-col w-full">
+              <Card className="w-full">
+                <NivoScatterPlot
+                  title="Contributor Distribution"
+                  showBots={showBots}
+                  setShowBots={setShowBots}
+                  data={[{ id: "Contributors", data: scatterChartData }]}
+                  maxFilesModified={maxFilesModified}
+                  isMobile={isMobile}
+                />
+              </Card>
+            </div>
+          </section>
+        </ClientOnly>
+      </div>
+    </ListPageLayout>
   );
 };
-
-ListsOverview.PageLayout = ListPageLayout;
 
 export default ListsOverview;
