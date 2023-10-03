0f6c986c94cddabef247b0c3d3262609858e9332
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-10-03 22:55:19 +0100

    chore: update multiselect styles

--- components/atoms/Select/multi-select.tsx
+++ components/atoms/Select/multi-select.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from "react";
 import { FiChevronDown } from "react-icons/fi";
 import { IoCheckmarkSharp } from "react-icons/io5";
 import { IoMdCloseCircle } from "react-icons/io";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
 
 import { clsx } from "clsx";
 import { Popover, PopoverContent, PopoverTrigger } from "components/molecules/Popover/popover";
@@ -48,79 +49,81 @@ const MultiSelect = ({
 
   return (
     <Popover open={open} onOpenChange={(value) => setOpen(value)}>
-      <div className="min-w-[250px] max-w-[100px] ">
-        <PopoverTrigger
-          asChild
-          className="p-1.5 border rounded-md bg-white data-[state=open]:border-orange-500  min-w-[250px] "
-        >
-          <button
-            aria-controls="select-menu-list"
-            role="combobox"
-            aria-expanded={open}
-            className="flex items-center justify-between text-foreground"
+      <PopoverPrimitive.Anchor className={clsx("w-full")}>
+        <div className="min-w-[250px] w-full">
+          <PopoverTrigger
+            asChild
+            className="!px-3 h-[2.1rem] py-1  border rounded-md bg-white data-[state=open]:border-orange-500  min-w-[250px] w-full"
           >
-            {selected.length > 0 ? (
-              <span className="truncate">
-                {selected[0].label}
-                {selected.length > 1 ? `, +${dummySelected.length - 1}` : null}
-              </span>
-            ) : (
-              <span className="opacity-50">{placeholder ?? "Select Items"}</span>
-            )}
+            <button
+              aria-controls="select-menu-list"
+              role="combobox"
+              aria-expanded={open}
+              className="flex items-center justify-between text-base text-foreground"
+            >
+              {selected.length > 0 ? (
+                <span className="truncate">
+                  {selected[0].label}
+                  {selected.length > 1 ? `, +${dummySelected.length - 1}` : null}
+                </span>
+              ) : (
+                <span className="opacity-50">{placeholder ?? "Select Items"}</span>
+              )}
 
-            {selected.length > 0 ? (
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  setDummySelected([]);
-                }}
-              >
-                <IoMdCloseCircle className="text-red-600" />
-              </button>
-            ) : (
-              <FiChevronDown className="ml-2 text-lg opacity-50 shrink-0" />
-            )}
-          </button>
-        </PopoverTrigger>
-        <PopoverContent className="!w-full !min-w-[250px] bg-white p-0 max-w-sm">
-          <Command loop onKeyDown={handleKeyDown} className="w-full px-0 bg-transparent">
-            <CommandInput
-              ref={inputRef}
-              placeholder={placeholder ?? "Search Items"}
-              value={inputValue}
-              onValueChange={setInputValue}
-            />
-            <CommandGroup className="flex flex-col !px-0 overflow-scroll max-h-48">
-              {open && options.length > 0
-                ? options.map((option) => (
-                    <CommandItem
-                      key={option.value}
-                      onMouseDown={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                      }}
-                      onSelect={(value) => {
-                        setInputValue("");
-                        toggleFramework(option);
-                      }}
-                      onClick={() => toggleFramework(option)}
-                      className={clsx(
-                        "!cursor-pointer flex justify-between items-center !px-1 rounded-md truncate break-words w-full",
-                        selected.some((s) => s.value === option.value) && "bg-gray-100"
-                      )}
-                    >
-                      {option.label}
-                      {selected.some((s) => s.value === option.value) && (
-                        <IoCheckmarkSharp className="w-5 h-5 ml-2 text-sauced-orange shrink-0" />
-                      )}
-                    </CommandItem>
-                  ))
-                : null}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </div>
+              {selected.length > 0 ? (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    setDummySelected([]);
+                  }}
+                >
+                  <IoMdCloseCircle className="text-red-600" />
+                </button>
+              ) : (
+                <FiChevronDown className="ml-2 text-lg opacity-50 shrink-0" />
+              )}
+            </button>
+          </PopoverTrigger>
+          <PopoverContent sideOffset={5} className={clsx("popover-width bg-white-400", className)}>
+            <Command loop onKeyDown={handleKeyDown} className="w-full px-0 bg-transparent">
+              <CommandInput
+                ref={inputRef}
+                placeholder={placeholder ?? "Search Items"}
+                value={inputValue}
+                onValueChange={setInputValue}
+              />
+              <CommandGroup className="flex flex-col !px-0 overflow-scroll max-h-48">
+                {open && options.length > 0
+                  ? options.map((option) => (
+                      <CommandItem
+                        key={option.value}
+                        onMouseDown={(e) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                        }}
+                        onSelect={(value) => {
+                          setInputValue("");
+                          toggleFramework(option);
+                        }}
+                        onClick={() => toggleFramework(option)}
+                        className={clsx(
+                          "!cursor-pointer flex justify-between items-center !px-1 rounded-md truncate break-words w-full",
+                          selected.some((s) => s.value === option.value) && "bg-gray-100"
+                        )}
+                      >
+                        {option.label}
+                        {selected.some((s) => s.value === option.value) && (
+                          <IoCheckmarkSharp className="w-5 h-5 ml-2 text-sauced-orange shrink-0" />
+                        )}
+                      </CommandItem>
+                    ))
+                  : null}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </div>
+      </PopoverPrimitive.Anchor>
     </Popover>
   );
 };
