a2b8c65dfe71d18485fa6d291730e12a4abd4afb
Nick Taylor <nick@nickyt.co>
2023-10-05 08:29:42 -0400

    chore: removed test data with associated graphs

--- pages/lists/[listId]/activity.tsx
+++ pages/lists/[listId]/activity.tsx
@@ -1,12 +1,10 @@
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
-import { eachDayOfInterval, subDays } from "date-fns";
 import { useState } from "react";
 import Error from "components/atoms/Error/Error";
 import { fetchApiData, validateListPath } from "helpers/fetchApiData";
 import ListPageLayout from "layouts/lists";
 import { ContributionEvolutionByTypeDatum } from "components/molecules/ContributionsEvolutionByTypeCard/contributions-evolution-by-type-card";
-import { ContributionStat } from "components/molecules/ContributionsEvolution/contributions-evolution-card";
 import MostActiveContributorsCard, {
   ContributorStat,
 } from "components/molecules/MostActiveContributorsCard/most-active-contributors-card";
@@ -19,7 +17,6 @@ interface ContributorListPageProps {
   isError: boolean;
   activityData: {
     contributionsByType: ContributionEvolutionByTypeDatum[];
-    contributionStats: { data: ContributionStat[]; meta: Meta };
     contributorStats: { data: ContributorStat[]; meta: Meta };
   };
 }
@@ -79,7 +76,6 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
       isError: error || contributorListError || mostActiveError,
       activityData: {
         contributionsByType,
-        contributionStats: generateContributionStats(),
         // TODO: remove map once totalContribtions is part of a ContributorStat
         contributorStats: {
           data: mostActiveData?.data?.map((contributor) => ({
@@ -95,8 +91,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 
 const ListActivityPage = ({ list, initialData, isError, activityData }: ContributorListPageProps) => {
   const isOwner = false;
-  const [contributionsByType, setContributionsByType] = useState(activityData.contributionsByType);
-  const [contributionStats, setContributionStats] = useState(activityData.contributionStats.data);
+  // const [contributionsByType, setContributionsByType] = useState(activityData.contributionsByType);
   const [contributorStats, setContributorStats] = useState(activityData.contributorStats.data);
 
   return (
@@ -106,9 +101,7 @@ const ListActivityPage = ({ list, initialData, isError, activityData }: Contribu
       ) : (
         <div className="grid grid-cols-2 grid-rows-2 gap-4">
           <MostActiveContributorsCard data={contributorStats} />
-          {/* <ContributionsEvolutionCard data={contributionStats} />
-          <ContributionsEvolutionCard data={contributionStats} />
-          <ContributionsEvolutionByType data={contributionsByType} /> */}
+          {/* <ContributionsEvolutionByType data={contributionsByType} /> */}
         </div>
       )}
     </ListPageLayout>
@@ -116,65 +109,3 @@ const ListActivityPage = ({ list, initialData, isError, activityData }: Contribu
 };
 
 export default ListActivityPage;
-
-// Test data
-function randomValueCloseToValue(value: number) {
-  return Math.max(value + Math.floor(Math.random() * 100 - 50), 0);
-}
-
-function generateData() {
-  // Use date-fns to get an array of the last 6 weeks
-  const today = new Date();
-  const sixWeeksAgo = subDays(today, 7 * 6);
-  const result = eachDayOfInterval({
-    start: sixWeeksAgo,
-    end: today,
-  });
-
-  // return result.map((date) => ({
-  //   startTime: date.toString(),
-  //   active: Math.floor(Math.random() * 500),
-  //   new: Math.floor(Math.random() * 500),
-  //   churned: Math.floor(Math.random() * 500),
-  // }));
-
-  // generate data for each day but make it so that the next value is a random value that is close to the previous value
-  let previousActive = Math.floor(Math.random() * 500);
-  let previousNew = Math.floor(Math.random() * 500);
-  let previousChurned = Math.floor(Math.random() * 500);
-  return result.map((date) => {
-    const newActive = randomValueCloseToValue(previousActive);
-    const newNew = randomValueCloseToValue(previousNew);
-    const newChurned = randomValueCloseToValue(previousChurned);
-
-    previousActive = newActive;
-    previousNew = newNew;
-    previousChurned = newChurned;
-
-    return {
-      startTime: date.toString(),
-      active: newActive,
-      new: newNew,
-      churned: newChurned,
-    };
-  });
-}
-
-function generateContributionStats() {
-  // Use date-fns to get an array of the last 7 days
-  const today = new Date();
-  const weekAgo = subDays(today, 6);
-  const result = eachDayOfInterval({
-    start: weekAgo,
-    end: today,
-  });
-
-  return result.map((date) => ({
-    startTime: date.toString(),
-    commits: Math.floor(Math.random() * 500),
-    prsCreated: Math.floor(Math.random() * 500),
-    prsReviewed: Math.floor(Math.random() * 500),
-    issuesCreated: Math.floor(Math.random() * 500),
-    comments: Math.floor(Math.random() * 500),
-  }));
-}
