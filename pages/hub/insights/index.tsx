ef310489ef189e21f0292879407b4edf943bc947
Brandon Roberts <robertsbt@gmail.com>
2023-10-10 15:18:36 -0500

    feat: display featured insight pages to new users

--- pages/hub/insights/index.tsx
+++ pages/hub/insights/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import clsx from "clsx";
 
 import InsightRow from "components/molecules/InsightRow/insight-row";
@@ -10,11 +12,66 @@ import HubLayout from "layouts/hub";
 import { WithPageLayout } from "interfaces/with-page-layout";
 import useUserInsights from "lib/hooks/useUserInsights";
 import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
+import useStore from "lib/store";
+import useSession from "lib/hooks/useSession";
+import { useToast } from "lib/hooks/useToast";
+import Text from "components/atoms/Typography/text";
 
 const InsightsHub: WithPageLayout = () => {
+  const router = useRouter();
   const { user } = useSupabaseAuth();
-
+  const store = useStore();
+  const dismissFeaturedInsights = useStore((store) => store.dismissFeaturedInsights);
+  const { toast } = useToast();
+  const { session } = useSession(true);
   const { data, meta, isError, isLoading, setPage } = useUserInsights();
+
+  function handleView() {
+    const insight = data.slice(0, 1).shift();
+    router.push(`/pages/${user?.user_metadata.user_name}/${insight!.id}/dashboard`);
+  }
+
+  function openInsightToast() {
+    const toaster = toast({
+      description: "Welcome to your Insights Hub!",
+      variant: "success",
+      action: (
+        <div className="flex flex-col">
+          <div className="flex align-middle">
+            <Text>
+              We&apos;ve included a featured Insight Page for you to test out. You can also create your own to get
+              insights on repositories.
+            </Text>
+          </div>
+          <div className="flex flex-start">
+            <div
+              onClick={() => {
+                toaster && toaster.dismiss();
+              }}
+            >
+              <Text className="pr-2 cursor-pointer">Dismiss</Text>
+            </div>
+            <div
+              onClick={() => {
+                toaster && toaster.dismiss();
+                handleView();
+              }}
+            >
+              <Text className="text-orange-500 cursor-pointer">Open Insight Page</Text>
+            </div>
+          </div>
+        </div>
+      ),
+    });
+
+    store.setDismissFeaturedInsights();
+  }
+
+  useEffect(() => {
+    if (session && session.insights_count === 0 && !dismissFeaturedInsights) {
+      openInsightToast();
+    }
+  }, [session, user]);
 
   return (
     <>
