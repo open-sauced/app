0b914bab081344fc1d2fc8c67fdcc1bc8e2f09ce
Nick Taylor <nick@nickyt.co>
2023-10-10 22:18:47 -0400

    fix: fixed API querystring param for repo_id for treemap API call

--- lib/hooks/api/useContributorsByProject.ts
+++ lib/hooks/api/useContributorsByProject.ts
@@ -5,7 +5,7 @@ import publicApiFetcher from "lib/utils/public-api-fetcher";
 export const useContributorsByProject = (listId: string, range: number) => {
   const [repoId, setRepoId] = useState<number | null>(null);
   const { data, error } = useSWR<DBProjectContributor[]>(
-    `lists/${listId}/stats/top-project-contributions-by-contributor?repoId=${repoId}&range=${range}`,
+    `lists/${listId}/stats/top-project-contributions-by-contributor?repo_id=${repoId}&range=${range}`,
     publicApiFetcher as Fetcher<DBProjectContributor[], Error>
   );
 
