3a37f8269b52ffbefca4b4b4344928a4eb98fa84
Nick Taylor <nick@nickyt.co>
2023-10-12 14:30:55 -0400

    chore: added some comments

--- lib/utils/analytics.ts
+++ lib/utils/analytics.ts
@@ -1,25 +1,30 @@
 import posthog from "posthog-js";
 
+interface AnalyticEvent {
+  title: string;
+  property: string;
+  value: string;
+  identifier: string;
+}
+
 function initiateAnalytics() {
   // eslint-disable-next-line
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_ID as string, { api_host: "https://app.posthog.com" });
 }
 
-function captureAnayltics({
-  // userId,
-  title,
-  property,
-  value,
-}: {
-  // userId: string;
-  title: string;
-  property: string;
-  value: string;
-}) {
+/**
+ * Captures an analytic event
+ *
+ * @param {string} title - The title of the event
+ * @param {string} property - The property of the event
+ * @param {string} value - The value of the event
+ * @param {string} identifier - The unique identifier for entity associated to the analytic event, typically a user id
+ */
+function captureAnayltics({ title, property, value, identifier }: AnalyticEvent) {
   const analyticsObject: Record<string, string> = {};
 
   analyticsObject[property] = value;
-  posthog.identify();
+  posthog.identify(identifier);
   posthog.capture(title, analyticsObject);
 }
 
