2b66955c51a8791ec874495d3dc674eb634c3e00
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-10-18 18:35:52 +0100

    fix: layout children alignment

--- layouts/lists.tsx
+++ layouts/lists.tsx
@@ -55,7 +55,7 @@ const ListPageLayout = ({
           </div>
         </div>
 
-        <main className="flex flex-col items-center flex-1 w-full px-3 py-8 md:px-16 bg-light-slate-2">
+        <main className="flex flex-col items-center flex-1 w-full py-8  bg-light-slate-2">
           <div className="container px-2 mx-auto md:px-16">{children}</div>
         </main>
       </div>
