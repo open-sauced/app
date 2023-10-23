c53328fa54be938a25acbd2edb7d6cc9184eb067
Shraddha <mailmeatshraddha@gmail.com>
2023-10-22 20:53:34 -0400

    refactor: remove destructure where not needed

--- layouts/hub-page.tsx
+++ layouts/hub-page.tsx
@@ -15,8 +15,9 @@ import SkeletonWrapper from "components/atoms/SkeletonLoader/skeleton-wrapper";
 const HubPageLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const { userId } = useSupabaseAuth();
-  const insightId = router.query.pageId as string;
-  const { data: insight, isLoading, isError } = useInsight(insightId);
+  const { pageId } = router.query;
+  const insightId = pageId as string;
+  const { data: insight, isLoading, isError } = useInsight(router.query.pageId as string);
   const repositories = insight?.repos.map((repo) => repo.repo_id);
 
   const { toolList, selectedTool, selectedFilter, userOrg } = useNav(repositories);
