ce0a5b8461cd4deeae56c3f53e9cbb1d3a690ec0
Nick Taylor <nick@nickyt.co>
2023-10-24 17:58:41 -0400

    chore: beginnings of form action to delete user

--- pages/api/delete-account.ts
+++ pages/api/delete-account.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { supabase } from "lib/utils/supabase";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
-// Create a Next.js API route that receives in the request body, an action and user_id field to delete an account where the action is delete_account and the user_id is the user_id of the user to delete.
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     res.status(405).json({
@@ -9,18 +8,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
   }
 
-  const sessionResponse = await supabase.auth.getSession();
+  const supabaseServerClient = createPagesServerClient({
+    req,
+    res,
+  });
+  const {
+    data: { user },
+  } = await supabaseServerClient.auth.getUser();
 
-  if (!sessionResponse.data.session) {
+  if (!user) {
     res.status(401).json({
       message: "Unauthorized",
     });
   } else {
     try {
-      console.log(`delete ${sessionResponse.data.session.user.id}`);
-      // TODO: actually delete account
+      supabaseServerClient.auth.signOut();
 
-      res.redirect("/?account-deleted");
+      res.redirect("/account-deleted");
     } catch (error) {
       res.status(500).json({
         message: "Internal server error",
