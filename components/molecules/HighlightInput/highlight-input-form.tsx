bc10ca879db5bf8bbef295027fb7899f59a0492f
Shamim Bin Nur <sbnsbn00@gmail.com>
2023-10-26 22:32:07 +0600

    Changed wording

--- components/molecules/HighlightInput/highlight-input-form.tsx
+++ components/molecules/HighlightInput/highlight-input-form.tsx
@@ -434,7 +434,7 @@ const HighlightInputForm = ({ refreshCallback }: HighlightInputFormProps): JSX.E
 
       // Check if the user has tagged at least one repo and ask them to tag at least one if they haven't
       if (taggedRepoList.length < 1) {
-        setError("Please tag at least one repo!");
+        setError("Please add at least one repository associated with your blog post");
         return;
       }
 
