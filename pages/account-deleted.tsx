494d0f44ac70a090bc537af18fbf24d2713af3a9
Nick Taylor <nick@nickyt.co>
2023-10-27 09:03:07 -0400

    chore: added some style to h1 for now since no base styles

--- pages/account-deleted.tsx
+++ pages/account-deleted.tsx
@@ -6,7 +6,7 @@ const AccountDeletedPage = () => {
       <TopNav />
       <main className="page-container flex flex-col m-4 pt-28 items-center">
         <div className="info-container container w-full">
-          <h1>Account Deleted</h1>
+          <h1 className="text-2xl">Account Deleted</h1>
           <p>Your account has been deleted.</p>
         </div>
       </main>
