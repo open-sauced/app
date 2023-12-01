7c4e63094399dfbe9942dfdf9429906ab937a206
Nick Taylor <nick@nickyt.co>
2023-11-30 19:12:19 -0500

    chore: now there is a fallback error message if the list contributor table crashes

--- pages/lists/[listId]/contributors.tsx
+++ pages/lists/[listId]/contributors.tsx
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext } from "next";
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/router";
 
+import { ErrorBoundary } from "react-error-boundary";
 import ListPageLayout from "layouts/lists";
 import { fetchApiData, validateListPath } from "helpers/fetchApiData";
 import Error from "components/atoms/Error/Error";
@@ -78,13 +79,17 @@ const ContributorsListPage = ({ list, initialData, isError, isOwner }: Contribut
       {isError ? (
         <Error errorMessage="Unable to load list of contributors" />
       ) : (
-        <ContributorsList
-          contributors={contributors}
-          meta={meta}
-          isLoading={isLoading}
-          setPage={setPage}
-          range={String(range) ?? "30"}
-        />
+        <ErrorBoundary
+          fallback={<div className="grid place-content-center">Error loading the list of contributors</div>}
+        >
+          <ContributorsList
+            contributors={contributors}
+            meta={meta}
+            isLoading={isLoading}
+            setPage={setPage}
+            range={String(range) ?? "30"}
+          />
+        </ErrorBoundary>
       )}
     </ListPageLayout>
   );
