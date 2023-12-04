feb30765c5db14c1e0c7944943ea0752f90dddb0
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-12-04 15:19:02 +0100

    chore: update static featured list to live version

--- pages/hub/lists/index.tsx
+++ pages/hub/lists/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import clsx from "clsx";
 
-import Link from "next/link";
-import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { GetServerSidePropsContext } from "next";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { WithPageLayout } from "interfaces/with-page-layout";
 import HubLayout from "layouts/hub";
 
@@ -19,40 +19,51 @@ import { useToast } from "lib/hooks/useToast";
 
 import { useFetchAllLists } from "lib/hooks/useList";
 import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
-import Card from "components/atoms/Card/card";
-import StackedAvatar from "components/molecules/StackedAvatar/stacked-avatar";
+import { fetchApiData, validateListPath } from "helpers/fetchApiData";
 
-const staticContributors = [
-  {
-    host_login: "brandonroberts",
-  },
-  {
-    host_login: "nickytonline",
-  },
-  {
-    host_login: "bbsmooth",
-  },
-  {
-    host_login: "lunaruan",
-  },
-  {
-    host_login: "bluwy",
-  },
-  {
-    host_login: "lasjorg",
-  },
-  {
-    host_login: "sukkaw",
-  },
-  {
-    host_login: "ogdev-01",
-  },
-  {
-    host_login: "0",
-  },
-];
+interface ListsHubProps {
+  featuredLists: DbUserList[];
+}
 
-const ListsHub: WithPageLayout = () => {
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const supabase = createPagesServerClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    const { data, error } = await fetchApiData<PagedData<DbUserList>>({
+      path: "lists/featured",
+      bearerToken: "",
+      pathValidator: validateListPath,
+    });
+
+    if (error?.status === 404) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        featuredLists: data?.data || [],
+      },
+    };
+  }
+
+  return {
+    props: {
+      featuredLists: [],
+    },
+  };
+}
+
+const ListsHub: WithPageLayout<
+  ListsHubProps & {
+    featuredLists: DbUserList[];
+  }
+> = ({ featuredLists }) => {
   const { data, isLoading, meta, setPage, mutate } = useFetchAllLists();
   const { toast } = useToast();
   const { sessionToken } = useSupabaseAuth();
@@ -128,51 +139,21 @@ const ListsHub: WithPageLayout = () => {
 
         {isLoading && sessionToken ? <SkeletonWrapper count={3} classNames="w-full" height={95} radius={10} /> : null}
 
-        {!sessionToken ? (
-          <div className="w-full -mt-4 ">
-            <Text className="text-base">
-              Welcome to List Pages, we&apos;ve included a featured List Page for you to test out. You can also create
-              your own to get insights on contributors.
-            </Text>
-
-            <Card className="mt-4">
-              <div className="flex flex-col items-start w-full gap-4 px-4 py-6 bg-white rounded-lg md:items-center md:justify-between md:flex-row lg:px-8 lg:gap-2">
-                <div className="flex flex-col flex-1 gap-4 lg:gap-6">
-                  <div className="flex items-center gap-4 lg:items-center ">
-                    <div className="w-4 h-4 rounded-full bg-light-orange-10"></div>
-                    <div className="flex justify-between text-xl text-light-slate-12">
-                      <Link href={`/lists/${testListId}/overview`}>Top 10 Open Source Contributors</Link>
-                    </div>
-                    <div className="px-2 border rounded-2xl text-light-slate-11">public</div>
-                  </div>
-                </div>
-                <div className="">
-                  <div className="flex items-center justify-end w-full gap-8">
-                    {/* Contributors section */}
-                    <div className="flex flex-col items-center flex-1 gap-1 mr-2">
-                      <span className="text-xs text-light-slate-11">Contributors</span>
-                      <Text className="flex items-center text-2xl">10</Text>
-                    </div>
-
-                    <div className="flex items-center">
-                      <StackedAvatar contributors={staticContributors} visibleQuantity={6} classNames="scale-125" />
-                    </div>
-                    <div className="justify-end flex-1 hidden md:flex">
-                      {/* Delete button */}
-
-                      <Link
-                        className="inline-block p-3 mr-2 border rounded-lg cursor-pointer bg-light-slate-1"
-                        href={`/lists/${testListId}/overview`}
-                      >
-                        <MdOutlineArrowForwardIos title="Go To List Page" className="text-lg text-light-slate-10" />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Card>
-          </div>
-        ) : null}
+        {!sessionToken && featuredLists.length > 0
+          ? featuredLists.map((list, i) => (
+              <ListCard
+                key={`featured_list_${i}`}
+                list={{
+                  id: list.id,
+                  user: { login: "bdougie", id: 1, name: "crap" },
+                  name: `Demo | ${list.name}`,
+                  created_at: " ",
+                  updated_at: "",
+                  is_public: list.is_public,
+                }}
+              />
+            ))
+          : null}
       </section>
       <div
         className={clsx("py-1 md:py-4 flex w-full md:mt-5 justify-between items-center", {
