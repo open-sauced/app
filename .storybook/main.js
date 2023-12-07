61ea0e89e5d14dc7bd225d5ccfab5262643cb574
Nick Taylor <nick@nickyt.co>
2023-12-07 17:57:56 -0500

    chore: change folders Storybook looks for stories in

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
