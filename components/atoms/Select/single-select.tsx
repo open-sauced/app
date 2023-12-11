6ce8eb9510a595f5a846f7656c7bf2615aee3e50
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-12-11 17:02:51 +0100

    fix: fix input focus accessibility issue

--- components/atoms/Select/single-select.tsx
+++ components/atoms/Select/single-select.tsx
@@ -1,9 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { RiArrowDownSLine } from "react-icons/ri";
+import { BiSearch } from "react-icons/bi";
 
 import { truncateString } from "lib/utils/truncate-string";
 
-import { Command, CommandGroup, CommandInput, CommandItem } from "../Cmd/command";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
 
 interface SingleSelectProps {
@@ -27,8 +27,31 @@ const SingleSelect = ({
 }: SingleSelectProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [inputValue, setInputValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const filteredOptions = options.filter((option) => option.label.toLowerCase().includes(inputValue.toLowerCase()));
+
+  const getOptions = () => (isSearchable ? filteredOptions : options);
+
+  useEffect(() => {
+    if (isOpen && isSearchable) {
+      inputRef.current?.focus();
+    }
+  }, [isOpen, isSearchable]);
+
   return (
-    <Select value={value} onValueChange={(value) => onValueChange(value)}>
+    <Select
+      value={value}
+      onValueChange={(value) => {
+        onValueChange(value);
+      }}
+      onOpenChange={(open) => {
+        setIsOpen(open);
+        if (!open) {
+          setInputValue("");
+        }
+      }}
+    >
       <SelectTrigger
         className="bg-white min-w-[200px] w-full border focus-within:border-orange-500 focus-within:ring-orange-100 focus-within:ring ring-light-slate-6"
         selectIcon={
@@ -41,46 +64,44 @@ const SingleSelect = ({
       </SelectTrigger>
 
       <SelectContent position={position ?? "item-aligned"} className="z-50 relative bg-white">
-        {isSearchable ? (
-          <Command loop className="w-full px-0 bg-transparent">
-            <CommandInput
-              ref={inputRef}
-              placeholder={inputPlaceholder ?? "Search Items"}
-              value={inputValue}
-              onValueChange={setInputValue}
-            />
-            <CommandGroup className="flex flex-col !px-0 overflow-scroll max-h-[25rem]">
-              {options.map((option, index) => (
-                <CommandItem
-                  key={`timezone_${index}`}
-                  onMouseDown={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                  }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      onValueChange(option.value);
-                      setInputValue("");
-                    }
-                  }}
-                  className="!px-3 rounded-md truncate"
-                >
-                  <SelectItem key={`timezone_${index}`} className="!px-0" title={option.label} value={option.value}>
-                    {truncateString(option.label, 30)}
-                  </SelectItem>
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        ) : (
-          <>
-            {options.map((option, index) => (
-              <SelectItem title={option.label} key={`timezone_${index}`} value={option.value}>
-                {truncateString(option.label, 30)}
-              </SelectItem>
-            ))}
-          </>
-        )}
+        <>
+          {isSearchable ? (
+            <div className="flex items-center px-3 py-1.5">
+              <BiSearch className="mr-1" />
+
+              <input
+                ref={inputRef}
+                value={inputValue}
+                className="w-full py-1.5 text-sm outline-none"
+                placeholder={inputPlaceholder ?? "Search Items"}
+                onChange={(e) => {
+                  setInputValue(e.target.value);
+                }}
+              />
+            </div>
+          ) : null}
+          {getOptions().map((option, index) => (
+            <SelectItem
+              onKeyDown={(e) => {
+                if (e.key === "ArrowUp" && index === 0) {
+                  inputRef.current?.focus();
+                }
+              }}
+              title={option.label}
+              key={`timezone_${index}`}
+              className="pl-3 !py-2 truncate"
+              value={option.value}
+            >
+              {truncateString(option.label, 30)}
+            </SelectItem>
+          ))}
+
+          {getOptions().length === 0 && (
+            <SelectItem className="pl-3 text-center" value={""}>
+              No results found
+            </SelectItem>
+          )}
+        </>
       </SelectContent>
     </Select>
   );
