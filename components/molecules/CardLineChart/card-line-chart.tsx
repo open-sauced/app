9a7196cb73aaeffc2ca4b4fe631df4b1d8f9e996
Nick Taylor <nick@nickyt.co>
2023-12-13 14:36:13 -0500

    Revert "fix: fix chart height and remove z-index from button"
    This reverts commit 1b85a231ff4508ce07586573759a0f8bd90425ae.

--- components/molecules/CardLineChart/card-line-chart.tsx
+++ components/molecules/CardLineChart/card-line-chart.tsx
@@ -1,29 +1,15 @@
 import React from "react";
-import ReactECharts from "echarts-for-react";
-
-/*
-Docs for eChart: https://echarts.apache.org/en/feature.html
-eChart Examples:  https://echarts.apache.org/examples/en/editor.html?c=scatter-simple&lang=ts
-Docs for ReactECharts: https://github.com/hustcc/echarts-for-react
- */
+import EChartWrapper from "components/atoms/EChartWrapper/echart-wrapper";
 
 interface CardLineChartProps {
   lineChartOption: Object;
   className?: string;
-  height?: `${number}px`;
 }
 
-const CardLineChart: React.FC<CardLineChartProps> = ({ lineChartOption, className, height = "130px" }) => {
+const CardLineChart: React.FC<CardLineChartProps> = ({ lineChartOption, className }) => {
   return (
     <>
-      <ReactECharts
-        option={lineChartOption}
-        className={className || ""}
-        notMerge={true}
-        lazyUpdate={true}
-        theme={"theme_name"}
-        style={{ height: height }}
-      />
+      <EChartWrapper option={lineChartOption} className={className} />
     </>
   );
 };
