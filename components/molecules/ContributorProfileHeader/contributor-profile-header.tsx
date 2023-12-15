e72a67eafd30ea6a29c8cb625e29daa4497cec55
Nick Taylor <nick@nickyt.co>
2023-12-15 14:29:44 -0500

    fix: removed empty className props

--- components/molecules/ContributorProfileHeader/contributor-profile-header.tsx
+++ components/molecules/ContributorProfileHeader/contributor-profile-header.tsx
@@ -148,7 +148,6 @@ const ContributorProfileHeader = ({
           <Avatar
             initialsClassName="text-[100px] -translate-y-2.5  leading-none"
             initials={githubName?.charAt(0)}
-            className=""
             hasBorder
             avatarURL={isConnected ? avatarUrl : ""}
             size={184}
@@ -170,7 +169,6 @@ const ContributorProfileHeader = ({
           <Avatar
             initialsClassName="text-[70px] -translate-y-1 leading-none"
             initials={githubName?.charAt(0)}
-            className=""
             hasBorder
             avatarURL={isConnected ? avatarUrl : ""}
             size={120}
@@ -259,7 +257,7 @@ const ContributorProfileHeader = ({
                       title="More options"
                       className="p-2 mr-3 bg-white rounded-full cursor-pointer "
                     >
-                      <TfiMoreAlt size={20} className="" />
+                      <TfiMoreAlt size={20} />
                     </DropdownMenuTrigger>
                   )}
                 </div>
