09cd169bdd420b03b3e28f58390d309a1d1e7bf1
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-12-20 14:06:08 +0100

    chore: update styles for menu items

--- components/Sidebar/sidebar-menu-item.tsx
+++ components/Sidebar/sidebar-menu-item.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import { UsersIcon, ChartBarSquareIcon } from "@heroicons/react/24/solid";
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { ChartBarSquareIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 
 interface MenuItemProps {
@@ -14,9 +15,9 @@ const SidebarMenuItem = ({ title, icon, type, href, isActive }: MenuItemProps) =
   const getIcon = (type: "repo" | "list") => {
     switch (type) {
       case "list":
-        return <UsersIcon className="w-5 h-5 text-slate-400" />;
+        return <UsersIcon className={clsx("w-5 h-5 text-slate-400", isActive && "text-slate-700")} />;
       case "repo":
-        return <ChartBarSquareIcon className="w-5 h-5 text-slate-400" />;
+        return <ChartBarSquareIcon className={clsx("w-5 h-5 text-slate-400", isActive && "text-slate-700")} />;
     }
   };
 
@@ -24,7 +25,7 @@ const SidebarMenuItem = ({ title, icon, type, href, isActive }: MenuItemProps) =
     <li className={clsx("list-none p-2 rounded-md", isActive ? "bg-slate-100" : "")}>
       <Link href={href} className=" flex gap-2 items-center">
         {type && !icon ? getIcon(type) : icon}
-        <span className="text-slate-700">{title}</span>
+        <span className={clsx("text-slate-700 font-medium", isActive && "text-slate-800")}>{title}</span>
       </Link>
     </li>
   );
