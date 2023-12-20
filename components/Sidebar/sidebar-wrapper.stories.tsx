de99d54975dfbcd169296640e24abe23ed1a8a6a
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-12-20 14:17:18 +0100

    chore: add more prototype to the story

--- components/Sidebar/sidebar-wrapper.stories.tsx
+++ components/Sidebar/sidebar-wrapper.stories.tsx
@@ -23,12 +23,54 @@ export const Default: Story = {
           <SidebarMenuItem title="Repositories" href="/repositories" type="repo" />
         </SidebarGroupWrapper>
 
-        <ul>
-          <MenuHeader>Shared with you</MenuHeader>
+        <MenuHeader>Shared with you</MenuHeader>
+        <SidebarGroupWrapper>
           <SidebarMenuItem title="Javascript frameworks" href="/community" type="repo" />
           <SidebarMenuItem title="Staff Picks" href="/trending" type="repo" />
           <SidebarMenuItem title="Potential Hires" href="/trending" type="repo" />
-        </ul>
+        </SidebarGroupWrapper>
+      </>
+    ),
+  },
+};
+
+export const WithActiveItem: Story = {
+  args: {
+    children: (
+      <>
+        <MenuHeader>Your insights</MenuHeader>
+        <SidebarGroupWrapper>
+          <SidebarMenuItem title="Dashboard" href="/dashboard" type="list" isActive />
+          <SidebarMenuItem title="Repositories" href="/repositories" type="repo" />
+        </SidebarGroupWrapper>
+
+        <MenuHeader>Shared with you</MenuHeader>
+        <SidebarGroupWrapper>
+          <SidebarMenuItem title="Javascript frameworks" href="/community" type="repo" />
+          <SidebarMenuItem title="Staff Picks" href="/trending" type="repo" />
+          <SidebarMenuItem title="Potential Hires" href="/trending" type="repo" />
+        </SidebarGroupWrapper>
+      </>
+    ),
+  },
+};
+
+export const WithLoading: Story = {
+  args: {
+    children: (
+      <>
+        <MenuHeader>Your insights</MenuHeader>
+        <SidebarGroupWrapper>
+          <SidebarMenuItem title="Dashboard" href="/dashboard" type="list" />
+          <SidebarMenuItem title="Repositories" href="/repositories" type="repo" />
+        </SidebarGroupWrapper>
+
+        <MenuHeader>Shared with you</MenuHeader>
+        <SidebarGroupWrapper isLoading>
+          <SidebarMenuItem title="Javascript frameworks" href="/community" type="repo" />
+          <SidebarMenuItem title="Staff Picks" href="/trending" type="repo" />
+          <SidebarMenuItem title="Potential Hires" href="/trending" type="repo" />
+        </SidebarGroupWrapper>
       </>
     ),
   },
