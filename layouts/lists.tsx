2486eca4bba1b2e0b073ee44305413baafa3ab84
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-12-21 09:47:30 +0100

    chore: refactor clientonly implementation

--- layouts/lists.tsx
+++ layouts/lists.tsx
@@ -8,7 +8,6 @@ import ListHeader from "components/ListHeader/list-header";
 import TabsList from "components/TabList/tab-list";
 import ComponentDateFilter from "components/molecules/ComponentDateFilter/component-date-filter";
 import { setQueryParams } from "lib/utils/query-params";
-import ClientOnly from "components/atoms/ClientOnly/client-only";
 
 const ListPageLayout = ({
   children,
@@ -37,17 +36,15 @@ const ListPageLayout = ({
       <div className="flex flex-col items-center pt-20 page-container grow md:pt-14">
         <div className="info-container md:px-16 container w-full min-h-[100px]">
           <Header classNames="px-0 md:px-0">
-            <ClientOnly>
-              {list && (
-                <ListHeader
-                  name={list.name}
-                  numberOfContributors={numberOfContributors}
-                  isPublic={list.is_public}
-                  listId={list.id}
-                  isOwner={isOwner}
-                />
-              )}
-            </ClientOnly>
+            {list && (
+              <ListHeader
+                name={list.name}
+                numberOfContributors={numberOfContributors}
+                isPublic={list.is_public}
+                listId={list.id}
+                isOwner={isOwner}
+              />
+            )}
           </Header>
 
           <div className="md:grid md:grid-cols-2 flex flex-col">
