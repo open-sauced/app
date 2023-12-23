d9934487849085a78855b4907690cf20c7cec136
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-12-23 23:30:51 +0530

    remove default base text styling

--- components/molecules/ContributorProfileInfo/contributor-profile-info.tsx
+++ components/molecules/ContributorProfileInfo/contributor-profile-info.tsx
@@ -67,11 +67,11 @@ const ContributorProfileInfo = ({
             <>
               <div className="flex gap-2 items-center">
                 <div className="flex gap-1 items-center">
-                  <span className="font-semibold text-light-slate-12 text-sm"> {followerCount} </span>
+                  <span className="font-semibold text-sm"> {followerCount} </span>
                   <span className="font-normal text-light-slate-11 text-sm"> followers</span>
                 </div>
                 <div className="flex gap-1 items-center">
-                  <span className="font-semibold text-light-slate-12 text-sm"> {followingCount} </span>
+                  <span className="font-semibold text-sm"> {followingCount} </span>
                   <span className="font-normal text-light-slate-11 text-sm"> following</span>
                 </div>
               </div>
