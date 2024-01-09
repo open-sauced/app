b385da3a8b891a8ac8f5ebbac2e8b1c0a240dcb5
Nick Taylor <nick@nickyt.co>
2024-01-08 22:04:13 -0500

    chore: generalized middleware matcher to match anything under /hub

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ import { pathToRegexp } from "path-to-regexp";
 // prettier-ignore
 const pathsToMatch = [
   "/",
-  "/hub/insights/:path*",
+  "/hub/:path*",
   "/feed/",
   "/user/notifications",
   "/user/settings",
