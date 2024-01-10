cf9be331e97529a1fa19b9924de23c33556b5856
Brandon Roberts <robertsbt@gmail.com>
2024-01-10 15:31:41 -0600

    feat: connect most active contributors graph to API v2 endpoint

--- lib/hooks/api/useMostActiveContributors.ts
+++ lib/hooks/api/useMostActiveContributors.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 import useSWR, { Fetcher } from "swr";
 import { useRouter } from "next/router";
 
-import { publicApiFetcher } from "lib/utils/public-api-fetcher";
+import { v2PublicApiFetcher } from "lib/utils/public-api-fetcher";
 import { ContributorStat } from "components/molecules/MostActiveContributorsCard/most-active-contributors-card";
 import { ContributorType } from "components/Graphs/shared/contributor-type-filter";
 
@@ -44,7 +44,7 @@ const useMostActiveContributors = ({
 
   const { data, error, mutate } = useSWR<PaginatedResponse, Error>(
     listId ? apiEndpoint : null,
-    publicApiFetcher as Fetcher<PaginatedResponse, Error>
+    v2PublicApiFetcher as Fetcher<PaginatedResponse, Error>
   );
 
   return {
