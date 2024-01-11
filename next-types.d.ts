4f6d6ad46b759ba3d6846c1245312c157d952667
Nick Taylor <nick@nickyt.co>
2024-01-11 17:16:43 -0500

    chore: added the Workspace type

--- next-types.d.ts
+++ next-types.d.ts
@@ -398,3 +398,12 @@ interface GhOrgTeamMember {
   id: number;
   login: string;
 }
+
+interface Workspace {
+  id: string;
+  name: string;
+  description: string;
+  created_at: string | null;
+  updated_at: string | null;
+  deleted_at: string | null;
+}
