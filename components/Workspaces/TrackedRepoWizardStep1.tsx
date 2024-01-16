37665138d2f74830448e8c320ba1a9f8dbd561e7
Nick Taylor <nick@nickyt.co>
2024-01-16 18:03:56 -0500

    chore: style tweaks

--- components/Workspaces/TrackedRepoWizardStep1.tsx
+++ components/Workspaces/TrackedRepoWizardStep1.tsx
@@ -20,7 +20,7 @@ export const TrackedRepoWizard = ({
   trackedReposCount,
 }: TrackedRepoWizardProps) => {
   return (
-    <Card className="p-0 w-max max-w-2xl">
+    <Card className="p-0 w-max max-w-3xl">
       <button className="flex gap-1 items-center ml-4 mt-4 border border-transparent" onClick={onCancel}>
         <FaArrowLeft /> back
       </button>
@@ -29,24 +29,24 @@ export const TrackedRepoWizard = ({
           <h2 className="font-semibold mb-4">Add repositories to track</h2>
           <div className="grid gap-6 md:grid-cols-2" data-tracked-repo-wizard>
             <button
-              className="flex flex-col gap-2 text-light-slate-12 p-8 border rounded-lg items-left focus-visible:!border-green-800 focus-visible:!ring-green-100"
+              className="flex flex-col text-light-slate-12 p-8 border rounded-lg focus-visible:!border-green-800 focus-visible:!ring-green-100"
               onClick={onSearchRepos}
             >
-              <FaSearch className="text-purple-800 " />
+              <FaSearch className="text-purple-800 mb-2" />
               <span data-button-title className="font-semibold">
                 Search for repositories
               </span>
-              <span>Search for specific repositories to track on your workspace.</span>
+              <span className="text-left">Search for specific repositories to track on your workspace.</span>
             </button>
             <button
-              className="flex flex-col gap-2 text-light-slate-12 p-8 border rounded-lg items-left focus-visible:!border-green-800 focus-visible:!ring-green-100"
+              className="flex flex-col text-light-slate-12 p-8 border rounded-lg focus-visible:!border-green-800 focus-visible:!ring-green-100"
               onClick={onImportOrg}
             >
-              <BiBarChartAlt2 className="text-purple-800" />
+              <BiBarChartAlt2 className="text-purple-800 mb-2" />
               <span data-button-title className="font-semibold">
                 Import a Github Organization
               </span>
-              <span>Search for organizations on Github and import any or all repositories.</span>
+              <span className="text-left">Search for organizations on Github and import any or all repositories.</span>
             </button>
           </div>
         </div>
