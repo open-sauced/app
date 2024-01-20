0af846b6369d823c9d3d9231985b80530eed5d83
Nick Taylor <nick@nickyt.co>
2024-01-20 00:39:56 -0500

    chore: removed dead code

--- components/Workspaces/SearchReposTable.tsx
+++ components/Workspaces/SearchReposTable.tsx
@@ -1,6 +1,3 @@
-import { BiBarChartAlt2 } from "react-icons/bi";
-import { FaPlus } from "react-icons/fa6";
-import Button from "components/atoms/Button/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "components/shared/Table";
 import { Avatar } from "components/atoms/Avatar/avatar-hover-card";
 import Checkbox from "components/atoms/Checkbox/checkbox";
@@ -12,27 +9,9 @@ interface SearchedReposTableProps {
   onSelect: (value: string) => void;
 }
 
-const EmptyState = ({ onAddRepos }: { onAddRepos: () => void }) => {
-  return (
-    <div className="grid place-content-center gap-4 my-8">
-      <BiBarChartAlt2 className="border rounded p-1 w-12 h-12 mx-auto" />
-      <div className="grid w-max max-w-sm mx-auto">
-        <span className="text-center text-lg font-semibold mb-2">Add repositories to track</span>
-        <p className="text-sm text-center">
-          Search and select the repositories you want to track and get insights on your entire Github ecosystem
-        </p>
-      </div>
-      <Button variant="primary" className="w-max mx-auto" onClick={onAddRepos}>
-        <FaPlus className="mr-2 text-lg" />
-        Add repositories
-      </Button>
-    </div>
-  );
-};
-
 export const SearchedReposTable = ({ repositories = [], onFilter, onSelect }: SearchedReposTableProps) => {
   return (
-    <div className="border border-light-slate-7 rounded-lg">
+    <div className="border border-light-slate-7 rounded-lg max-w-lg">
       <Table className="not-sr-only">
         <TableHeader>
           <TableRow className="bg-light-slate-3">
