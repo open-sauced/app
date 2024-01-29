bc95c81c60bc2b345c6d2b9c62a1607af7b2fbef
Nick Taylor <nick@nickyt.co>
2024-01-29 10:20:32 -0500

    chore: added repository stats card component

---
+++ components/Workspaces/RepositoryStatCard.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { RepositoryStatCard } from "./RepositoryStatCard";
+
+type Story = StoryObj<typeof RepositoryStatCard>;
+
+const meta: Meta<typeof RepositoryStatCard> = {
+  title: "Components/Workspaces/Repository Stat Card",
+  component: RepositoryStatCard,
+  args: {
+    isLoading: false,
+  },
+};
+
+export default meta;
+
+export const PullRequests: Story = {
+  args: {
+    type: "pulls",
+    stats: { opened: 10, merged: 5, velocity: 2 },
+  },
+};
+
+export const Issues: Story = {
+  args: {
+    type: "issues",
+    stats: { opened: 10, closed: 5, velocity: 0.5 },
+  },
+};
+
+export const Engagement: Story = {
+  args: {
+    type: "engagement",
+    stats: { stars: 10, forks: 5, health: 2 },
+  },
+};
+
+export const PullRequestsLoading: Story = {
+  args: {
+    type: "pulls",
+    isLoading: true,
+  },
+};
+
+export const IssuesLoading: Story = {
+  args: {
+    type: "issues",
+    isLoading: true,
+  },
+};
+
+export const EngagementLoading: Story = {
+  args: {
+    type: "engagement",
+    isLoading: true,
+  },
+};
