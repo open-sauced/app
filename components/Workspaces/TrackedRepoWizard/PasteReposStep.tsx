22d61a3a6d4fa99ac7aa16ed16495562cb801fef
Zeu Capua <coding@zeu.dev>
2024-02-07 10:53:00 -0800

    error checking for empty text area, remove empty lines in input

--- components/Workspaces/TrackedRepoWizard/PasteReposStep.tsx
+++ components/Workspaces/TrackedRepoWizard/PasteReposStep.tsx
@@ -9,8 +9,12 @@ export const PasteReposStep = ({ onBulkAddRepos }: PasteReposStepProps) => {
   const [pastedInput, setPastedInput] = useState("");
 
   const parseInput = () => {
-    // split each line into a trimmed string
-    const repos = pastedInput.split("\n").map((line) => line.trim());
+    // split each line into a trimmed string and filter out any empty lines
+    const repos = pastedInput
+      .split("\n")
+      .map((line) => line.trim())
+      .filter((line) => line !== "");
+
     onBulkAddRepos(repos);
   };
 
@@ -23,7 +27,12 @@ export const PasteReposStep = ({ onBulkAddRepos }: PasteReposStepProps) => {
         placeholder="Paste repositories here line by line (e.g. open-sauced/app)"
         className="p-4 border rounded-xl h-full"
       />
-      <Button variant="primary" className="w-fit self-end" onClick={parseInput}>
+      <Button
+        onClick={parseInput}
+        variant="primary"
+        disabled={pastedInput.trim().length === 0}
+        className="w-fit self-end"
+      >
         Import
       </Button>
     </div>
