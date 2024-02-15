b64dfbc428fc44a811d63bd8e195eb672e3db0be
Brandon Roberts <robertsbt@gmail.com>
2024-02-15 12:01:42 -0600

    fix: use workspace insight endpoint when workspaceId is provided

--- components/organisms/InsightPage/InsightPage.tsx
+++ components/organisms/InsightPage/InsightPage.tsx
@@ -201,22 +201,26 @@ const InsightPage = ({ edit, insight, pageRepos, workspaceId }: InsightPageProps
       toast({ description: "You must be logged in to create a page", variant: "danger" });
       return;
     }
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/insights`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${sessionToken}`,
-      },
-      body: JSON.stringify({
-        name,
-        repos: repos.map((repo) => ({ id: repo.id, fullName: repo.full_name })),
-        is_public: isPublic,
-      }),
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/${workspaceId ? `workspaces/${workspaceId}` : user}/insights`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${sessionToken}`,
+        },
+        body: JSON.stringify({
+          name,
+          repos: repos.map((repo) => ({ id: repo.id, fullName: repo.full_name })),
+          is_public: isPublic,
+        }),
+      }
+    );
     setCreateLoading(false);
     if (response.ok) {
       const { id } = await response.json();
       toast({ description: "Page created successfully", variant: "success" });
+      // TODO: redirect to workspaces page
       router.push(`/pages/${user.user_metadata.user_name}/${id}/dashboard`);
     }
 
@@ -502,7 +506,7 @@ const InsightPage = ({ edit, insight, pageRepos, workspaceId }: InsightPageProps
       <div className="flex flex-col gap-8">
         <div className="pb-6 border-b border-light-slate-8">
           <Title className="!text-2xl !leading-none mb-4" level={1}>
-            {edit ? "Update" : "Create New"} Insight Page
+            {edit ? "Update" : "Create New"} {workspaceId ? "Repository Insight" : "Insight Page"}
           </Title>
           <Text className="my-8">
             An insight page is a dashboard containing selected repositories that you and your team can get insights
