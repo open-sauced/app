9b2ed44bb4cecae8028ba65f21f9f7ea1e1cd804
Brandon Roberts <robertsbt@gmail.com>
2024-02-16 10:47:58 -0600

    feat: add repository insight view pages to workspaces

---
+++ pages/workspaces/[workspaceId]/repository-insights/[insightId]/dashboard.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import { GetServerSidePropsContext } from "next";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+
+import SEO from "layouts/SEO/SEO";
+import fetchSocialCard from "lib/utils/fetch-social-card";
+import getInsightTeamMemberAccess from "lib/utils/get-insight-team-member";
+import { MemberAccess } from "components/molecules/TeamMembersConfig/team-members-config";
+import useInsightRepositories from "lib/hooks/useInsightRepositories";
+import { WorkspaceLayout } from "components/Workspaces/WorkspaceLayout";
+import HubPageLayout from "layouts/hub-page";
+import Dashboard from "components/organisms/Dashboard/dashboard";
+
+interface InsightPageProps {
+  insight: DbUserInsight;
+  ogImage?: string;
+  workspaceId: string;
+}
+
+const HubPage = ({ insight, ogImage, workspaceId }: InsightPageProps) => {
+  const { data: insightRepos } = useInsightRepositories(insight.id);
+  const repositories = insightRepos.map((repo) => repo.repo_id);
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) {
+    return (
+      <>
+        <SEO
+          title={`${insight.name} | Open Sauced Insights `}
+          description={`${insight.name} Insights on OpenSauced`}
+          image={ogImage}
+          twitterCard="summary_large_image"
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <SEO
+        title={`${insight.name} | Open Sauced Insights`}
+        description={`${insight.name} Insights on OpenSauced`}
+        image={ogImage}
+        twitterCard="summary_large_image"
+      />
+      <WorkspaceLayout workspaceId={workspaceId}>
+        <HubPageLayout page="dashboard">
+          <Dashboard repositories={repositories} />
+        </HubPageLayout>
+      </WorkspaceLayout>
+    </>
+  );
+};
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const supabase = createPagesServerClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const bearerToken = session ? session.access_token : "";
+  const workspaceId = ctx.params!["workspaceId"] as string;
+  const insightId = ctx.params!["insightId"] as string;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/insights/${insightId}?include=none`);
+  const insight = response.ok ? ((await response.json()) as DbUserInsight) : null;
+
+  if (!insight) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  const userId = session?.user?.user_metadata.sub as string;
+  let teamMemberAccess: MemberAccess | null = null;
+
+  if (!insight.is_public) {
+    // check if user is insight page team member
+    teamMemberAccess = await getInsightTeamMemberAccess(Number(insightId), bearerToken, userId);
+  }
+
+  if (!insight.is_public && (!teamMemberAccess || teamMemberAccess === "pending")) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  // Keeping this here so we are sure the page is not private before we fetch the social card.
+  const ogImage = await fetchSocialCard(`insights/${insightId}`);
+
+  return {
+    props: {
+      insight,
+      workspaceId,
+      ogImage,
+    },
+  };
+};
+
+export default HubPage;
