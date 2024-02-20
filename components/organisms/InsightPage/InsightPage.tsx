5ea857762520b1614633696f8349a2669c50b3ad
mani pandit <manivpandit18@gmail.com>
2024-02-20 11:20:22 +0530

    fix: add Save and Create btn to maintain uniformity

--- components/organisms/InsightPage/InsightPage.tsx
+++ components/organisms/InsightPage/InsightPage.tsx
@@ -505,9 +505,21 @@ const InsightPage = ({ edit, insight, pageRepos, workspaceId }: InsightPageProps
     <section className="flex flex-col justify-center w-full py-4 xl:flex-row xl:gap-20 xl:pl-28 ">
       <div className="flex flex-col gap-8">
         <div className="pb-6 border-b border-light-slate-8">
-          <Title className="!text-2xl !leading-none mb-4" level={1}>
-            {edit ? "Update" : "Create New"} {workspaceId ? "Repository Insight" : "Insight Page"}
-          </Title>
+          <div className="flex justify-between items-center">
+            <Title className="!text-2xl !leading-none mb-4" level={1}>
+              {edit ? "Update" : "Create New"} {workspaceId ? "Repository Insight" : "Insight Page"}
+            </Title>
+            <Button
+              variant="primary"
+              type="submit"
+              className="mb-4 text-white bg-light-orange-9 rounded-lg text-sm"
+              loading={createLoading}
+              disabled={disableCreateButton() || repos.length === 0}
+              onClick={edit ? handleUpdateInsightPage : handleCreateInsightPage}
+            >
+              {edit ? "Update Page" : "Create Page"}
+            </Button>
+          </div>
           <Text className="my-8">
             An insight page is a dashboard containing selected repositories that you and your team can get insights
             from.
