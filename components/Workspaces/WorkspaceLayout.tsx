f40ce82c741de2e97b6aaee8d09da18bfe0d2901
Nick Taylor <nick@nickyt.co>
2024-02-23 08:47:38 -0500

    fix: fixed hydration error caused by <AppSidebar /> component

--- components/Workspaces/WorkspaceLayout.tsx
+++ components/Workspaces/WorkspaceLayout.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import TopNav from "components/organisms/TopNav/top-nav";
 import { AppSideBar } from "components/shared/AppSidebar/AppSidebar";
 import { useMediaQuery } from "lib/hooks/useMediaQuery";
+import ClientOnly from "components/atoms/ClientOnly/client-only";
 
 interface WorkspaceLayoutProps {
   workspaceId: string | null;
@@ -24,17 +25,19 @@ export const WorkspaceLayout = ({ workspaceId, children }: WorkspaceLayoutProps)
         <TopNav />
       </div>
       <div className="relative px-8 pb-20">
-        <span ref={sidebarRef}>
-          <AppSideBar workspaceId={workspaceId} hideSidebar={hideSidebar} sidebarCollapsed={showingSidebar} />
-        </span>
-        {!showingSidebar && (
-          <button
-            onClick={() => setShowingSidebar(true)}
-            className="fixed z-50 border-8 border-solid border-black inset-x-0 top-16 w-fit bg-white rounded-r-md shadow-lg p-2"
-          >
-            <LuArrowRightToLine className="w-4 h-4 text-gray-500" />
-          </button>
-        )}
+        <ClientOnly>
+          <span ref={sidebarRef}>
+            <AppSideBar workspaceId={workspaceId} hideSidebar={hideSidebar} sidebarCollapsed={showingSidebar} />
+          </span>
+          {!showingSidebar && (
+            <button
+              onClick={() => setShowingSidebar(true)}
+              className="fixed z-50 border-8 border-solid border-black inset-x-0 top-16 w-fit bg-white rounded-r-md shadow-lg p-2"
+            >
+              <LuArrowRightToLine className="w-4 h-4 text-gray-500" />
+            </button>
+          )}
+        </ClientOnly>
         {children}
       </div>
     </div>
