c6bbca9f549c2e28b9fa4019b6b6a8c276ee3eb6
Brandon Roberts <robertsbt@gmail.com>
2024-02-27 13:44:06 -0600

    fix: only show workspace edit button for workspace owner

--- components/Workspaces/WorkspaceHeader.tsx
+++ components/Workspaces/WorkspaceHeader.tsx
@@ -1,12 +1,17 @@
 import { SquareFillIcon } from "@primer/octicons-react";
 import { FaEdit } from "react-icons/fa";
 import Button from "components/atoms/Button/button";
+import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
 
 interface WorkspaceHeaderProps {
   workspace: Workspace;
 }
 
 export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
+  const { userId } = useSupabaseAuth();
+  const isOwner =
+    userId && workspace.members.find((member) => Number(member.user_id) === Number(userId) && member.role === "owner");
+
   return (
     <section className="w-full grid lg:grid-cols-[1fr,auto] gap-2">
       <h1 className="flex gap-2 items-center uppercase text-2xl md:text-3xl font-semibold w-max max-w-[20ch] sm:max-w-[35ch] lg:max-w-2xl xl:max-w-4xl">
@@ -16,14 +21,16 @@ export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
           {workspace.name}
         </span>
       </h1>
-      <Button
-        variant="primary"
-        href={`/workspaces/${workspace.id}/settings`}
-        className="my-auto gap-2 items-center shrink-0 place-self-end"
-      >
-        <FaEdit />
-        Edit
-      </Button>
+      {isOwner && (
+        <Button
+          variant="primary"
+          href={`/workspaces/${workspace.id}/settings`}
+          className="my-auto gap-2 items-center shrink-0 place-self-end"
+        >
+          <FaEdit />
+          Edit
+        </Button>
+      )}
     </section>
   );
 };
