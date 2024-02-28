b353ba8aa6bfd9c9513e00bf9f3b547ab7e01b46
Zeu Capua <coding@zeu.dev>
2024-02-28 13:56:54 -0800

    change repo limit to 100

--- pages/workspaces/[workspaceId]/repository-insights/[insightId]/dashboard.tsx
+++ pages/workspaces/[workspaceId]/repository-insights/[insightId]/dashboard.tsx
@@ -24,7 +24,7 @@ const HubPage = ({ insight, isOwner, ogImage, workspaceId }: InsightPageProps) =
 
   const { hasReports } = useSession(true); // to check if the user is a PRO account
   const { data: isWorkspaceUpgraded } = useIsWorkspaceUpgraded({ workspaceId });
-  const showBanner = isOwner && !hasReports && !isWorkspaceUpgraded && repositories.length > 0;
+  const showBanner = isOwner && !hasReports && !isWorkspaceUpgraded && repositories.length > 100;
 
   useEffect(() => {
     setHydrated(true);
