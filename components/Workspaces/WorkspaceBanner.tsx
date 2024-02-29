cb6b27925fe15b6d51ca225b2f829cc13fa24d54
Zeu Capua <coding@zeu.dev>
2024-02-29 13:17:24 -0800

    add underline

--- components/Workspaces/WorkspaceBanner.tsx
+++ components/Workspaces/WorkspaceBanner.tsx
@@ -8,7 +8,7 @@ export default function WorkspaceBanner({ openModal }: WorkspaceBannerProps) {
       onClick={openModal}
       className="absolute top-0 inset-x-0 w-full h-fit px-4 py-2 bg-light-orange-10 text-white"
     >
-      This insight page is over the free limit. <span className="font-bold">Upgrade to a PRO Workspace.</span>
+      This insight page is over the free limit. <span className="font-bold underline">Upgrade to a PRO Workspace.</span>
     </button>
   );
 }
