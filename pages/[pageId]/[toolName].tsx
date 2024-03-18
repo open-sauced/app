2bacc0a72f2498c78fd2a6e5931a2963028a7c6a
Zeu Capua <coding@zeu.dev>
2024-03-18 12:29:35 -0700

    Revert "remove catchall and /pages subfolder routing"
    This reverts commit 05bc31774fdf54ca7ed887188d516f5d4dc27043.

---
+++ pages/[pageId]/[toolName].tsx
@@ -0,0 +1,50 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { GetServerSidePropsContext } from "next";
+import Tool from "components/organisms/ToolsDisplay/tools-display";
+import changeCapitalization from "lib/utils/change-capitalization";
+import FilterLayout from "../../layouts/filter";
+import { WithPageLayout } from "../../interfaces/with-page-layout";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const pageId = ctx.params!["pageId"] as string;
+  const toolName = ctx.params!["toolName"] as string;
+  const selectedFilter = ctx.params!["selectedFilter"] as string;
+
+  if (toolName === "repositories") {
+    return {
+      redirect: {
+        destination: `/${pageId}/dashboard/${selectedFilter}`,
+        permanent: true,
+      },
+    };
+  }
+
+  return {
+    props: {
+      pageId,
+      toolName,
+    },
+  };
+};
+const Filter: WithPageLayout = () => {
+  const router = useRouter();
+
+  const { pageId, toolName } = router.query;
+
+  const title = `OpenSauced Insights ${pageId ? ` - ${changeCapitalization(pageId!.toString(), true)}` : ""} ${
+    toolName ? ` / ${changeCapitalization(toolName.toString(), true)}` : ""
+  }`;
+
+  useEffect(() => {
+    Filter.updateSEO!({
+      title: title,
+    });
+  }, [title]);
+
+  return <Tool tool={toolName ? changeCapitalization(toolName.toString(), true) : undefined} />;
+};
+
+Filter.PageLayout = FilterLayout;
+
+export default Filter;
---
+++ pages/[pageId]/index.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import Loader from "components/templates/Loader/loader";
+
+const Filter: React.FC = () => {
+  const router = useRouter();
+
+  const { pageId: topic } = router.query;
+
+  useEffect(() => {
+    if (topic) {
+      router.push(`${topic}/dashboard/filter/recent`).then(() => {});
+    }
+  }, [router, topic]);
+
+  return <Loader theme={"dark"} />;
+};
+
+export default Filter;
