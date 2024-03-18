0b2cecebe9a79806620466a195cf8f27827064f5
Nick Taylor <nick@nickyt.co>
2024-03-18 19:10:49 -0400

    chore: centered error message

--- pages/500.tsx
+++ pages/500.tsx
@@ -14,29 +14,15 @@ export default function Custom500() {
           gridTemplateRows: "auto 1fr auto",
         }}
       >
-        <div className="grid items-center justify-center place-content-start py-7 px-4 z-50 md:grid-flow-col  md:justify-between">
+        <div className="grid items-center justify-center place-content-start py-7 px-4 md:grid-flow-col  md:justify-between">
           <HeaderLogo withBg={false} />
         </div>
-        <main className="grid md:grid-cols-2 place-content-center py-6">
-          <div className="text-center px-6 relative z-10">
-            <h1 className="text-8xl font-bold mb-2">500</h1>
-            <div className="text-3xl mb-2">uh oh! looks like there&apos;s an issue with the pizza oven</div>
-            <Link href="/" className="text-orange-600 hover:text-orange-500">
-              Take me home &rarr;
-            </Link>
-          </div>
-          <div className="hidden md:grid">
-            <div
-              className="grid"
-              style={{
-                position: "absolute",
-                top: 0,
-                right: 0,
-                bottom: 0,
-                width: "50%",
-              }}
-            ></div>
-          </div>
+        <main className="grid place-content-center p-6">
+          <h1 className="text-8xl font-bold mb-2">500</h1>
+          <div className="text-3xl mb-2">uh oh! looks like there&apos;s an issue with the pizza oven</div>
+          <Link href="/" className="text-orange-600 hover:text-orange-500">
+            Take me home &rarr;
+          </Link>
         </main>
       </div>
     </FullHeightContainer>
