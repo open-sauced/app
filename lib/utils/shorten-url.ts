92d2f16c98c8aebbeb68a4d67128f529a80bd9cd
Brandon Roberts <robertsbt@gmail.com>
2024-03-20 15:52:58 -0600

    chore: log error to Sentry if URL shortener fails

--- lib/utils/shorten-url.ts
+++ lib/utils/shorten-url.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/nextjs";
+
 export async function shortenUrl(url: string) {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/url/shorten?url=${encodeURIComponent(url)}`);
@@ -6,7 +8,9 @@ export async function shortenUrl(url: string) {
       const data = (await response.json()) as { shortUrl: string };
       return data.shortUrl;
     }
-  } catch (e) {}
+  } catch (e) {
+    Sentry.captureException(new Error(`Failed to shorten URL ${url}`, { cause: e }));
+  }
 
   return url;
 }
