b4a68b8c1b6e799bcd378d33889040b0aeb93e6d
Nick Taylor <nick@nickyt.co>
2024-03-25 19:13:13 -0400

    test: add some E2E tests for explore pages

---
+++ e2e/explore-page.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, Page } from "@playwright/test";
+
+type TabId = "Dashboard" | "Contributors" | "Activity";
+const TabSelectorNames: Record<TabId, string | RegExp> = {
+  Dashboard: "Dashboard",
+  Contributors: /Contributors\s+\d+/,
+  Activity: "Activity",
+};
+
+async function checkExploreTabs(page: Page, tabId: TabId) {
+  await expect(page.getByRole("link", { name: TabSelectorNames["Dashboard"] })).toBeVisible();
+  await expect(page.getByRole("link", { name: TabSelectorNames["Contributors"] })).toBeVisible();
+  await expect(page.getByRole("link", { name: TabSelectorNames["Activity"] })).toBeVisible();
+
+  await expect(page.getByRole("tab", { name: TabSelectorNames[tabId], exact: true })).toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+}
+
+test("Loads explore dashboard page", async ({ page }) => {
+  await page.goto("/explore/topic/javascript");
+
+  await checkExploreTabs(page, "Dashboard");
+});
+
+test("Loads explore contributors page", async ({ page }) => {
+  await page.goto("/explore/topic/javascript/contributors");
+
+  await checkExploreTabs(page, "Contributors");
+});
+
+test("Loads explore activity page", async ({ page }) => {
+  await page.goto("/explore/topic/javascript/activity");
+
+  await checkExploreTabs(page, "Activity");
+});
