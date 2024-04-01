3597286fe100200195f80b34f180ec9ab0f24213
Zeu Capua <coding@zeu.dev>
2024-04-01 12:04:15 -0700

    working cumulative

--- components/Graphs/StarsChart.tsx
+++ components/Graphs/StarsChart.tsx
@@ -1,22 +1,35 @@
 import { FaStar } from "react-icons/fa6";
-import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar, Tooltip, type TooltipProps } from "recharts";
+import {
+  ResponsiveContainer,
+  BarChart,
+  LineChart,
+  XAxis,
+  YAxis,
+  Bar,
+  Tooltip,
+  type TooltipProps,
+  Line,
+} from "recharts";
 import { ValueType, NameType } from "recharts/types/component/DefaultTooltipContent";
 import { useState } from "react";
 import Card from "components/atoms/Card/card";
 
-import { getDailyStarsHistogramToDays } from "lib/utils/repo-page-utils";
+import { getCumulativeStarsHistogramToDays, getDailyStarsHistogramToDays } from "lib/utils/repo-page-utils";
 import { type StatsType } from "lib/hooks/api/useFetchMetricStats";
 import Button from "components/shared/Button/button";
 
 type StarsChartProps = {
   stats: StatsType[] | undefined;
+  total: number;
   syncId: string;
   range: number;
 };
 
-export default function StarsChart({ stats, syncId, range = 30 }: StarsChartProps) {
+export default function StarsChart({ stats, total, syncId, range = 30 }: StarsChartProps) {
   const [category, setCategory] = useState<"daily" | "cumulative">("daily");
   const dailyData = getDailyStarsHistogramToDays({ stats, range });
+  const cumulativeData = getCumulativeStarsHistogramToDays({ stats, total, range });
+
   return (
     <Card className="flex flex-col gap-8 w-full lg:min-w-[64rem] h-full items-center pt-8 px-8">
       <section className="flex w-full items-center justify-between">
@@ -35,13 +48,18 @@ export default function StarsChart({ stats, syncId, range = 30 }: StarsChartProp
       <ResponsiveContainer width="100%" height={300}>
         {category === "daily" ? (
           <BarChart data={dailyData} syncId={syncId}>
-            <XAxis dataKey="bucket" tick={false} padding={{ left: 12, right: 12 }} />
+            <XAxis dataKey="bucket" tick={false} />
             <YAxis interval={1} />
             <Tooltip content={CustomTooltip} filterNull={false} />
             <Bar dataKey="star_count" fill="#FF5100" />
           </BarChart>
         ) : (
-          <p>Hi</p>
+          <LineChart data={cumulativeData} syncId={syncId}>
+            <XAxis dataKey="bucket" tick={false} />
+            <YAxis domain={["auto", "auto"]} />
+            <Tooltip content={CustomTooltip} filterNull={false} />
+            <Line dataKey="star_count" stroke="#FF5100" />
+          </LineChart>
         )}
       </ResponsiveContainer>
     </Card>
--- lib/utils/repo-page-utils.ts
+++ lib/utils/repo-page-utils.ts
@@ -45,3 +45,59 @@ export function getDailyStarsHistogramToDays({ stats, range }: { stats: StatsTyp
   result.reverse();
   return result;
 }
+
+export function getCumulativeStarsHistogramToDays({
+  stats,
+  range,
+  total,
+}: {
+  stats: StatsType[] | undefined;
+  range: number;
+  total: number;
+}) {
+  let currentTotal = total;
+  let previousCount = 0;
+
+  const today = new Date();
+  const allDays = stats?.reduce((days: { [count: number]: StatsType }, current: StatsType) => {
+    const count = differenceInDays(today, new Date(current.bucket));
+
+    if (days[count]) {
+      days[count].star_count! += currentTotal - current.star_count!;
+    } else {
+      if (count - previousCount > 1) {
+        for (let missed = 1; missed < count - previousCount; missed++) {
+          const missingDay = new Date();
+          missingDay.setDate(today.getDate() - (previousCount + missed));
+          days[previousCount + missed] = {
+            bucket: missingDay.toLocaleDateString(),
+            star_count: currentTotal,
+          };
+        }
+      }
+      const newDay = new Date();
+      newDay.setDate(today.getDate() - count);
+      days[count] = { bucket: newDay.toLocaleDateString(), star_count: currentTotal - current.star_count! };
+    }
+
+    previousCount = count;
+    currentTotal -= current.star_count!;
+    return days;
+  }, {});
+
+  // convert to array
+  const result: StatsType[] = [];
+  currentTotal = total;
+  for (let i = 0; i < range; i++) {
+    let temp = allDays?.[i];
+    if (!temp) {
+      const today = new Date();
+      temp = { bucket: new Date(today.setDate(today.getDate() - i)).toLocaleDateString(), star_count: currentTotal };
+    }
+    result.push(temp);
+    currentTotal = temp.star_count!;
+  }
+  result.reverse();
+
+  return result;
+}
