8956ad92b9b0e3574e2268c2374b0ec59aba6771
Nick Taylor <nick@nickyt.co>
2024-04-03 14:39:04 -0400

    chore: fixed the flippening

--- components/Graphs/MetricCard.tsx
+++ components/Graphs/MetricCard.tsx
@@ -1,4 +1,5 @@
 import EChartsReact from "echarts-for-react";
+import { useMemo } from "react";
 import { StatsType } from "lib/hooks/api/useFetchMetricStats";
 import Card from "components/atoms/Card/card";
 import humanizeNumber from "lib/utils/humanizeNumber";
@@ -11,36 +12,41 @@ type MetricCardProps = {
 
 export default function MetricCard({ stats, variant, range }: MetricCardProps) {
   const countProperty = variant === "stars" ? "star_count" : "forks_count";
-  const seriesData = stats?.map((stat) => stat[countProperty] ?? 0) ?? [];
-  const bucketData = stats?.map((stat) => new Date(stat.bucket).toDateString()) ?? [];
+  const seriesData = useMemo(() => stats?.map((stat) => stat[countProperty] ?? 0) ?? [], [countProperty, stats]);
+  const bucketData = useMemo(() => stats?.map((stat) => new Date(stat.bucket).toDateString()) ?? [], [stats]);
 
-  const option = {
-    xAxis: {
-      type: "category",
-      data: bucketData,
-      show: false,
-    },
-    yAxis: {
-      type: "value",
-      show: false,
-    },
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        type: "shadow",
+  const option = useMemo(() => {
+    return {
+      xAxis: {
+        type: "category",
+        data: bucketData,
+        show: false,
       },
-    },
-    series: [
-      {
-        data: seriesData,
-        symbol: "none",
-        type: variant === "stars" ? "line" : "bar",
+      yAxis: {
+        type: "value",
+        show: false,
       },
-    ],
-    color: "hsla(19, 100%, 50%, 1)",
-  };
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      series: [
+        {
+          data: seriesData,
+          symbol: "none",
+          type: variant === "stars" ? "line" : "bar",
+        },
+      ],
+      color: "hsla(19, 100%, 50%, 1)",
+    };
+  }, [bucketData, seriesData, variant]);
 
-  const total = seriesData?.reduce((stat, currentValue) => (stat || 0) + (currentValue || 0), 0);
+  const total = useMemo(
+    () => seriesData?.reduce((stat, currentValue) => (stat || 0) + (currentValue || 0), 0),
+    [seriesData]
+  );
 
   return (
     <Card className="w-full xl:max-w-lg h-fit p-5 pl-6">
