868c21db52a8207035d9dc9ad9d872dd28a1be10
Nick Taylor <nick@nickyt.co>
2024-04-05 09:23:53 -0400

    test: added tests for a repository page OG image

--- e2e/repo-page.spec.ts
+++ e2e/repo-page.spec.ts
@@ -6,3 +6,12 @@ test("Loads a repository page", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "open-sauced/app", exact: true })).toBeVisible();
   await expect(page.getByRole("combobox", { name: "Period:", exact: true, expanded: false })).toBeVisible();
 });
+
+test("repository page has an OG image", async ({ page }) => {
+  await page.goto("/s/open-sauced/app");
+  const expectedUrl = "http://localhost:3000/og-images/repository/open-sauced/app/30?description=%F0%9F%8D%95+The+path+to+your+next+contribution."
+
+  await expect(page.locator('meta[property="og:image"]')).toHaveAttribute('content', expectedUrl)
+  await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute('content', expectedUrl)
+  await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute('content', 'summary_large_image')
+});
