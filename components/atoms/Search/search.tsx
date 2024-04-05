50ef5cfc1e0a092f6b206780a3bc835d6671ee71
Ayu Adiati <retno.ayu98@gmail.com>
2024-04-05 04:31:59 +0200

    fix: change max width of span from 12.3 to 13rem

--- components/atoms/Search/search.tsx
+++ components/atoms/Search/search.tsx
@@ -145,7 +145,7 @@ const Search = ({
                 className={clsx(
                   cursor === index && "_cursorActive bg-slate-100",
                   "px-4 py-2 hover:bg-light-slate-2",
-                  "[&_span]:max-w-[12.3rem]"
+                  "[&_span]:max-w-[13rem]"
                 )}
                 style={suggestionsStyle}
                 key={index}
