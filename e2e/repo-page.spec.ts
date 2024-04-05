b7156687a58a31558ccbfc9c518a9eb4de8e5f10
Nick Taylor <nick@nickyt.co>
2024-04-05 09:36:29 -0400

    chore: cleaned up tests to use Playwright config base URL for OG image checks

--- e2e/repo-page.spec.ts
+++ e2e/repo-page.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import config from "../playwright.config";
 
 test("Loads a repository page", async ({ page }) => {
   await page.goto("/s/open-sauced/app");
@@ -9,9 +10,9 @@ test("Loads a repository page", async ({ page }) => {
 
 test("repository page has an OG image", async ({ page }) => {
   await page.goto("/s/open-sauced/app");
-  const expectedUrl = "http://localhost:3000/og-images/repository/open-sauced/app/30?description=%F0%9F%8D%95+The+path+to+your+next+contribution."
+  const expectedUrl = `${config.use?.baseURL}/og-images/repository/open-sauced/app/30?description=%F0%9F%8D%95+The+path+to+your+next+contribution.`;
 
-  await expect(page.locator('meta[property="og:image"]')).toHaveAttribute('content', expectedUrl)
-  await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute('content', expectedUrl)
-  await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute('content', 'summary_large_image')
+  await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
 });
