3568c69202f1f794357a20f0708836d5a759012e
Brandon Roberts <robertsbt@gmail.com>
2024-04-10 11:23:14 -0500

    chore: adjust Sentry configuration and add explicit DSN (#3150)

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,10 +5,12 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+
   enabled: process.env.NODE_ENV !== "development",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.25,
+  tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
