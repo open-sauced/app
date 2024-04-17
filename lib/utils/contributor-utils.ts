3c6646733df9750842cce4c7c100509e34b3d45e
Brandon Roberts <robertsbt@gmail.com>
2024-04-17 12:12:06 -0400

    chore: review feedback fixes

--- lib/utils/contributor-utils.ts
+++ lib/utils/contributor-utils.ts
@@ -1,8 +1,9 @@
 export function getTopContributorLanguages(contributor: DbUser) {
   // Note that it's possible for a user to have no languages
-  const languages: { language: string; percentageUsed: number }[] = Object.entries(contributor.languages).map(
-    ([language, percentageUsed]) => ({ language, percentageUsed: percentageUsed })
-  );
+  const languages = Object.entries(contributor.languages).map(([language, percentageUsed]) => ({
+    language,
+    percentageUsed: percentageUsed,
+  }));
   const langArray = languages.slice().sort((a, b) => b.percentageUsed - a.percentageUsed);
   const sortedLangArray = langArray
     .sort((a, b) => (a.percentageUsed < b.percentageUsed ? 1 : -1))
