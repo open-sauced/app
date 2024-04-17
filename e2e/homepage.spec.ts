417e98bf416370bb842bba6ada28b627cc0df6ab
Brandon Roberts <robertsbt@gmail.com>
2024-04-17 15:58:05 -0400

    feat: switch #100DaysOfOSS banner with Workspaces banner on feed page (#3216)

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -12,5 +12,7 @@ test("Loads home page (logged out user)", async ({ page }) => {
   await expect(page.getByRole("complementary", { name: "Repositories", exact: true })).toBeVisible();
   await expect(page.getByRole("complementary", { name: "Featured Highlights", exact: true })).toBeVisible();
   await expect(page.getByRole("complementary", { name: "Subscribe to our newsletter", exact: true })).toBeVisible();
-  await expect(page.getByRole("complementary", { name: "#100DaysOfOSS 🚀", exact: true })).toBeVisible();
+  await expect(
+    page.getByRole("complementary", { name: "Track your project with Workspaces!", exact: true })
+  ).toBeVisible();
 });
