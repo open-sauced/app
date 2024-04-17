1181575f053ab8c808c8451676a67da720afadd1
Brandon Roberts <robertsbt@gmail.com>
2024-04-17 11:43:28 -0400

    fix: update logic for most used languages to match contributor profile

--- lib/utils/contributor-utils.ts
+++ lib/utils/contributor-utils.ts
@@ -1,6 +1,13 @@
 export function getTopContributorLanguages(contributor: DbUser) {
   // Note that it's possible for a user to have no languages
-  const languages = Object.entries(contributor.languages).map(([language]) => language);
+  const languages: { language: string; percentageUsed: number }[] = Object.entries(contributor.languages).map(
+    ([language, percentageUsed]) => ({ language, percentageUsed: percentageUsed })
+  );
+  const langArray = languages.slice().sort((a, b) => b.percentageUsed - a.percentageUsed);
+  const sortedLangArray = langArray
+    .sort((a, b) => (a.percentageUsed < b.percentageUsed ? 1 : -1))
+    .slice(0, 2)
+    .map((lang) => lang.language);
 
-  return languages.sort((a, b) => (a < b ? -1 : 1)).slice(0, 2);
+  return sortedLangArray;
 }
