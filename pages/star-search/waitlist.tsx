0cc2a16aa6e1fa184a8c6f824681cc818b849fc3
Nick Taylor <nick@nickyt.co>
2024-04-22 09:53:59 -0400

    wip

--- pages/star-search/waitlist.tsx
+++ pages/star-search/waitlist.tsx
@@ -1,23 +1,96 @@
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
+import Button from "components/shared/Button/button";
+import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
+import humanizeNumber from "lib/utils/humanizeNumber";
+import GitHubIcon from "img/icons/github-icon.svg";
+import Icon from "components/atoms/Icon/icon";
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {};
-export default function StarSearchWaitListPage() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const supabase = createPagesServerClient(context);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  let isWaitlisted = false;
+  let waitlistCount = 0;
+
+  if (session) {
+    const [waitlistCountResponse, userResponse] = await Promise.all([
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/waitlisted`),
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${session.user.user_metadata.user_name}`, {
+        headers: {
+          accept: "application/json",
+        },
+      }),
+    ]);
+
+    const [waitlistPayload, user] = await Promise.all([waitlistCountResponse.json(), userResponse.json()]);
+
+    isWaitlisted = user.is_waitlisted;
+    waitlistCount = waitlistPayload.waitlisted_users;
+  }
+
+  return {
+    props: {
+      isWaitlisted,
+      waitlistCount,
+    },
+  };
+};
+
+interface StarSearchWaitListPageProps {
+  isWaitlisted: boolean;
+  waitlistCount: number;
+}
+
+export default function StarSearchWaitListPage({ isWaitlisted, waitlistCount }: StarSearchWaitListPageProps) {
+  const { sessionToken, signIn, user } = useSupabaseAuth();
+
+  async function joinWaitlist(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const bearerToken = sessionToken ? sessionToken : undefined;
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/waitlist`, {
+      method: "POST",
+      headers: {
+        "Accept-Type": "application/json",
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    });
+
+    return response.status === 201;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-4">
       <h1 className="text-2xl font-bold text-center">Star Search Waitlist</h1>
-      <p className="text-center">
-        Star Search is currently in private beta. Please join the waitlist to get early access.
-      </p>
-      <div className="flex flex-col items-center space-y-2">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="w-full p-2 text-center border border-gray-300 rounded-md focus:outline-none focus:border-orange-500"
-        />
-        <button className="px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600 focus:outline-none">
-          Join Waitlist
-        </button>
-      </div>
+      {isWaitlisted ? (
+        <p>You&apos;re one of the {humanizeNumber(waitlistCount)} people on the Star Search waitlist!</p>
+      ) : (
+        <>
+          <p>Star Search is currently in private beta. Please join the waitlist to get early access.</p>
+          {sessionToken ? (
+            <form onSubmit={joinWaitlist}>
+              <Button variant="primary">Join Waitlist</Button>
+            </form>
+          ) : (
+            <Button
+              variant="primary"
+              onClick={async () => {
+                const params = new URLSearchParams();
+                params.set("redirectedFrom", "/star-search");
+
+                await signIn({
+                  provider: "github",
+                  options: { redirectTo: `/?${params}` },
+                });
+              }}
+            >
+              Connect with GitHub <Icon IconImage={GitHubIcon} className="ml-2" />
+            </Button>
+          )}
+        </>
+      )}
     </div>
   );
 }
