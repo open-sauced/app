43cffbbb4a1d38fcb3cdd10f5fb074924e92f3f4
PaglaDasu <snehasishbabin2001@gmail.com>
2024-04-23 13:10:15 +0530

    Fix: display message if the bio hits max

--- components/organisms/UserSettingsPage/user-settings-page.tsx
+++ components/organisms/UserSettingsPage/user-settings-page.tsx
@@ -289,9 +289,15 @@ const UserSettingsPage = ({ user }: UserSettingsPageProps) => {
                 </div>
               </label>
 
-              <p aria-live="polite" className="text-xs">
-+                {bio?.length}/255
-+             </p>
+              {bio?.length > 255 ? (
+                <p aria-live="assertive" className="text-light-red-10 text-xs">
+                  Bio too long
+                </p>
+              ) : (
+                <p aria-live="polite" className="text-xs">
+                  {bio?.length}/255
+                </p>
+              )}
             </div>
             <TextInput
               className="bg-light-slate-4"
