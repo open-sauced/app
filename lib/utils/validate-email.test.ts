b4f053b8eb09156bc1845adf1bb1f4674e7c82e0
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-27 06:54:59 +0200

    add regression test for #3271

--- lib/utils/validate-email.test.ts
+++ lib/utils/validate-email.test.ts
@@ -8,6 +8,13 @@ describe("[lib] validateEmail()", () => {
     expect(result).toBeTruthy();
   });
 
+  it("(regression #3271) Should return true if string is a valid email", () => {
+    const testString = "self@bjoern-buettner.me";
+    const result = validateEmail(testString);
+
+    expect(result).toBeTruthy();
+  });
+
   it("Should return false if string is not a valid email", () => {
     let testString = "ahmedatwayahoo.com";
     let result = validateEmail(testString);
