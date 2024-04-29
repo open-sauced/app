59eb8ccd8fe54843c5a51cc93f667dd953b1b42a
Brandon Roberts <robertsbt@gmail.com>
2024-04-29 09:47:50 -0500

    fix: remove filter prefetch and additional filters when searching by repository (#3247)

--- lib/hooks/useFilterPrefetch.ts
+++ lib/hooks/useFilterPrefetch.ts
@@ -1,15 +1,11 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { useSWRConfig } from "swr";
-import getFilterKey from "lib/utils/get-filter-key";
-import { publicApiFetcher } from "lib/utils/public-api-fetcher";
 import useFilterOptions from "./useFilterOptions";
 
 type FilterValues = { [name: string]: number | undefined };
 
 const useFilterPrefetch = () => {
   const router = useRouter();
-  const { mutate } = useSWRConfig();
   const filterOptions = useFilterOptions();
   const [filterValues, setFilterValues] = useState<FilterValues>({});
   const { pageId: topic } = router.query;
@@ -17,27 +13,12 @@ const useFilterPrefetch = () => {
   useEffect(() => {
     if (topic) {
       filterOptions.forEach(async (filterName) => {
-        const filterKey = getFilterKey(filterName);
-        const url = `repos/search?topic=${topic}&filter=${filterKey}&page=1`;
-
-        try {
-          // @ts-ignore
-          const result: { meta: Meta } = await mutate(url, publicApiFetcher(url));
-
-          setFilterValues((values) => {
-            return {
-              ...values,
-              [filterKey]: result.meta.itemCount,
-            };
-          });
-        } catch (e) {
-          setFilterValues((values) => {
-            return {
-              ...values,
-              [filterKey]: undefined,
-            };
-          });
-        }
+        setFilterValues((values) => {
+          return {
+            ...values,
+            [filterName]: 0,
+          };
+        });
       });
     }
   }, [topic]);
