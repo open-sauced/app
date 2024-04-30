8b66bbd2d76f50c329210f7a2f0a3c2abc8837b9
Jay Pokale <jay.pokale.35@gmail.com>
2024-04-30 11:15:29 +0530

    feat: Layout shift fix after search bar toggle

--- lib/hooks/useLockBody.ts
+++ lib/hooks/useLockBody.ts
@@ -3,8 +3,23 @@ import React from "react";
 const useLockBody = () => {
   React.useLayoutEffect((): (() => void) => {
     const originalStyle: string = window.getComputedStyle(document.body).overflow;
+    const oldWidth = document.documentElement.clientWidth;
+    const header = document.getElementsByTagName("header")?.[0];
+    const paddingRight: string = window.getComputedStyle(header).paddingRight;
+    const paddingRightInPixel = Number(paddingRight.slice(0, -2)) || 0;
+
     document.body.style.overflow = "hidden";
-    return () => (document.body.style.overflow = originalStyle);
+
+    const newWidth = document.documentElement.clientWidth;
+    const scrollbarWidth = Math.max(0, newWidth - oldWidth);
+    document.body.style.marginRight = `${scrollbarWidth}px`;
+    header.style.paddingRight = `${scrollbarWidth + paddingRightInPixel}px`;
+
+    return () => {
+      document.body.style.overflow = originalStyle;
+      document.body.style.marginRight = "0";
+      header.style.paddingRight = paddingRight;
+    };
   }, []);
 };
 
