53c8487f4387095273ee73a617d5f572d7972dd6
Nick Taylor <nick@nickyt.co>
2024-05-02 11:42:12 -0400

    chore: reverted optional footer in ProfileLayout

--- layouts/profile.tsx
+++ layouts/profile.tsx
@@ -3,19 +3,14 @@ import React from "react";
 import Footer from "../components/organisms/Footer/footer";
 import TopNav from "../components/organisms/TopNav/top-nav";
 
-interface ProfileLayoutProps {
-  children: React.ReactNode;
-  showFooter?: boolean;
-}
-
-const ProfileLayout = ({ children, showFooter = true }: ProfileLayoutProps) => {
+const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <TopNav />
       <div className="page-container flex grow flex-col items-center pt-8 lg:pt-20 md:pt-14">
         <main className="flex w-full flex-1 flex-col items-center bg-light-slate-2">{children}</main>
       </div>
-      {showFooter ? <Footer /> : null}
+      <Footer />
     </div>
   );
 };
