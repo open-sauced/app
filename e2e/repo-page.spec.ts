9942f43b9fbd7b56f50704e8666f10db891133ee
Nick Taylor <nick@nickyt.co>
2024-05-03 16:58:44 -0400

    test: updated test

--- e2e/repo-page.spec.ts
+++ e2e/repo-page.spec.ts
@@ -5,7 +5,7 @@ test("Loads a repository page", async ({ page }) => {
   await page.goto("/s/open-sauced/app");
 
   await expect(page.getByRole("heading", { name: "open-sauced/app", exact: true })).toBeVisible();
-  await expect(page.getByRole("combobox", { name: "Period:", exact: true, expanded: false })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Range:", exact: true, expanded: false })).toBeVisible();
 });
 
 test("repository page has an OG image", async ({ page }) => {
