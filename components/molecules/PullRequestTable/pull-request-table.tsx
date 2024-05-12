e16a0d1afa1a055443dc6ae35df1dcd8131c817c
Zeu Capua <coding@zeu.dev>
2024-05-11 19:29:58 -0700

    localize PullRequestTable header and row

--- components/molecules/PullRequestTable/pull-request-table.tsx
+++ components/molecules/PullRequestTable/pull-request-table.tsx
@@ -1,19 +1,17 @@
+import { VscGitMerge, VscGitPullRequest, VscGitPullRequestClosed, VscGitPullRequestDraft } from "react-icons/vsc";
+import { FaRegCalendar } from "react-icons/fa6";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { GoDiff } from "react-icons/go";
+import { BsFileDiff } from "react-icons/bs";
 import useContributorPullRequests from "lib/hooks/api/useContributorPullRequests";
 
 import SkeletonWrapper from "components/atoms/SkeletonLoader/skeleton-wrapper";
-import LatestPrTableHeader from "../LatestPrTableHeader/latest-pr-table-header";
-import LatestPrTableRow from "../LatestPrTableRow/latest-pr-table-row";
+import Text from "components/atoms/Typography/text";
+import { calcDistanceFromToday } from "lib/utils/date-utils";
+import humanizeNumber from "lib/utils/humanizeNumber";
+import Tooltip from "components/atoms/Tooltip/tooltip";
 
-interface PRs {
-  prStatus: string;
-  prName: string;
-  prIssuedTime: string;
-  prClosedTime: string;
-  noOfFilesChanged: number;
-  noOfLinesChanged: number;
-}
-
-interface CardTableProps {
+interface PullRequestTableProps {
   contributor: string;
   topic: string;
   repositories?: number[];
@@ -29,7 +27,7 @@ const PullRequestTable = ({
   limit,
   isHoverCard,
   range,
-}: CardTableProps): JSX.Element => {
+}: PullRequestTableProps): JSX.Element => {
   const { data, isLoading } = useContributorPullRequests({
     contributor,
     topic,
@@ -41,43 +39,35 @@ const PullRequestTable = ({
   return data.length > 0 ? (
     <>
       <div className="flex flex-col">
-        <LatestPrTableHeader isHoverCard={isHoverCard} />
+        <div className="flex gap-2 items-center bg-light-slate-3 rounded-md px-2 py-1 ">
+          <div className="w-3/5">
+            <Text className=" ">Latest PRs</Text>
+          </div>
+          <div className={`${isHoverCard && "ml-auto"} justify-end w-[calc(10%-4px)]`}>
+            <Tooltip content="Last Commit Date">
+              <FaRegCalendar className="text-slate-500 w-3.5 h-3.5" />
+            </Tooltip>
+          </div>
+          <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)]`}>
+            <Tooltip content="Date Approved">
+              <FaRegCheckCircle className="text-slate-500 w-3.5 h-3.5" />
+            </Tooltip>
+          </div>
+          <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)]`}>
+            <Tooltip content="Files Touched">
+              <GoDiff className="text-slate-500 w-3.5 h-3.5" />
+            </Tooltip>
+          </div>
+          <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)]`}>
+            <Tooltip content="Lines Touched">
+              <BsFileDiff className="text-slate-500 w-3.5 h-3.5" />
+            </Tooltip>
+          </div>
+        </div>
         <div className="flex flex-col gap-0.5">
-          {data.map(
-            (
-              {
-                pr_title: prName,
-                pr_state: prStatus,
-                pr_is_merged: merged,
-                pr_is_draft: draft,
-                pr_merged_at: prMergedTime,
-                pr_created_at: prIssuedTime,
-                pr_changed_files: noOfFilesChanged,
-                pr_additions: additions,
-                pr_deletions: deletions,
-                pr_number: prNumber,
-                repo_name: fullName,
-                pr_updated_at: prUpdatedTime,
-              },
-              index
-            ) => {
-              const latestPrs = {
-                prName,
-                prIssuedTime,
-                prMergedTime,
-                prStatus,
-                merged,
-                draft,
-                noOfFilesChanged,
-                noOfLinesChanged: Math.abs(additions - deletions),
-                repoFullName: fullName,
-                prNumber,
-                prUpdatedTime,
-              };
-
-              return <LatestPrTableRow isHoverCard={isHoverCard} key={index} {...latestPrs} />;
-            }
-          )}
+          {data.map((pr, index) => (
+            <PullRequestTableRow key={index} pullRequest={pr} isHoverCard={isHoverCard} />
+          ))}
         </div>
       </div>
     </>
@@ -87,3 +77,46 @@ const PullRequestTable = ({
 };
 
 export default PullRequestTable;
+
+function PullRequestTableRow({ pullRequest, isHoverCard }: { pullRequest: DbRepoPREvents; isHoverCard?: boolean }) {
+  const renderIcon = () => {
+    if (pullRequest.pr_is_draft) {
+      return <VscGitPullRequestDraft title="Draft Pull Request" className="w-3 h-3 mt-1.5 text-slate-400" />;
+    } else if (pullRequest.pr_is_merged) {
+      return <VscGitMerge title="Merged Pull Request" className="w-3 h-3 mt-1.5 text-purple-600" />;
+    } else {
+      switch (pullRequest.pr_state) {
+        case "open":
+          return <VscGitPullRequest title="Open Pull Request" className="w-3 h-3 mt-1.5 text-green-600" />;
+        case "closed":
+          return <VscGitPullRequestClosed title="Closed Pull Request" className="w-3 h-3 mt-1.5 text-red-500" />;
+      }
+    }
+  };
+
+  return (
+    <div className="flex gap-2 items-center px-2 py-1">
+      <div className={`flex item-center gap-2 w-3/5 ${isHoverCard && "w-5/6"}`}>
+        {renderIcon()}
+        <Text title="updated date">{calcDistanceFromToday(new Date(pullRequest.pr_updated_at))}</Text>
+        <Text title={pullRequest.pr_title} className="!text-light-slate-12 !w-32 md:!w-72 !truncate">
+          <a href={`https://github.com/${pullRequest.repo_name}/pull/${pullRequest.pr_number}`} target="_blank">
+            {pullRequest.pr_title}
+          </a>
+        </Text>
+      </div>
+      <div className={`${isHoverCard && "ml-auto"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {calcDistanceFromToday(new Date(pullRequest.pr_updated_at))}
+      </div>
+      <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {pullRequest.pr_is_merged ? calcDistanceFromToday(new Date(pullRequest.pr_merged_at)) : "-"}
+      </div>
+      <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {humanizeNumber(pullRequest.pr_changed_files, "abbreviation")}
+      </div>
+      <div className={`${isHoverCard ? "hidden" : "flex"} justify-end w-[calc(10%-4px)] text-sm text-light-slate-11`}>
+        {humanizeNumber(Math.abs(pullRequest.pr_additions - pullRequest.pr_deletions), "abbreviation")}
+      </div>
+    </div>
+  );
+}
