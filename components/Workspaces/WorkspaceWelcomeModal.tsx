749d0ebf9e8fad7ea76077a79402e6ceb72045f9
Nick Taylor <nick@nickyt.co>
2024-05-15 08:04:54 -0400

    fix: now on smaller screens a drawer opens for welcome to workspaces

--- components/Workspaces/WorkspaceWelcomeModal.tsx
+++ components/Workspaces/WorkspaceWelcomeModal.tsx
@@ -1,17 +1,77 @@
 import { RiExternalLinkLine } from "react-icons/ri";
+import { useEffect, useRef } from "react";
 import Button from "components/shared/Button/button";
 import Title from "components/atoms/Typography/title";
 import { Dialog, DialogTitle, DialogContent } from "components/molecules/Dialog/dialog";
 import Card from "components/atoms/Card/card";
 import Text from "components/atoms/Typography/text";
+import { useMediaQuery } from "lib/hooks/useMediaQuery";
+import { Drawer } from "components/shared/Drawer";
 
 interface WorkspaceWelcomeModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const TITLE = "Welcome to Workspaces";
 export default function WorkspaceWelcomeModal({ isOpen, onClose }: WorkspaceWelcomeModalProps) {
-  return (
+  const description = (
+    <Text>
+      Keep track of repositories and contributors easily with our new feature
+      <span className="font-semibold"> Workspaces!</span> If you&apos;ve used OpenSauced before, your insights and lists
+      are now part of your personal workspace.
+    </Text>
+  );
+
+  const content = (
+    <div className="grid gap-4 place-content-center">
+      <p className="font-medium text-light-orange-10">
+        Create a new workspace and explore open source like never before!
+      </p>
+    </div>
+  );
+
+  const isSmallScreen = useMediaQuery("(max-width: 768px)");
+  const drawerButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (isSmallScreen && isOpen && drawerButtonRef.current) {
+      drawerButtonRef.current.click();
+    }
+  }, [isSmallScreen, isOpen]);
+
+  return isSmallScreen ? (
+    <Drawer
+      title={TITLE}
+      description={
+        <div className="text-center flex flex-col items-center justify-between gap-4 !p-8">
+          <img
+            src="/assets/workspace_overview.png"
+            alt="Workspace screenshot from documentation"
+            className="border-2 border-light-orange-9 shadow-md rounded-lg mb-4"
+          />
+          {description}
+        </div>
+      }
+      trigger={<button ref={drawerButtonRef} />}
+      onClose={onClose}
+    >
+      <>
+        {content}
+        <div className="flex gap-4 place-content-center">
+          <Button
+            href="https://docs.opensauced.pizza/features/workspaces"
+            className="w-fit flex gap-4"
+            variant="primary"
+            target="_blank"
+          >
+            Learn more about Workspaces
+            <RiExternalLinkLine className="w-4 h-4" />
+          </Button>
+        </div>
+      </>
+    </Drawer>
+  ) : (
     <Dialog open={isOpen}>
       <DialogContent autoStyle={false} onEscapeKeyDown={onClose} onPointerDownOutside={onClose}>
         <Card className="sm:w-96 md:w-max max-w-xl text-center flex flex-col items-center justify-between gap-4 !p-8">
@@ -29,10 +89,8 @@ export default function WorkspaceWelcomeModal({ isOpen, onClose }: WorkspaceWelc
             <span className="font-semibold"> Workspaces!</span> If you&apos;ve used OpenSauced before, your insights and
             lists are now part of your personal workspace.
           </Text>
-          <p className="font-medium text-light-orange-10">
-            Create a new workspace and explore open source like never before!
-          </p>
-          <section className="flex gap-4">
+          {content}
+          <div className="flex gap-4 place-content-center">
             <Button
               href="https://docs.opensauced.pizza/features/workspaces"
               className="w-fit flex gap-4"
@@ -45,7 +103,7 @@ export default function WorkspaceWelcomeModal({ isOpen, onClose }: WorkspaceWelc
             <Button variant="outline" onClick={onClose}>
               Continue
             </Button>
-          </section>
+          </div>
         </Card>
       </DialogContent>
     </Dialog>
