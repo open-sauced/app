85f8ef88c4fb86f033f6598b8361a9a989f044f5
Brandon Roberts <robertsbt@gmail.com>
2024-05-28 10:12:12 -0500

    chore: update user profile E2E tests

--- e2e/user-profile.spec.ts
+++ e2e/user-profile.spec.ts
@@ -4,7 +4,7 @@ import playwrightConfig from "../playwright.config";
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? playwrightConfig.use?.baseURL ?? "http://localhost:3000";
 
 test("Loads user profile page", async ({ page }) => {
-  await page.goto("/user/bdougie");
+  await page.goto("/u/bdougie");
 
   expect(await page.title()).toBe("bdougie | OpenSauced");
 
@@ -12,13 +12,13 @@ test("Loads user profile page", async ({ page }) => {
 });
 
 test("Redirects to user profile page", async ({ page }) => {
-  await page.goto("/u/bdougie");
+  await page.goto("/user/bdougie");
 
-  expect(await page.url()).toBe(`${new URL("/user/bdougie", baseUrl)}`);
+  expect(await page.url()).toBe(`${new URL("/u/bdougie", baseUrl)}`);
 });
 
 test("Redirects to user profile page with splat", async ({ page }) => {
-  await page.goto("/u/bdougie?tab=contributions&range=180");
+  await page.goto("/user/bdougie?tab=contributions&range=180");
 
-  expect(await page.url()).toBe(`${new URL("/user/bdougie?tab=contributions&range=180", baseUrl)}`);
+  expect(await page.url()).toBe(`${new URL("/u/bdougie?tab=contributions&range=180", baseUrl)}`);
 });
