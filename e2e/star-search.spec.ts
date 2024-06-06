cc7d86e1a43fa94a425a165f6d77cfd6c8318dee
Nick Taylor <nick@nickyt.co>
2024-06-06 08:38:35 -0400

    test: fixed some tests

--- e2e/star-search.spec.ts
+++ e2e/star-search.spec.ts
@@ -5,24 +5,24 @@ import { starSearchResponse } from "./fixtures/star-search/streamResponse";
 test("StarSearch (Logged Out Flow)", async ({ page }) => {
   await page.goto("/star-search");
 
-  expect(await page.getByRole("list", { name: "suggested prompts" })).toBeVisible();
-  expect(await page.getByRole("heading", { name: "StarSearch" })).toBeVisible();
-  expect(await page.getByRole("heading", { name: "Copilot, but for git history" })).toBeVisible();
+  await expect(page.getByRole("list", { name: "suggested prompts" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "StarSearch" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Copilot, but for git history" })).toBeVisible();
 
   // Ensure all suggestions are present
-  const firstSuggestsion = await page.getByRole("button", {
+  const firstSuggestsion = page.getByRole("button", {
     name: "Get information on contributor activity",
     exact: true,
   });
-  expect(firstSuggestsion).toBeVisible();
+  await expect(firstSuggestsion).toBeVisible();
 
   const firstSuggestsionSelector = `#${await firstSuggestsion.getAttribute("aria-describedby")}`;
-  expect(page.locator(firstSuggestsionSelector)).toHaveText(
+  await expect(page.locator(firstSuggestsionSelector)).toHaveText(
     "What type of pull requests has @brandonroberts worked on?"
   );
 
-  const secondSuggestion = await page.getByRole("button", { name: "Identify key contributors", exact: true });
-  expect(secondSuggestion).toBeVisible();
+  const secondSuggestion = page.getByRole("button", { name: "Identify key contributors", exact: true });
+  await expect(secondSuggestion).toBeVisible();
 
   const secondSuggestionDescription = `#${await secondSuggestion.getAttribute("aria-describedby")}`;
   expect(page.locator(secondSuggestionDescription)).toHaveText(
@@ -36,30 +36,30 @@ test("StarSearch (Logged Out Flow)", async ({ page }) => {
   expect(thirdSuggestion).toBeVisible();
 
   const thirdSuggestionDescription = `#${await thirdSuggestion.getAttribute("aria-describedby")}`;
-  expect(page.locator(thirdSuggestionDescription)).toHaveText(
+  await expect(page.locator(thirdSuggestionDescription)).toHaveText(
     "Show me the lottery factor for contributors in the remix-run/react-router project?"
   );
 
-  const fourthSuggestion = await page.getByRole("button", { name: "Find experts", exact: true });
-  expect(fourthSuggestion).toBeVisible();
+  const fourthSuggestion = page.getByRole("button", { name: "Find experts", exact: true });
+  await expect(fourthSuggestion).toBeVisible();
 
   const fourthSuggestionDescription = `#${await fourthSuggestion.getAttribute("aria-describedby")}`;
-  expect(page.locator(fourthSuggestionDescription)).toHaveText(
+  await expect(page.locator(fourthSuggestionDescription)).toHaveText(
     "Who are the best developers that know Tailwind and are interested in Rust?"
   );
 
-  const submitButton = await page.getByRole("button", { name: "Submit your question to StarSearch", exact: true });
-  expect(submitButton).toBeVisible();
+  const submitButton = page.getByRole("button", { name: "Submit your question to StarSearch", exact: true });
+  await expect(submitButton).toBeVisible();
 
-  const promptInput = await page.getByRole("textbox", { name: "Ask a question", exact: true });
-  expect(promptInput).toBeVisible();
+  const promptInput = page.getByRole("textbox", { name: "Ask a question", exact: true });
+  await expect(promptInput).toBeVisible();
 
   // ensures the login dialog appears if the user is not logged in and they
   // click a suggested prompt
-  const loginDialog = await page.getByRole("dialog", { name: "Login to try StarSearch", exact: true });
-  expect(loginDialog).toBeHidden();
+  let loginDialog = page.getByRole("dialog", { name: "Login to try StarSearch", exact: true });
+  await expect(page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeHidden();
 
-  firstSuggestsion.click();
+  await firstSuggestsion.click();
 
   await expect(loginDialog).toBeVisible();
 
@@ -70,12 +70,10 @@ test("StarSearch (Logged Out Flow)", async ({ page }) => {
   // click the ask a question text input
   await expect(loginDialog).toBeHidden();
 
-  promptInput.click();
+  await promptInput.click();
 
   await expect(loginDialog).toBeVisible();
-
-  // Ensure there is a login via GitHub button in the dialog.
-  expect(loginDialog.getByRole("button", { name: "Connect with GitHub", exact: true })).toBeVisible();
+  await expect(loginDialog.getByRole("button", { name: "Connect with GitHub", exact: true })).toBeVisible();
 });
 
 test("StarSearch OG image should exist", async ({ page }) => {
@@ -99,54 +97,55 @@ test("StarSearch shared prompt (Logged Out Flow)", async ({ page }) => {
 
   await page.goto("/star-search/?prompt=Who+are+the+most+prevalent+contributors+to+the+TypeScript+ecosystem%3F");
 
-  expect(await page.getByRole("list", { name: "suggested prompts" })).not.toBeVisible();
-  // expect(await page.getByRole("heading", { name: "StarSearch" })).not.toBeVisible();
-  expect(await page.getByRole("heading", { name: "Copilot, but for git history" })).not.toBeVisible();
-  expect(await page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeHidden();
+  await expect(page.getByRole("list", { name: "suggested prompts" })).not.toBeVisible();
+  // await expect(page.getByRole("heading", { name: "StarSearch" })).not.toBeVisible();
+  await expect(page.getByRole("heading", { name: "Copilot, but for git history" })).not.toBeVisible();
+  await expect(page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeHidden();
 
-  const promptInput = await page.getByRole("textbox", { name: "Ask a question", exact: true });
-  expect(promptInput).toBeVisible();
-  // expect(promptInput).toBeDisabled();
+  const promptInput = page.getByRole("textbox", { name: "Ask a question", exact: true });
+  await expect(promptInput).toBeVisible();
+  // await expect(promptInput).toBeDisabled();
 
   // get an element with role feed and name "StarSearch conversation" and check that it exists.
-  const feed = await page.getByRole("feed", { name: "StarSearch conversation" });
-  // expect(feed).toBeVisible();
-  // expect(feed).toHaveAttribute("aria-busy", "true");
+  const feed = page.getByRole("feed", { name: "StarSearch conversation" });
+  await expect(feed).toBeVisible();
+  // await expect(feed).toHaveAttribute("aria-busy", "true");
 
-  const firstArticle = await feed.locator("article").first();
-  expect(await firstArticle.getByRole("heading", { name: "You" })).toBeVisible();
-  expect(await firstArticle.getByLabel("chat message")).toHaveText(
+  const firstArticle = feed.locator("article").first();
+  await expect(firstArticle.getByRole("heading", { name: "You" })).toBeVisible();
+  await expect(firstArticle.getByLabel("chat message")).toHaveText(
     "Who are the most prevalent contributors to the TypeScript ecosystem?"
   );
 
-  // expect(await feed.getByRole("progressbar", { name: "Loading..." })).toBeVisible();
+  // await expect(feed.getByRole("progressbar", { name: "Loading..." })).toBeVisible();
 
-  expect(await feed.getByRole("progressbar", { name: "Loading..." })).not.toBeVisible();
+  await expect(feed.getByRole("progressbar", { name: "Loading..." })).not.toBeVisible();
 
-  const secondArticle = await feed.locator("article").nth(1);
-  expect(await secondArticle.getByRole("heading", { name: "StarSearch" })).toBeVisible();
-  expect(await secondArticle.getByLabel("chat message")).toHaveText(starSearchResponse);
+  const secondArticle = feed.locator("article").nth(1);
+  await expect(secondArticle.getByRole("heading", { name: "StarSearch" })).toBeVisible();
+  await expect(secondArticle.getByLabel("chat message")).toHaveText(starSearchResponse);
 
-  expect(await page.getByRole("button", { name: "Clear chat history", exact: true })).toBeVisible();
-  expect(await page.getByRole("button", { name: "Thumbs up", exact: true })).toBeVisible();
-  expect(await page.getByRole("button", { name: "Thumbs down", exact: true })).toBeVisible();
-  const sharePopupMenuTrigger = await page.getByRole("button", { name: "Share prompt options", exact: true });
-  expect(sharePopupMenuTrigger).toBeVisible();
-  expect(sharePopupMenuTrigger).toHaveAttribute("aria-haspopup", "menu");
+  await expect(page.getByRole("button", { name: "Clear chat history", exact: true })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Thumbs up", exact: true })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Thumbs down", exact: true })).toBeVisible();
+
+  const sharePopupMenuTrigger = page.getByRole("button", { name: "Share prompt options", exact: true });
+  await expect(sharePopupMenuTrigger).toBeVisible();
+  await expect(sharePopupMenuTrigger).toHaveAttribute("aria-haspopup", "menu");
 
   // open share prompt options menu
   await sharePopupMenuTrigger.click();
 
-  expect(await page.getByRole("menuitem", { name: "Share to Twitter/X", exact: true })).toBeVisible();
-  expect(await page.getByRole("menuitem", { name: "Share to LinkedIn", exact: true })).toBeVisible();
-  expect(await page.getByRole("menuitem", { name: "Copy link", exact: true })).toBeVisible();
+  await expect(page.getByRole("menuitem", { name: "Share to Twitter/X", exact: true })).toBeVisible();
+  await expect(page.getByRole("menuitem", { name: "Share to LinkedIn", exact: true })).toBeVisible();
+  await expect(page.getByRole("menuitem", { name: "Copy link", exact: true })).toBeVisible();
 
   // close share prompt options menu
   await sharePopupMenuTrigger.click();
-  expect(sharePopupMenuTrigger).not.toHaveAttribute("aria-expanded");
-  expect(await page.getByRole("menuitem", { name: "Share to Twitter/X", exact: true })).not.toBeVisible();
-  expect(await page.getByRole("menuitem", { name: "Share to LinkedIn", exact: true })).not.toBeVisible();
-  expect(await page.getByRole("menuitem", { name: "Copy link", exact: true })).not.toBeVisible();
+  await expect(sharePopupMenuTrigger).not.toHaveAttribute("aria-expanded");
+  await expect(page.getByRole("menuitem", { name: "Share to Twitter/X", exact: true })).not.toBeVisible();
+  await expect(page.getByRole("menuitem", { name: "Share to LinkedIn", exact: true })).not.toBeVisible();
+  await expect(page.getByRole("menuitem", { name: "Copy link", exact: true })).not.toBeVisible();
 
   // check for OG image
   const expectedUrl = `${config.use?.baseURL}/og-images/star-search/?prompt=Who+are+the+most+prevalent+contributors+to+the+TypeScript+ecosystem%3F`;
@@ -155,7 +154,7 @@ test("StarSearch shared prompt (Logged Out Flow)", async ({ page }) => {
   await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
   await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
 
-  // after the shared prompt runs for a logged out user, if they try to add their own prompt, it will be gated by a login dialog.
-  promptInput.click();
-  expect(await page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeVisible();
+  // // after the shared prompt runs for a logged out user, if they try to add their own prompt, it will be gated by a login dialog.
+  await page.getByRole("textbox", { name: "Ask a question", exact: true }).click();
+  await expect(page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeVisible();
 });
