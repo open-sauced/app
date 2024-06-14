d5d26d3f4a99f612a8de456b0e76d5fe12fca574
Nick Taylor <nick@nickyt.co>
2024-06-13 22:49:52 -0400

    test: updated share StarSearch conversation test

--- e2e/star-search.spec.ts
+++ e2e/star-search.spec.ts
@@ -88,15 +88,9 @@ test("StarSearch OG image should exist", async ({ page }) => {
 });
 
 test("StarSearch shared prompt (Logged Out Flow)", async ({ page, browserName }) => {
-  await page.route(/\/v2\/star-search\/stream/, async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: "text/event-stream",
-      path: "./e2e/fixtures/star-search/stream.txt",
-    });
-  });
-
-  await page.goto("/star-search/?prompt=Who+are+the+most+prevalent+contributors+to+the+TypeScript+ecosystem%3F");
+  // Note: If this page fails to load at some point, the development database has been reset, and
+  // new shared StarSearch conversations will need to be created.
+  await page.goto("/star-search?id=900686cc-8926-47fd-a1d6-0e19da967f48");
 
   await expect(page.getByRole("list", { name: "suggested prompts" })).not.toBeVisible();
   // await expect(page.getByRole("heading", { name: "StarSearch" })).not.toBeVisible();
@@ -117,69 +111,110 @@ test("StarSearch shared prompt (Logged Out Flow)", async ({ page, browserName })
   const firstArticle = feed.locator("article").first();
   await expect(firstArticle.getByRole("heading", { name: "You" })).toBeVisible();
   await expect(firstArticle.getByLabel("chat message")).toHaveText(
-    "Who are the most prevalent contributors to the TypeScript ecosystem?"
+    "What type of pull requests has @brandonroberts worked on?"
   );
 
   // This is commented out for now because I haven't figured out a way yet to get the mocked stream response to slow down a bit.
   // await expect(feed.getByRole("progressbar", { name: "Loading..." })).toBeVisible();
 
-  // TODO: Update this end to end test as a part of #3551
+  await expect(feed.getByRole("progressbar", { name: "Loading..." })).not.toBeVisible();
+  const secondArticle = feed.locator("article").nth(1);
+  await expect(secondArticle.getByRole("heading", { name: "StarSearch" })).toBeVisible();
+  await expect(secondArticle.getByLabel("chat message"))
+    .toHaveText(`Here are some recent pull requests that @brandonroberts has worked on:
+Repository: analogjs/analog
 
-  // await expect(feed.getByRole("progressbar", { name: "Loading..." })).not.toBeVisible();
-  // const secondArticle = feed.locator("article").nth(1);
-  // await expect(secondArticle.getByRole("heading", { name: "StarSearch" })).toBeVisible();
-  // await expect(secondArticle.getByLabel("chat message")).toHaveText(starSearchResponse);
 
-  // await expect(page.getByRole("button", { name: "Clear chat history", exact: true })).toBeVisible();
-  // await expect(page.getByRole("button", { name: "Thumbs up", exact: true })).toBeVisible();
-  // await expect(page.getByRole("button", { name: "Thumbs down", exact: true })).toBeVisible();
+fix(create-analog): add fixes for v18 template
 
-  // const sharePopupMenuTrigger = page.getByRole("button", { name: "Share prompt options", exact: true });
-  // await expect(sharePopupMenuTrigger).toBeVisible();
-  // await expect(sharePopupMenuTrigger).toHaveAttribute("aria-haspopup", "menu");
+Type: Bugfix
+Details: Fixed issues with the v18 template in create-analog package to ensure consistent behavior.
+Additions/Deletions: +2/-0 across 2 files.
 
-  // // open share prompt options menu
-  // await sharePopupMenuTrigger.click();
 
-  // const shareToTwitterMenuItem = page.getByRole("menuitem", { name: "Share to Twitter/X", exact: true });
-  // const shareToLinkedInMenuItem = page.getByRole("menuitem", { name: "Share to LinkedIn", exact: true });
-  // const copyLinkMenuItem = page.getByRole("menuitem", { name: "Copy link", exact: true });
 
-  // await expect(shareToTwitterMenuItem).toBeVisible();
-  // await expect(shareToTwitterMenuItem.locator("a")).toHaveAttribute(
-  //   "href",
-  //   `https://twitter.com/intent/tweet?text=Here%27s+my+StarSearch+prompt%21%0A%0ATry+it+out+for+yourself.+%23StarSearch&url=https%3A%2F%2Fdub.sh%2Fte9DaAI`
-  // );
+chore: release 1.5.0
 
-  // await expect(shareToLinkedInMenuItem).toBeVisible();
-  // await expect(shareToLinkedInMenuItem.locator("a")).toHaveAttribute(
-  //   "href",
-  //   `https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fdub.sh%2Fte9DaAI`
-  // );
-  // await expect(copyLinkMenuItem).toBeVisible();
+Type: Release
+Details: Prepared and executed the release of version 1.5.0 of the analog library. Included merged pull requests, updated documentation, and version bump.
+Additions/Deletions: +2114/-3117 across 101 files.
 
-  // if (["webkit", "Desktop Safari", "Mobile Safari"].includes(browserName)) {
-  //   // Webkit does not support clipboard API access in Playwright at the moment.
-  //   await sharePopupMenuTrigger.click();
-  // } else {
-  //   // Ensure link was copied correctly
-  //   await copyLinkMenuItem.click();
-  //   await expect(await page.evaluate("navigator.clipboard.readText()")).toEqual("https://dub.sh/te9DaAI");
-  // }
 
-  // await expect(sharePopupMenuTrigger).not.toHaveAttribute("aria-expanded");
-  // await expect(shareToTwitterMenuItem).not.toBeVisible();
-  // await expect(shareToLinkedInMenuItem).not.toBeVisible();
-  // await expect(copyLinkMenuItem).not.toBeVisible();
 
-  // // check for shared prompt OG image
-  // const expectedUrl = `${BASE_URL}/og-images/star-search/?prompt=Who+are+the+most+prevalent+contributors+to+the+TypeScript+ecosystem%3F`;
+feat: update deps and apps to Angular v18 release
 
-  // await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
-  // await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
-  // await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
+Type: Feature
+Details: Updated dependencies and applications in the analog repository to use Angular v18 release.
+Additions/Deletions: +1642/-2035 across 24 files.
 
-  // // // after the shared prompt runs for a logged out user, if they try to add their own prompt, it will be gated by a login dialog.
-  // await page.getByRole("textbox", { name: "Ask a question", exact: true }).click();
-  // await expect(page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeVisible();
+
+
+chore: release 1.4.0
+
+Type: Release
+Details: Prepared for the new release 1.4.0, including several dependency upgrades, bug fixes, and version updates.
+Additions/Deletions: +3906/-2445 across 119 files.
+
+
+
+fix(content): add migration for marked-mangle dependency
+
+Type: Bugfix
+Details: Added migration for marked-mangle dependency during latest version upgrades.
+Additions/Deletions: +20/-1 across 3 files.
+
+`);
+
+  await expect(page.getByRole("button", { name: "Clear chat history", exact: true })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Thumbs up", exact: true })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Thumbs down", exact: true })).toBeVisible();
+
+  const sharePopupMenuTrigger = page.getByRole("button", { name: "Share prompt options", exact: true });
+  await expect(sharePopupMenuTrigger).toBeVisible();
+  await expect(sharePopupMenuTrigger).toHaveAttribute("aria-haspopup", "menu");
+
+  // open share prompt options menu
+  await sharePopupMenuTrigger.click();
+
+  const shareToTwitterMenuItem = page.getByRole("menuitem", { name: "Share to Twitter/X", exact: true });
+  const shareToLinkedInMenuItem = page.getByRole("menuitem", { name: "Share to LinkedIn", exact: true });
+  const copyLinkMenuItem = page.getByRole("menuitem", { name: "Copy link", exact: true });
+
+  await expect(shareToTwitterMenuItem).toBeVisible();
+  await expect(shareToTwitterMenuItem.locator("a")).toHaveAttribute(
+    "href",
+    "https://twitter.com/intent/tweet?text=Here%27s+my+StarSearch+prompt%21%0A%0ATry+it+out+for+yourself.+%23StarSearch&url=https%3A%2F%2Fdub.sh%2FCsj383d"
+  );
+
+  await expect(shareToLinkedInMenuItem).toBeVisible();
+  await expect(shareToLinkedInMenuItem.locator("a")).toHaveAttribute(
+    "href",
+    "https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fdub.sh%2FCsj383d"
+  );
+  await expect(copyLinkMenuItem).toBeVisible();
+
+  if (["webkit", "Desktop Safari", "Mobile Safari"].includes(browserName)) {
+    // Webkit does not support clipboard API access in Playwright at the moment.
+    await sharePopupMenuTrigger.click();
+  } else {
+    // Ensure link was copied correctly
+    await copyLinkMenuItem.click();
+    await expect(await page.evaluate("navigator.clipboard.readText()")).toEqual("https://dub.sh/Csj383d");
+  }
+
+  await expect(sharePopupMenuTrigger).not.toHaveAttribute("aria-expanded");
+  await expect(shareToTwitterMenuItem).not.toBeVisible();
+  await expect(shareToLinkedInMenuItem).not.toBeVisible();
+  await expect(copyLinkMenuItem).not.toBeVisible();
+
+  // check for shared prompt OG image
+  const expectedUrl = `${BASE_URL}/og-images/star-search/?id=900686cc-8926-47fd-a1d6-0e19da967f48`;
+
+  await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
+
+  // // after the shared prompt runs for a logged out user, if they try to add their own prompt, it will be gated by a login dialog.
+  await page.getByRole("textbox", { name: "Ask a question", exact: true }).click();
+  await expect(page.getByRole("dialog", { name: "Login to try StarSearch", exact: true })).toBeVisible();
 });
