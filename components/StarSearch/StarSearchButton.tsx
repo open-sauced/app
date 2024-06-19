18f340b7968a5cadcd6d6146b6323587c64011c2
Nick Taylor <nick@nickyt.co>
2024-06-19 15:18:10 -0400

    chore: now tooltip only auto shows until they hover over the StarSearch button

--- components/StarSearch/StarSearchButton.tsx
+++ components/StarSearch/StarSearchButton.tsx
@@ -1,19 +1,22 @@
 import Tooltip from "components/atoms/Tooltip/tooltip";
 import { StarSearchLogo } from "./StarSearchLogo";
+import { useLocalStorage } from "react-use";
 
 interface StarSearchButtonProps {
   onOpen: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const StarSearchButton = ({ onOpen }: StarSearchButtonProps) => {
+  const [tooltipOpen, setTooltipOpen] = useLocalStorage("show-star-search-button-tooltip", true);
+
   return (
     <Tooltip
       direction="top"
-      defaultOpen={true}
+      defaultOpen={tooltipOpen}
       className="text-center w-36"
       content="Ask StarSearch about your Workspace"
     >
-      <button onClick={onOpen}>
+      <button onClick={onOpen} onMouseEnter={() => setTooltipOpen(false)}>
         <StarSearchLogo />
       </button>
     </Tooltip>
