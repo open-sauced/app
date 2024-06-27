727860f1332dbfe45609e93b5c945754f084aac4
Nick Taylor <nick@nickyt.co>
2024-06-26 21:08:15 -0400

    chore: added a tooltip to the OSCR component

--- components/Contributors/OscrRating.tsx
+++ components/Contributors/OscrRating.tsx
@@ -1,7 +1,12 @@
 import Pill from "components/atoms/Pill/pill";
+import Tooltip from "components/atoms/Tooltip/tooltip";
 
 export const OscrRating = ({ rating }: { rating: number }) => {
   const percentageRating = Math.floor(rating * 100);
 
-  return <Pill color="purple" size="small" text={`OSCR: ${percentageRating}%`} />;
+  return (
+    <Tooltip direction="top" content="Open Source Contributor Rating (OSCR)">
+      <Pill color="purple" size="small" text={`OSCR: ${percentageRating}%`} />
+    </Tooltip>
+  );
 };
