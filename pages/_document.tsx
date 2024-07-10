ff126e68339c78d48c9e4a704ed58d1a7462fa7c
Nick Taylor <nick@nickyt.co>
2024-07-10 16:28:04 -0400

    chore: fixed top nav height

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html style={{ "--top-nav-height": "3.3rem" }}>
+    <Html style={{ "--top-nav-height": "3rem" }}>
       <Head />
       <body className="selection:bg-light-orange-8 selection:text-light-slate-12">
         <Main />
