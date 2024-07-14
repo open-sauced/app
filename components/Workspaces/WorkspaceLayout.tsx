c22bad231574ed8c9cd3156717a3308fcb1bbf32
Nick Taylor <nick@nickyt.co>
2024-07-14 09:05:19 -0400

    chore: now StarchSearch menu is no longer obscured so you can start a new conversation

--- components/Workspaces/WorkspaceLayout.tsx
+++ components/Workspaces/WorkspaceLayout.tsx
@@ -61,7 +61,7 @@ export const WorkspaceLayout = ({ workspaceId, banner, children, footer }: Works
             {!showingSidebar && (
               <button
                 onClick={() => setShowingSidebar(true)}
-                className="fixed z-50 border-8 border-solid border-black inset-x-0 top-16 w-fit bg-white rounded-r-md shadow-lg p-2"
+                className="fixed z-50 border-8 border-solid border-black inset-x-0 top-24 w-fit bg-white rounded-r-md shadow-lg p-2"
               >
                 <LuArrowRightToLine className="w-4 h-4 text-gray-500" />
               </button>
