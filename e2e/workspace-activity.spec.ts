e988ac9dc0e96c0b3fbf22a17f0d55de658bcd7a
Nick Taylor <nick@nickyt.co>
2024-07-17 19:20:51 -0400

    chore: refactored workspace E2E specs

--- e2e/workspace-activity.spec.ts
+++ e2e/workspace-activity.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 const WORKSPACE_ID = "21c5e583-f1fd-4033-9ec9-7801f67ff727";
 
-test("workspace activity page", async ({ page }) => {
+test("workspace activity page loads", async ({ page }) => {
   await page.goto(`/workspaces/${WORKSPACE_ID}/activity`);
 
   await expect(page.getByRole("tab", { name: "Activity", exact: true })).toBeVisible();
@@ -25,11 +25,8 @@ test("workspace activity page", async ({ page }) => {
   await expect(thead.getByText("Merged At")).toBeVisible();
 
   await expect(table.locator("tbody").locator("tr")).toHaveCount(10);
-});
 
-test("workspace dashboard has StarSearch for workspaces CTA to log in", async ({ page }) => {
-  await page.goto(`/workspaces/${WORKSPACE_ID}/activity`);
-
+  // Ensure the StarSearch for Workspaces CTA is visible
   const starSearchButton = await page.getByRole("button", { name: "Sign in to use StarSearch", exact: true });
   await expect(starSearchButton).toHaveAttribute("aria-disabled", "false");
   await expect(starSearchButton).toBeVisible();
@@ -52,11 +49,8 @@ test.describe("{ width: 640, height: 1136 }", () => {
 
     await expect(thead.getByText("Pull Requests")).toBeVisible();
     await expect(table.locator("tbody").locator("tr")).toHaveCount(10);
-  });
 
-  test("workspace dashboard has StarSearch for workspaces CTA to log in", async ({ page }) => {
-    await page.goto(`/workspaces/${WORKSPACE_ID}/activity`);
-
+    // Ensure the StarSearch for Workspaces CTA is visible
     const starSearchButton = await page.getByRole("button", { name: "Sign in to use StarSearch", exact: true });
     await expect(starSearchButton).toHaveAttribute("aria-disabled", "false");
     await expect(starSearchButton).toBeVisible();
--- e2e/workspace-dashboard.spec.ts
+++ e2e/workspace-dashboard.spec.ts
@@ -3,21 +3,18 @@ import config from "../playwright.config";
 
 const WORKSPACE_ID = "21c5e583-f1fd-4033-9ec9-7801f67ff727";
 
-test("workspace dashboard has an OG image", async ({ page }) => {
+test("workspace dashboard page loads", async ({ page }) => {
   await page.goto(`/workspaces/${WORKSPACE_ID}`);
   const expectedUrl = `${config.use?.baseURL}/og-images/workspace/${WORKSPACE_ID}/30?wname=OpenSauced`;
 
   await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
   await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
   await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
-});
 
-test("workspace dashboard has StarSearch for workspaces CTA to log in", async ({ page }) => {
-  await page.goto(`/workspaces/${WORKSPACE_ID}`);
-
+  // The welcome modal should be visible
   const dialog = await page.getByRole("dialog", { name: "Welcome to Workspaces", exact: true });
 
-  // In case the this screen size test runs first
+  // In case the this screen size test runs first and the welcome modal no longer loads
   if (await dialog.isVisible()) {
     await dialog.getByRole("button", { name: "Continue", exact: true }).click();
   }
@@ -30,12 +27,18 @@ test("workspace dashboard has StarSearch for workspaces CTA to log in", async ({
 test.describe("{ width: 640, height: 1136 }", () => {
   test.use({ viewport: { width: 640, height: 1136 } });
 
-  test("workspace activity has StarSearch for workspaces CTA to log in", async ({ page }) => {
-    await page.goto(`/workspaces/${WORKSPACE_ID}/activity`);
+  test("workspace dashboard page loads", async ({ page }) => {
+    await page.goto(`/workspaces/${WORKSPACE_ID}`);
+    const expectedUrl = `${config.use?.baseURL}/og-images/workspace/${WORKSPACE_ID}/30?wname=OpenSauced`;
 
+    await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
+    await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
+    await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
+
+    // The welcome modal should be visible
     const dialog = await page.getByRole("dialog", { name: "Welcome to Workspaces", exact: true });
 
-    // In case the this screen size test runs first
+    // In case the this screen size test runs first and the welcome modal no longer loads
     if (await dialog.isVisible()) {
       await dialog.getByRole("button", { name: "Close", exact: true }).click();
     }
