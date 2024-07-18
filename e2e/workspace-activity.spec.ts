cdb4ebdc5aa81f2c5ef0417e5c7e569406896099
Nick Taylor <nick@nickyt.co>
2024-07-17 20:06:56 -0400

    test: added e2e tests for workspace activity issues page

--- e2e/workspace-activity.spec.ts
+++ e2e/workspace-activity.spec.ts
@@ -32,6 +32,15 @@ test("workspace activity page loads", async ({ page }) => {
   await expect(starSearchButton).toBeVisible();
 });
 
+test("workspace activity issues page loads", async ({ page }) => {
+  await page.goto(`/workspaces/${WORKSPACE_ID}/activity`);
+
+  // Ensure the StarSearch for Workspaces CTA is visible
+  const starSearchButton = await page.getByRole("button", { name: "Sign in to use StarSearch", exact: true });
+  await expect(starSearchButton).toHaveAttribute("aria-disabled", "false");
+  await expect(starSearchButton).toBeVisible();
+});
+
 test.describe("{ width: 640, height: 1136 }", () => {
   test.use({ viewport: { width: 640, height: 1136 } });
   test("workspace activity page (small screen)", async ({ page }) => {
@@ -49,6 +58,15 @@ test.describe("{ width: 640, height: 1136 }", () => {
 
     await expect(thead.getByText("Pull Requests")).toBeVisible();
     await expect(table.locator("tbody").locator("tr")).toHaveCount(10);
+
+    // Ensure the StarSearch for Workspaces CTA is visible
+    const starSearchButton = await page.getByRole("button", { name: "Sign in to use StarSearch", exact: true });
+    await expect(starSearchButton).toHaveAttribute("aria-disabled", "false");
+    await expect(starSearchButton).toBeVisible();
+  });
+
+  test("workspace activity issues page loads", async ({ page }) => {
+    await page.goto(`/workspaces/${WORKSPACE_ID}/activity`);
 
     // Ensure the StarSearch for Workspaces CTA is visible
     const starSearchButton = await page.getByRole("button", { name: "Sign in to use StarSearch", exact: true });
