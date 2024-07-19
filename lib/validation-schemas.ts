cb9e10d5e72d7d43f994e36300177166d952e9e6
Nick Taylor <nick@nickyt.co>
2024-07-19 15:02:24 -0400

    chore: added safeParse for validation

--- lib/validation-schemas.ts
+++ lib/validation-schemas.ts
@@ -1,5 +1,6 @@
-import { parse, pipe, string, uuid } from "valibot";
+import { parse, safeParse as rawSafeParse, pipe, string, uuid } from "valibot";
 
 export const parseSchema = parse;
+export const safeParse = rawSafeParse;
 
 export const UuidSchema = pipe(string(), uuid("The UUID is badly formatted."));
