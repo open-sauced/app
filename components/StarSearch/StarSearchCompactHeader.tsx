ac10931724e01f2ac89559628e6bf4362faf58a6
Nick Taylor <nick@nickyt.co>
2024-07-22 09:14:08 -0400

    chore: added tootlips to StarSearch menu

--- components/StarSearch/StarSearchCompactHeader.tsx
+++ components/StarSearch/StarSearchCompactHeader.tsx
@@ -1,5 +1,6 @@
 import { ArrowLeftIcon, HistoryIcon, PlusIcon, XIcon } from "@primer/octicons-react";
 import { HiOutlineShare } from "react-icons/hi";
+import Tooltip from "components/atoms/Tooltip/tooltip";
 import { StarSearchLogo } from "./StarSearchLogo";
 
 type OnClick = (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -11,14 +12,6 @@ interface StarSearchCompactHeaderProps {
   onShowHistory: OnClick;
   onClose: OnClick;
 }
-
-const CloseButton = ({ onClick }: { onClick: OnClick }) => {
-  return (
-    <button onClick={onClick} className="hover:text-orange-500">
-      <XIcon />
-    </button>
-  );
-};
 
 export const StarSearchCompactHeader = ({
   onBack,
@@ -32,25 +25,43 @@ export const StarSearchCompactHeader = ({
   return (
     <div className="flex items-center justify-between gap-2 [&_button]:text-slate-600 h-9 p-1 bg-slate-50">
       <div className="flex items-center gap-2">
-        <button onClick={onBack} className={buttonHoverStyle}>
-          <ArrowLeftIcon />
-        </button>
+        <Tooltip direction="top" className="text-center w-36" content="Back">
+          <button onClick={onBack} className={buttonHoverStyle}>
+            <ArrowLeftIcon />
+          </button>
+        </Tooltip>
         <StarSearchLogo size="small" />
         <span className="font-semibold text-slate-800">StarSearch</span>
       </div>
       <div className="flex items-center gap-2">
-        <button onClick={onNewChat} className={buttonHoverStyle}>
-          <PlusIcon />
-        </button>
-        {onShare ? (
-          <button onClick={onShare} className={buttonHoverStyle}>
-            <HiOutlineShare />
+        <Tooltip direction="top" className="text-center w-36" content="New Chat">
+          <button onClick={onNewChat} className={buttonHoverStyle}>
+            <span className="sr-only">New Chat</span>
+            <PlusIcon />
           </button>
+        </Tooltip>
+        {onShare ? (
+          <Tooltip direction="top" className="text-center w-36" content="Share current conversation">
+            <button onClick={onShare} className={buttonHoverStyle}>
+              <span className="sr-only">Share current conversation</span>
+              <HiOutlineShare />
+            </button>
+          </Tooltip>
         ) : null}
-        <button onClick={onShowHistory} className={buttonHoverStyle}>
-          <HistoryIcon />
-        </button>
-        {onClose ? <CloseButton onClick={onClose} /> : null}
+        <Tooltip direction="top" className="text-center w-36" content="Chat history">
+          <button onClick={onShowHistory} className={buttonHoverStyle}>
+            <span className="sr-only">Chat history</span>
+            <HistoryIcon />
+          </button>
+        </Tooltip>
+        {onClose ? (
+          <Tooltip direction="top" className="text-center w-36" content="Close StarSearch">
+            <button onClick={onClose} className="hover:text-orange-500">
+              <span className="sr-only">Close StarSearch</span>
+              <XIcon />
+            </button>
+          </Tooltip>
+        ) : null}
       </div>
     </div>
   );
