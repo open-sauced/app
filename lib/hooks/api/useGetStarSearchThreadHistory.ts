c5405537f2ae23d913dbb3d1654c8ffcbe70e717
Nick Taylor <nick@nickyt.co>
2024-07-25 09:29:45 -0400

    chore: added some guards for potential invalid UUIDs

--- lib/hooks/api/useGetStarSearchThreadHistory.ts
+++ lib/hooks/api/useGetStarSearchThreadHistory.ts
@@ -1,9 +1,22 @@
 import useSWR, { Fetcher } from "swr";
+import { safeParse } from "valibot";
 import { publicApiFetcher } from "lib/utils/public-api-fetcher";
+import { UuidSchema } from "lib/validation-schemas";
 
 export const useGetStarSearchThreadHistory = (threadId: string | null, workspaceId?: string) => {
+  const safeThreadId = safeParse(UuidSchema, threadId);
+  const safeWorkspaceId = workspaceId ? safeParse(UuidSchema, workspaceId) : undefined;
+
+  if ((threadId && !safeThreadId.success) || (workspaceId && !safeWorkspaceId?.success)) {
+    throw new Error("Invalid thread or workspace ID");
+  }
+
   const { data, error, isLoading, mutate } = useSWR<StarSearchThread, Error>(
-    !threadId ? null : workspaceId ? `workspaces/${workspaceId}/star-search/${threadId}` : `star-search/${threadId}`,
+    !threadId
+      ? null
+      : safeWorkspaceId
+      ? `workspaces/${safeWorkspaceId.output}/star-search/${safeThreadId.output}`
+      : `star-search/${safeThreadId.output}`,
     publicApiFetcher as Fetcher<StarSearchThread, Error>
   );
 
