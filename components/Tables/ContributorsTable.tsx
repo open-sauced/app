e530bfc69202922122394e0aa5d2b8839cb38527
Zeu Capua <coding@zeu.dev>
2024-07-29 15:57:12 -0700

    add error and loading states

--- components/Tables/ContributorsTable.tsx
+++ components/Tables/ContributorsTable.tsx
@@ -14,6 +14,7 @@ import { FaSortDown, FaSortUp } from "react-icons/fa6";
 import { useMemo, useState } from "react";
 import { BsArrowsCollapse, BsArrowsExpand } from "react-icons/bs";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 import Avatar from "components/atoms/Avatar/avatar";
 import { getAvatarByUsername } from "lib/utils/github";
 import HoverCardWrapper from "components/molecules/HoverCardWrapper/hover-card-wrapper";
@@ -25,6 +26,8 @@ import Pagination from "components/molecules/Pagination/pagination";
 import Checkbox from "components/atoms/Checkbox/checkbox";
 import Button from "components/shared/Button/button";
 import InfoTooltip from "components/shared/InfoTooltip";
+import SkeletonWrapper from "components/atoms/SkeletonLoader/skeleton-wrapper";
+import errorImage from "../../public/assets/images/lotto-factor-empty.png";
 
 const AddToContributorInsightModal = dynamic(() => import("components/Contributors/AddToContributorInsightModal"), {
   ssr: false,
@@ -226,107 +229,123 @@ export default function ContributorsTable({
         </div>
       )}
 
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead key={header.id} className="font-semibold">
-                  {header.column.columnDef.enableSorting ? (
-                    <button
-                      onClick={() => {
-                        const { enableSorting } = header.column.columnDef;
-                        const isAscending = Boolean(sorting.find((item) => item.id === header.id && !item.desc));
-
-                        if (enableSorting) {
-                          setOscrSorting(isAscending ? "ASC" : "DESC");
-
-                          setSorting((currentState) => {
-                            // future-proof, set other column sorting to false
-                            const state = currentState
-                              .filter((item) => item.id !== header.id)
-                              .map(({ id }) => ({ id, desc: false }));
-
-                            return [
-                              ...state,
-                              {
-                                id: header.id,
-                                desc: isAscending,
-                              },
-                            ];
-                          });
-                        }
-                      }}
-                      className="flex gap-2 w-fit items-center"
-                    >
-                      <h2 className="font-semibold">{header.column.columnDef.header?.toString()}</h2>
-                      {header.column.columnDef.enableSorting &&
-                        (Boolean(sorting.find((item) => item.id === header.id && !item.desc)) ? (
-                          <FaSortUp />
-                        ) : (
-                          <FaSortDown />
-                        ))}
-                    </button>
-                  ) : (
-                    <>{flexRender(header.column.columnDef.header, header.getContext())}</>
-                  )}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-
-        <TableBody>
-          {table.getRowModel().rows.map((row) => (
-            <>
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-                ))}
-              </TableRow>
-              {row.getIsExpanded() && (
-                <TableRow key={`expanded_${row.id}`}>
-                  <TableCell colSpan={row.getVisibleCells().length}>
-                    <div className="flex flex-col gap-2 divide-y-2">
-                      <div className="flex justify-between font-semibold">
-                        <div className="flex gap-2 w-fit items-center">
-                          <h2>Contributions</h2>
-                          <InfoTooltip information="A total of PR, issue, and commit contributions" />
-                        </div>
-                        <span className="font-normal">{row.original.total_contributions}</span>
-                      </div>
-                      <p className="flex justify-between font-semibold pt-2">
-                        Company:
-                        <span className="font-normal">{row.original.company}</span>
-                      </p>
-                      <p className="flex justify-between font-semibold pt-2">
-                        Location:
-                        <span className="font-normal">{row.original.location}</span>
-                      </p>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              )}
-            </>
-          ))}
-        </TableBody>
-      </Table>
-
-      {meta && (
-        <div className="self-end">
-          <Pagination
-            showPages={!isMobile}
-            showTotalPages={true}
-            onPageChange={(page) => {
-              setQueryParams({ page: `${page}` });
-            }}
-            hasNextPage={meta.hasNextPage}
-            hasPreviousPage={meta.hasPreviousPage}
-            totalPage={meta.pageCount}
-            page={meta.page}
-            goToPage={true}
-          />
+      {isLoading && (
+        <div className="flex flex-col w-full gap-4 px-4">
+          <SkeletonWrapper count={4} height={32} />
         </div>
+      )}
+
+      {!isLoading && (isError || !contributors) && (
+        <section className="flex flex-col gap-4 items-center">
+          <Image src={errorImage} alt="Contributors Table error image" />
+          <h2 className="font-semibold">There are no contributors loaded.</h2>
+        </section>
+      )}
+
+      {!isError && !isLoading && (
+        <>
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <TableHead key={header.id} className="font-semibold">
+                      {header.column.columnDef.enableSorting ? (
+                        <button
+                          onClick={() => {
+                            const { enableSorting } = header.column.columnDef;
+                            const isAscending = Boolean(sorting.find((item) => item.id === header.id && !item.desc));
+
+                            if (enableSorting) {
+                              setOscrSorting(isAscending ? "ASC" : "DESC");
+
+                              setSorting((currentState) => {
+                                // future-proof, set other column sorting to false
+                                const state = currentState
+                                  .filter((item) => item.id !== header.id)
+                                  .map(({ id }) => ({ id, desc: false }));
+
+                                return [
+                                  ...state,
+                                  {
+                                    id: header.id,
+                                    desc: isAscending,
+                                  },
+                                ];
+                              });
+                            }
+                          }}
+                          className="flex gap-2 w-fit items-center"
+                        >
+                          <h2 className="font-semibold">{header.column.columnDef.header?.toString()}</h2>
+                          {header.column.columnDef.enableSorting &&
+                            (Boolean(sorting.find((item) => item.id === header.id && !item.desc)) ? (
+                              <FaSortUp />
+                            ) : (
+                              <FaSortDown />
+                            ))}
+                        </button>
+                      ) : (
+                        <>{flexRender(header.column.columnDef.header, header.getContext())}</>
+                      )}
+                    </TableHead>
+                  ))}
+                </TableRow>
+              ))}
+            </TableHeader>
+
+            <TableBody>
+              {table.getRowModel().rows.map((row) => (
+                <>
+                  <TableRow key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                    ))}
+                  </TableRow>
+                  {row.getIsExpanded() && (
+                    <TableRow key={`expanded_${row.id}`}>
+                      <TableCell colSpan={row.getVisibleCells().length}>
+                        <div className="flex flex-col gap-2 divide-y-2">
+                          <div className="flex justify-between font-semibold">
+                            <div className="flex gap-2 w-fit items-center">
+                              <h2>Contributions</h2>
+                              <InfoTooltip information="A total of PR, issue, and commit contributions" />
+                            </div>
+                            <span className="font-normal">{row.original.total_contributions}</span>
+                          </div>
+                          <p className="flex justify-between font-semibold pt-2">
+                            Company:
+                            <span className="font-normal">{row.original.company}</span>
+                          </p>
+                          <p className="flex justify-between font-semibold pt-2">
+                            Location:
+                            <span className="font-normal">{row.original.location}</span>
+                          </p>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </>
+              ))}
+            </TableBody>
+          </Table>
+          {meta && (
+            <div className="self-end">
+              <Pagination
+                showPages={!isMobile}
+                showTotalPages={true}
+                onPageChange={(page) => {
+                  setQueryParams({ page: `${page}` });
+                }}
+                hasNextPage={meta.hasNextPage}
+                hasPreviousPage={meta.hasPreviousPage}
+                totalPage={meta.pageCount}
+                page={meta.page}
+                goToPage={true}
+              />
+            </div>
+          )}
+        </>
       )}
 
       <AddToContributorInsightModal
