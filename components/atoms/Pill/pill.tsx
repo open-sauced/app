034cb1a468d66cf9f552306ba1703ab739120b99
Nick Taylor <nick@nickyt.co>
2024-07-30 16:37:04 -0400

    feat: added OSCR to avatar hover card (#3803)

--- components/atoms/Pill/pill.tsx
+++ components/atoms/Pill/pill.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 export type PillColor = "slate" | "green" | "yellow" | "red" | "purple";
@@ -8,9 +9,10 @@ export interface PillProps {
   color?: "slate" | "green" | "yellow" | "red" | "purple";
   size?: "base" | "small" | "xsmall";
   icon?: JSX.Element;
+  blurText?: boolean;
 }
 
-const Pill: React.FC<PillProps> = ({ className, text, color = "slate", size = "base", icon }) => {
+const Pill: React.FC<PillProps> = ({ className, text, color = "slate", size = "base", icon, blurText = false }) => {
   return (
     <div
       className={`
@@ -31,8 +33,7 @@ const Pill: React.FC<PillProps> = ({ className, text, color = "slate", size = "b
       {icon}
 
       <p
-        className={`
-        ${
+        className={clsx(
           color === "green"
             ? "text-light-grass-11"
             : color === "yellow"
@@ -41,11 +42,14 @@ const Pill: React.FC<PillProps> = ({ className, text, color = "slate", size = "b
             ? "text-light-red-11"
             : color === "purple"
             ? "text-purple-600"
-            : "text-light-slate-11"
-        }
-        ${size === "xsmall" ? "text-xs" : "text-sm"}
-        leading-none
-        `}
+            : "text-light-slate-11",
+          size === "xsmall" ? "text-xs" : "text-sm",
+          blurText ? "blur-sm" : "",
+          "leading-none"
+        )}
+        style={{
+          "--tw-blur": "blur(2.85px)",
+        }}
       >
         {text}
       </p>
