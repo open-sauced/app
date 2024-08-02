486660f5cffa626b63e22354e16576c62d668ddc
Zeu Capua <coding@zeu.dev>
2024-08-02 11:36:22 -0700

    implement ossf request modal, posthog captures

--- components/Repositories/OssfChart.tsx
+++ components/Repositories/OssfChart.tsx
@@ -1,11 +1,16 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { FaShieldAlt } from "react-icons/fa";
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+import { usePostHog } from "posthog-js/react";
 import SkeletonWrapper from "components/atoms/SkeletonLoader/skeleton-wrapper";
 import Card from "components/atoms/Card/card";
 import Button from "components/shared/Button/button";
+import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
+import { useToast } from "lib/hooks/useToast";
+import OssfRequestModal from "./OssfRequestModal";
 
 type OssfChartProps = {
+  repository: string;
   totalScore: number | null;
   dependencyUpdateScore: number | null;
   fuzzingScore: number | null;
@@ -17,6 +22,7 @@ type OssfChartProps = {
 };
 
 export default function OssfChart({
+  repository,
   totalScore,
   dependencyUpdateScore,
   fuzzingScore,
@@ -26,6 +32,10 @@ export default function OssfChart({
   onLearnMoreClick,
   className,
 }: OssfChartProps) {
+  const posthog = usePostHog();
+  const { toast } = useToast();
+  const { user } = useSupabaseAuth();
+  const [isOssfRequestModalOpen, setIsOssfRequestModalOpen] = useState(false);
   const data = useMemo(
     () => [
       { name: "totalScore", value: totalScore ?? 0 },
@@ -33,6 +43,18 @@ export default function OssfChart({
     ],
     [totalScore]
   );
+
+  const onRequestClick = () => {
+    if (!user) {
+      setIsOssfRequestModalOpen(true);
+    } else {
+      posthog.capture(`Repo Page: requested OpenSSF score`, {
+        repository,
+        email: user.email,
+      });
+      toast({ description: "Request sent! We'll notify you when it's ready.", variant: "success" });
+    }
+  };
 
   const getValueBasedOnScore = ({
     low,
@@ -77,73 +99,81 @@ export default function OssfChart({
   };
 
   return (
-    <Card className={`${className ?? ""} flex flex-col gap-4 w-full h-fit items-center py-8`}>
-      <header className="flex items-center justify-between w-full px-4">
-        <div className="flex items-center gap-2">
-          <FaShieldAlt className="text-xl" />
-          <h3 className="text-sm font-semibold xl:text-lg text-slate-800">OpenSSF Score</h3>
-        </div>
-        <a
-          href="https://opensauced.pizza/docs/features/repo-pages/#insights-into-the-ossf-scorecard"
-          onClick={onLearnMoreClick}
-          className="text-xs font-semibold text-sauced-orange xl:text-sm hover:underline"
-        >
-          Learn More
-        </a>
-      </header>
-
-      {!isError && (isLoading || !totalScore) ? ( // check if other scores are null
-        <SkeletonWrapper width={300} height={100} />
-      ) : (
-        <section className="flex justify-between items-center w-full gap-2 lg:flex-col xl:flex-row h-fit max-h-24 lg:max-h-full xl:max-h-24">
-          <div className="relative w-full !max-w-[14rem] lg:max-w-full lg:mx-auto h-full lg:max-h-24 xl:h-full">
-            {isError && (
-              <div className="absolute inset-0 z-50 flex items-center justify-center">
-                <Button variant="primary" className="!text-xs">
-                  I want this now!
-                </Button>
-              </div>
-            )}
-            <ResponsiveContainer width="100%" height={150} className={`${isError && "blur-[2.5px]"}`}>
-              <PieChart>
-                <Pie
-                  data={data}
-                  label={renderCustomLabel}
-                  labelLine={false}
-                  dataKey="value"
-                  startAngle={180}
-                  endAngle={0}
-                  innerRadius={40}
-                  paddingAngle={0}
-                >
-                  {data.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={entry.name === "totalScore" ? pieColor : "#e2e8f0"} />
-                  ))}
-                </Pie>
-              </PieChart>
-            </ResponsiveContainer>
+    <>
+      <Card className={`${className ?? ""} flex flex-col gap-4 w-full h-fit items-center py-8`}>
+        <header className="flex items-center justify-between w-full px-4">
+          <div className="flex items-center gap-2">
+            <FaShieldAlt className="text-xl" />
+            <h3 className="text-sm font-semibold xl:text-lg text-slate-800">OpenSSF Score</h3>
           </div>
-          <section className="flex flex-col gap-1 lg:text-center xl:text-start">
-            <h3 className="font-medium text-sm text-slate-700">{projectStatus}</h3>
-            <p className="text-xs text-slate-600">
-              {projectDescription}
-              {!isError && (
-                <span>
-                  You can run the full test{" "}
-                  <a
-                    href="https://github.com/ossf/scorecard?tab=readme-ov-file#view-a-projects-score"
-                    target="_blank"
-                    className="text-orange-500"
-                  >
-                    here
-                  </a>
-                  .
-                </span>
+          <a
+            href="https://opensauced.pizza/docs/features/repo-pages/#insights-into-the-ossf-scorecard"
+            onClick={onLearnMoreClick}
+            className="text-xs font-semibold text-sauced-orange xl:text-sm hover:underline"
+          >
+            Learn More
+          </a>
+        </header>
+
+        {!isError && (isLoading || !totalScore) ? ( // check if other scores are null
+          <SkeletonWrapper width={300} height={100} />
+        ) : (
+          <section className="flex justify-between items-center w-full gap-2 lg:flex-col xl:flex-row h-fit max-h-24 lg:max-h-full xl:max-h-24">
+            <div className="relative w-full !max-w-[14rem] lg:max-w-full lg:mx-auto h-full lg:max-h-24 xl:h-full">
+              {isError && (
+                <div className="absolute inset-0 z-50 flex items-center justify-center">
+                  <Button variant="primary" onClick={onRequestClick} className="!text-xs">
+                    I want this now!
+                  </Button>
+                </div>
               )}
-            </p>
+              <ResponsiveContainer width="100%" height={150} className={`${isError && "blur-[2.5px]"}`}>
+                <PieChart>
+                  <Pie
+                    data={data}
+                    label={renderCustomLabel}
+                    labelLine={false}
+                    dataKey="value"
+                    startAngle={180}
+                    endAngle={0}
+                    innerRadius={40}
+                    paddingAngle={0}
+                  >
+                    {data.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={entry.name === "totalScore" ? pieColor : "#e2e8f0"} />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+            <section className="flex flex-col gap-1 lg:text-center xl:text-start">
+              <h3 className="font-medium text-sm text-slate-700">{projectStatus}</h3>
+              <p className="text-xs text-slate-600">
+                {projectDescription}
+                {!isError && (
+                  <span>
+                    You can run the full test{" "}
+                    <a
+                      href="https://github.com/ossf/scorecard?tab=readme-ov-file#view-a-projects-score"
+                      target="_blank"
+                      className="text-orange-500"
+                    >
+                      here
+                    </a>
+                    .
+                  </span>
+                )}
+              </p>
+            </section>
           </section>
-        </section>
-      )}
-    </Card>
+        )}
+      </Card>
+
+      <OssfRequestModal
+        repository={repository}
+        isOpen={isOssfRequestModalOpen}
+        onClose={() => setIsOssfRequestModalOpen(false)}
+      />
+    </>
   );
 }
