5047e1f9ae3f90e2ecd71556ae415dfff148ac51
Nick Taylor <nick@nickyt.co>
2024-08-09 17:26:53 -0400

    feat: implemented the dev card OG image (#3903)

--- e2e/user-profile.spec.ts
+++ e2e/user-profile.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
-import playwrightConfig from "../playwright.config";
+import config from "../playwright.config";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? playwrightConfig.use?.baseURL ?? "http://localhost:3000";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? config.use?.baseURL ?? "http://localhost:3000";
 
 test("Loads user profile page", async ({ page }) => {
   await page.goto("/u/bdougie");
@@ -10,12 +10,32 @@ test("Loads user profile page", async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "bdougie", exact: true })).toBeVisible();
 
+  // Check for the OG image
+  const expectedUrl = `${config.use?.baseURL}/og-images/dev-card?username=bdougie`;
+
+  await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
+
   // Check for login button for viewing OSCR
   await page
     .getByRole("button", { name: "Log in to view  Open Source Contributor Rating (OSCR)", exact: true })
     .click();
 
   await expect(page.url()).toContain("https://github.com/login");
+});
+
+test("Loads user dev card page", async ({ page }) => {
+  await page.goto("/u/bdougie/card");
+
+  expect(await page.title()).toBe("bdougie | OpenSauced");
+
+  // Check for the OG image
+  const expectedUrl = `${config.use?.baseURL}/og-images/dev-card?username=bdougie`;
+
+  await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:image"]')).toHaveAttribute("content", expectedUrl);
+  await expect(page.locator('meta[name="twitter:card"]')).toHaveAttribute("content", "summary_large_image");
 });
 
 test("Redirects to user profile page", async ({ page }) => {
