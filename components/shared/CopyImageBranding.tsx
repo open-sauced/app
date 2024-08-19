9256b172fbd18350ab591cea5185aa4024ea4d71
Zeu Capua <coding@zeu.dev>
2024-08-19 13:01:02 -0700

    fix banner branding

--- components/shared/CopyImageBranding.tsx
+++ components/shared/CopyImageBranding.tsx
@@ -1,24 +1,22 @@
 import Image from "next/image";
 import Avatar from "components/atoms/Avatar/avatar";
 import { getAvatarByUsername } from "lib/utils/github";
-import openSaucedImg from "img/openSauced-icon.png";
+import openSaucedImg from "img/logo-slice-gradient.svg";
 
 export default function CopyImageBranding({ repository }: { repository: string }) {
   return (
     <section
       data-copy-image-branding
-      className="hidden flex w-full justify-between rounded-t-lg h-fit p-4 bg-dark-slate-100"
+      className="hidden flex w-full justify-between rounded-t-lg h-fit p-4 bg-black text-white"
     >
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <Avatar size="sm" avatarURL={getAvatarByUsername(repository.split("/")[0])} />
-        <p>{repository}</p>
+        <p className="font-medium text-sm">{repository}</p>
       </div>
 
-      <div className="flex justify-center my-2">
-        <Image className="rounded" alt="OpenSauced Logo" width={13} height={13} src={openSaucedImg} />
-        <p className={"font-semibold text-white ml-1"} style={{ fontSize: "8px" }}>
-          OpenSauced
-        </p>
+      <div className="flex items-center">
+        <Image className="rounded" alt="OpenSauced Logo" width={16} height={16} src={openSaucedImg} />
+        <p className="font-semibold text-sm ml-2">OpenSauced</p>
       </div>
     </section>
   );
