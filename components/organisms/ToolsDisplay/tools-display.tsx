3c366150a4b658f792b4c4e084dbac1fd7c74f14
Zeu Capua <coding@zeu.dev>
2024-08-21 09:38:47 -0700

    implement tanstack ContributorsTable to explore 'Contributors' tab

--- components/organisms/ToolsDisplay/tools-display.tsx
+++ components/organisms/ToolsDisplay/tools-display.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import { captureAnalytics } from "lib/utils/analytics";
 import useSupabaseAuth from "lib/hooks/useSupabaseAuth";
 import { useFetchUser } from "lib/hooks/useFetchUser";
-import Contributors from "../Contributors/contributors";
-import Dashboard from "../Dashboard/dashboard";
+import useContributors from "lib/hooks/api/useContributors";
+import ContributorsTable from "components/Tables/ContributorsTable";
+import { OrderDirection } from "lib/utils/sorting";
+import { setQueryParams } from "lib/utils/query-params";
 import Activity from "../Activity/activity";
+import Dashboard from "../Dashboard/dashboard";
 
 interface ToolProps {
   tool?: string;
@@ -12,8 +16,21 @@ interface ToolProps {
 }
 
 const Tool = ({ tool, repositories }: ToolProps): JSX.Element => {
+  const router = useRouter();
   const { user } = useSupabaseAuth();
   const { data: userInfo, isLoading } = useFetchUser(user?.user_metadata.user_name);
+
+  const orderDirection = router.query.orderDirection as OrderDirection;
+  const setOscrSortDirection = (direction: OrderDirection) => {
+    setQueryParams({ orderDirection: direction, orderBy: "oscr" });
+  };
+
+  const {
+    data: contributors,
+    meta: contributorsMeta,
+    isError: isContributorsError,
+    isLoading: isContributorsLoading,
+  } = useContributors(Number(router.query.limit ?? 10), repositories);
 
   useEffect(() => {
     if (isLoading) {
@@ -26,8 +43,18 @@ const Tool = ({ tool, repositories }: ToolProps): JSX.Element => {
   switch (tool) {
     case "Dashboard":
       return <Dashboard repositories={repositories} />;
-    case "Contributors":
-      return <Contributors repositories={repositories} personalWorkspaceId={userInfo?.personal_workspace_id} />;
+    case "Contributors": {
+      return (
+        <ContributorsTable
+          contributors={contributors}
+          isLoading={isContributorsLoading}
+          isError={isContributorsError}
+          meta={contributorsMeta}
+          oscrSorting={orderDirection}
+          setOscrSorting={setOscrSortDirection}
+        />
+      );
+    }
 
     case "Activity":
       return <Activity repositories={repositories} />;
