9d2e1f873ac46b1a41739fccc23353fa2909c7e0
Brandon Roberts <robertsbt@gmail.com>
2024-08-23 15:39:52 -0500

    chore: update e2e test

--- e2e/explore-page.spec.ts
+++ e2e/explore-page.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("Loads explore dashboard page", async ({ page }) => {
   await page.goto("/explore/topic/javascript");
-  await expect(page.getByRole("complementary", { name: "pull request highlights", exact: true })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Connect with GitHub", exact: true })).toBeVisible();
 });
