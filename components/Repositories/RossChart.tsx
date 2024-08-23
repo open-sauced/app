581497e85de3e0f32e201d507181b20371e1c812
zeudev <coding@zeu.dev>
2024-08-23 15:36:28 -0700

    feat: copy repo page Contributors chart as image (#4025)

--- components/Repositories/RossChart.tsx
+++ components/Repositories/RossChart.tsx
@@ -166,7 +166,7 @@ export default function RossChart({ stats, isLoading, error, range, onFilterClic
             !filterOutside && "opacity-60"
           } transition-all duration-300 hover:bg-slate-100 rounded-lg px-2 py-1`}
         >
-          <span className={`w-4 h-4 rounded-sm bg-[#60A5FA] inline-block`} />
+          <span data-copy-image-legend-square className={`w-4 h-4 rounded-sm bg-[#60A5FA] inline-block`} />
           Outside
         </button>
 
@@ -179,7 +179,7 @@ export default function RossChart({ stats, isLoading, error, range, onFilterClic
             !filterRecurring && "opacity-60"
           } transition-all duration-300 hover:bg-slate-100 rounded-lg px-2 py-1`}
         >
-          <span className={`w-4 h-4 rounded-sm bg-[#2563EB] inline-block`} />
+          <span data-copy-image-legend-square className={`w-4 h-4 rounded-sm bg-[#2563EB] inline-block`} />
           Recurring
         </button>
 
@@ -192,7 +192,7 @@ export default function RossChart({ stats, isLoading, error, range, onFilterClic
             !filterInternal && "opacity-60"
           } transition-all duration-300 hover:bg-slate-100 rounded-lg px-2 py-1`}
         >
-          <span className={`w-4 h-4 rounded-sm bg-[#1E3A8A] inline-block`} />
+          <span data-copy-image-legend-square className={`w-4 h-4 rounded-sm bg-[#1E3A8A] inline-block`} />
           Internal
         </button>
       </fieldset>
