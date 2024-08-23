64a04f5d3a8fbfe4647f8c43c07eca13e7ea0ef9
zeudev <coding@zeu.dev>
2024-08-23 13:32:02 -0700

    fix: `RossChart` order changing on filter (#4018)

--- components/Repositories/RossChart.tsx
+++ components/Repositories/RossChart.tsx
@@ -45,20 +45,26 @@ export default function RossChart({ stats, isLoading, error, range, onFilterClic
   }, [stats]);
 
   const weeklyData = useMemo(() => {
-    return stats?.contributors.reverse().map((week) => {
-      return {
-        new: filterOutside ? week.new : 0,
-        recurring: filterRecurring ? week.recurring : 0,
-        internal: filterInternal ? week.internal : 0,
-        bucket: new Date(week.bucket).toLocaleDateString(undefined, { month: "numeric", day: "numeric" }),
-      };
-    });
+    return stats?.contributors
+      .slice()
+      .reverse()
+      .map((week) => {
+        return {
+          new: filterOutside ? week.new : 0,
+          recurring: filterRecurring ? week.recurring : 0,
+          internal: filterInternal ? week.internal : 0,
+          bucket: new Date(week.bucket).toLocaleDateString(undefined, { month: "numeric", day: "numeric" }),
+        };
+      });
   }, [stats, filterOutside, filterRecurring, filterInternal]);
 
   const bucketTicks = useMemo(() => {
-    return stats?.contributors.reverse().map((week) => {
-      return new Date(week.bucket).toLocaleDateString(undefined, { month: "numeric", day: "numeric" });
-    });
+    return stats?.contributors
+      .slice()
+      .reverse()
+      .map((week) => {
+        return new Date(week.bucket).toLocaleDateString(undefined, { month: "numeric", day: "numeric" });
+      });
   }, [stats]);
 
   const CONTRIBUTOR_COLORS: Record<string, string> = {
