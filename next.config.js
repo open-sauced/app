fcb5c9319fc726f4741f15d83f0b28ab730cd74b
Brandon Roberts <robertsbt@gmail.com>
2024-09-03 13:30:37 -0500

    chore: unhide sourcemaps

--- next.config.js
+++ next.config.js
@@ -177,7 +177,7 @@ module.exports = millionLint(
       // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
       tunnelRoute: "/monitoring",
       // Hides source maps from generated client bundles
-      hideSourceMaps: true,
+      hideSourceMaps: false,
       // Automatically tree-shake Sentry logger statements to reduce bundle size
       disableLogger: true,
     }
