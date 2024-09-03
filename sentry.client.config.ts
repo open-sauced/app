3a852be958b100094dde522abb690c3f4750bd8a
Brandon Roberts <robertsbt@gmail.com>
2024-09-03 10:31:03 -0500

    Revert "chore: fix build issue with latest Sentry package"
    This reverts commit d49705c58cd0b0775d60bf97a2a79698c1670b6a.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -23,7 +23,7 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    Sentry.replayIntegration({
+    new Sentry.Replay({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
       blockAllMedia: true,
