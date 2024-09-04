40ac84b6bdeeed1101088600b2e35afa027bd05c
Brandon Roberts <robertsbt@gmail.com>
2024-09-04 09:54:19 -0500

    chore: rewrite frames

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import * as SentryBrowser from "@sentry/browser";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
@@ -23,9 +24,7 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    // SentryBrowser.rewriteFramesIntegration({
-    //   root: process.cwd(),
-    // }),
+    SentryBrowser.rewriteFramesIntegration(),
     Sentry.replayIntegration({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
