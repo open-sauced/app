19ebe264088d548b6824de81b867bf5aa95faa73
Zeu Capua <coding@zeu.dev>
2024-09-12 10:29:38 -0700

    change e2e explore url

--- e2e/explore-page.spec.ts
+++ e2e/explore-page.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("Loads explore dashboard page", async ({ page }) => {
-  await page.goto("/explore/topic/javascript");
+  await page.goto("/explore");
   await expect(page.getByRole("button", { name: "Connect with GitHub", exact: true })).toBeVisible();
 });
