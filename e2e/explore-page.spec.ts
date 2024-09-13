6cca968648850467bebe0e385d44b1a6641da68c
Zeu Capua <coding@zeu.dev>
2024-09-13 11:31:22 -0700

    update explore page e2e test

--- e2e/explore-page.spec.ts
+++ e2e/explore-page.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("Loads explore dashboard page", async ({ page }) => {
   await page.goto("/explore");
-  await expect(page.getByRole("button", { name: "Connect with GitHub", exact: true })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Connect with GitHub" }).first()).toBeVisible();
 });
