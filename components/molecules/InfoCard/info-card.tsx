bd33e3c613af17e7b263f06e176f68282208f942
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-10-15 20:29:28 +0300

    fix: add keyboard support to div

--- components/molecules/InfoCard/info-card.tsx
+++ components/molecules/InfoCard/info-card.tsx
@@ -9,7 +9,18 @@ export interface FavoriteRepoCardProps {
 
 const InfoCard = ({ icon, title, description, handleClick }: FavoriteRepoCardProps): JSX.Element => {
   return (
-    <div className="p-3 bg-white border-2 rounded-xl cursor-pointer" onClick={handleClick}>
+    <div
+      className="p-3 bg-white border-2 rounded-xl cursor-pointer"
+      onClick={handleClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === "enter") {
+          e.preventDefault();
+          handleClick();
+        }
+      }}
+    >
       <div className="flex items-center gap-2.5">
         {/* Avatar */}
         {icon === "github" && <FiGithub size={24} className="text-orange-200" />}
