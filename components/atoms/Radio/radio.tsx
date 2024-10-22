fd5f1197e0df358e1cb0c6bf75078663b46a6de2
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-10-23 01:02:35 +0300

    fix: make radio component span full width

--- components/atoms/Radio/radio.tsx
+++ components/atoms/Radio/radio.tsx
@@ -19,7 +19,7 @@ const Radio = ({ className, withLabel, id, children, value, checked = false, onC
       className={
         `${
           checked && "!bg-light-orange-3"
-        }  px-2 py-1.5 text-sm text-slate-600   group  rounded-md  hover:bg-light-orange-3 hover:text-slate-800 focus:outline-none focus-visible:border-orange-500 focus-visible:ring focus-visible:ring-orange-200 transition` +
+        }  w-full px-2 py-1.5 text-sm text-slate-600   group  rounded-md  hover:bg-light-orange-3 hover:text-slate-800 focus:outline-none focus-visible:border-orange-500 focus-visible:ring focus-visible:ring-orange-200 transition` +
         `${className}`
       }
     >
