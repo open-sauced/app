9d8ae39c77d8e9e45f40e3d64a381bb3eb66582b
Anish0203 <45678877+Anish0203@users.noreply.github.com>
2024-10-23 00:19:36 +0530

    fix: Make the "create workspace" option more obvious (#4177)

--- components/Contributors/AddToContributorInsightModal.tsx
+++ components/Contributors/AddToContributorInsightModal.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { BsGithub } from "react-icons/bs";
 import { usePostHog } from "posthog-js/react";
 import { safeParse } from "valibot";
+import { PlusIcon } from "@heroicons/react/24/outline";
 import Card from "components/atoms/Card/card";
 import SingleSelect from "components/atoms/Select/single-select";
 import { Dialog, DialogContent } from "components/molecules/Dialog/dialog";
@@ -119,7 +120,16 @@ export default function AddToContributorInsightModal({
                 ) : (
                   <SingleSelect
                     options={[
-                      { label: "Create new insight...", value: "new" },
+                      {
+                        label: "Create new insight...",
+                        value: "new",
+                        icon: (
+                          <PlusIcon
+                            style={{ strokeWidth: "3px" }}
+                            className="w-5 text-gray-500 h-5 p-0.5 -ml-1 text-semibold group-hover:bg-orange-100 rounded-md"
+                          />
+                        ),
+                      },
                       ...contributorInsights.map(({ id, name }) => ({
                         label: name,
                         value: id,
